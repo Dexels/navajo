ac94e2650c4a89b4a92bb5d7be926989fac042bd
Arjen Schoneveld <aschoneveld@dexels.com>
2002-06-10 15:14:25 +0000

    Navajo Classloader stuff

---
+++ src/com/dexels/navajo/loader/NavajoClassLoader.java
@@ -0,0 +1,87 @@
+package com.dexels.navajo.loader;
+
+/**
+ * Title:        Navajo Product Project
+ * Description:  This is the official source for the Navajo server
+ * Copyright:    Copyright (c) 2002
+ * Company:      Dexels BV
+ * @author Arjen Schoneveld
+ * @version 1.0
+ */
+import org.dexels.utils.*;
+import java.io.*;
+
+/**
+ * This class implements the Navajo Class Loader. It is used to implement re-loadable and hot-loadable Navajo adapter classes
+ * and Navajo Expression Language functions.
+ * It uses a cache for fast multiple instantiation of loaded Classes.
+ */
+
+class JarFilter implements FilenameFilter {
+  public boolean accept(File f, String name) {
+    if (name.endsWith("jar"))
+      return true;
+    else
+      return false;
+  }
+}
+
+public class NavajoClassLoader extends MultiClassLoader {
+
+  private String adapterPath = "";
+
+  public NavajoClassLoader(String adapterPath) {
+    System.out.println("Initializing NavajoClassLoader: adapterPath = " + adapterPath);
+    this.adapterPath = adapterPath;
+  }
+
+  /**
+   * Use clearCache() to clear the Class cache, allowing a re-load of new jar files.
+   */
+  public void clearCache() {
+    super.clearCache();
+  }
+
+  /**
+   * Always use this method to load a class. It uses the cache first before retrieving the class from a jar resource.
+   */
+  public Class getClass(String className) throws ClassNotFoundException {
+    Class c = (Class) classes.get(className);
+    if (c == null) {
+      return Class.forName(className, false, this);
+    }
+    else {
+      System.out.println("Found class in cache");
+      return c;
+    }
+  }
+
+  protected byte[] loadClassBytes (String className)
+  {
+    // Support the MultiClassLoader's class name munging facility.
+    className = formatClassName (className);
+
+    File f = new File(adapterPath);
+    File [] files = f.listFiles(new JarFilter());
+    byte [] resource = null;
+    for (int i = 0; i < files.length; i++) {
+      try {
+        //System.out.println("Locating " + className + " in jar file: " + files[i].getName());
+        JarResources d = new JarResources(files[i]);
+        resource =  d.getResource(className);
+        if (resource != null) {
+          break;
+        }
+      } catch (Exception e) {
+        //System.out.println("ERROR: " + e.getMessage());
+      }
+    }
+
+    return resource;
+
+  }
+
+  public void finalize() {
+    System.out.println("In NavajoClassLoader finalize(): Killing class loader");
+  }
+}
---
+++ src/org/dexels/utils/JarResources.java
@@ -0,0 +1,207 @@
+package org.dexels.utils;
+
+import java.io.*;
+import java.util.*;
+import java.util.zip.*;
+
+/**
+ * JarResources: JarResources maps all resources included in a
+ * Zip or Jar file. Additionaly, it provides a method to extract one
+ * as a blob.
+ */
+public final class JarResources {
+
+   // external debug flag
+   public boolean debugOn=false;
+
+   // jar resource mapping tables
+   private Hashtable htSizes=new Hashtable();
+   private Hashtable htJarContents=new Hashtable();
+
+   // a jar file
+   private File jarFile;
+
+   /**
+    * creates a JarResources. It extracts all resources from a Jar
+    * into an internal hashtable, keyed by resource names.
+    * @param jarFileName a jar or zip file
+    */
+   public JarResources(File jarFile) {
+      this.jarFile=jarFile;
+      init();
+   }
+
+   /**
+    * Extracts a jar resource as a blob.
+    * @param name a resource name.
+    */
+   public byte[] getResource(String name) throws Exception {
+      byte [] resource =  (byte[]) htJarContents.get(name);
+      if (resource == null)
+        throw new Exception("Resource not found");
+      else
+        return resource;
+   }
+
+   /**
+    * Path "/" means, get first level files/directories.
+    */
+   public Iterator getResources(String path) {
+      HashSet inPath = new HashSet();
+      Iterator iter = getResources();
+      while (iter.hasNext()) {
+        String orig = (String) iter.next();
+        String name = orig;
+          name = "/" + orig;
+          if (name.startsWith(path)) {
+            name =  name.substring(path.length(), name.length());
+            //int end = name.lastIndexOf("/");
+            // Strip path from orig.
+            if (name != null) {
+              int first = name.indexOf("/");
+              if (first != -1) {  // Directory
+                  name = name.substring(0, first);
+                  if (!inPath.contains(name))
+                    inPath.add(name+"/");
+               } else { // File
+                  inPath.add(name);
+               }
+             }
+           }
+
+      }
+      return inPath.iterator();
+   }
+
+   public Iterator getResources() {
+      return htJarContents.keySet().iterator();
+   }
+
+   public Iterator getDirectories() {
+      return null;
+   }
+
+   /**
+    * initializes internal hash tables with Jar file resources.
+    */
+   private void init() {
+      try {
+          // extracts just sizes only.
+          System.out.println("init(): jarFile = " + jarFile.getName());
+          ZipFile zf=new ZipFile(jarFile);
+          Enumeration e=zf.entries();
+
+          while (e.hasMoreElements()) {
+              ZipEntry ze=(ZipEntry)e.nextElement();
+              if (debugOn) {
+                 System.out.println(dumpZipEntry(ze));
+              }
+              //System.out.println(ze.getName());
+              htSizes.put(ze.getName(),new Integer((int)ze.getSize()));
+          }
+          zf.close();
+
+          // extract resources and put them into the hashtable.
+          FileInputStream fis=new FileInputStream(jarFile);
+          BufferedInputStream bis=new BufferedInputStream(fis);
+          ZipInputStream zis=new ZipInputStream(bis);
+          ZipEntry ze=null;
+          while ((ze=zis.getNextEntry())!=null) {
+             if (ze.isDirectory()) {
+                ze.getName();
+             }
+             if (debugOn) {
+                System.out.println(
+                   "ze.getName()="+ze.getName()+","+"getSize()="+ze.getSize()
+                   );
+             }
+             int size=(int)ze.getSize();
+             // -1 means unknown size.
+             if (size==-1) {
+                size=((Integer)htSizes.get(ze.getName())).intValue();
+             }
+             byte[] b=new byte[(int)size];
+             int rb=0;
+             int chunk=0;
+             while (((int)size - rb) > 0) {
+                 chunk=zis.read(b,rb,(int)size - rb);
+                 if (chunk==-1) {
+                    break;
+                 }
+                 rb+=chunk;
+             }
+             // add to internal resource hashtable
+             htJarContents.put(ze.getName(),b);
+             if (debugOn) {
+                System.out.println(
+                   ze.getName()+"  rb="+rb+
+                   ",size="+size+
+                   ",csize="+ze.getCompressedSize()
+                   );
+             }
+          }
+       } catch (NullPointerException e) {
+          System.out.println("done.");
+       } catch (FileNotFoundException e) {
+          e.printStackTrace();
+       } catch (IOException e) {
+          e.printStackTrace();
+       }
+   }
+
+   /**
+    * Dumps a zip entry into a string.
+    * @param ze a ZipEntry
+    */
+   private String dumpZipEntry(ZipEntry ze) {
+       StringBuffer sb=new StringBuffer();
+       if (ze.isDirectory()) {
+          sb.append("d ");
+       } else {
+          sb.append("f ");
+       }
+       if (ze.getMethod()==ZipEntry.STORED) {
+          sb.append("stored   ");
+       } else {
+          sb.append("defalted ");
+       }
+       sb.append(ze.getName());
+       sb.append("\t");
+       sb.append(""+ze.getSize());
+       if (ze.getMethod()==ZipEntry.DEFLATED) {
+          sb.append("/"+ze.getCompressedSize());
+       }
+       return (sb.toString());
+   }
+
+   /**
+    * Is a test driver. Given a jar file and a resource name, it trys to
+    * extract the resource and then tells us whether it could or not.
+    *
+    * <strong>Example</strong>
+    * Let's say you have a JAR file which jarred up a bunch of gif image
+    * files. Now, by using JarResources, you could extract, create, and display
+    * those images on-the-fly.
+    * <pre>
+    *     ...
+    *     JarResources JR=new JarResources("GifBundle.jar");
+    *     Image image=Toolkit.createImage(JR.getResource("logo.gif");
+    *     Image logo=Toolkit.getDefaultToolkit().createImage(
+    *                   JR.getResources("logo.gif")
+    *                   );
+    *     ...
+    * </pre>
+    */
+   public static void main(String[] args) throws IOException {
+
+       JarResources jr=new JarResources(new File("/home/arjen/projecten/Navajo/tml.dtd"));
+       Iterator iter = jr.getResources("/com/dexels/navajo/document/");
+       while (iter.hasNext()) {
+          String name = (String) iter.next();
+          System.out.println(name);
+       }
+       //byte [] file = jr.getResource("org/dexels/toolbox/studio/BPFLMethodPanel.class");
+
+   }
+
+}	// End of JarResources class.
