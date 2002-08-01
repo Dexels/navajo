2307a2651792cce4b26e733cef56e3ff726469ee
Arjen Schoneveld <aschoneveld@dexels.com>
2002-08-01 14:37:19 +0000

    <No Comment Entered>

---
+++ src/com/dexels/navajo/functions/DateField.java
@@ -0,0 +1,56 @@
+package com.dexels.navajo.functions;
+
+import com.dexels.navajo.parser.*;
+import java.util.*;
+
+/**
+ * Title:        Navajo Product Project
+ * Description:  This is the official source for the Navajo server
+ * Copyright:    Copyright (c) 2002
+ * Company:      Dexels BV
+ * @author Arjen Schoneveld
+ * @version 1.0
+ */
+
+public class DateField extends FunctionInterface {
+
+  public DateField() {
+  }
+
+  public Object evaluate() throws com.dexels.navajo.parser.TMLExpressionException {
+    java.util.Date date = (java.util.Date) this.getOperands().get(0);
+    String field = (String) this.getOperands().get(1);
+    Calendar cal = Calendar.getInstance();
+    cal.setTime(date);
+
+    System.out.println("date  = " + date.toString());
+    System.out.println("field = " + field);
+    System.out.println("WEEK  = " + cal.get(Calendar.WEEK_OF_YEAR));
+
+    if (field.equals("WEEK"))
+       return new Integer(cal.get(Calendar.WEEK_OF_YEAR));
+    else if (field.equals("YEAR"))
+       return new Integer(cal.get(Calendar.YEAR));
+    else if (field.equals("DAY"))
+       return new Integer(cal.get(Calendar.DAY_OF_MONTH));
+    else
+       return new Integer(-1);
+  }
+
+  public String usage() {
+    return "DateField(date, field)";
+  }
+
+  public String remarks() {
+    return "";
+  }
+
+  public static void main (String args[]) {
+
+     Calendar cal = Calendar.getInstance();
+     cal.set(2001, 1, 1);
+     System.out.println(cal.getTime().toString());
+     System.out.println(cal.get(cal.WEEK_OF_YEAR));
+
+  }
+}
---
+++ src/com/dexels/navajo/functions/ToDouble.java
@@ -0,0 +1,32 @@
+package com.dexels.navajo.functions;
+
+/**
+ * Title:        Navajo Product Project
+ * Description:  This is the official source for the Navajo server
+ * Copyright:    Copyright (c) 2002
+ * Company:      Dexels BV
+ * @author Arjen Schoneveld
+ * @version 1.0
+ */
+
+import com.dexels.navajo.parser.*;
+
+public class ToDouble extends FunctionInterface {
+
+  public ToDouble()  {
+  }
+
+   public Object evaluate() throws com.dexels.navajo.parser.TMLExpressionException {
+    String s = (String) this.getOperands().get(0);
+    return new Double(s);
+  }
+
+  public String usage() {
+    return "";
+  }
+
+  public String remarks() {
+    return "";
+  }
+
+}
