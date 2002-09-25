8ae52d422ab84b7abb9f62dbbce69997dd931a87
Arjen Schoneveld <aschoneveld@dexels.com>
2002-09-25 12:05:57 +0000

    Changed Navajo configuration method, dropped all use of .properties files and uses xml configuration files instead. Dropped references to Context object, therefore ejb API is no longer required to compile and run Navajo. Changed ServiceHandler interface. Added NavajoConfig class. Changed Mappable interface.

---
+++ src/com/dexels/navajo/server/NavajoConfig.java
@@ -0,0 +1,54 @@
+package com.dexels.navajo.server;
+
+/**
+ * Title:        Navajo Product Project
+ * Description:  This is the official source for the Navajo server
+ * Copyright:    Copyright (c) 2002
+ * Company:      Dexels BV
+ * @author Arjen Schoneveld
+ * @version 1.0
+ */
+
+import java.util.*;
+import com.dexels.navajo.document.Navajo;
+import com.dexels.navajo.loader.NavajoClassLoader;
+
+public class NavajoConfig {
+
+  protected String adapterPath;
+  protected String scriptPath;
+  protected HashMap properties;
+  protected String configPath;
+  protected NavajoClassLoader classloader;
+  protected com.dexels.navajo.server.Repository repository;
+  protected Navajo configuration;
+
+  public Navajo getConfiguration() {
+    return configuration;
+  }
+
+  public String getAdapterPath() {
+    return adapterPath;
+  }
+
+  public String getScriptPath() {
+    return scriptPath;
+  }
+
+  public HashMap getProperties() {
+    return properties;
+  }
+  public String getConfigPath() {
+    return configPath;
+  }
+  public NavajoClassLoader getClassloader() {
+    return classloader;
+  }
+  public void setRepository(com.dexels.navajo.server.Repository newRepository) {
+    repository = newRepository;
+  }
+  public com.dexels.navajo.server.Repository getRepository() {
+    return repository;
+  }
+
+}
