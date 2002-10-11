83b375786f5560d2a2e8546e6c94d4e90490dca9
Matthew Eichler <matthew.eichler@aventinesolutions.org>
2002-10-11 11:37:24 +0000

    added JAXM

--- bin/studio.cygwin.sh
+++ bin/studio.cygwin.sh
@@ -14,12 +14,21 @@
 PROJECTHOME=D:/Projecten/Navajo
 
 # required libraries, edit accordingly
-NAVAJOJAR="D:\\Projecten\Navajo\\Navajo.jar;D:\\Projecten\Navajo\lib\Grus.jar"
-JRUNTIMEJAR="C:\\JBuilder6\\jdk1.3.1\\jre\\lib\\rt.jar"
-XMLJARS="C:\\JBuilder6\\cocoon\\lib\\xerces_1_2.jar;C:\\JBuilder6\\extras\\BorlandXML\\lib\\jaxp-patch.jar"
-BORLANDJAR="C:\\JBuilder6\\lib\\jbcl.jar"
 
-export CLASSPATH="${JRUNTIMEJAR};${NAVAJOJAR};${XMLJARS};${BORLANDJAR}"
+NAVAJOHOME="D:\\Projecten\\Navajo"
+BORLANDHOME="C:\\JBuilder6"
+
+NAVAJOJAR="${NAVAJOHOME}\\Navajo.jar;${NAVAJOHOME}\\lib\\Grus.jar"
+JRUNTIMEJAR="${BORLANDHOME}\\jdk1.3.1\\jre\\lib\\rt.jar"
+
+XMLJARS="${BORLANDHOME}\\cocoon\\lib\\xerces_1_2.jar;"
+XMLJARS="${XMLJARS};${BORLANDHOME}\\extras\\BorlandXML\\lib\\jaxp-patch.jar"
+XMLJARS="${XMLJARS};${NAVAJOHOME}\\lib\\jaxm-api.jar"
+
+REGEXJAR="${NAVAJOHOME}\\lib\\gnu-regexp-1.0.8.jar"
+BORLANDJAR="${BORLANDHOME}\\lib\\jbcl.jar"
+
+export CLASSPATH="${JRUNTIMEJAR};${REGEXJAR};${NAVAJOJAR};${XMLJARS};${BORLANDJAR}"
 
 # kludge, the images don't seem to be found in the jar
 # so we change directory to the Navajo project
