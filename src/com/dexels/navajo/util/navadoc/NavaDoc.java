51af9e17ef320b4958808104477f08d977093b6c
Matthew Eichler <matthew.eichler@aventinesolutions.org>
2002-10-17 15:57:04 +0000

    logger is now configured by a DOM

--- src/com/dexels/navajo/util/navadoc/NavaDoc.java
+++ src/com/dexels/navajo/util/navadoc/NavaDoc.java
@@ -1,5 +1,6 @@
 package com.dexels.navajo.util.navadoc;
 
+
 /**
  * <p>Title: NavaDoc</p>
  * <p>Description: Automated documentation utility for Navajo Web Services</p>
@@ -13,76 +14,86 @@ package com.dexels.navajo.util.navadoc;
 import java.io.File;
 import java.util.Properties;
 
+// XML Document & Parssers
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Element;
+
 // logging
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.apache.commons.logging.LogConfigurationException;
 import org.apache.log4j.PropertyConfigurator;
+import org.apache.log4j.xml.DOMConfigurator;
 import org.apache.log4j.Logger;
 import org.apache.log4j.Priority;
+
 
 public class NavaDoc {
 
   public static final Logger logger =
-    Logger.getLogger( NavaDoc.class.getName() ) ;
+    Logger.getLogger( NavaDoc.class.getName() );
 
   private Configurator config = null;
   private String[] args = null;
 
-
   public static void main( String[] args )
     throws Exception {
     NavaDoc documenter = new NavaDoc();
+
     documenter.setArguments( args );
     documenter.configure();
     logger.log( Priority.INFO, "finished" );
 
   }
 
-  public void setArguments( String[] args ) { this.args = args; }
+  public void setArguments( String[] args ) {
+    this.args = args;
+  }
+
   public void configure()
     throws Exception {
-     config = new Configurator( this.args, this.getClass() );
+    config = new Configurator( this.args );
   }
 
   private class Configurator {
 
-     private File configPath =
-       new File( "." + File.pathSeparator + "navadoc-config.xml" );
-     private Logger logger = null;
+    private File configPath = new File( "navadoc.xml" );
 
-     public Configurator( String[] args, Class parent )
-       throws Exception {
+    public File servicePath = null;
+    public File sylesheetPath = null;
 
-       for ( int i = 1; i < args.length; i++ ) {
-         if ( args[i].compareToIgnoreCase( "--config" ) == 0 ) {
-           configPath = new File( args[ i + 1 ] );
-           i++;
-         }
-       }
+    public Configurator( String[] args )
+      throws Exception {
 
-       // @todo: configure logger from central XML file
+      for ( int i = 0; i < args.length; i++ ) {
+        if ( args[i].compareToIgnoreCase( "--config" ) == 0 ) {
+          configPath = new File( args[ i + 1 ] );
+          i++;
+        }
+      }
 
-      Properties lProps = new Properties();
-      lProps.setProperty( "log4j.rootLogger", "debug, stdout, R" );
-      lProps.setProperty( "log4j.appender.stdout", "org.apache.log4j.ConsoleAppender" );
-      lProps.setProperty( "log4j.appender.stdout.layout", "org.apache.log4j.PatternLayout" );
-      lProps.setProperty( "log4j.appender.stdout.layout.ConversionPattern", "%d %5p [%t] (%F:%L) - %m%n" );
-      lProps.setProperty( "log4j.appender.R", "org.apache.log4j.RollingFileAppender" );
-      lProps.setProperty( "log4j.appender.R.File", "D:/Projecten/NavaDoc/log/navadoc.log" );
-      lProps.setProperty( "log4j.appender.R.MaxFileSize", "100KB" );
-      lProps.setProperty( "log4j.appender.R.MaxBackupIndex", "1" );
-      lProps.setProperty( "log4j.appender.R.layout", "org.apache.log4j.PatternLayout" );
-      lProps.setProperty( "log4j.appender.R.layout.ConversionPattern", "%d %5p [%t] (%F:%L) - %m%n" );
-      PropertyConfigurator.configure( lProps );
+      // get configuration as DOM
+      DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+      DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+      Document xConf = dBuilder.parse( this.configPath );
+
+      DOMConfigurator.configure(
+        (Element) xConf.getElementsByTagName( "log4j:configuration" ).item( 0 ) );
 
       Logger.getLogger( this.getClass() ).log( Priority.DEBUG, "started logging" );
 
-     } // Configurator()
+      // get NavaDoc configuration from DOM
+      Element navConf = (Element) xConf.getElementsByTagName( "configuration" ).item( 0 );
+      Element servConf = (Element) navConf.getElementsByTagName( "services-path" ).item( 0 );
+      Element xslConf = (Element) navConf.getElementsByTagName( "stylesheet-path" ).item( 0 );
+
+
+
+    } // Configurator()
 
   } // private class configurator
 
 } // public class NavaDoc
-
 
 // EOF: $RCSfile$ //
