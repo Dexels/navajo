2f1f4cb70da8059eb61bff117459c2907edcc408
Matthew Eichler <matthew.eichler@aventinesolutions.org>
2002-10-18 15:48:55 +0000

    added REGEXP library

--- bin/navadoc.cygwin.sh
+++ bin/navadoc.cygwin.sh
@@ -11,16 +11,17 @@ export CLASSPATH="${LIB_BASE}\\lib\\xalan_1_2_D02.jar"
 export CLASSPATH="${CLASSPATH};${LIB_BASE}\\lib\\xerces_1_2.jar"
 export CLASSPATH="${CLASSPATH};${LIB_BASE}\\lib\\commons-logging.jar"
 export CLASSPATH="${CLASSPATH};${LIB_BASE}\\lib\\log4j-1.2.7.jar"
+export CLASSPATH="${CLASSPATH};${LIB_BASE}\\lib\\gnu-regexp-1.1.4.jar"
 export CLASSPATH="${CLASSPATH};${LIB_BASE}\\classes"
 
-# Configuration location
-# CONFIG="--config D:\\Projecten\\NavaDoc\\config\\navadoc.xml"
+# Configuration location as a URI (Xerces is happier getting documents by URI)
+CONFIG="-DconfigUri=file:///d:/Projecten/NavaDoc/config/navadoc.xml"
 
 # project home, Cygwin style
 PROJECTHOME=D:Projecten/NavaDoc
 
-cd ${PROJECTHOME}/classes &&
-  java -cp ${CLASSPATH} ${LOGPARAM} com.dexels.navajo.util.navadoc.NavaDoc ${CONFIG}
+# cd ${PROJECTHOME}/classes &&
+java -cp ${CLASSPATH} ${LOGPARAM} com.dexels.navajo.util.navadoc.NavaDoc ${CONFIG}
 
 ### EOF: $RCSfile$ ###
 
