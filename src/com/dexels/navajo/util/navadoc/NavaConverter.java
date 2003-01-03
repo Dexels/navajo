fcc88d8ab439e358a1f3e1abf5377a353cbddd1a
Matthew Eichler <matthew.eichler@aventinesolutions.org>
2003-01-03 17:35:50 +0000

    initial version, not exactly working

---
+++ src/com/dexels/navajo/util/navadoc/NavaConverter.java
@@ -0,0 +1,167 @@
+package com.dexels.navajo.util.navadoc;
+
+/**
+ * <p>Title: NavaDoc: NavaConverter</p>
+ * <p>Description: converts from two-file old Navajo script format
+ * to new single file script format</p>
+ * <p>Copyright: Copyright (c) 2002</p>
+ * <p>Company: Dexels.com</p>
+ * @author Matthew Eichler
+ * @version 1.0
+ */
+
+import java.io.File;
+import java.util.Iterator;
+
+// logging
+import org.apache.log4j.Logger;
+import org.apache.log4j.Priority;
+
+// W3C DOM
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Attr;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.NamedNodeMap;
+
+// XML Parsers
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.OutputKeys;
+
+public class NavaConverter {
+
+  public static final String vcIdent = "$Id$";
+
+  public static final Logger logger =
+    Logger.getLogger( NavaConverter.class.getName() );
+
+  private NavaDocConfigurator config = new NavaDocConfigurator();
+
+  // paths
+  private File servicesPath = null;
+  private File targetPath = null;
+
+  private ServicesList list = null;
+
+  // document builder & transformer
+  DocumentBuilder docBuilder = null;
+  Transformer transformer = null;
+
+  // document paths and DOM's
+  private String tmlPath = null;
+  private String xslPath = null;
+
+  private File tmlFile = null;
+  private File xslFile = null;
+
+  private Document tmlDoc = null;
+  private Document xslDoc = null;
+
+  // ------------------------------------------------------------ public methods
+
+  public NavaConverter()
+    throws ConfigurationException {
+
+    config.configure();
+
+    this.servicesPath = config.getPathProperty( "services-path" );
+    this.targetPath = config.getPathProperty( "target-path" );
+
+    this.list = new ServicesList( this.servicesPath );
+
+    try {
+
+      this.docBuilder =
+        DocumentBuilderFactory.newInstance().newDocumentBuilder();
+
+      this.transformer = TransformerFactory.newInstance().newTransformer(); ;
+      this.transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+      transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+      Iterator iter = this.list.iterator();
+
+      while ( iter.hasNext() ) {
+        String baseName = (String) iter.next();
+        this.tmlPath = this.servicesPath + File.separator +
+          baseName + ".tml";
+        this.tmlFile = new File ( this.tmlPath );
+        this.tmlDoc = docBuilder.parse( this.tmlFile );
+        this.xslPath = this.servicesPath + File.separator +
+          baseName + ".xsl";
+        this.xslFile = new File ( this.xslPath );
+        this.xslDoc = docBuilder.parse( this.xslFile );
+
+        this.convert();
+        this.write( baseName );
+
+      }
+
+    } catch ( Exception e ) {
+      logger.log( Priority.ERROR, e.toString() );
+    }
+
+
+  } // public NavaConverter()
+
+  public static void main( String[] args )
+    throws ConfigurationException {
+
+    NavaConverter converter = new NavaConverter();
+
+    logger.log( Priority.INFO, "finished" );
+
+  } // main()
+
+  // ----------------------------------------------------------- private methods
+
+  private void convert() {
+
+    Element rootElem = this.tmlDoc.getDocumentElement();
+
+    // copy the attributes of the TML root node to the XSL document
+    // these are the documentation attributes
+    NamedNodeMap attrMap = rootElem.getAttributes();
+
+    if ( attrMap != null ) {
+      for ( int i = 0; i < attrMap.getLength(); i++ ) {
+        Attr a = (Attr) attrMap.item( i );
+        Attr b = xslDoc.createAttribute( a.getName() );
+        b.setValue( a.getValue() );
+      }
+    }
+
+    // copy the children of the TML over to the XSL document
+    NodeList children = rootElem.getChildNodes();
+    if ( children != null ) {
+      for ( int i = 0; i < children.getLength(); i++ ) {
+        this.xslDoc.importNode( children.item( i ), true );
+      }
+      logger.log( Priority.DEBUG, "number of children is " +
+        children.getLength() );
+    }
+
+  }
+
+  private void write( String name )
+    throws TransformerException {
+
+    File outputFile = new File (
+      this.targetPath + File.separator + name + ".xsl" );
+
+    this.transformer.transform(
+      new DOMSource( this.xslDoc ), new StreamResult( outputFile ) );
+
+    logger.log( Priority.INFO, "wrote new XSL file to " +
+      outputFile.getAbsoluteFile() );
+  }
+
+} // public class NavaConverter
+
+// EOF: $RCSfile$ //
