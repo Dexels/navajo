a569e5c38c18c406f2da0b7e8b314e9b99eea8ab
Frank Lyaruu <frank@dexels.com>
2003-04-24 13:44:20 +0000

    no message

---
+++ src/com/dexels/navajo/tipi/tipixml/CaseSensitiveXMLElement.java
@@ -0,0 +1,9 @@
+package com.dexels.navajo.tipi.tipixml;
+import java.util.*;
+
+public class CaseSensitiveXMLElement extends XMLElement {
+
+  public CaseSensitiveXMLElement() {
+    super(new Hashtable(),true,false);
+  }
+}
