46b3516cd9f1c1c3cefd7c14910183576e129153
Matthew Eichler <matthew.eichler@aventinesolutions.org>
2003-04-29 14:44:15 +0000

    unabashedly walks the directory tree of web services

--- src/com/dexels/navajo/util/navadoc/NavaDoc.java
+++ src/com/dexels/navajo/util/navadoc/NavaDoc.java
@@ -13,6 +13,7 @@ package com.dexels.navajo.util.navadoc;
 import java.io.File;
 import java.io.FileFilter;
 import java.util.Iterator;
+import java.util.Stack;
 
 // logging
 import org.apache.log4j.Logger;
@@ -36,9 +37,16 @@ public class NavaDoc {
   private File servicesPath = null;
   private File targetPath = null;
 
+  // output settings
+  private String pname = null;
+  private String cssUri = null;
+  private String indent = null;
+
   private ServicesList list = null;
   private NavaDocTransformer transformer = null;
   private NavaDocIndexDOM index = null;
+
+  private Stack subDirs = new Stack();
 
   /**
    * Outside mediator object which controls all the
@@ -61,74 +69,13 @@ public class NavaDoc {
     this.targetPath = config.getPathProperty( "target-path" );
 
     // optional parameters, null's OK
-    String pname = config.getStringProperty( "project-name" );
-    String cssUri = config.getStringProperty( "css-uri" );
-    String indent = config.getStringProperty( "indent" );
-
-    try {
-      this.transformer = new NavaDocTransformer(
-            this.styleSheetPath, this.servicesPath, indent );
-      this.list = new ServicesList( this.servicesPath );
-
-      // set optional parameters, nulls OK
-      this.transformer.setProjectName( pname );
-      this.transformer.setCssUri( cssUri );
-
-      // set-up an index DOM
-      this.index = new NavaDocIndexDOM( pname, cssUri );
-
-    } catch ( ConfigurationException ce ) {
-      // set configuration URI to inform user and throw upwards
-      ce.setConfigUri( this.config.getConfigUri() );
-      throw ( ce );
-    } catch ( TransformerConfigurationException tce ) {
-      ConfigurationException ce =
-        new ConfigurationException( tce.toString(),
-          this.config.getConfigUri() );
-
-      throw ( ce );
-    } catch ( ParserConfigurationException pce ) {
-      ConfigurationException ce =
-        new ConfigurationException( pce.toString(),
-          this.config.getConfigUri() );
-
-      throw ( ce );
-    }
+    this.pname = this.config.getStringProperty( "project-name" );
+    this.cssUri = this.config.getStringProperty( "css-uri" );
+    this.indent = this.config.getStringProperty( "indent" );
 
     this.walkTree( this.servicesPath, this.config.getFileFilter() );
 
-    this.document();
-
-    // output the index page
-    NavaDocOutputter idxOut =
-      new NavaDocOutputter( this.index, this.targetPath );
-
   }
-
-  /**
-   * does all the work of going through the list of web
-   * services and using the transformer to generate the
-   * documentation.  There will be a single document combining
-   * BPFL and BPCL documentation for a logical web service.
-   * A link in the index page is created after each service
-   * has an HTML page generated.
-   */
-
-  public void document() {
-    Iterator iter = this.list.iterator();
-
-    while ( iter.hasNext() ) {
-
-      String sname = (String) iter.next();
-      System.out.println("sname = " + sname);
-
-      this.transformer.transformWebService( sname );
-      NavaDocOutputter outputter =
-        new NavaDocOutputter( this.transformer, this.targetPath );
-
-      this.index.addEntry( sname, this.transformer.getNotes() );
-    }
-  } // public void document()
 
   /**
    * Provides a count of all the web services found based
@@ -153,7 +100,9 @@ public class NavaDoc {
 
   // ----------------------------------------------------------- private methods
 
-  private void walkTree( final File dir, final FileFilter filter ) {
+  private void walkTree( final File dir, final FileFilter filter )
+   throws ConfigurationException {
+
     this.logger.log( Priority.DEBUG, "scripts directory '" +
       dir.getAbsolutePath() + "' found." );
     final File[] contents = dir.listFiles( filter );
@@ -162,6 +111,106 @@ public class NavaDoc {
         this.walkTree( contents[i], filter );
       }
     }
+    String d = "";
+    if ( ! dir.equals( this.servicesPath ) ) {
+      if ( this.subDirs.empty() ) {
+        d = dir.getName();
+      } else {
+        d = (String) this.subDirs.peek() + File.separator + dir.getName();
+      }
+
+      this.subDirs.push( d );
+    }
+
+    // document this directory
+    this.setTransformer( d );
+    this.document( d );
+
+    // output the index page
+    NavaDocOutputter idxOut =
+      new NavaDocOutputter( this.index, new File( this.targetPath, d ) );
+
+  } // private void walkTree()
+
+  private void setTransformer( final String dir )
+    throws ConfigurationException {
+
+    final File sPath = new File( this.servicesPath, dir );
+
+    try {
+      this.transformer = new NavaDocTransformer(
+          this.styleSheetPath, sPath, this.indent);
+      this.list = new ServicesList( sPath );
+
+      // set optional parameters, nulls OK
+      this.transformer.setProjectName( this.pname );
+      this.transformer.setCssUri( this.cssUri );
+
+      // set-up an index DOM
+      this.index = new NavaDocIndexDOM( this.pname, this.cssUri);
+    }
+    catch (ConfigurationException ce) {
+      // set configuration URI to inform user and throw upwards
+      ce.setConfigUri(this.config.getConfigUri());
+      throw (ce);
+    }
+    catch (TransformerConfigurationException tce) {
+      ConfigurationException ce =
+          new ConfigurationException(tce.toString(),
+                                     this.config.getConfigUri());
+      throw (ce);
+    }
+    catch (ParserConfigurationException pce) {
+      ConfigurationException ce =
+          new ConfigurationException(pce.toString(),
+                                     this.config.getConfigUri());
+      throw (ce);
+    }
+
+  }
+
+  /**
+   * does all the work of going through the list of web
+   * services and using the transformer to generate the
+   * documentation.  There will be a single document combining
+   * BPFL and BPCL documentation for a logical web service.
+   * A link in the index page is created after each service
+   * has an HTML page generated.
+   */
+
+  private void document( final String dir )
+   throws ConfigurationException {
+
+    final File tPath = new File( this.targetPath, dir );
+    this.checkTarget( tPath );
+    final Iterator iter = this.list.iterator();
+
+    while ( iter.hasNext() ) {
+
+      final String sname = (String) iter.next();
+
+      this.transformer.transformWebService( sname );
+      NavaDocOutputter outputter =
+        new NavaDocOutputter( this.transformer, tPath );
+
+      this.index.addEntry( sname, this.transformer.getNotes() );
+    }
+
+  } // private void document()
+
+  // checks documentation target directory and creates it if possible
+  private void checkTarget( final File path )
+   throws ConfigurationException {
+
+     if ( ! path.exists() ) {
+        final boolean result = path.mkdir();
+        if ( ! result ) {
+          throw ( new ConfigurationException(
+            "unable to create documentation target directory '" +
+            path, this.config.getConfigUri() ) );
+        }
+     }
+
   }
 
 } // public class NavaDoc
