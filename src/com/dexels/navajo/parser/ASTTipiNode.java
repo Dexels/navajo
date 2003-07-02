3df59c79ef2646b6c57faf7f7d654ce6db72d593
Arnoud Philip <arnoud@dexels.com>
2003-07-02 09:29:18 +0000

    no message

--- src/com/dexels/navajo/parser/ASTTipiNode.java
+++ src/com/dexels/navajo/parser/ASTTipiNode.java
@@ -2,6 +2,8 @@
 
 package com.dexels.navajo.parser;
 
+import com.dexels.navajo.tipi.*;
+
 public class ASTTipiNode extends SimpleNode {
 
   String val = "";
@@ -11,14 +13,14 @@ public class ASTTipiNode extends SimpleNode {
   }
 
   public Object interpret() throws TMLExpressionException {
-
     System.out.println("in ASTTipiNode(), val = " + val);
-
+    // TipiLink link = blblala
+    // return link.evaluateExpression(val);
     return  "AAP";
   }
 
   public static void main(String [] args) throws Exception {
-      java.io.StringReader reader  = new java.io.StringReader("@property:/.:.:/Club/Aap + 5");
+      java.io.StringReader reader  = new java.io.StringReader("property:/.:.:/Club/Aap+ 5");
       TMLParser parser = new TMLParser(reader);
       parser.Expression();
       Object result = parser.jjtree.rootNode().interpret();
---
+++ src/com/dexels/navajo/tipi/TipiLink.java
@@ -0,0 +1,14 @@
+package com.dexels.navajo.tipi;
+
+/**
+ * <p>Title: Navajo Product Project</p>
+ * <p>Description: This is the official source for the Navajo server</p>
+ * <p>Copyright: Copyright (c) 2002</p>
+ * <p>Company: Dexels BV</p>
+ * @author not attributable
+ * @version 1.0
+ */
+
+public interface TipiLink {
+  public Object evaluateExpression(String expression);
+}
