81bb26ff55092b9e350de452eff90b78cefeb57d
Arjen Schoneveld <aschoneveld@dexels.com>
2003-09-26 13:51:04 +0000

    *** empty log message ***

---
+++ src/com/dexels/navajo/functions/CurrentTimeMillis.java
@@ -0,0 +1,24 @@
+package com.dexels.navajo.functions;
+
+import com.dexels.navajo.parser.*;
+
+
+public class CurrentTimeMillis extends FunctionInterface {
+
+  public String remarks() {
+   return "CurrentTimeMillis(), returns the time in millis since 1/1/1970";
+  }
+  public Object evaluate() throws com.dexels.navajo.parser.TMLExpressionException {
+    return System.currentTimeMillis()+"";
+  }
+  public String usage() {
+   return "CurrentTimeMillis()";
+  }
+
+  public static void main(String [] args) throws Exception {
+    com.dexels.navajo.parser.Expression expr = new com.dexels.navajo.parser.Expression();
+    Operand o = expr.evaluate("CurrentTimeMillis()", null);
+    System.err.println("o.type = " + o.type);
+    System.err.println("o.value = " + o.value);
+  }
+}
---
+++ src/com/dexels/navajo/functions/Now.java
@@ -0,0 +1,27 @@
+package com.dexels.navajo.functions;
+
+import com.dexels.navajo.parser.*;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+public class Now extends FunctionInterface {
+
+  public String remarks() {
+    return "Now() returns the current timestamp as a string in the following format: yyyy/MM/dd HH:uu:mm";
+  }
+  public Object evaluate() throws com.dexels.navajo.parser.TMLExpressionException {
+    Date today = new Date();
+    SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+    return format.format(today);
+  }
+  public String usage() {
+    return "Now()";
+  }
+
+  public static void main(String [] args) throws Exception {
+    Now n = new Now();
+    n.reset();
+    String result = (String) n.evaluate();
+    System.err.println("result = " + result);
+  }
+}
