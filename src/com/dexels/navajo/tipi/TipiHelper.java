d992488cf4b3ee774a65dfd8d4c236764b772a2a
Frank Lyaruu <frank@dexels.com>
2003-10-17 08:54:12 +0000

    <No Comment Entered>

---
+++ src/com/dexels/navajo/tipi/TipiHelper.java
@@ -0,0 +1,20 @@
+package com.dexels.navajo.tipi;
+
+
+
+/**
+ * <p>Title: </p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2003</p>
+ * <p>Company: </p>
+ * @author not attributable
+ * @version 1.0
+ */
+
+public interface TipiHelper {
+  public void initHelper(TipiComponent tc);
+  public void setComponentValue(String name, Object object);
+  public Object getComponentValue(String name);
+  public void registerEvent(TipiEvent te);
+  public void  deregisterEvent(TipiEvent te);
+}
