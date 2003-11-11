8a5a885c9022272366be61338fa780084bd916af
Frank Lyaruu <frank@dexels.com>
2003-11-11 13:20:57 +0000

    <No Comment Entered>

--- src/tipi/MainApplication.java
+++ src/tipi/MainApplication.java
@@ -6,6 +6,7 @@ import com.dexels.navajo.tipi.impl.*;
 import java.io.*;
 import com.dexels.navajo.tipi.tipixml.*;
 import java.util.ResourceBundle;
+import java.util.*;
 
 public class MainApplication {
   static public void main(String[] args) throws Exception {
@@ -16,10 +17,14 @@ public class MainApplication {
     System.setProperty("com.dexels.navajo.DocumentImplementation", "com.dexels.navajo.document.nanoimpl.NavajoFactoryImpl");
     System.setProperty("com.dexels.navajo.propertyMap", "tipi.propertymap");
     UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+
+    checkForProperties(args);
+
     UIManager.put("Button.showMnemonics", Boolean.TRUE);
     TipiContext context = TipiContext.getInstance();
     boolean studiomode = args[0].equals("-studio");
 //    boolean classicmode = args[0].equals("-classic");
+
     context.setStudioMode(studiomode);
     if (studiomode) {
       DefaultTipiSplash dts = new DefaultTipiSplash("splash_studio.jpg");
@@ -29,14 +34,36 @@ public class MainApplication {
       TipiContext.getInstance().parseFile(args[args.length - 1]);
     }
     else {
-//    context.setResourceURL(MainApplication.class.getResource(""));
         System.err.println("Opening: " + TipiContext.getInstance().getResourceURL(args[args.length - 1]));
         TipiContext.getInstance().parseURL(TipiContext.getInstance().getResourceURL(args[args.length - 1]));
-//      else {
-//        DefaultTipiSplash dts = new DefaultTipiSplash("splash.jpg");
-//        dts.show();
-//        context.setSplash(dts);
-//      }
     }
+  }
+
+  private static void checkForProperties(String[] args) {
+    for (int i = 0; i < args.length; i++) {
+      String current = args[i];
+      if (current.startsWith("-D")) {
+        String prop = current.substring(2);
+        System.err.println("System property found: "+prop);
+        try {
+          StringTokenizer st = new StringTokenizer(prop, "=");
+          String name = st.nextToken();
+          String value = st.nextToken();
+          System.err.println("Setting property: "+name+" to value: "+value);
+          System.setProperty(name,value);
+          String verify = System.getProperty(name);
+          if (value.equals(verify)) {
+            System.err.println("Verify ok");
+          } else {
+            throw new RuntimeException("Error: System property set, but it did not really stick. Value: "+verify+" expected: "+value);
+          }
+        }
+        catch (NoSuchElementException ex) {
+          System.err.println("Error parsing systen property");
+        }
+      }
+
+    }
+
   }
 }
