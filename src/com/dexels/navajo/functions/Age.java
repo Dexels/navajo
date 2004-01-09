98b775398e924beb465bf062fa470460e72fd1a5
Matthew Eichler <matthew.eichler@aventinesolutions.org>
2004-01-09 17:02:51 +0000

    the Age function allows a second, option parameter of an "as of" date ... in other words, an age may be calculated as of a given date rather than today

--- src/com/dexels/navajo/functions/Age.java
+++ src/com/dexels/navajo/functions/Age.java
@@ -1,6 +1,5 @@
 package com.dexels.navajo.functions;
 
-
 import com.dexels.navajo.parser.*;
 
 /**
@@ -13,61 +12,83 @@ import com.dexels.navajo.parser.*;
  */
 import java.util.*;
 
+public class Age
+    extends FunctionInterface {
 
-public class Age extends FunctionInterface {
+  public Age() {}
 
-    public Age() {}
+  public Object evaluate() throws com.dexels.navajo.parser.
+      TMLExpressionException {
 
-    public Object evaluate() throws com.dexels.navajo.parser.TMLExpressionException {
-
-        Object o = this.getOperands().get(0);
-        if (o == null | !(o instanceof java.util.Date))
-          throw new TMLExpressionException("Age: could not return value for input: " + o);
-
-        java.util.Date datum = (java.util.Date) o;
-
-        Calendar cal = Calendar.getInstance();
-
-        cal.setTime(datum);
-        Calendar today = Calendar.getInstance();
-
-        today.setTime(new java.util.Date());
-        int yToday = today.get(Calendar.YEAR);
-        int dToday = cal.get(Calendar.YEAR);
-        int result = yToday - dToday;
-
-        if ((cal.get(Calendar.MONTH) > today.get(Calendar.MONTH))) {
-          result--;
-        }
-        else if ((cal.get(Calendar.MONTH) == today.get(Calendar.MONTH)) &&
-            (cal.get(Calendar.DAY_OF_MONTH) > today.get(Calendar.DAY_OF_MONTH))) {
-          result--;
-        }
-
-        return new Integer(result);
+    final Object o = this.getOperands().get(0);
+    if (o == null | ! (o instanceof java.util.Date)) {
+      throw new TMLExpressionException(
+          "Age: could not return value for input: " + o);
     }
 
-    public String usage() {
-        return "";
+    final java.util.Date datum = (java.util.Date) o;
+
+    final Calendar cal = Calendar.getInstance();
+
+    cal.setTime(datum);
+
+    /* if a second operand is provided, calculate the age from that
+        date, otherwise calculate the age as of today */
+    Calendar asofCal = Calendar.getInstance();
+    asofCal.setTime(new java.util.Date());
+
+    if ( this.getOperands().size() > 1 ) {
+      final Object p = this.getOperands().get(1);
+      if ( (p != null) && (p instanceof java.util.Date)) {
+        asofCal.setTime( (java.util.Date) p);
+      }
     }
 
-    public String remarks() {
-        return "";
+    int yToday = asofCal.get(Calendar.YEAR);
+    int dToday = cal.get(Calendar.YEAR);
+    int result = yToday - dToday;
+
+    if ( (cal.get(Calendar.MONTH) > asofCal.get(Calendar.MONTH))) {
+      result--;
+    }
+    else if ( (cal.get(Calendar.MONTH) == asofCal.get(Calendar.MONTH)) &&
+             (cal.get(Calendar.DAY_OF_MONTH) >
+              asofCal.get(Calendar.DAY_OF_MONTH))) {
+      result--;
     }
 
-    public static void main(String args[]) throws Exception {
-        Age a = new Age();
-        Calendar c = Calendar.getInstance();
+    return new Integer(result);
+  }
 
-        c.set(1966, 11, 20);
-        System.err.println("c =" + c.getTime());
-        a.reset();
-        a.insertOperand(c.getTime());
-        Integer age = (Integer) a.evaluate();
+  public String usage() {
+    return "Age( BirthDate [, AsOf Date] )";
+  }
 
-        System.out.println("age = " + age.intValue());
+  public String remarks() {
+    return "calculates the age given a birth date.  The calculation is as of the second date parameter, otherwise, if not provided, will be as of today.";
+  }
 
-        System.out.println("MAX INT = " + Integer.MAX_VALUE);
-        System.out.println("MAX LONG = " + Long.MAX_VALUE);
-    }
+  public static void main(String args[]) throws Exception {
+    final Age a = new Age();
+    final Calendar c = Calendar.getInstance();
+    c.setTime(new java.util.Date());
+    final java.util.Date now = c.getTime();
+
+    c.set(1966, 11, 20);
+    System.err.println("birth date is " + c.getTime());
+    a.reset();
+    a.insertOperand(c.getTime());
+    Integer age = (Integer) a.evaluate();
+
+    System.err.println("age = " + age.intValue() + ", as of " + now);
+
+    final Calendar asof = Calendar.getInstance();
+    asof.set(1972, 4, 14);
+    a.reset();
+    a.insertOperand(c.getTime());
+    a.insertOperand(asof.getTime());
+    age = (Integer) a.evaluate();
+    System.err.println("age = " + age.intValue() + ", as of " + asof.getTime());
+
+  }
 }
