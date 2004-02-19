a3fb9b0d7faff91044dbf5f09805bb693d483451
Arjen Schoneveld <aschoneveld@dexels.com>
2004-02-19 15:35:33 +0000

    Improved clocktime parsing logic.

--- src/com/dexels/navajo/document/types/ClockTime.java
+++ src/com/dexels/navajo/document/types/ClockTime.java
@@ -27,11 +27,37 @@ public class ClockTime {
   public ClockTime(String s) throws Exception {
     StringTokenizer tokens = new StringTokenizer(s, ":");
     if (tokens.countTokens() == 1) {
+      if (s.startsWith("00")) {
+        s = s.substring(2);
+        if (Integer.parseInt(s) >= 60)
+          throw new Exception("Invalid clocktime: " + s);
+        value = df.parse("00:"+s+":00");
+        return;
+      }
+      if (s.startsWith("0"))
+        s = s.substring(1);
       int h = Integer.parseInt(s);
       if (h >= 0 && h <= 9)
         s = "0"+s+":00:00";
-      else
+      else if (h < 24)
         s = s+":00:00";
+      else if (h >= 100 && h <= 959) {
+        int hh = Integer.parseInt( s.substring(0, 1) );
+        int mm = Integer.parseInt( s.substring(1, 3) );
+        if (mm >= 60)
+          throw new Exception("Invalid clocktime: " + s);
+        s = "0" + hh + ":" + mm + ":00";
+      } else if (h >= 1000 && h <= 2359) {
+        int hh = Integer.parseInt( s.substring(0, 2) );
+        int mm = Integer.parseInt( s.substring(2, 4) );
+        if (hh >= 24)
+          throw new Exception("Invalid clocktime: " + s);
+        if (mm >= 60)
+          throw new Exception("Invalid clocktime: " + s);
+        s = hh + ":" + mm + ":00";
+      } else {
+        throw new Exception("Invalid clocktime: " + s);
+      }
     } else
     if (tokens.countTokens() == 2) {
       s += ":00";
@@ -56,9 +82,9 @@ public class ClockTime {
     // Tests.
 
     System.out.println("value = " + new ClockTime(new java.util.Date()).toString());
-    ClockTime ck = new ClockTime("9");
+    ClockTime ck = new ClockTime("945");
     System.out.println("ck = " + ck);
-    ClockTime ck2 = new ClockTime("10");
+    ClockTime ck2 = new ClockTime("0055");
     System.out.println("ck2 = " + ck2);
     System.out.println("date ck = " + ck.dateValue().getTime());
     System.out.println("date ck2 = " + ck2.dateValue().getTime());
