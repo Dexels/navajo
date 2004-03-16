34686e9590498391b0acac29f1042aaf149e8f01
Matthew Eichler <matthew.eichler@aventinesolutions.org>
2004-03-16 14:28:45 +0000

    added optional Locate to FormateDate()

--- src/com/dexels/navajo/functions/FormatDate.java
+++ src/com/dexels/navajo/functions/FormatDate.java
@@ -20,8 +20,8 @@ public final class FormatDate extends FunctionInterface {
 
     public final Object evaluate() throws com.dexels.navajo.parser.TMLExpressionException {
 
-        if (this.getOperands().size() != 2)
-          throw new TMLExpressionException("usage: FormatDate(date, format)");
+        if (this.getOperands().size() < 2 || this.getOperands().size() > 3)
+          throw new TMLExpressionException(this.usage());
 
         java.util.Date date = (java.util.Date) this.getOperands().get(0);
         if (date == null)
@@ -31,26 +31,51 @@ public final class FormatDate extends FunctionInterface {
         if (format == null)
           throw new TMLExpressionException("FormatDate: format cannot be null");
 
-        java.text.SimpleDateFormat formatter = new java.text.SimpleDateFormat(format);
+        java.text.SimpleDateFormat formatter = null;
+
+        if (this.getOperands().size() > 2) {
+          final String loc = (String)this.getOperands().get(2);
+          if (loc != null && loc.length() > 0) {
+            final Locale l = new Locale(loc);
+            if (l == null) {
+              throw new TMLExpressionException(
+                  "FormatDate: unavailable or invalid locale: '" + loc + "'");
+            }
+            formatter = new java.text.SimpleDateFormat(format, l);
+          }
+        }
+        if (formatter == null) {
+          formatter = new java.text.SimpleDateFormat(format);
+        }
 
         return formatter.format(date);
     }
 
     public String usage() {
-        return "FormatDate(date, format)";
+        return "FormatDate(date, format[, locale])";
     }
 
     public String remarks() {
-        return "";
+        return "formats a date according to a format string plus an optional locale";
     }
 
     public static void main(String args[]) {
         Calendar c = Calendar.getInstance();
 
-        c.set(1949, 3, 2);
-        java.text.SimpleDateFormat formatter = new java.text.SimpleDateFormat("MM-dd-yyyy");
+        {
+          c.set(1949, 3, 2);
+          java.text.SimpleDateFormat formatter = new java.text.SimpleDateFormat(
+              "MM-dd-yyyy");
 
-        System.out.println(formatter.format(c.getTime()));
+          System.out.println(formatter.format(c.getTime()));
+        }
+        {
+          c.set(1958, 3, 27);
+          java.text.SimpleDateFormat formatter = new java.text.SimpleDateFormat(
+              "dd-MMM-yy", new Locale("nl"));
+
+          System.out.println(formatter.format(c.getTime()));
+        }
 
     }
 }
