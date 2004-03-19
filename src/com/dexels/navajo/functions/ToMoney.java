f4a9629809dfc80de121f5299c1495b39e4e8f91
Arjen Schoneveld <aschoneveld@dexels.com>
2004-03-19 10:12:24 +0000

    *** empty log message ***

---
+++ src/com/dexels/navajo/functions/ToMoney.java
@@ -0,0 +1,54 @@
+package com.dexels.navajo.functions;
+
+import com.dexels.navajo.parser.*;
+import com.dexels.navajo.document.types.Money;
+
+
+/**
+ * <p>Title: Navajo Product Project</p>
+ * <p>Description: This is the official source for the Navajo server</p>
+ * <p>Copyright: Copyright (c) 2002</p>
+ * <p>Company: Dexels BV</p>
+ * @author Arjen Schoneveld
+ * @version 1.0
+ */
+
+public class ToMoney extends FunctionInterface {
+  public ToMoney() {
+  }
+
+  public String remarks() {
+    return "Cast a string/double/integer to a money object";
+  }
+
+  public Object evaluate() throws com.dexels.navajo.parser.TMLExpressionException {
+    Object o = getOperand(0);
+   if (o == null)
+     return null;
+   else
+     return new Money(o);
+  }
+
+  public String usage() {
+    return "ToMoney(String/Integer/Double): Money";
+  }
+
+  public static void main(String [] args) throws Exception {
+
+    java.util.Locale.setDefault(new java.util.Locale("nl", "NL"));
+    // Tests.
+    ToMoney tm = new ToMoney();
+    tm.reset();
+    tm.insertOperand(new Double(1024.4990));
+    System.out.println("result = " + ((Money) tm.evaluate()).formattedString());
+
+    // Using expressions.
+    String expression = "ToMoney(1024.50) + 500 - 5.7 + ToMoney(1)/2";
+    Operand o = Expression.evaluate(expression, null);
+    System.out.println("o = " + o.value);
+    System.out.println("type = " + o.type);
+
+
+  }
+
+}
