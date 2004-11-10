1a7867dd2cd39964af320bde127370cd6ef14b88
Arnoud Philip <arnoud@dexels.com>
2004-11-10 16:43:46 +0000

    no message

---
+++ src/com/dexels/navajo/tipi/components/core/TipiTimerImpl.java
@@ -0,0 +1,78 @@
+package com.dexels.navajo.tipi.components.core;
+
+import com.dexels.navajo.tipi.internal.TipiAction;
+import com.dexels.navajo.tipi.internal.TipiEvent;
+import com.dexels.navajo.tipi.TipiBreakException;
+
+/**
+ * <p>Title: </p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2003</p>
+ * <p>Company: </p>
+ * @author not attributable
+ * @version 1.0
+ */
+public class TipiTimerImpl
+    extends TipiComponentImpl
+    implements Runnable {
+  private boolean isRunning;
+  private long interval = 1000; //standard is one second
+  private Thread t;
+  public TipiTimerImpl() {
+  }
+
+  private void start() {
+    if (t == null) {
+      t = new Thread(this);
+      t.start();
+      isRunning = true;
+    }
+  }
+
+  private final void stop() {
+    isRunning = false;
+  }
+
+  public Object createContainer() {
+    return null;
+  }
+
+  public void run() {
+    while (isRunning) {
+      try {
+        this.performTipiEvent("onTimer", null, true);
+        t.sleep(interval);
+      }
+      catch (Exception e) {
+        System.err.println("Timer had an exception, stopping");
+        e.printStackTrace();
+        isRunning = false;
+      }
+    }
+    t = null;
+  }
+
+  public void setComponentValue(final String name, final Object object) {
+    if (name.equals("interval")) {
+      this.interval = ( (Integer) object).longValue();
+    }
+  }
+
+  /**
+   * performMethod
+   *
+   * @param methodName String
+   * @param invocation TipiAction
+   * @param event TipiEvent
+   * @throws TipiBreakException
+   * @todo Implement this com.dexels.navajo.tipi.TipiComponent method
+   */
+  public void performMethod(String methodName, TipiAction invocation, TipiEvent event) throws TipiBreakException {
+    if("start".equals(methodName)){
+      start();
+    }
+    if("stop".equals(methodName)){
+      stop();
+    }
+  }
+}
