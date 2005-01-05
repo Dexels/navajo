ff10e494ed0fc3c929197ee6ac50366e978ceef9
Arjen Schoneveld <aschoneveld@dexels.com>
2005-01-05 21:20:54 +0000

    Added threadsafe collections for HashMaps.

--- src/com/dexels/navajo/mapping/AsyncStore.java
+++ src/com/dexels/navajo/mapping/AsyncStore.java
@@ -31,11 +31,12 @@ import com.dexels.navajo.server.Access;
  * ====================================================================
  */
 
-public final class AsyncStore implements Runnable {
+public final class AsyncStore
+    implements Runnable {
 
   private static AsyncStore instance = null;
-  public HashMap objectStore = null;
-  public HashMap accessStore = null;
+  public Map objectStore = null;
+  public Map accessStore = null;
   private float timeout;
   private long threadWait = 20000;
 
@@ -49,7 +50,7 @@ public final class AsyncStore implements Runnable {
   }
 
   /**
-   * Get the singleton AsyncStore object instance given an async inactive timeout.
+       * Get the singleton AsyncStore object instance given an async inactive timeout.
    *
    * @param timeout
    * @return
@@ -58,10 +59,11 @@ public final class AsyncStore implements Runnable {
     if (instance == null) {
       instance = new AsyncStore();
       instance.timeout = timeout;
-      if (instance.timeout < instance.threadWait)
+      if (instance.timeout < instance.threadWait) {
         instance.threadWait = (long) (instance.timeout / 2);
-      instance.objectStore = new HashMap();
-      instance.accessStore = new HashMap();
+      }
+      instance.objectStore = Collections.synchronizedMap(new HashMap());
+      instance.accessStore = Collections.synchronizedMap(new HashMap());
       Thread thread = new Thread(instance);
       thread.start();
     }
@@ -72,13 +74,14 @@ public final class AsyncStore implements Runnable {
    * Start the main AsyncStore loop.
    */
   public final void run() {
-    System.err.println("Started garbage collect thread for async store version $Id$, timeout = " + instance.timeout + ", thread wait = " + instance.threadWait);
+    System.err.println("Started garbage collect thread for async store version $Id$, timeout = " +
+                       instance.timeout + ", thread wait = " +
+                       instance.threadWait);
     long maxAge;
     try {
       while (true) {
-       // synchronized ( instance ) {
-          Thread.sleep(threadWait);
-          //wait(threadWait);
+        Thread.sleep(threadWait);
+        synchronized (instance) {
           Set s = new HashSet(objectStore.keySet());
           Iterator iter = s.iterator();
           while (iter.hasNext()) {
@@ -86,17 +89,22 @@ public final class AsyncStore implements Runnable {
             AsyncMappable a = (AsyncMappable) objectStore.get(ref);
             long now = System.currentTimeMillis();
             if ( (now - a.getLastAccess()) > timeout || a.isKilled()) {
-              if (!a.isKilled())
-                System.err.println("REMOVED " + ref + " FROM OBJECT STORE DUE TO TIME-OUT, now = " + now + ", lastAccess() = " + a.getLastAccess() + ", timeout = " + timeout);
-              else
+              if (!a.isKilled()) {
+                System.err.println("REMOVED " + ref +
+                                   " FROM OBJECT STORE DUE TO TIME-OUT, now = " +
+                                   now + ", lastAccess() = " + a.getLastAccess() +
+                                   ", timeout = " + timeout);
+              }
+              else {
                 System.err.println("REMOVED " + ref + " FROM OBJECT STORE DUE TO KILLONFINNISH");
+              }
               a.kill();
               objectStore.remove(ref);
               accessStore.remove(ref);
               a = null;
             }
           }
-       // }
+        }
       }
     }
     catch (InterruptedException e) {
@@ -111,24 +119,26 @@ public final class AsyncStore implements Runnable {
    * @return the unique reference to the async map
    */
   public final String addInstance(AsyncMappable o, Access a) {
-    String ref = o.hashCode()+"";
-    objectStore.put(ref+"", o);
-    accessStore.put(ref+"", a);
+    String ref = o.hashCode() + "";
+    objectStore.put(ref + "", o);
+    accessStore.put(ref + "", a);
     return ref;
   }
 
   /**
-   * Get the access object of a async map's service given it's unique reference.
+       * Get the access object of a async map's service given it's unique reference.
    *
    * @param ref
    * @return
    */
   public final Access getAccessObject(String ref) {
     Object o = accessStore.get(ref);
-    if (o == null)
+    if (o == null) {
       return null;
-    else
+    }
+    else {
       return (Access) o;
+    }
   }
 
   /**
@@ -139,10 +149,12 @@ public final class AsyncStore implements Runnable {
    */
   public final AsyncMappable getInstance(String ref) {
     Object o = objectStore.get(ref);
-    if (o == null)
+    if (o == null) {
       return null;
-    else
+    }
+    else {
       return (AsyncMappable) o;
+    }
   }
 
   /**
