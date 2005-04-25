2e1ed235913d013c8d9aecd2b54c1657741b9270
Arjen Schoneveld <aschoneveld@dexels.com>
2005-04-25 08:36:12 +0000

    *** empty log message ***

--- src/com/dexels/navajo/server/GenericHandler.java
+++ src/com/dexels/navajo/server/GenericHandler.java
@@ -119,6 +119,7 @@ public final class GenericHandler extends ServiceHandler {
                     }
                     catch (SystemException ex) {
                       sourceFile.delete();
+                      throw ex;
                     }
                   }
                 }
