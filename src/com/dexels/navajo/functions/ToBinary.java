66a1096830c8ca77c657a37e44607bf43a652bd5
Martin Bergman <mbergman@dexels.com>
2005-05-09 12:49:40 +0000

    Added conversion function for binary objects.

---
+++ src/com/dexels/navajo/functions/ToBinary.java
@@ -0,0 +1,43 @@
+package com.dexels.navajo.functions;
+
+import com.dexels.navajo.parser.*;
+import com.dexels.navajo.document.types.Binary;
+import com.dexels.navajo.document.types.Percentage;
+import com.dexels.navajo.document.Operand;
+
+
+/**
+ * <p>Title: Navajo Product Project</p>
+ * <p>Description: This is the official source for the Navajo server</p>
+ * <p>Copyright: Copyright (c) 2002</p>
+ * <p>Company: Dexels BV</p>
+ * @author Arjen Schoneveld
+ * @version 1.0
+ */
+
+public class ToBinary extends FunctionInterface {
+  public ToBinary() {
+  }
+
+  public String remarks() {
+    return "Cast a string to a binary object";
+  }
+
+  public Object evaluate() throws com.dexels.navajo.parser.TMLExpressionException {
+    Object o = getOperand(0);
+   if (o == null) {
+System.err.println("TOBBBBBBBBBBBBBBBBBBB: NULLLLLLLLLLLLL");
+     return new Binary((byte[]) null);
+   }
+   else {
+System.err.println("TOBBBBBBBBBBBBBBBBBBB: NNNNNNNOT NULLLLL");
+     return new Binary(o.toString().getBytes());
+   }
+  }
+
+  public String usage() {
+    return "ToBinary(String): Binary";
+  }
+
+
+}
