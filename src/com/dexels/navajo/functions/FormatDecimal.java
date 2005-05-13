56244f07735b5428ae1ad45f29553e38bed903d7
Matthijs Philip <matthijs@dexels.com>
2005-05-13 16:02:36 +0000

    *** empty log message ***

---
+++ src/com/dexels/navajo/functions/FormatDecimal.java
@@ -0,0 +1,56 @@
+/*
+ * Created on May 13, 2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package com.dexels.navajo.functions;
+
+import java.text.DecimalFormat;
+
+import com.dexels.navajo.parser.FunctionInterface;
+import com.dexels.navajo.parser.TMLExpressionException;
+import com.sun.rsasign.d;
+
+/**
+ * @author matthijs
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class FormatDecimal extends FunctionInterface {
+
+    /* (non-Javadoc)
+     * @see com.dexels.navajo.parser.FunctionInterface#remarks()
+     */
+    public String remarks() {
+       return "Formats a number";
+    }
+
+    /* (non-Javadoc)
+     * @see com.dexels.navajo.parser.FunctionInterface#usage()
+     */
+    public String usage() {
+       return "DecimalFormat(Object number, String format)";
+    }
+
+    /* (non-Javadoc)
+     * @see com.dexels.navajo.parser.FunctionInterface#evaluate()
+     */
+    public Object evaluate() throws TMLExpressionException {
+       if (getOperands().size() != 2) {
+           throw new TMLExpressionException(this, "Invalid number of arguments");
+       }
+       Object o1 = getOperand(0);
+       if (!(o1 instanceof Double) || !(o1 instanceof Integer)) {
+           throw new TMLExpressionException(this, "Invalid argument: " + o1);
+       }
+       Object o2 = getOperand(1);
+       if (!(o2 instanceof String) || o2 == null ) {
+           throw new TMLExpressionException(this, "Invalid argument: " + o2);
+       }
+       DecimalFormat df = new DecimalFormat(o2.toString());
+       return df.format(o1);
+    }
+
+}
