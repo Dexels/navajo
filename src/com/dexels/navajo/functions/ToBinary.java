9b7afcefb50d70e5033e4f3c13c33cbd38648cd5
Arjen Schoneveld <aschoneveld@dexels.com>
2005-05-19 14:12:50 +0000

    Fixed bug.

--- src/com/dexels/navajo/functions/ToBinary.java
+++ src/com/dexels/navajo/functions/ToBinary.java
@@ -24,11 +24,16 @@ public class ToBinary extends FunctionInterface {
   }
 
   public Object evaluate() throws com.dexels.navajo.parser.TMLExpressionException {
-    Object o = getOperand(0);
+   Object o = getOperand(0);
+   
+   System.err.println("In ToBinary(): " + o);
+   
    if (o == null) {
      return new Binary((byte[]) null);
-   }
-   else {
+   } else if ( o instanceof Binary ) {
+   	 System.err.println("I AM A BINARY!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+   	 return o;
+   } else {
      return new Binary(o.toString().getBytes());
    }
   }
