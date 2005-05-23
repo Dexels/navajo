f76089df159d488113d3669f9db8c9771ec67082
Arjen Schoneveld <aschoneveld@dexels.com>
2005-05-23 16:19:00 +0000

    *** empty log message ***

---
+++ src/com/dexels/navajo/functions/Abs.java
@@ -0,0 +1,51 @@
+/*
+ * Created on May 23, 2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package com.dexels.navajo.functions;
+
+import com.dexels.navajo.parser.FunctionInterface;
+import com.dexels.navajo.parser.TMLExpressionException;
+
+/**
+ * @author arjen
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class Abs extends FunctionInterface {
+
+	/* (non-Javadoc)
+	 * @see com.dexels.navajo.parser.FunctionInterface#remarks()
+	 */
+	public String remarks() {
+		return "Returns absolute value of a number";
+	}
+
+	/* (non-Javadoc)
+	 * @see com.dexels.navajo.parser.FunctionInterface#usage()
+	 */
+	public String usage() {
+		return "Abs(Float|Integer)";
+	}
+
+	/* (non-Javadoc)
+	 * @see com.dexels.navajo.parser.FunctionInterface#evaluate()
+	 */
+	public Object evaluate() throws TMLExpressionException {
+		Object o = getOperand(1);
+		if (o == null) {
+			return null;
+		}
+		if (o instanceof Double) {
+			return new Double(Math.abs(((Double) o).doubleValue()));
+		} else if (o instanceof Integer) {
+			return new Integer(Math.abs(((Integer) o).intValue()));
+		} else {
+			throw new TMLExpressionException(this, "Invalid operand: " + o.getClass().getName());
+		}
+	}
+
+}
