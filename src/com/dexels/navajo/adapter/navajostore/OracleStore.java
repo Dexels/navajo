8bbdfd9590ceb49d2fa1a488b35c1059b108f3e5
Arjen Schoneveld <aschoneveld@dexels.com>
2005-06-07 09:13:32 +0000

    *** empty log message ***

---
+++ src/com/dexels/navajo/adapter/navajostore/OracleStore.java
@@ -0,0 +1,46 @@
+/*
+ * Created on Jun 7, 2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package com.dexels.navajo.adapter.navajostore;
+
+import java.util.Map;
+
+import com.dexels.navajo.server.Access;
+import com.dexels.navajo.server.statistics.StoreInterface;
+
+/**
+ * @author arjen
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class OracleStore implements StoreInterface {
+
+	/* (non-Javadoc)
+	 * @see com.dexels.navajo.server.statistics.StoreInterface#storeAccess(com.dexels.navajo.server.Access)
+	 */
+	public void storeAccess(Access a) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.dexels.navajo.server.statistics.StoreInterface#setDatabaseUrl(java.lang.String)
+	 */
+	public void setDatabaseUrl(String url) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.dexels.navajo.server.statistics.StoreInterface#setDatabaseParameters(java.util.Map)
+	 */
+	public void setDatabaseParameters(Map m) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
