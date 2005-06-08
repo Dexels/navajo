4e16cc3a3efb3c88b0f4d3384085db4e8b2e67d2
Arjen Schoneveld <aschoneveld@dexels.com>
2005-06-08 13:18:57 +0000

    *** empty log message ***

--- src/com/dexels/navajo/adapter/navajostore/OracleStore.java
+++ src/com/dexels/navajo/adapter/navajostore/OracleStore.java
@@ -1,9 +1,3 @@
-/*
- * Created on Jun 7, 2005
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
 package com.dexels.navajo.adapter.navajostore;
 
 import com.dexels.navajo.server.Access;
