4b11dc775287c5fe7df565dc75041e8155e6b8ef
Frank Lyaruu <frank@dexels.com>
2005-06-29 16:05:28 +0000

    for exporting clubforms, added the 'askfile' action, which shows a file dialog

---
+++ src/com/dexels/navajo/tipi/components/swingimpl/actions/TipiAskFile.java
@@ -0,0 +1,49 @@
+/*
+ * Created on Jun 29, 2005
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package com.dexels.navajo.tipi.components.swingimpl.actions;
+
+import java.awt.*;
+import java.io.*;
+
+import javax.swing.*;
+
+import com.dexels.navajo.document.*;
+import com.dexels.navajo.tipi.*;
+import com.dexels.navajo.tipi.internal.*;
+
+/**
+ * @author Administrator
+ * 
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+public class TipiAskFile extends TipiAction {
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.dexels.navajo.tipi.internal.TipiAction#execute(com.dexels.navajo.tipi.internal.TipiEvent)
+     */
+    protected void execute(TipiEvent event) throws TipiBreakException, TipiException {
+        Operand globalvalue = getEvaluatedParameter("value", event);
+        JFileChooser jf = new JFileChooser();
+        Object o = myComponent.getContainer();
+        Container c = null;
+        if (o instanceof Container) {
+            c = (Container) c;
+        }
+        jf.setCurrentDirectory(new File(System.getProperty("user.home")));
+        int result = jf.showSaveDialog(c);
+        if (result != JFileChooser.APPROVE_OPTION) {
+            throw new TipiBreakException(0);
+        }
+        File f = jf.getSelectedFile();
+
+        myContext.setGlobalValue((String) globalvalue.value, "" + f.getAbsolutePath());
+    }
+
+}
