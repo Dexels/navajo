addbd9c77a28cb7a131b553a4d5ee1cc026407b3
Arjen Schoneveld <aschoneveld@dexels.com>
2005-07-01 10:09:01 +0000

    Removed System.err and added final qualifiers

--- src/com/dexels/navajo/tipi/actions/TipiSetValue.java
+++ src/com/dexels/navajo/tipi/actions/TipiSetValue.java
@@ -12,27 +12,28 @@ import com.dexels.navajo.tipi.internal.*;
  * @author not attributable
  * @version 1.0
  */
-public class TipiSetValue
-    extends TipiAction {
-  public void execute(TipiEvent event) throws com.dexels.navajo.tipi.TipiException, com.dexels.navajo.tipi.TipiBreakException {
+public final class TipiSetValue extends TipiAction {
+  
+	public final void execute(TipiEvent event) throws com.dexels.navajo.tipi.TipiException, com.dexels.navajo.tipi.TipiBreakException {
+    
     String path = getParameter("to").getValue();
     String value = getParameter("from").getValue();
-    System.err.println("Evaluating setValue. From: "+value+" to: "+path);
+    //System.err.println("Evaluating setValue. From: "+value+" to: "+path);
     Operand evaluated = evaluate(path,event);
     Operand evaluatedValue = evaluate(value,event);
-    System.err.println("parsed to value: "+evaluatedValue.value);
-    System.err.println("parsed to type: "+evaluatedValue.type);
+    //System.err.println("parsed to value: "+evaluatedValue.value);
+    //System.err.println("parsed to type: "+evaluatedValue.type);
 
     if (evaluated == null) {
-      System.err.println(">>>>>>>>WARNING: NULL evaluation (to) in SETVALUE: path: " + path + " from: " + value + " in component: " + myComponent.getPath());
+      //System.err.println(">>>>>>>>WARNING: NULL evaluation (to) in SETVALUE: path: " + path + " from: " + value + " in component: " + myComponent.getPath());
       return;
     }
     if (evaluated.value == null) {
-      System.err.println(">>>>>>>>WARNING: NULL value evaluation (to) in SETVALUE: path: " + path + " from: " + value + " in component: " + myComponent.getPath());
+      //System.err.println(">>>>>>>>WARNING: NULL value evaluation (to) in SETVALUE: path: " + path + " from: " + value + " in component: " + myComponent.getPath());
     }
     else {
       if (evaluated.value instanceof Property) {
-          System.err.println("Value (To) identified as property!");
+          //System.err.println("Value (To) identified as property!");
         Property p = (Property) evaluated.value;
 //        Operand evaluatedValue = evaluate(value,event);
         p.setAnyValue(evaluatedValue.value);
@@ -56,12 +57,12 @@ public class TipiSetValue
 //          }
 //        }
       }
-        System.err.println("Evaluated from. Null? "+evaluatedValue==null);
+        //System.err.println("Evaluated from. Null? "+evaluatedValue==null);
       if (evaluatedValue!=null) {
-        System.err.println("Value: "+evaluatedValue.value+" type: "+evaluatedValue.type);
+        //System.err.println("Value: "+evaluatedValue.value+" type: "+evaluatedValue.type);
     }
       if (evaluated.value instanceof TipiReference) {
-          System.err.println("Value (To) identified as attributeref!");
+          //System.err.println("Value (To) identified as attributeref!");
           TipiReference p = (TipiReference) evaluated.value;
 //            System.err.println("Attribute belongs to: "+p.getTipiComponent().getPath());
             p.setValue(evaluatedValue.value,myComponent);
