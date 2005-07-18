4fa701ad53ef66611750959d516dc8d8fb87c50a
Frank Lyaruu <frank@dexels.com>
2005-07-18 10:25:54 +0000

    Refactored the handling of the tipipath parameter. No big deal.

--- src/com/dexels/navajo/tipi/actions/TipiPerformMethod.java
+++ src/com/dexels/navajo/tipi/actions/TipiPerformMethod.java
@@ -79,43 +79,46 @@ public class TipiPerformMethod
       }
 
 
-    /** @todo REWRITE THIS STRANGE CONSTRUCTION. LOOKS OLD. SHOULD BE MUCH EASIER NOW */
+//    /** @todo REWRITE THIS STRANGE CONSTRUCTION. LOOKS OLD. SHOULD BE MUCH EASIER NOW */
     TipiValue sourceTipi = getParameter("tipipath");
     Operand method = getEvaluatedParameter("method",event);
     TipiDataComponent evalTipi = null;
-//    String evalMethod = null;
-    try {
-      evalTipi = (TipiDataComponent) evaluate(sourceTipi.getValue(),event).value;
-    }
-    catch (Exception ex) {
-      ex.printStackTrace();
-      throw new RuntimeException("Can not evaluate tipi path expression. Switching to manual.");
-    }
+    Operand tipi = getEvaluatedParameter("tipipath",event);
+    evalTipi = (TipiDataComponent) tipi.value;
+
+    //    String evalMethod = null;
+//    try {
+//      evalTipi = (TipiDataComponent) evaluate(sourceTipi.getValue(),event).value;
+//    }
+//    catch (Exception ex) {
+//      ex.printStackTrace();
+//      throw new RuntimeException("Can not evaluate tipi path expression. Switching to manual.");
+//    }
     if (method == null) {
       throw new IllegalArgumentException("Error performing method. Method evaluated to null.");
     }
     
     
-    System.err.println("\nPerformMethod. to service "+method.value+" my event: "+event.toString()+"\n**********************************************\n");
+//    System.err.println("\nPerformMethod. to service "+method.value+" my event: "+event.toString()+"\n**********************************************\n");
     
     // if no sending tipi has been specified:
-    if (sourceTipi == null || "".equals(sourceTipi)) {
-      // If it can not even find a suitable Navajo, just create a blank one
-      if (myComponent.getNearestNavajo() != null) {
-        myContext.performTipiMethod(null, myComponent.getNearestNavajo(), destination, method.value.toString(),breakOnError,event,expirationInterval, hostUrl,username,password,keystore,keypass);
-      }
-      // use the closest navajo
-      else {
-        myContext.performTipiMethod(null, NavajoFactory.getInstance().createNavajo(), destination, method.value.toString(),breakOnError,event,expirationInterval, hostUrl,username,password,keystore,keypass );
-      }
-      return;
-    }
+//    if (sourceTipi == null || "".equals(sourceTipi)) {
+//      // If it can not even find a suitable Navajo, just create a blank one
+//      if (myComponent.getNearestNavajo() != null) {
+//        myContext.performTipiMethod(null, myComponent.getNearestNavajo(), destination, method.value.toString(),breakOnError,event,expirationInterval, hostUrl,username,password,keystore,keypass);
+//      }
+//      // use the closest navajo
+//      else {
+//        myContext.performTipiMethod(null, NavajoFactory.getInstance().createNavajo(), destination, method.value.toString(),breakOnError,event,expirationInterval, hostUrl,username,password,keystore,keypass );
+//      }
+//      return;
+//    }
     // If it can not find the 'sending' tipi:
     if (evalTipi == null) {
       if (myComponent.getNearestNavajo() != null) {
         Navajo n = myComponent.getNearestNavajo();
 //        System.err.println("Not a blank NAvajo!!!");
-        myContext.performTipiMethod(null, n, destination, method.value.toString(),breakOnError,event,expirationInterval,hostUrl,username,password,keystore,keypass);
+         myContext.performTipiMethod(null, n, destination, method.value.toString(),breakOnError,event,expirationInterval,hostUrl,username,password,keystore,keypass);
       }
       else {
         System.err.println("Could not evaluate tipi. Calling service with blank navajo");
