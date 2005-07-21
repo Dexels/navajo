bab0d738c7aecdb472a0a736ee37d21fe8bf8bef
Frank Lyaruu <frank@dexels.com>
2005-07-21 12:11:09 +0000

    Touches a script or directory, causing a recompile

---
+++ src/com/dexels/navajo/studio/script/plugin/actions/RecompileAction.java
@@ -0,0 +1,64 @@
+/*
+ * Created on Apr 4, 2005
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package com.dexels.navajo.studio.script.plugin.actions;
+
+import java.util.*;
+
+import org.eclipse.core.internal.resources.*;
+import org.eclipse.core.resources.*;
+import org.eclipse.core.runtime.*;
+import org.eclipse.jface.action.*;
+import org.eclipse.jface.dialogs.*;
+import org.eclipse.jface.viewers.*;
+import org.eclipse.ui.*;
+
+import com.dexels.navajo.studio.script.plugin.*;
+
+/**
+ * @author Administrator
+ * 
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+public class RecompileAction extends BaseNavajoAction {
+
+    public RecompileAction() {
+        super();
+    }
+
+    /**
+     * The action has been activated. The argument of the method represents the
+     * 'real' action sitting in the workbench UI.
+     * 
+     * @see IWorkbenchWindowActionDelegate#run
+     */
+    public void run(IAction action) {
+        try {
+            for (Iterator iter = selectionList.iterator(); iter.hasNext();) {
+                IResource element = (IResource) iter.next();
+                element.touch(null);
+                if (element instanceof IFolder) {
+                    IFolder ff = (IFolder)element;
+                    touchRecursive(ff);
+                }
+            }
+//                file.touch(null);
+            } catch (CoreException e) {
+                e.printStackTrace();
+            }
+    }
+
+    	private void touchRecursive(IFolder fold) throws CoreException {
+    	    IResource[] ir =  fold.members();
+    	    for (int i = 0; i < ir.length; i++) {
+                if (ir[i] instanceof IFolder) {
+                    touchRecursive(((IFolder)ir[i]));
+                } 
+                ir[i].touch(null);
+            }
+    	}
+}
