bb2d7645b893e76a99f0960d2d75891700ce3919
Frank Lyaruu <frank@dexels.com>
2005-08-23 09:09:16 +0000

    Added tab to the expression language, so the parser has been rebuilt. The parser has been rebuilt using JavaCC 3.2 (The last version was built using JavaCC 2.1) It is a good thing because the code generated by 2.1 was not compliant with java 1.5 (That is, it used 'enum' as a variable name, which is a reserved keyword from 1.5 on). For the rest I think it works ok.

--- src/com/dexels/navajo/parser/ASTStringConstantNode.java
+++ src/com/dexels/navajo/parser/ASTStringConstantNode.java
@@ -1,5 +1,9 @@
 package com.dexels.navajo.parser;
 
+import java.io.*;
+
+import com.dexels.navajo.document.*;
+
 
 public final class ASTStringConstantNode extends SimpleNode {
 
@@ -16,4 +20,26 @@ public final class ASTStringConstantNode extends SimpleNode {
         return t;
 //        return new String(val.substring(1, val.length() - 1));
     }
+    
+    public static void main(String[] args) throws Exception{
+        System.err.println("alright.");
+        System.setProperty("com.dexels.navajo.DocumentImplementation", "com.dexels.navajo.document.nanoimpl.NavajoFactoryImpl");
+      NavajoFactory.getInstance().setExpressionEvaluator(new DefaultExpressionEvaluator());
+
+    Navajo n = NavajoFactory.getInstance().createNavajo();
+        Message m = NavajoFactory.getInstance().createMessage(n, "Aap");
+        Property p = NavajoFactory.getInstance().createProperty(n, "Prop", Property.EXPRESSION_PROPERTY, "'\t'", 10, "", Property.DIR_IN, null);
+        n.addMessage(m);
+        m.addProperty(p);
+        System.err.println("Value: >"+p.getValue()+"<");
+        System.err.println("Value: >"+p.getTypedValue()+"<");
+        StringWriter sw = new StringWriter();
+        n.write(sw);
+        ByteArrayInputStream sr = new ByteArrayInputStream(sw.toString().getBytes());
+        Navajo nn = NavajoFactory.getInstance().createNavajo(sr);
+        
+        System.err.println("AGAIN:");
+        nn.write(System.err);
+        
+        }
 }
