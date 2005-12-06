652c00917f817a71195af19d6b8bee8bd641b171
Frank Lyaruu <frank@dexels.com>
2005-12-06 17:14:02 +0000

    effe teruggdraaid. werkt niet in de plugin nu

--- src/com/dexels/navajo/client/socket/SocketConnection.java
+++ src/com/dexels/navajo/client/socket/SocketConnection.java
@@ -48,7 +48,7 @@ public class SocketConnection implements Runnable {
         
         try {
             while (true) {
-                Dispatcher.doClearScriptCache();
+                Dispatcher.doClearCache();
 
                 Navajo n = NavajoFactory.getInstance().createNavajo(in);
                 String service = n.getHeader().getRPCName();
