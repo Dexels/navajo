34c204994629ce54e0ab2252ecffb8347be7387a
Frank Lyaruu <frank@dexels.com>
2006-01-13 14:45:43 +0000

    Added height support

--- src/com/dexels/navajo/tipi/components/swingimpl/TipiGridPanel.java
+++ src/com/dexels/navajo/tipi/components/swingimpl/TipiGridPanel.java
@@ -1,12 +1,15 @@
 package com.dexels.navajo.tipi.components.swingimpl;
 
+import java.awt.Color;
 import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.util.*;
 
 import javax.swing.Box;
+import javax.swing.JComponent;
 import javax.swing.JPanel;
 
 import com.dexels.navajo.tipi.tipixml.XMLElement;
@@ -35,9 +38,11 @@ public class TipiGridPanel extends TipiSwingDataComponentImpl {
 
 	private int currentx = 0;
 	private int currenty = 1;
+//	private int count = 0;
 	private int gridwidth = 0;
 	private final ArrayList myWidths = new ArrayList();
-	
+	private final Map heightStrutComponentMap = new HashMap();
+//	private final Map heightStrutConstraiintMap = new HashMap();
 	// List of array of boolean
 	private final Set availabilityMatrix = new HashSet(); 
 	
@@ -49,11 +54,16 @@ public class TipiGridPanel extends TipiSwingDataComponentImpl {
 		gridComponent.setLayout(new GridBagLayout());
 		return gridComponent;
 	}
-
+	
 	 public void addToContainer(Object o, Object constraints){
 		 String constr = (String)constraints;
 		 Component c = (Component)o;
-		 gridComponent.add(c,parseGridConstraints(constr));
+		 JComponent jc = null;
+		 if (c instanceof JComponent) {
+			 jc = (JComponent)c;
+		 }
+//		 c.setBackground(new Color(t.nextFloat(),t.nextFloat(),t.nextFloat()));
+		 gridComponent.add(c,parseGridConstraints(constr,jc));
 	 }
 	 
 	  public void initBeforeBuildingChildren(XMLElement instance, XMLElement classdef) {
@@ -62,37 +72,68 @@ public class TipiGridPanel extends TipiSwingDataComponentImpl {
 
 	  }	 
 	 
-	 public GridBagConstraints parseGridConstraints(String txt) {
+	 public GridBagConstraints parseGridConstraints(String txt, JComponent component) {
 		 GridBagConstraints myData = new GridBagConstraints();
-		 if (txt==null) {
-			return myData;
-		}
-		 System.err.println("Parsing constraints: "+txt+" >> "+getPath()+" gridwidth: "+gridwidth);
-		 StringTokenizer st = new StringTokenizer(txt,";");
+		 myData.fill=GridBagConstraints.BOTH;
+		 myData.weightx=0;
+		 myData.weighty=0;
+//			return myData;
+//		 myData.weightx = 1;
+//		 System.err.println("Parsing constraints: "+txt+" >> "+getPath()+" gridwidth: "+gridwidth);
 		 myData.gridx = currentx;
 		 myData.gridy = currenty;
-		 System.err.println("Adding at: "+currentx+"/"+currenty+" grid: "+gridwidth);
-		 while (st.hasMoreTokens()) {
-			String next = st.nextToken();
-			StringTokenizer current = new StringTokenizer(next,":");
-			String key = current.nextToken();
-			String value = current.nextToken();
-			setProperty(key,value,myData);
+//		 System.err.println("Adding at: "+currentx+"/"+currenty+" grid: "+gridwidth);
+		 if (txt!=null) {
+			 StringTokenizer st = new StringTokenizer(txt,";");
+			 while (st.hasMoreTokens()) {
+				String next = st.nextToken();
+				StringTokenizer current = new StringTokenizer(next,":");
+				String key = current.nextToken();
+				String value = current.nextToken();
+				setProperty(key,value,myData, component);
+			}
 		}
-			System.err.println("updateAvailability to constraint: ");
-		updateAvailability(myData);
+//			System.err.println("updateAvailability to constraint: ");
+		 updateComponentSize(component,myData);
+		updateAvailability(myData.gridx,myData.gridy,myData.gridx+myData.gridwidth,myData.gridy+myData.gridheight);
 		advance();
-		 System.err.println("Advanced to: "+currentx+"/"+currenty);
-		 System.err.println("Grid constraints: "+myData.gridx+"/"+myData.gridy+" size: "+myData.gridwidth+"/"+myData.gridheight+" I am: "+getId()+" gridwidth: "+gridwidth);
+//		 System.err.println("Advanced to: "+currentx+"/"+currenty);
+//		 System.err.println("Grid constraints: "+myData.gridx+"/"+myData.gridy+" size: "+myData.gridwidth+"/"+myData.gridheight+" I am: "+getId()+" gridwidth: "+gridwidth);
 		 return myData;
 	 }
 	 
-	 private void updateAvailability(GridBagConstraints myData) {
+	 private void updateComponentSize(JComponent component, GridBagConstraints myData) {
+		 if (component==null) {
+			return;
+		 }
+		 Integer h = ((Integer)heightStrutComponentMap.get(component));
+		 int height = 0;
+		 if (h==null) {
+			 height = component.getPreferredSize().height;
+		 } else {
+			 height = h.intValue();
+		 }
+		 Insets ins = myData.insets;
+		 int vertical = height;
+		 vertical -= ins.top;
+		 vertical -= ins.bottom;
+		 int horizontal = ((Integer)myWidths.get(currentx)).intValue();
+		 horizontal -= ins.left;
+		 horizontal -= ins.right;
+		 
+		 if (component!=null) {
+				component.setMaximumSize(new Dimension(0,height));
+				component.setPreferredSize(new Dimension(horizontal,vertical));
+			}
+		
+	}
+
+	private void updateAvailability(int xstart, int ystart, int xend, int yend) {
 		 // make sure the grid is high enough:
 //		 int maxy = myData.gridy + myData.gridheight;
 		 // double nested loop to flip availability:
-		 for (int y = 1; y <= myData.gridheight; y++) {
- 			 for (int x = 0; x < myData.gridwidth; x++) {
+		 for (int y = ystart; y < yend; y++) {
+ 			 for (int x = xstart; x < xend; x++) {
 				 boolean c = isOccupied(x,y);
 				 if (c) {
 					System.err.println("Oh dear, already occupied!");
@@ -115,35 +156,35 @@ public class TipiGridPanel extends TipiSwingDataComponentImpl {
 //	}
 
 	private void advance() {
-		System.err.println("ADVANCING. currentx: "+currentx+"/"+currenty);
+//		System.err.println("ADVANCING. currentx: "+currentx+"/"+currenty);
 		while (isOccupied(currentx,currenty)) {
 			System.err.println("checking");
 			if (currentx==gridwidth-1) {
-				System.err.println("New row");
+//				System.err.println("New row");
 				currentx = 0;
 				currenty++;
 			} else {
-				System.err.println("next column");
+//				System.err.println("next column");
 				currentx++;
 			}
 		}
 	 }
 
 	 private boolean isOccupied(int x, int y) {
-			System.err.println("Checking: "+x+"/"+y);
-			System.err.println("# of availability stuff: "+availabilityMatrix.size());
+//			System.err.println("Checking: "+x+"/"+y);
+//			System.err.println("# of availability : "+availabilityMatrix);
 			for (Iterator iter = availabilityMatrix.iterator(); iter.hasNext();) {
-			Coordinate element = (Coordinate) iter.next();
-			if (element.equals(new Coordinate(x,y))) {
-				return true;
+				Coordinate element = (Coordinate) iter.next();
+				if (element.equals(new Coordinate(x,y))) {
+					return true;
+				}
 			}
-		}
 			return false;
 			//		 boolean c = availabilityMatrix.contains(new Coordinate(x,y));
 //		 return c;
 	 }
 	 
-	private void setProperty(String key, String value, GridBagConstraints myData) {
+	private void setProperty(String key, String value, GridBagConstraints myData, JComponent current) {
 		if ("align".equals(key)) {
 			myData.anchor = parseAlignment(value,myData);
 		}
@@ -156,8 +197,13 @@ public class TipiGridPanel extends TipiSwingDataComponentImpl {
 		if ("rowspan".equals(key)) {
 			myData.gridheight = Integer.parseInt(value);
 		}
+		if ("height".equals(key)) {
+			int height = Integer.parseInt(value);
+			addHeightStrut(currenty, height, current);
+		}
 		 
 	}
+
 
 	private int parseAlignment(String value,GridBagConstraints myData) {
 		StringTokenizer st = new StringTokenizer(value," ");
@@ -169,7 +215,7 @@ public class TipiGridPanel extends TipiSwingDataComponentImpl {
 		if ("left".equals(horizontalStr) && "top".equals(verticalStr)) {
 			anchor = GridBagConstraints.NORTHWEST;
 		}	
-		if ("left".equals(horizontalStr)&& "center".equals(verticalStr)) {
+		if ("left".equals(horizontalStr)&& "middle".equals(verticalStr)) {
 			anchor = GridBagConstraints.WEST;
 		}
 		if ("left".equals(horizontalStr)&& "bottom".equals(verticalStr)) {
@@ -179,7 +225,7 @@ public class TipiGridPanel extends TipiSwingDataComponentImpl {
 		if ("center".equals(horizontalStr) && "top".equals(verticalStr)) {
 			anchor = GridBagConstraints.NORTH;
 		}	
-		if ("center".equals(horizontalStr)&& "center".equals(verticalStr)) {
+		if ("center".equals(horizontalStr)&& "middle".equals(verticalStr)) {
 			anchor = GridBagConstraints.CENTER;
 		}
 		if ("center".equals(horizontalStr)&& "bottom".equals(verticalStr)) {
@@ -189,7 +235,7 @@ public class TipiGridPanel extends TipiSwingDataComponentImpl {
 		if ("right".equals(horizontalStr) && "top".equals(verticalStr)) {
 			anchor = GridBagConstraints.NORTHEAST;
 		}	
-		if ("right".equals(horizontalStr)&& "center".equals(verticalStr)) {
+		if ("right".equals(horizontalStr)&& "middle".equals(verticalStr)) {
 			anchor = GridBagConstraints.EAST;
 		}
 		if ("right".equals(horizontalStr)&& "bottom".equals(verticalStr)) {
@@ -222,15 +268,32 @@ public class TipiGridPanel extends TipiSwingDataComponentImpl {
 		 }
 
 		 // Effe denken wat ik hier mee moet.
-//		 if ("height".equals(name)) {
-//			 int height = ((Integer)object).intValue();
+		 if ("height".equals(name)) {
+			 int height = ((Integer)object).intValue();
 //			 gridComponent.setHeight(new Extent(height,Extent.PX));
-//		 }
+			 gridComponent.setMinimumSize(new Dimension(0,height));
+		 }
 		super.setComponentValue(name, object);
+	}
+	private void addHeightStrut(int y, int height, JComponent current) {
+		int actualHeight = height;
+		Component existingComponent = (Component)heightStrutComponentMap.get(new Integer(y));
+
+		if (existingComponent!=null) {
+//			GridBagLayout gbl = (GridBagLayout)gridComponent.getLayout();
+//			GridBagConstraints gb = gbl.getConstraints(existingComponent);
+			if (existingComponent.getHeight()>height) {
+				actualHeight = existingComponent.getHeight();
+			}
+			gridComponent.remove(existingComponent);
+		}
+		Component c = Box.createVerticalStrut(actualHeight); 
+		heightStrutComponentMap.put(new Integer(y),c);
+			gridComponent.add(c,new GridBagConstraints(0,y,1,1,0,0,GridBagConstraints.CENTER,GridBagConstraints.NONE,new Insets(0,0,0,0),0,0));
 	}
 
 	private void parseColumns(String sizes) {
-		System.err.println("PARSING COLUMNS::::::::::: "+sizes);
+//		System.err.println("PARSING COLUMNS::::::::::: "+sizes);
 		StringTokenizer st = new StringTokenizer(sizes);
 		myWidths.clear();
 		int count = 0;
@@ -239,13 +302,15 @@ public class TipiGridPanel extends TipiSwingDataComponentImpl {
 			int val = new Integer(element).intValue();
 			myWidths.add(new Integer(val));
 			addColumnStrut(count, val);
+			count++;
 		}
         gridwidth = myWidths.size();
 	}
 
 	private void addColumnStrut(int x, int val) {
-		Component c = Box.createHorizontalStrut(val); 
-		gridComponent.add(c,new GridBagConstraints(x,0,1,1,0,0,GridBagConstraints.CENTER,GridBagConstraints.NONE,new Insets(0,0,0,0),0,0));
+        System.err.println(" Adding columnSTRUUUUUUUUUUT : "+x+" / "+val);
+		Component c = Box.createHorizontalStrut(1); 
+		gridComponent.add(c,new GridBagConstraints(x,0,1,1,0,0,GridBagConstraints.CENTER,GridBagConstraints.NONE,new Insets(0,0,0,0),val-1,0));
 	}
 
 	
@@ -254,7 +319,7 @@ public class TipiGridPanel extends TipiSwingDataComponentImpl {
 		public int y;
 		public Coordinate(int x, int y) {
 			this.x = x;
-			this.x = y;
+			this.y = y;
 		}
 		public boolean equals(Object o) {
 			if (o==null) {
@@ -264,7 +329,11 @@ public class TipiGridPanel extends TipiSwingDataComponentImpl {
 				return false;
 			}
 			Coordinate c = (Coordinate)o;
+//			System.err.println("Comparing: "+this+" and "+c);
 			return c.x == x && c.y==y;
+		}
+		public String toString() {
+			return "{"+x+","+y+"}";
 		}
 	}
 }
