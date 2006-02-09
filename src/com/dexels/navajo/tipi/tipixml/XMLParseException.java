acf2b5e68c85c9933746bd0df71795d6d583408a
Frank Lyaruu <frank@dexels.com>
2006-02-09 14:34:26 +0000

    added advanced text handling functions in XML parser

--- src/com/dexels/navajo/tipi/tipixml/XMLParseException.java
+++ src/com/dexels/navajo/tipi/tipixml/XMLParseException.java
@@ -25,7 +25,10 @@
  *
  *  3. This notice may not be removed or altered from any source distribution.
  *****************************************************************************/
+
+
 package com.dexels.navajo.tipi.tipixml;
+
 
 /**
  * An XMLParseException is thrown when an error occures while parsing an XML
@@ -40,76 +43,108 @@ package com.dexels.navajo.tipi.tipixml;
  * @version $Name$, $Revision$
  */
 public class XMLParseException
-    extends RuntimeException {
-  /**
-   * Indicates that no line number has been associated with this exception.
-   */
-  public static final int NO_LINE = -1;
-  /**
-   * The line number in the source code where the error occurred, or
-   * <code>NO_LINE</code> if the line number is unknown.
-   *
-   * <dl><dt><b>Invariants:</b></dt><dd>
-   * <ul><li><code>lineNr &gt 0 || lineNr == NO_LINE</code>
-   * </ul></dd></dl>
-   */
-  private int lineNr;
-  /**
-   * Creates an exception.
-   *
-   * @param name    The name of the element where the error is located.
-   * @param message A message describing what went wrong.
-   *
-   * </dl><dl><dt><b>Preconditions:</b></dt><dd>
-   * <ul><li><code>message != null</code>
-   * </ul></dd></dl>
-   *
-   * <dl><dt><b>Postconditions:</b></dt><dd>
-   * <ul><li>getLineNr() => NO_LINE
-   * </ul></dd></dl><dl>
-   */
-  public XMLParseException(String name,
-                           String message) {
-    super("XML Parse Exception during parsing of "
-          + ( (name == null) ? "the XML definition"
-             : ("a " + name + " element"))
-          + ": " + message);
-    this.lineNr = XMLParseException.NO_LINE;
-  }
+    extends RuntimeException
+{
 
-  /**
-   * Creates an exception.
-   *
-   * @param name    The name of the element where the error is located.
-   * @param lineNr  The number of the line in the input.
-   * @param message A message describing what went wrong.
-   *
-   * </dl><dl><dt><b>Preconditions:</b></dt><dd>
-   * <ul><li><code>message != null</code>
-   *     <li><code>lineNr &gt; 0</code>
-   * </ul></dd></dl>
-   *
-   * <dl><dt><b>Postconditions:</b></dt><dd>
-   * <ul><li>getLineNr() => lineNr
-   * </ul></dd></dl><dl>
-   */
-  public XMLParseException(String name,
-                           int lineNr,
-                           String message) {
-    super("XML Parse Exception during parsing of "
-          + ( (name == null) ? "the XML definition"
-             : ("a " + name + " element"))
-          + " at line " + lineNr + ": " + message);
-    this.lineNr = lineNr;
-  }
+    /**
+     * Indicates that no line number has been associated with this exception.
+     */
+    public static final int NO_LINE = -1;
 
-  /**
-   * Where the error occurred, or <code>NO_LINE</code> if the line number is
-   * unknown.
-   *
-   * @see nanoxml.XMLParseException#NO_LINE
-   */
-  public int getLineNr() {
-    return this.lineNr;
-  }
+
+    /**
+     * The line number in the source code where the error occurred, or
+     * <code>NO_LINE</code> if the line number is unknown.
+     *
+     * <dl><dt><b>Invariants:</b></dt><dd>
+     * <ul><li><code>lineNr &gt 0 || lineNr == NO_LINE</code>
+     * </ul></dd></dl>
+     */
+    private int lineNr;
+
+    private int offSet;
+
+    private XMLElement source;
+    /**
+     * Creates an exception.
+     *
+     * @param name    The name of the element where the error is located.
+     * @param message A message describing what went wrong.
+     *
+     * </dl><dl><dt><b>Preconditions:</b></dt><dd>
+     * <ul><li><code>message != null</code>
+     * </ul></dd></dl>
+     *
+     * <dl><dt><b>Postconditions:</b></dt><dd>
+     * <ul><li>getLineNr() => NO_LINE
+     * </ul></dd></dl><dl>
+     */
+    public XMLParseException(String name,
+                             String message)
+    {
+        super("XML Parse Exception during parsing of "
+              + ((name == null) ? "the XML definition"
+                                : ("a " + name + " element"))
+              + ": " + message);
+        this.lineNr = XMLParseException.NO_LINE;
+    }
+
+
+    /**
+     * Creates an exception.
+     *
+     * @param name    The name of the element where the error is located.
+     * @param lineNr  The number of the line in the input.
+     * @param message A message describing what went wrong.
+     *
+     * </dl><dl><dt><b>Preconditions:</b></dt><dd>
+     * <ul><li><code>message != null</code>
+     *     <li><code>lineNr &gt; 0</code>
+     * </ul></dd></dl>
+     *
+     * <dl><dt><b>Postconditions:</b></dt><dd>
+     * <ul><li>getLineNr() => lineNr
+     * </ul></dd></dl><dl>
+     */
+    public XMLParseException(String name,
+                             int    lineNr,
+                             String message)
+    {
+        super("XML Parse Exception during parsing of "
+              + ((name == null) ? "the XML definition"
+                                : ("a " + name + " element"))
+              + " at line " + lineNr + ": " + message);
+        this.lineNr = lineNr;
+    }
+
+    public XMLParseException(XMLElement source, String message)
+            {
+            super("XML Parse Exception during parsing of "
+            + ((source.getName() == null) ? "the XML definition"
+                           : ("a " + source.getName() + " element"))
+            + " at line " + source.getLineNr() + ": " + message+" ("+source.getStartOffset()+":"+source.getOffset()+")");
+            this.lineNr = source.getLineNr();
+            this.offSet = source.getOffset();
+            this.source = source;
+            }
+
+    
+    /**
+     * Where the error occurred, or <code>NO_LINE</code> if the line number is
+     * unknown.
+     *
+     * @see nanoxml.XMLParseException#NO_LINE
+     */
+    public int getLineNr()
+    {
+        return this.lineNr;
+    }
+
+    public int getOffset() {
+        return this.offSet;
+    }
+    
+    public XMLElement getSource() {
+        return source;
+    }
 }
