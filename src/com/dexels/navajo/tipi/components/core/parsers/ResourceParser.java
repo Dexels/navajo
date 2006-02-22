cee92642b756d810996b22b959bcd88c6f27dbc8
Frank Lyaruu <frank@dexels.com>
2006-02-22 17:21:13 +0000

    simplified, moved functionality to classloader

--- src/com/dexels/navajo/tipi/components/core/parsers/ResourceParser.java
+++ src/com/dexels/navajo/tipi/components/core/parsers/ResourceParser.java
@@ -16,36 +16,7 @@ import java.io.*;
 public class ResourceParser
     extends BaseTipiParser {
   public Object parse(TipiComponent source, String expression, TipiEvent event) {
-    URL u = myContext.getResourceURL(expression);
-    if (u!=null) {
-      return u;
-    }
-    if (myContext.isStudioMode()) {
-      String project = System.getProperty("tipi.project.dir");
-      if (project==null) {
-        return null;
-      }
-      File projectDir = new File(project);
-      File resourceDir = new File(projectDir,"resource");
-//      System.err.println("Looking for file: "+expression);
-      File resFile = new File(resourceDir,expression);
-      if (resFile.exists()) {
-        URL url = null;
-        try {
-          url = resFile.toURL();
-        }
-        catch (MalformedURLException ex) {
-          ex.printStackTrace();
-          return null;
-        }
-        return url;
-      } else {
-        System.err.println("File not found: "+resFile.toString());
-        return null;
-      }
-    } else {
-      return null;
-    }
+    return myContext.getResourceURL(expression);
   }
   public String toString(Object o, TipiComponent source) {
     return "Not possible";
