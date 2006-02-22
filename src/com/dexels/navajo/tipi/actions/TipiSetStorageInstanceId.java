0a485cc46e7b0010062cf640ceca6d38634df8f8
Frank Lyaruu <frank@dexels.com>
2006-02-22 17:19:17 +0000

    added setStorageId action for tipi

---
+++ src/com/dexels/navajo/tipi/actions/TipiSetStorageInstanceId.java
@@ -0,0 +1,25 @@
+package com.dexels.navajo.tipi.actions;
+
+import com.dexels.navajo.tipi.*;
+import com.dexels.navajo.tipi.internal.*;
+
+/**
+ * <p>Title: </p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2003</p>
+ * <p>Company: </p>
+ * @author not attributable
+ * @version 1.0
+ */
+public class TipiSetStorageInstanceId
+    extends TipiAction {
+  public void execute(TipiEvent event) throws com.dexels.navajo.tipi.TipiException, com.dexels.navajo.tipi.TipiBreakException {
+    String instanceId = (String) evaluate(getParameter("id").getValue(),event).value;
+    if (instanceId != null) {
+      myContext.getStorageManager().setInstanceId(instanceId);
+    }
+    else {
+      throw new TipiException("File is NULL!");
+    }
+  }
+}
