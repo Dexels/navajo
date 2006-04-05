56673068da3a39467e27d313f4be453ee184c2ea
Frank Lyaruu <frank@dexels.com>
2006-04-05 13:26:13 +0000

    general commit

--- src/com/dexels/navajo/tipi/components/swingimpl/swing/TipiSwingScrollPane.java
+++ src/com/dexels/navajo/tipi/components/swingimpl/swing/TipiSwingScrollPane.java
@@ -11,26 +11,38 @@ import java.awt.*;
 import javax.swing.*;
 
 public class TipiSwingScrollPane extends JScrollPane {
-    private Dimension checkMax(Dimension preferredSize) {
-        Dimension maximumSize = getMaximumSize();
+
+    private JComponent scrollingComponent = null;
+    
+    private Dimension checkMax(Dimension preferredSize, Dimension maximumSize) {
+//        Dimension maximumSize = getMaximumSize();
         if (maximumSize==null) {
             return preferredSize;
         }
         return new Dimension(Math.min(preferredSize.width, maximumSize.width),Math.min(preferredSize.height, maximumSize.height));
     }
-    private Dimension checkMin(Dimension preferredSize) {
-        Dimension minimumSize = getMinimumSize();
+    private Dimension checkMin(Dimension preferredSize, Dimension minimumSize) {
+//        Dimension minimumSize = getMinimumSize();
         if (minimumSize==null) {
             return preferredSize;
         }
         return new Dimension(Math.max(preferredSize.width, minimumSize.width),Math.max(preferredSize.height, minimumSize.height));
     }
 
-    public Dimension checkMaxMin(Dimension d) {
-        return checkMin(checkMax(d));
+    public Dimension checkMaxMin(Dimension preferredSize, Dimension maximumSize, Dimension minimumSize) {
+        return checkMin(checkMax(preferredSize,maximumSize),minimumSize);
     }
-    public Dimension getPreferredSize() {
-        return checkMaxMin(super.getPreferredSize());
-    }
+//    public Dimension getPreferredSize() {
+//        return checkMaxMin(getViewport().getPreferredSize(), getViewport().getMaximumSize(), getViewport().getMaximumSize());
+//    }
+//    public void setPreferredSize(Dimension d) {
+//        System.err.println("SETPREF. in scroller called.: "+d);
+//        Thread.dumpStack();
+//        }
 
+//    public void setScrollingComponent(JComponent c) {
+//        scrollingComponent = c;
+//        getViewport().add(c);
+//        
+//    }
 }
