2574f6d7ba205c3ad591dca62397d7ebf940dd53
Frank Lyaruu <frank@dexels.com>
2006-05-23 12:26:37 +0000

    contains the hacked single-streaming QDXML parser

--- src/com/dexels/navajo/document/saximpl/qdxml/DocHandler.java
+++ src/com/dexels/navajo/document/saximpl/qdxml/DocHandler.java
@@ -1,5 +1,6 @@
 package com.dexels.navajo.document.saximpl.qdxml;
 
+import java.io.*;
 import java.util.*;
 
 public interface DocHandler {
@@ -7,5 +8,6 @@ public interface DocHandler {
   public void endElement(String tag) throws Exception;
   public void startDocument() throws Exception;
   public void endDocument() throws Exception;
-  public void text(String str) throws Exception;
+  public void text(Reader r) throws Exception;
+  public String quoteStarted(int quoteCharacter, Reader r, String attributeName,String tagName) throws IOException ;
 }
--- src/com/dexels/navajo/document/saximpl/qdxml/QDParser.java
+++ src/com/dexels/navajo/document/saximpl/qdxml/QDParser.java
@@ -8,6 +8,9 @@ import java.util.*;
  * based parser, but with much less functionality.
  */
 public class QDParser {
+    
+    public static int PUSHBACK_SIZE = 64000;
+    
     private static int popMode(Stack st) {
         if (!st.empty())
             return ((Integer) st.pop()).intValue();
@@ -18,7 +21,8 @@ public class QDParser {
     private final static int TEXT = 1, ENTITY = 2, OPEN_TAG = 3, CLOSE_TAG = 4, START_TAG = 5, ATTRIBUTE_LVALUE = 6, ATTRIBUTE_EQUAL = 9,
             ATTRIBUTE_RVALUE = 10, QUOTE = 7, IN_TAG = 8, SINGLE_TAG = 12, COMMENT = 13, DONE = 11, DOCTYPE = 14, PRE = 15, CDATA = 16;
 
-    public static void parse(DocHandler doc, Reader r) throws Exception {
+    public static void parse(DocHandler doc, Reader re) throws Exception {
+        PushbackReader r = new PushbackReader(re, PUSHBACK_SIZE);
         Stack st = new Stack();
         int depth = 0;
         int mode = PRE;
@@ -65,10 +69,10 @@ public class QDParser {
                 if (c == '<') {
                     st.push(new Integer(mode));
                     mode = START_TAG;
-                    if (sb.length() > 0) {
-                        doc.text(sb.toString());
-                        sb.setLength(0);
-                    }
+                    // if (sb.length() > 0) {
+                    // doc.text(sb.toString());
+                    // sb.setLength(0);
+                    // }
                 } else if (c == '&') {
                     st.push(new Integer(mode));
                     mode = ENTITY;
@@ -94,7 +98,8 @@ public class QDParser {
             } else if (mode == CDATA) {
                 if (c == '>' && sb.toString().endsWith("]]")) {
                     sb.setLength(sb.length() - 2);
-                    doc.text(sb.toString());
+                    System.err.println("Warning: ignoring cdata!");
+                    // doc.text(sb.toString());
                     sb.setLength(0);
                     mode = popMode(st);
                 } else
@@ -112,8 +117,8 @@ public class QDParser {
                 // We are outside the root tag element
             } else if (mode == PRE) {
                 if (c == '<') {
-                    mode = TEXT;
-                    st.push(new Integer(mode));
+                    // mode = TEXT;
+                    // st.push(new Integer(mode));
                     mode = START_TAG;
                 }
 
@@ -204,6 +209,15 @@ public class QDParser {
                     tagName = null;
                     attrs = new Hashtable();
                     mode = popMode(st);
+                    // TODO TEST
+                    skipWhitespace(r);
+                    char ccc = nextChar(r);
+                    if (ccc != '<') {
+                        doc.text(r);
+                        char cc = (char) r.read();
+                        popMode(st);
+                        // / mode = CLOSE_TAG;
+                    }
                 } else if (c == '/') {
                     mode = SINGLE_TAG;
                 } else if (c == '-' && sb.toString().equals("!-")) {
@@ -232,8 +246,10 @@ public class QDParser {
                     mode = IN_TAG;
                     // See section the XML spec, section 3.3.3
                     // on normalization processing.
-                } else if (" \r\n\u0009".indexOf(c) >= 0) {
-                    sb.append(' ');
+
+                    // HMMMMMMMMM DOES THIS BREAK BASE64?
+                    // } else if (" \r\n\u0009".indexOf(c) >= 0) {
+                    // sb.append(' ');
                 } else if (c == '&') {
                     st.push(new Integer(mode));
                     mode = ENTITY;
@@ -245,7 +261,13 @@ public class QDParser {
             } else if (mode == ATTRIBUTE_RVALUE) {
                 if (c == '"' || c == '\'') {
                     quotec = c;
-                    mode = QUOTE;
+                    rvalue = doc.quoteStarted(quotec, r, lvalue, tagName);
+                    if (rvalue != null) {
+                        attrs.put(lvalue, rvalue);
+                    }
+                    mode = IN_TAG;
+
+                    // mode = QUOTE;
                 } else if (Character.isWhitespace((char) c)) {
                     ;
                 } else {
@@ -281,6 +303,16 @@ public class QDParser {
                     depth++;
                     tagName = null;
                     attrs = new Hashtable();
+                    skipWhitespace(r);
+//                    System.err.println("Char: " + (char) nextChar(r));
+                    if (nextChar(r) != '<') {
+                        doc.text(r);
+//                        r.unread('<');
+                    } 
+                    // char cc = (char)r.read();
+                    // /Lookahead(r);
+                    // mode = TEXT;
+
                 } else if (c == '/') {
                     mode = SINGLE_TAG;
                 } else if (Character.isWhitespace((char) c)) {
@@ -295,6 +327,72 @@ public class QDParser {
             doc.endDocument();
         else
             exc("missing end tag", line, col);
+    }
+
+    private static String displayMode(int mode) {
+        // TODO Auto-generated method stub
+        switch (mode) {
+        case 1:
+            return "TEXT";
+        case 2:
+            return "ENTITY";
+        case 3:
+            return "OPEN_TAG";
+        case 4:
+            return "CLOSE_TAG";
+        case 5:
+            return "START_TAG";
+        case 6:
+            return "ATTRIBUTE_LVALUE";
+        case 7:
+            return "QUOTE";
+        case 8:
+            return "IN_TAG";
+        case 9:
+            return "ATTRIBUTE_EQUAL";
+        case 10:
+            return "ATTRIBUTE_RVALUE";
+        case 11:
+            return "DONE";
+        case 12:
+            return "SINGLE_TAG";
+        case 13:
+            return "COMMENT";
+        case 14:
+            return "DOCTYPE";
+        case 15:
+            return "PRE";
+        case 16:
+            return "CDATA";
+
+        default:
+            break;
+        }
+        return null;
+    }
+
+    private static void showLookahead(PushbackReader r) throws IOException {
+        char[] c = new char[10];
+        r.read(c);
+        System.err.print("LOOKAHEAD: >");
+        for (int i = 0; i < c.length; i++) {
+            System.err.print(c[i]);
+        }
+        System.err.println("<\n");
+        r.unread(c);
+    }
+
+    private static char nextChar(PushbackReader r) throws IOException {
+        // TODO Auto-generated method stub
+        int c = r.read();
+        r.unread(c);
+        return (char) c;
+    }
+
+    private static void skipWhitespace(PushbackReader r) throws IOException {
+        while (Character.isWhitespace(nextChar(r))) {
+            r.read();
+        }
     }
 
     private static void exc(String s, int line, int col) throws Exception {
