d2b2fe48ad613fafd7bc557b115362471e6c1b9d
Frank Lyaruu <frank@dexels.com>
2006-06-12 13:33:13 +0000

    Added image scale functions

---
+++ src/com/dexels/navajo/functions/ScaleImageMax.java
@@ -0,0 +1,57 @@
+/*
+ * Created on May 23, 2005
+ *
+ */
+package com.dexels.navajo.functions;
+
+import java.io.*;
+
+import com.dexels.navajo.document.types.*;
+import com.dexels.navajo.functions.scale.*;
+import com.dexels.navajo.parser.FunctionInterface;
+import com.dexels.navajo.parser.TMLExpressionException;
+
+/**
+ * @author arjen
+ *
+ */
+public class ScaleImageMax extends FunctionInterface {
+    private final static double DEFAULT_COMPRESSION = 0.8;
+
+	/* (non-Javadoc)
+	 * @see com.dexels.navajo.parser.FunctionInterface#remarks()
+	 */
+	public String remarks() {
+        return "Scales an image to the specified dimensions. Keeps aspect ratio, and uses the largest value of the dimensions";
+	}
+
+	/* (non-Javadoc)
+	 * @see com.dexels.navajo.parser.FunctionInterface#usage()
+	 */
+	public String usage() {
+		return "ScaleImageMax(Binary,int width,int height)";
+	}
+
+	/* (non-Javadoc)
+	 * @see com.dexels.navajo.parser.FunctionInterface#evaluate()
+	 */
+	public Object evaluate() throws TMLExpressionException {
+	    if (getOperands().size()!=3) {
+            throw new TMLExpressionException(this, "Three operands expected. ");
+        }
+        Object o = getOperand(0);
+        Binary b = (Binary)getOperand(0);
+        Integer width = (Integer)getOperand(1);
+        Integer height = (Integer)getOperand(2);
+
+        try {
+            Binary res =ImageScaler.scaleToMax(b, width.intValue(), height.intValue(), DEFAULT_COMPRESSION);
+            return res;
+        } catch (IOException e) {
+           e.printStackTrace();
+           throw new TMLExpressionException(this, "Error scaling image!");
+        }
+      
+	}
+
+}
