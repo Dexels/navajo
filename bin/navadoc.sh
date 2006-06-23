caf200be826619de95e5815839f22cdcb42a2064
Martin Bergman <mbergman@dexels.com>
2006-06-23 13:25:51 +0000

    Made script (somewhat) more generic to use.

--- bin/navadoc.sh
+++ bin/navadoc.sh
@@ -1,22 +1,40 @@
 #
-# text runner for NavaDoc Test Suite
+# Text runner for NavaDoc Test Suite
 #
 # $Id$
 #
 
-# enter your project home here
-export PROJECTHOME="${HOME}/projects/NavaDoc"
+#
+# If PROJECTHOME is set use it, otherwise try to guess it
+# and set NAVADOCDIR based on the PROJECTHOME
+#
+NAVADOCDIR=
+
+if [ -z $PROJECTHOME ] ; then
+    [ -d $HOME/Projects ]  && PROJECTHOME=$HOME/Projects
+    [ -d $HOME/Projecten ] && PROJECTHOME=$HOME/Projecten
+    [ -d $HOME/Project ]   && PROJECTHOME=$HOME/Project
+    [ -d $HOME/projects ]  && PROJECTHOME=$HOME/projects
+    [ -d $HOME/projecten ] && PROJECTHOME=$HOME/projecten
+    [ -d $HOME/project ]   && PROJECTHOME=$HOME/project
+    [ -d $HOME/workspace ] && PROJECTHOME=$HOME/workspace
+fi
+
+NAVADOCDIR=$PROJECTHOME/NavaDoc
+
+if [ ! -d $NAVADOCDIR ] ; then
+    echo "Could not determine NavaDoc directory; set you PROJECTHOME variable"
+    exit 1
+fi
 
 # Configuration location as a URI (Xerces is happier getting documents by URI)
-CONFIG="-DconfigUri=file://${PROJECTHOME}/config/navadoc.xml -Dbase=/home/arjen/projecten"
+CONFIG="-DconfigUri=file://${NAVADOCDIR}/config/navadoc.xml -Dbase=${PROJECTHOME}"
 
 # set this if you want debugging output from the logging facility
 # LOGDEBUG="-Dlog4j.debug=yes"
 
-cd ${PROJECTHOME} && \
+cd ${NAVADOCDIR} && \
     java -cp NavaDoc.jar ${CONFIG} ${LOGDEBUG} \
         com.dexels.navajo.util.navadoc.NavaDoc
 
 ### EOF: $RCSfile$ ###
-
-
