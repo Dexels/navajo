15311531a3829b2050e228726d0cd3d6d44e6e35
Frank Lyaruu <frank@dexels.com>
2006-08-08 12:35:54 +0000

    added log

--- src/com/dexels/navajo/tipi/internal/TipiAction.java
+++ src/com/dexels/navajo/tipi/internal/TipiAction.java
@@ -35,7 +35,8 @@ public abstract class TipiAction implements TipiExecutable {
     }
 
     public void performAction(TipiEvent te) throws TipiBreakException, TipiException {
-        myEvent = te;
+    	myContext.debugLog("action", myType);
+    	myEvent = te;
         if (myComponent.isDisposed()) {
             System.err.println("\n**** BREAKING. COMPONENT DISPOSED: " + myComponent.getPath());
 //            Thread.dumpStack();
