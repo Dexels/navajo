ebba3e1bd0da4c75598fc71bca1964aca6fdee94
Arjen Schoneveld <aschoneveld@dexels.com>
2006-10-16 14:38:34 +0000

    *** empty log message ***

--- src/navajo/Version.java
+++ src/navajo/Version.java
@@ -87,16 +87,18 @@ package navajo;
  * 5.5.2 Support for mappable adapter statistics
  * 
  * 5.5.3 Support for orderby construct in array messages.
+ * 
+ * 5.6.0 Restructured Navajo Classloader.
  */
 
 public class Version extends dexels.Version {
 
 	public static final int MAJOR = 5;
-	public static final int MINOR = 5;
-	public static final int PATCHLEVEL = 3;
+	public static final int MINOR = 6;
+	public static final int PATCHLEVEL = 0;
 	public static final String VENDOR = "Dexels";
 	public static final String PRODUCTNAME = "Navajo Kernel";
-	public static final String RELEASEDATE = "2006-10-05";
+	public static final String RELEASEDATE = "2006-10-16";
 	
 //	 Included packages.
 	String [] includes = {"navajodocument.Version"};
