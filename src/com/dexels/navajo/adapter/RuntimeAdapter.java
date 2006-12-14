13378554432635ca0bc6ded2f4df795e0167c3c1
Arjen Schoneveld <aschoneveld@dexels.com>
2006-12-14 10:03:30 +0000

    Now using java 1.5 ProcessBuilder class for running external programs.

--- src/com/dexels/navajo/adapter/RuntimeAdapter.java
+++ src/com/dexels/navajo/adapter/RuntimeAdapter.java
@@ -8,6 +8,8 @@ import com.dexels.navajo.server.NavajoConfig;
 import com.dexels.navajo.server.UserException;
 import com.dexels.navajo.mapping.MappableException;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 /**
  * <p>Title: </p>
@@ -25,11 +27,14 @@ public class RuntimeAdapter implements Mappable {
   public String parameter = "";
   public String output = "";
   public String error = "";
-
+  public String dir = "";
+  
   private String parameterList = null;
 
   private boolean outputFinished = false;
   private boolean errorFinished = false;
+  
+  private static Object semaphore = new Object();
 
   public void load(Parameters parms, Navajo inMessage, Access access, NavajoConfig config) throws MappableException, UserException {
 
@@ -44,84 +49,98 @@ public class RuntimeAdapter implements Mappable {
        parameterList = s;
      else
        parameterList += " " + s;
-    //System.err.println("in setParameter(" +  s + ")");
-    //System.err.println("parameterList = " + parameterList);
   }
 
+  public void setDir(String s) {
+	  this.dir = s;
+  }
+  
   public void setRun(boolean b) throws UserException {
-    try {
-      String command = script;
-      if (parameterList != null)
-        command += " " + parameterList;
 
-      //System.err.println("in RuntimeAdapter: about to execute: " + command);
+	  // Note, this must be synchronized since Process is not thread safe.
+	  synchronized (semaphore) {
 
-      Runtime rt = Runtime.getRuntime();
-      final Process p = rt.exec(command);
-      final StringWriter outputWriter = new StringWriter();
+		  try {
+			  String command = script;
+			  if (parameterList != null)
+				  command += " " + parameterList;
 
-      new Thread(new Runnable() {
-        public void run() {
-          try {
-            BufferedReader br_in = new BufferedReader(new InputStreamReader(p.
-                getInputStream()));
-            String buff = null;
-            while ( (buff = br_in.readLine()) != null) {
-              outputWriter.write(buff + "\n");
-              try {
-                Thread.sleep(100);
-              }
-              catch (Exception e) {}
-            }
-            br_in.close();
-          }
-          catch (IOException ioe) {
-            System.out.println("Exception caught printing javac result");
-            ioe.printStackTrace();
-          }
-          outputFinished = true;
-        }
-      }).start();
+			  //System.err.println("in RuntimeAdapter: about to execute: " + command);
 
-      final StringWriter errorWriter = new StringWriter();
+			  //Runtime rt = Runtime.getRuntime();
+			  StringTokenizer tk = new StringTokenizer(command, ";");
 
-           new Thread(new Runnable() {
-             public void run() {
-               try {
-                 BufferedReader br_in = new BufferedReader(new InputStreamReader(p.
-                     getErrorStream()));
-                 String buff = null;
-                 while ( (buff = br_in.readLine()) != null) {
-                   errorWriter.write(buff + "\n");
-                   try {
-                     Thread.sleep(100);
-                   }
-                   catch (Exception e) {}
-                 }
-                 br_in.close();
-               }
-               catch (IOException ioe) {
-                 System.out.println("Exception caught printing javac result");
-                 ioe.printStackTrace();
-               }
-               errorFinished = true;
-             }
-           }).start();
+			  while (tk.hasMoreTokens()) {
 
-      while (!(errorFinished && outputFinished)) {
-        Thread.sleep(1000);
-        //System.err.println("WAITING FOR SCRIPT TO FINISH....");
-      }
+				  ArrayList cmds = new ArrayList();
+				  String cmdString = tk.nextToken();
+				  System.err.println("cmdString = " + cmdString);
+				  StringTokenizer tk2 = new StringTokenizer(cmdString, " ");
+				  while (tk2.hasMoreTokens() ) {
+					  cmds.add(tk2.nextToken());
+				  }
 
-      output = outputWriter.toString();
-      error = errorWriter.toString();
-      //println("output = " + output);
-      //System.err.println("error = " + error);
+				  ProcessBuilder pb = new ProcessBuilder(cmds);
 
-      //System.err.println("in RuntimeAdapter: finished script.");
-    } catch (Exception e) {
-      throw new UserException(-1, e.getMessage());
-    }
+				  if ( dir != null && !dir.equals("") ) {
+					  pb.directory(new File(dir));
+				  }
+
+				  Process p = pb.start();
+
+				  StringWriter outputWriter = new StringWriter();
+
+				  try {
+					  BufferedReader br_in = new BufferedReader(new InputStreamReader(p.
+							  getInputStream()));
+					  String buff = null;
+					  while ( (buff = br_in.readLine()) != null) {
+						  outputWriter.write(buff + "\n");
+					  }
+					  br_in.close();
+				  }
+				  catch (IOException ioe) {
+					  System.out.println("Exception caught printing javac result");
+					  ioe.printStackTrace();
+				  }
+				  outputFinished = true;
+
+
+				  StringWriter errorWriter = new StringWriter();
+
+				  try {
+					  BufferedReader br_in = new BufferedReader(new InputStreamReader(p.
+							  getErrorStream()));
+					  String buff = null;
+					  while ( (buff = br_in.readLine()) != null) {
+						  errorWriter.write(buff + "\n");
+					  }
+					  br_in.close();
+				  }
+				  catch (IOException ioe) {
+					  System.out.println("Exception caught printing javac result");
+					  ioe.printStackTrace();
+				  }
+				  errorFinished = true;
+
+				  while (!(errorFinished && outputFinished)) {
+					  Thread.sleep(1000);
+					  //System.err.println("WAITING FOR SCRIPT TO FINISH....");
+				  }
+
+				  output += outputWriter.toString();
+				  error += errorWriter.toString();
+				  //println("output = " + output);
+				  //System.err.println("error = " + error);
+			  }
+
+			  //System.err.println("in RuntimeAdapter: finished script.");
+		  } catch (Exception e) {
+			  e.printStackTrace(System.err);
+			  throw new UserException(-1, e.getMessage());
+		  }
+
+	  }
   }
 
   public String getOutput() {
@@ -142,10 +161,13 @@ public class RuntimeAdapter implements Mappable {
 
   public static void main (String [] args) throws Exception {
     RuntimeAdapter ra = new RuntimeAdapter();
-    ra.setScript("/home/arjen/projecten/sportlink-serv/navajo-tester/ssl/generate_client_keystore.sh");
-    ra.setParameter("/home/arjen/projecten/sportlink-serv/navajo-tester/ssl");
-    ra.setParameter("ONGELOOFELIJK");
+    
+    ra.setScript("cat aap;ls -la");
+    ra.setDir("/home/arjen/projecten/MigratieGlidepath");
+//    ra.setParameter("/home/arjen/projecten/sportlink-serv/navajo-tester/ssl");
+//    ra.setParameter("ONGELOOFELIJK");
     ra.setRun(true);
+    
     System.err.println("FINITO: " + ra.getOutput());
     System.err.println("ERROR STRING:>" + ra.getError() + "<");
   }
