8ae29fe15d9c2120264fb497caddba1730afebd8
Frank Lyaruu <frank@dexels.com>
2006-12-20 10:40:23 +0000

    added sublocale support + https switching

---
+++ src/com/dexels/navajo/tipi/actions/TipiSetPassword.java
@@ -0,0 +1,23 @@
+package com.dexels.navajo.tipi.actions;
+
+import com.dexels.navajo.client.NavajoClientFactory;
+import com.dexels.navajo.document.Operand;
+import com.dexels.navajo.tipi.*;
+import com.dexels.navajo.tipi.internal.*;
+
+/**
+ * <p>Title: </p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2003</p>
+ * <p>Company: </p>
+ * @author not attributable
+ * @version 1.0
+ */
+public class TipiSetPassword
+    extends TipiAction {
+  public void execute(TipiEvent event) throws com.dexels.navajo.tipi.TipiException, com.dexels.navajo.tipi.TipiBreakException {
+	  // TODO Add support for multi-servers
+	    final Operand pass = getEvaluatedParameter("password", event);
+	    NavajoClientFactory.getClient().setPassword(""+pass.value);
+  }
+}
