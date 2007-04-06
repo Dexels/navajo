51632091fce4db37abe14e95d06ad50706a07019
Frank Lyaruu <frank@dexels.com>
2007-04-06 13:31:06 +0000

    Bla

--- src/com/dexels/navajo/server/descriptionprovider/DescriptionProvider.java
+++ src/com/dexels/navajo/server/descriptionprovider/DescriptionProvider.java
@@ -6,7 +6,7 @@ import com.dexels.navajo.document.NavajoException;
 
 public interface DescriptionProvider {
 	public void updatePropertyDescriptions(Navajo in, Navajo out) throws NavajoException;
-	public Message dumpCacheMessage(Navajo n) throws NavajoException;
+//	public Message dumpCacheMessage(Navajo n) throws NavajoException;
 	public int getCacheSize();
 	public void flushCache();
 	public void flushUserCache(String user);
