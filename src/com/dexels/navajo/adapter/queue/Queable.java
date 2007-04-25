b67d097b216fcfe3c8e6ed901feecf8a667b46ed
Arjen Schoneveld <aschoneveld@dexels.com>
2007-04-25 09:34:43 +0000

    Added comment.

--- src/com/dexels/navajo/adapter/queue/Queable.java
+++ src/com/dexels/navajo/adapter/queue/Queable.java
@@ -8,6 +8,24 @@ import com.dexels.navajo.server.Access;
 
 public interface Queable extends Serializable, QueableMXBean {
 	
+	/**
+	 * Public setters to be used from web service script.
+	 */
+    // Public method to be used from web service script.
+	public void setQueuedSend(boolean b);
+    // Set maximum number of retries before giving up.
+	public void setMaxRetries(int r);
+    // Set waiting time interval to use between retries.
+	public void setWaitUntil(long w);
+	
+	/**
+	 * Public getters/setters to be used by requestresponse queue for lifecycle management and administration.
+	 */
+    // Get maximum number of retries before giving up.
+	public int getMaxRetries();
+    // Get waiting time interval to use between retries.
+	public long getWaitUntil();
+	
 	// send() should implement the work that should be done upon processing of the queable object.
 	public boolean send();
 	// getResponse() will retrieve the response of the queable object when finished.
@@ -15,19 +33,12 @@ public interface Queable extends Serializable, QueableMXBean {
 	// getRequest() will return the request payload for to be used in queable object send() method.
 	public Binary getRequest();
 	
+	// Get access object associated with the request.
 	public Access getAccess();
+	// Get tha navajo object associated with the request.
 	public Navajo getNavajo();
 	
-	// Public method to be used from web service script.
-	public void setQueuedSend(boolean b);
-	
-	// Set waiting time interval to use between retries.
-	public void setWaitUntil(long w);
-	public long getWaitUntil();
-	
-	// Set maximum number of retries before giving up.
-	public void setMaxRetries(int r);
-	public int getMaxRetries();
+	// Reset retry counter.
 	public void resetRetries();
 	// Get number of processed retries thus far.
 	public int getRetries();
