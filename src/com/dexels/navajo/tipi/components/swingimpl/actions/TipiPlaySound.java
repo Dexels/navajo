5c21881c8680e6d14b38b4e6a550f0b010ccbd2f
Frank Lyaruu <frank@dexels.com>
2007-06-27 08:33:12 +0000

    added playsound action. Applets only, for now.

---
+++ src/com/dexels/navajo/tipi/components/swingimpl/actions/TipiPlaySound.java
@@ -0,0 +1,46 @@
+/*
+ * Created on Jun 29, 2005
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package com.dexels.navajo.tipi.components.swingimpl.actions;
+
+import java.awt.*;
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.swing.*;
+
+import com.dexels.navajo.document.*;
+import com.dexels.navajo.tipi.*;
+import com.dexels.navajo.tipi.components.swingimpl.SwingTipiContext;
+import com.dexels.navajo.tipi.components.swingimpl.TipiApplet;
+import com.dexels.navajo.tipi.components.swingimpl.swing.*;
+import com.dexels.navajo.tipi.internal.*;
+
+/**
+ * @author Administrator
+ * 
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+public class TipiPlaySound extends TipiAction {
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.dexels.navajo.tipi.internal.TipiAction#execute(com.dexels.navajo.tipi.internal.TipiEvent)
+     */
+    protected void execute(TipiEvent event) throws TipiBreakException, TipiException {
+        Operand url  = getEvaluatedParameter("url", event);
+        URL urlVal = (URL)url.value;
+        TipiApplet rr = ((SwingTipiContext)myContext).getAppletRoot();
+        if(rr!=null) {
+        	rr.play(urlVal);
+			
+        }
+    }
+
+}
