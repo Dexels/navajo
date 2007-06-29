474eb8b9bed029e25550db0fc40dcf0c383acbba
Arjen Schoneveld <aschoneveld@dexels.com>
2007-06-29 13:13:35 +0000

    Reverted to before StateStep.

--- src/com/dexels/navajo/workflow/WorkFlowTask.java
+++ src/com/dexels/navajo/workflow/WorkFlowTask.java
@@ -7,18 +7,50 @@ import com.dexels.navajo.scheduler.Task;
 import com.dexels.navajo.scheduler.TaskListener;
 import com.dexels.navajo.scheduler.TaskRunner;
 
-public class WorkFlowTask extends StateStep implements Serializable {
+public class WorkFlowTask implements Serializable, TaskListener {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -681149898590019046L;
-	
+
+	private Task myTask = null;
+	private State myState = null;
 	private boolean finished = false;
 	
 	public WorkFlowTask(State s, Task t) {
 		myTask = t;
 		myState = s;
+	}
+	
+	/**
+	 * Activates a workflowtask, e.g. registers associated task with the taskrunner manager and registers the transition
+	 * as a task listener.
+	 *
+	 */
+	public void activate() {
+		TaskRunner.getInstance().addTaskListener(this);
+		if ( myTask.getId() == null ) { // It's a first task.
+			TaskRunner.getInstance().addTask(myTask);
+		} else { // It's a revived task.
+			TaskRunner.getInstance().addTask(myTask.getId(), myTask, true);
+		}
+	}
+
+	public void cleanup() {
+		TaskRunner.getInstance().removeTaskListener(this);
+		TaskRunner.getInstance().removeTask(myTask.getId());
+	}
+	
+	private final boolean isMyTaskTrigger(Task t) {
+		return ( t.getId().equals(myTask.getId()) &&
+				 ( myState == null ||
+				   myState.getWorkFlow() == null ||
+				     ( myState.getWorkFlow().getDefinition().equals(t.getWorkflowDefinition()) &&
+				       myState.getWorkFlow().getMyId().equals(t.getWorkflowId())
+				     )
+				 )
+		 );
 	}
 	
 	public void afterTask(Task t, Navajo request) {
