6cde1d6c0b714764409cab7a1c7e79606211719f
Arjen Schoneveld <aschoneveld@dexels.com>
2007-09-04 12:55:42 +0000

    Added support for alternative Navajo sources

--- src/com/dexels/navajo/workflow/ConditionalExpression.java
+++ src/com/dexels/navajo/workflow/ConditionalExpression.java
@@ -1,6 +1,7 @@
 package com.dexels.navajo.workflow;
 
 import java.io.Serializable;
+import java.util.StringTokenizer;
 
 public final class ConditionalExpression implements Serializable {
 
@@ -11,14 +12,16 @@ public final class ConditionalExpression implements Serializable {
 	
 	private final String condition;
 	private final String expression; // value
+	private final String source; // value
 	
-	public ConditionalExpression(String condition, String expression) {
+	public ConditionalExpression(String condition, String expression, String source) {
 		if ( condition != null ) {
 			this.condition = condition;
 		} else {
 			this.condition = "true";
 		}
 		this.expression = expression;
+		this.source = source;
 	}
 	
 	public final String getCondition() {
@@ -29,5 +32,32 @@ public final class ConditionalExpression implements Serializable {
 		return expression;
 	}
 	
+	public boolean hasDefinedSourceNavajo() {
+		return ( source != null);
+	}
 	
+	public String getSourceState() {
+		if ( source == null ) {
+			return null;
+		}
+		StringTokenizer sts = new StringTokenizer(source, ":");
+		if ( sts.hasMoreTokens() ) {
+			return sts.nextToken();
+		}
+		return null;
+	}
+	
+	public String getSourceDirection() {
+		if ( source == null ) {
+			return null;
+		}
+		StringTokenizer sts = new StringTokenizer(source, ":");
+		if ( sts.hasMoreTokens() ) {
+			sts.nextToken();
+			if ( sts.hasMoreTokens() ) {
+				return sts.nextToken();
+			}
+		}
+		return "request";
+	}
 }
