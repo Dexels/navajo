384fc299671a969178b141e644d99ca6b9a47905
Frank Lyaruu <frank@dexels.com>
2007-09-04 14:20:00 +0000

    implemented fallback to remote: When looking for validation.properties, will also check in remote path

--- src/com/dexels/navajo/tipi/internal/BaseTipiErrorHandler.java
+++ src/com/dexels/navajo/tipi/internal/BaseTipiErrorHandler.java
@@ -1,5 +1,6 @@
 package com.dexels.navajo.tipi.internal;
 
+import java.io.*;
 import java.util.*;
 
 import com.dexels.navajo.document.*;
@@ -29,10 +30,15 @@ public abstract class BaseTipiErrorHandler implements TipiErrorHandler {
 	private ResourceBundle res;
 
 	public BaseTipiErrorHandler() {
+		initResource();
+	}
+
+	private void initResource() {
 		try {
 			res = ResourceBundle.getBundle("tipi.validation");
+			
 		} catch (Exception ex) {
-			System.err.println("No resource found.");
+			System.err.println("No tipi.validation resource found.");
 			res = null;
 		}
 	}
@@ -72,8 +78,10 @@ public abstract class BaseTipiErrorHandler implements TipiErrorHandler {
 	private String getConditionErrorDescription(String id) {
 		String description = id;
 		try {
-			String found = res.getString(id);
-			description = found;
+			if(res!=null) {
+				String found = res.getString(id);
+				description = found;
+			}
 		} catch (MissingResourceException ex) {
 			System.err.println("----> Cannot find reference for condition errorId: " + id);
 		}
@@ -82,6 +90,19 @@ public abstract class BaseTipiErrorHandler implements TipiErrorHandler {
 
 	public void setContext(TipiContext c) {
 		context = c;
+		if(res==null) {
+			// attempt remote propertyresource bundle;
+			try {
+				InputStream tipiResourceStream = c.getTipiResourceStream("validation.properties");
+				if(tipiResourceStream!=null) {
+					res = new PropertyResourceBundle(tipiResourceStream);
+				} else {
+					System.err.println("Remote retrieve of validation.properties failed.");
+				}
+			} catch (IOException e) {
+				System.err.println("Getting resource from server failed.");
+			}
+		}
 	}
 
 	public TipiContext getContext() {
