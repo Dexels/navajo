6a1729d6b80da35a4b370c3d59d0dc06836d0ca9
Frank Lyaruu <frank@dexels.com>
2007-09-04 14:31:52 +0000

    binary fix

--- src/com/dexels/navajo/echoclient/components/BinaryPropertyImage.java
+++ src/com/dexels/navajo/echoclient/components/BinaryPropertyImage.java
@@ -9,11 +9,11 @@ import java.util.Random;
 import com.dexels.navajo.document.NavajoException;
 import com.dexels.navajo.document.Property;
 import com.dexels.navajo.document.types.Binary;
+import com.dexels.navajo.functions.scale.*;
 
 import echopointng.image.URLImageReference;
 
-import nextapp.echo2.app.ResourceImageReference;
-import nextapp.echo2.app.StreamImageReference;
+import nextapp.echo2.app.*;
 
 public class BinaryPropertyImage extends StreamImageReference {
 
@@ -22,14 +22,34 @@ public class BinaryPropertyImage extends StreamImageReference {
     private final Property myProperty;
     private final Binary myBinary;
     private static final int BUFFER_SIZE = 1000;
-
-    public BinaryPropertyImage(Property p) {
+    private int height = 0;
+    private int width = 0;
+    
+    public BinaryPropertyImage(Property p, int width, int height) {
     	 Binary b = (Binary) p.getTypedValue();
        	myBinary = b;
          myProperty = p;
+         this.height = height;
+         this.width = width;
     }
 
-      public String getContentType() {
+    
+    
+      @Override
+	public Extent getHeight() {
+    	  return new Extent(height, Extent.PX);
+      }
+
+
+
+	@Override
+	public Extent getWidth() {
+  	  return new Extent(width, Extent.PX);
+	}
+
+
+
+	public String getContentType() {
         // TODO Auto-generated method stub
     	if(myBinary==null) {
     		
@@ -37,6 +57,7 @@ public class BinaryPropertyImage extends StreamImageReference {
     	}
     	String contentType = myBinary.guessContentType();
     	System.err.println("Binary image: Content Type: "+contentType);
+    	System.err.println("Length: "+myBinary.getLength());
     	if(contentType!=null) {
     		return contentType;
     	}
@@ -49,8 +70,20 @@ public class BinaryPropertyImage extends StreamImageReference {
             System.err.println("Oh dear, bad binary");
             in =  getClass().getClassLoader().getResource("com/dexels/navajo/tipi/components/echoimpl/resource/image/bw_questionmark.gif").openStream();
         } else {
-        	System.err.println("Good binary: rendering. Size: "+myBinary.getLength());
-            in = myBinary.getDataAsStream();
+//            Binary b = new Binary(myBinary.getDataAsStream(),false);
+            
+            Binary b = ImageScaler.scaleToMax(myBinary, width, height, 0.8);
+            if(b!=null) {
+            	System.err.println("Good binary: rendering. Size: "+myBinary.getLength()+" width: "+width+" height: "+height);
+                in = b.getDataAsStream();
+                if(in==null) {
+                	return;
+                }
+            	
+            } else {
+            	System.err.println("Trouble, not rendering");
+            	return;
+            }
         }
         byte[] buffer = new byte[BUFFER_SIZE];
         int bytesRead = 0;
