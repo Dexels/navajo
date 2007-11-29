6ca1d3f697ccdb422e365189802a83ae6ff2b013
Frank Lyaruu <frank@dexels.com>
2007-11-29 11:12:46 +0000

    Added support for layout + gap

--- src/com/dexels/navajo/tipi/components/swingimpl/layout/TipiFlowLayout.java
+++ src/com/dexels/navajo/tipi/components/swingimpl/layout/TipiFlowLayout.java
@@ -3,6 +3,7 @@ package com.dexels.navajo.tipi.components.swingimpl.layout;
 import java.awt.*;
 import com.dexels.navajo.tipi.*;
 import com.dexels.navajo.tipi.components.core.*;
+import com.dexels.navajo.tipi.tipixml.*;
 
 /**
  * <p>Title: </p>
@@ -20,14 +21,32 @@ public class TipiFlowLayout
 
   public void createLayout() {
     FlowLayout layout = new FlowLayout();
+    String align = myDefinition.getStringAttribute("alignment");
+    if(align.equals("left")) {
+        layout.setAlignment(FlowLayout.LEFT);
+    }
+    if(align.equals("right")) {
+        layout.setAlignment(FlowLayout.RIGHT);
+    }
+    if(align.equals("center")) {
+        layout.setAlignment(FlowLayout.CENTER);
+    }
+
+    String gap = myDefinition.getStringAttribute("gap");
+    if(gap!=null) {
+    	int g = Integer.parseInt(gap);
+        layout.setHgap(g);
+    }
     setLayout(layout);
   }
 
   public Object parseConstraint(String text, int index) {
     return null;
   }
+  
+  
 
-  protected void setValue(String name, TipiValue tv) {
+protected void setValue(String name, TipiValue tv) {
     throw new UnsupportedOperationException("Not implemented yet. But I should.");
   }
 }
