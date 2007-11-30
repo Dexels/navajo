ee1925eb3c0bc4e1569ec1b2106ae5395c494f2f
Arjen Schoneveld <aschoneveld@dexels.com>
2007-11-30 10:27:13 +0000

    Now proper condition semantics for "methods"

--- src/com/dexels/navajo/mapping/compiler/meta/MethodDefinition.java
+++ src/com/dexels/navajo/mapping/compiler/meta/MethodDefinition.java
@@ -4,6 +4,7 @@ import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Random;
 import java.util.TreeMap;
 import java.util.Vector;
 
@@ -16,6 +17,7 @@ public class MethodDefinition {
 	
 	private HashMap<String, ParameterDefinition> parameters = new HashMap<String, ParameterDefinition>();
 	private TreeMap<Integer, XMLElement> orderedParameters = new TreeMap<Integer, XMLElement>();
+	private final static Random rand = new Random(System.currentTimeMillis());
 	
 	public MethodDefinition(String name, HashMap<String, ParameterDefinition> parameters) {
 		this.name = name;
@@ -38,6 +40,10 @@ public class MethodDefinition {
 		return md;
 	}
 	
+	private String generateParamName() {
+		return "Dummy" + Math.abs(MethodDefinition.rand.nextInt());
+	}
+	
 	public void generateCode(XMLElement in, XMLElement out, String filename) throws Exception {
 		
 		if ( in.getChildren().size() > 0 ) {
@@ -45,14 +51,20 @@ public class MethodDefinition {
 		}
 		
 		String condition = (String) in.getAttribute("condition");
+		boolean hasCondition = false;
+		String tempParamName = null;
 		if ( condition != null && !condition.equals("condition")) {
+			// Generate a temp. param to evaluate the condition expression.
 			XMLElement c = new CaseSensitiveXMLElement();
-			c.setName("message");
-			c.setAttribute("name", "__dummy__");
-			c.setAttribute("condition", condition);
-			c.setAttribute("mode", "ignore");
+			c.setName("param");
+			tempParamName = generateParamName();
+			c.setAttribute("name", tempParamName);
+			XMLElement exp = new CaseSensitiveXMLElement();
+			exp.setName("expression");
+			exp.setAttribute("value", condition);
+			c.addChild(exp);
 			out.addChild(c);
-			out = c;
+			hasCondition = true;
 		}
 		Enumeration attributes = in.enumerateAttributeNames();
 		HashSet<String> required = new HashSet<String>();
@@ -61,7 +73,7 @@ public class MethodDefinition {
 		while ( auto.hasNext() )  {
 			ParameterDefinition pd = auto.next();
 			if ( pd.getRequired().equals("automatic") ) {
-				XMLElement pdx = pd.generateCode(pd.getValue(), null, out, false, filename);
+				XMLElement pdx = pd.generateCode(pd.getValue(), ( hasCondition ? "[/@" + tempParamName + "]" : null ), out, false, filename);
 				orderedParameters.put(new Integer(pd.getOrder()), pdx);
 			} else if ( pd.getRequired().equals("true") ) {
 				required.add(pd.getName());
@@ -76,7 +88,7 @@ public class MethodDefinition {
 				throw new UnknownParameterException(getName(), attribName, in.getLineNr(), filename );
 			}
 			if ( pd != null && !pd.getRequired().equals("automatic")) {
-				XMLElement pdx = pd.generateCode(attribValue, null, out, false, filename);
+				XMLElement pdx = pd.generateCode(attribValue, ( hasCondition ? "[/@" + tempParamName + "]" : null ), out, false, filename);
 				orderedParameters.put(new Integer(pd.getOrder()), pdx);
 				required.remove(pd.getName());
 			}
