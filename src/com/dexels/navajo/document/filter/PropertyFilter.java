59c9fe61caefcef1fddd1d4911066d5cacdd4b33
Frank Lyaruu <frank@dexels.com>
2007-12-31 11:18:07 +0000

    Moved from SwingClient, as it has not ui dependency whatsoever, update SwingClient too.

---
+++ src/com/dexels/navajo/document/filter/PropertyFilter.java
@@ -0,0 +1,131 @@
+package com.dexels.navajo.document.filter;
+
+/**
+ * <p>Title: </p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2003</p>
+ * <p>Company: </p>
+ * @author not attributable
+ * @version 1.0
+ */
+
+import com.dexels.navajo.document.*;
+
+import java.util.*;
+
+public final class PropertyFilter {
+  private String myPropertyName;
+  private Property myValue;
+  private String myOperator;
+
+  public PropertyFilter(String propName, Property value, String operator) {
+    myPropertyName = propName;
+    myValue = value;
+    myOperator = operator;
+  }
+
+  public PropertyFilter(String propName, String value, String type, String operator) throws NavajoException {
+	    myPropertyName = propName;
+	    // TODO: Add support for 
+	    myValue = NavajoFactory.getInstance().createProperty(null, "unknown", type, value, 0, "", Property.DIR_IN);
+	    myOperator = operator;
+	  }
+
+  
+  public final boolean compliesWith(Message m) throws NavajoException {
+
+    Property p = m.getProperty(myPropertyName);
+
+    if (p == null) {
+      System.err.println("Property can not be found");
+      return true;
+    }
+    if ("==".equals(myOperator)) {
+      return p.compareTo(myValue)==0;
+    }
+    if ("!=".equals(myOperator)) {
+      return p.compareTo(myValue)!=0;
+    }
+    if (">".equals(myOperator)) {
+      return p.compareTo(myValue)>0;
+    }
+    if ("<".equals(myOperator)) {
+      return p.compareTo(myValue)<0;
+    }
+    if (">=".equals(myOperator)) {
+      return p.compareTo(myValue)>=0;
+    }
+    if ("<=".equals(myOperator)) {
+      return p.compareTo(myValue)<=0;
+    }
+    if ("startsWith".equals(myOperator) && "string".equals(p.getType())) {
+    	if(p.getValue() != null){
+        return p.getValue().toLowerCase().startsWith(myValue.getValue().toLowerCase());
+    	}else{
+    		return false;
+    	}
+    }
+    if ("startsWith".equals(myOperator)) {
+      if(p.getType().equals(Property.SELECTION_PROPERTY)){
+        return p.getSelected().getName().startsWith(myValue.getSelected().getName().toLowerCase());
+      }else{
+        return p.getValue().startsWith(myValue.getValue());
+      }
+    }
+    if ("endsWith".equals(myOperator) && "string".equals(p.getType())) {
+    	if(p.getValue() != null){
+        return p.getValue().toLowerCase().endsWith(myValue.getValue().toLowerCase());
+    	}else{
+    		return false;
+    	}
+    }
+    if ("endsWith".equals(myOperator)) {
+      if(p.getType().equals(Property.SELECTION_PROPERTY)){
+        return p.getSelected().getName().endsWith(myValue.getSelected().getName().toLowerCase());
+      }else{
+        return p.getValue().endsWith(myValue.getValue());
+      }
+    }
+    if ("contains".equals(myOperator) && "string".equals(p.getType())) {
+    	if(p.getValue() != null){
+        return p.getValue().toLowerCase().indexOf(myValue.getValue().toLowerCase()) >= 0;
+    	}else{
+    		return false;
+    	}
+    }
+    if ("contains".equals(myOperator)) {
+      if(p.getType().equals(Property.SELECTION_PROPERTY)){
+        String name = "";
+        if(myValue.getType().equals(Property.SELECTION_PROPERTY)){
+          name = myValue.getSelected().getName();
+        }else{
+          name = myValue.getValue();
+        }
+        ArrayList l = p.getAllSelections();
+        for(int i=0;i<l.size();i++){
+          Selection s = (Selection)l.get(i);
+          System.err.println("Comparing: " + name + ": " + s.getName());
+          if(s.getName().equals(name)){
+            return true;
+          }
+        }
+        return false;
+      }else{
+      	if(p.getValue() != null){
+      		return p.getValue().indexOf(myValue.getValue()) >= 0;
+      	}else{
+      		return false;
+      	}
+      }
+    }
+    if ("regularExpression".equals(myOperator)) {
+    	if(p.getValue() != null){
+    		return p.getValue().matches(myValue.getValue());
+    	}else{
+    		return false;
+    	}
+    }
+
+    return true;
+  }
+}
