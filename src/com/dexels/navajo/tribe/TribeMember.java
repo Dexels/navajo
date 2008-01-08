340145a30a767f2e1fdccdf650402abf4917b30b
Arjen Schoneveld <aschoneveld@dexels.com>
2008-01-08 16:38:21 +0000

    Removed System.err

--- src/com/dexels/navajo/tribe/TribeMember.java
+++ src/com/dexels/navajo/tribe/TribeMember.java
@@ -59,7 +59,6 @@ public class TribeMember implements Serializable, Mappable {
 
 	public void setStatus(PingAnswer status) {
 		this.status = status;
-		System.err.println(memberName + " load " + status.cpuLoad);
 	}
 
 	public void kill() {
