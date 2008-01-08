47a41ee27ab13a23d8fb1c10c0a708d24b49e656
Arjen Schoneveld <aschoneveld@dexels.com>
2008-01-08 16:36:53 +0000

    For web service listeners only perform (de)registrations and event occurrences  on server other than originating server.

--- src/com/dexels/navajo/tribe/SmokeSignal.java
+++ src/com/dexels/navajo/tribe/SmokeSignal.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 import com.dexels.navajo.scheduler.BeforeWebserviceTrigger;
 import com.dexels.navajo.scheduler.ListenerStore;
 import com.dexels.navajo.scheduler.WebserviceTrigger;
+import com.dexels.navajo.server.Dispatcher;
 import com.dexels.navajo.util.AuditLog;
 import com.dexels.navajo.workflow.WorkFlowManager;
 
@@ -21,7 +22,8 @@ public class SmokeSignal implements Serializable {
 	public Object value;
 	
 	/**
-	 * Objects.
+	 * Objects and their keys (attributes).
+	 * Objects basically defines a processing domain. Each processing domain uses keys for specific processing instructions.
 	 */
 	public static final String OBJECT_LISTENERS = "listeners";
 	public static final String KEY_LISTENERS_ADD_WEBSERVICE = "add_webservice";
@@ -58,12 +60,18 @@ public class SmokeSignal implements Serializable {
 	
 	/**
 	 * Process the message.
+	 * NOTE: A SmokeSignal message may or may not, depending on its semantics, be processed on the original sender
+	 * of the broadcast message.
 	 * 
 	 * @param m
 	 */
 	public void processMessage() {
-		AuditLog.log(AuditLog.AUDIT_MESSAGE_TRIBEMANAGER, "SmokeSignal: PROCESS MESSAGE (" + getObject() + "/" + getKey() + "/" + getValue() + ")");
-		if ( getObject().equals(OBJECT_LISTENERS)) {
+		
+		// Do not process message for object listeners where sender equals myself.
+		if ( getObject().equals(OBJECT_LISTENERS) && !sender.equals(Dispatcher.getInstance().getNavajoConfig().getInstanceName())) {
+			
+			AuditLog.log(AuditLog.AUDIT_MESSAGE_TRIBEMANAGER, "SmokeSignal: PROCESS MESSAGE (" + getObject() + "/" + getKey() + "/" + getValue() + ")");
+			
 			ListenerStore ls = ListenerStore.getInstance();
 			// Determine key.
 			if ( getKey().equals(KEY_LISTENERS_ADD_WEBSERVICE)) {
@@ -84,6 +92,9 @@ public class SmokeSignal implements Serializable {
 				}
 			} 
 		} else if ( getObject().equals(OBJECT_MEMBERSHIP) ) {
+			
+			AuditLog.log(AuditLog.AUDIT_MESSAGE_TRIBEMANAGER, "SmokeSignal: PROCESS MESSAGE (" + getObject() + "/" + getKey() + "/" + getValue() + ")");
+			
 			if ( getKey().equals(KEY_INTRODUCTION)  ) {
 				TribeMember tm = (TribeMember) getValue();
 				TribeManager.getInstance().addTribeMember(tm);
