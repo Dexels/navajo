fe187e4185c31b85639c76f740460c0e78736540
Frank Lyaruu <frank@dexels.com>
2008-01-17 15:40:01 +0000

    veet

---
+++ src/com/dexels/navajo/tipi/components/swingimpl/springbutton/SpringFrame.java
@@ -0,0 +1,37 @@
+package com.dexels.navajo.tipi.components.swingimpl.springbutton;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Frank
+ */
+public class SpringFrame extends JFrame {
+ 
+    public SpringFrame() {
+        super("SpringButton api!");
+        getContentPane().setLayout(new FlowLayout());
+        //setupGlassPane();
+    } 
+   
+    public static void main(String[] args) {
+          SpringFrame sf = new SpringFrame();
+          final SpringButton sp = new SpringButton();
+          final SpringButton sp2 = new SpringButton();
+
+          sf.getContentPane().add(sp);
+          sf.getContentPane().add(sp2);
+          sf.setVisible(true);
+          sf.setSize(400,250);
+    }
+    
+    
+}
