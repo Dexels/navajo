c3c7ac0f539bded9da566083c344a8c3e6da2e40
Frank Lyaruu <frank@dexels.com>
2008-01-23 10:14:24 +0000

    Added deprecation

--- src/com/dexels/navajo/document/databinding/MessageDataListener.java
+++ src/com/dexels/navajo/document/databinding/MessageDataListener.java
@@ -1,4 +1,5 @@
 package com.dexels.navajo.document.databinding;
+@Deprecated, use javax.beans version instead
 
 public interface MessageDataListener {
 	public void propertyDataChanged(String oldValue, String newValue);
--- src/com/dexels/navajo/document/databinding/PropertyDataListener.java
+++ src/com/dexels/navajo/document/databinding/PropertyDataListener.java
@@ -2,6 +2,8 @@ package com.dexels.navajo.document.databinding;
 
 import com.dexels.navajo.document.*;
 
+@Deprecated, use javax.beans version instead
+
 public interface PropertyDataListener {
 	public void propertyDataChanged(Property p, String oldValue, String newValue);
 
