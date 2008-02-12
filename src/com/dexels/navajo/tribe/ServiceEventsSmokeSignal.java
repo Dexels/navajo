09fe95feef68897ed6dcf3cd747fd81692e43f41
Arjen Schoneveld <aschoneveld@dexels.com>
2008-02-12 22:25:47 +0000

    Before webservice events are NEVER broadcasted due to synchronous nature.

--- src/com/dexels/navajo/tribe/ServiceEventsSmokeSignal.java
+++ src/com/dexels/navajo/tribe/ServiceEventsSmokeSignal.java
@@ -1,10 +1,8 @@
 package com.dexels.navajo.tribe;
 
-import com.dexels.navajo.scheduler.BeforeWebserviceTrigger;
 import com.dexels.navajo.scheduler.ListenerStore;
-import com.dexels.navajo.scheduler.WebserviceTrigger;
+import com.dexels.navajo.scheduler.AfterWebserviceTrigger;
 import com.dexels.navajo.server.enterprise.tribe.SmokeSignal;
-import com.dexels.navajo.util.AuditLog;
 import com.dexels.navajo.workflow.WorkFlowManager;
 
 public class ServiceEventsSmokeSignal extends SmokeSignal {
@@ -17,15 +15,12 @@ public class ServiceEventsSmokeSignal extends SmokeSignal {
 	public static final String ADD_WEBSERVICE = "add_webservice";
 	public static final String REMOVE_WEBSERVICE = "remove_webservice";
 	public static final String AFTERWEBSERVICE_EVENT = "after_webservice_event";
-	public static final String BEFOREWEBSERVICE_EVENT = "after_webservice_event";
 	
 	public ServiceEventsSmokeSignal(String sender, String key, Object value) {
 		super(sender, key, value);
 	}
 
 	public void processMessage() {
-
-		//AuditLog.log(AuditLog.AUDIT_MESSAGE_TRIBEMANAGER, "ServiceEventsSmokeSignal: PROCESS MESSAGE (" + getKey() + "/" + getValue() + ")");
 
 		// Do not process this smokesignal if I am the sender.
 		if ( !iAmTheSender() ) {
@@ -39,14 +34,9 @@ public class ServiceEventsSmokeSignal extends SmokeSignal {
 				String value = (String) getValue();
 				ls.removeRegisteredWebservice(value);
 			} else if ( getKey().equals(AFTERWEBSERVICE_EVENT)) {
-				WebserviceTrigger awt = (WebserviceTrigger) getValue();
+				AfterWebserviceTrigger awt = (AfterWebserviceTrigger) getValue();
 				if ( awt.getTask().getWorkflowId() == null || WorkFlowManager.getInstance().hasWorkflowId(awt.getTask().getWorkflowId()) ) {
 					awt.perform();
-				}
-			} else if ( getKey().equals(BEFOREWEBSERVICE_EVENT)) {
-				BeforeWebserviceTrigger bwt = (BeforeWebserviceTrigger) getValue();
-				if ( bwt.getTask().getWorkflowId() == null || WorkFlowManager.getInstance().hasWorkflowId(bwt.getTask().getWorkflowId()) ) {
-					bwt.perform();
 				}
 			} 
 		}
