9a563fdbca13d8d8ca1b819d2d675ad3a93e6779
Arjen Schoneveld <aschoneveld@dexels.com>
2008-02-22 10:23:24 +0000

    Code clean up.

--- src/com/dexels/navajo/mapping/AsyncStore.java
+++ src/com/dexels/navajo/mapping/AsyncStore.java
@@ -98,24 +98,8 @@ public final class AsyncStore extends GenericThread implements AsyncStoreMXBean 
 			  String ref = (String) iter.next();
 			  AsyncMappable a = (AsyncMappable) objectStore.get(ref);
 			  long now = System.currentTimeMillis();
-			  if ( (now - a.getLastAccess()) > timeout || a.isKilled()) {
-//				  if (!a.isKilled()) {
-//					  System.err.println("REMOVED " + ref +
-//							  " FROM OBJECT STORE DUE TO TIME-OUT, now = " +
-//							  now + ", lastAccess() = " + a.getLastAccess() +
-//							  ", timeout = " + timeout);
-//				  }
-//				  else {
-//					  System.err.println("REMOVED " + ref +
-//					  " FROM OBJECT STORE DUE TO KILLONFINNISH");
-//				  }
-				  
+			  if ( (now - a.getLastAccess()) > timeout ) {
 				  a.kill();
-				 // AsyncMappable am = (AsyncMappable) objectStore.get(ref);
-//				  try {
-//						JMXHelper.deregisterMXBean(JMXHelper.ASYNC_DOMAIN, am.getClassName() + "-" + ref);
-//					} catch (Throwable e) {
-//					}
 				  objectStore.remove(ref);
 				  accessStore.remove(ref);
 				  a = null;
@@ -180,22 +164,19 @@ public final class AsyncStore extends GenericThread implements AsyncStoreMXBean 
    * @param ref
    */
   public final synchronized void removeInstance(String ref) {
-//	System.err.println("About to remove async instance: " + ref);
-    Object o = objectStore.get(ref);
-//    try {
-//		JMXHelper.deregisterMXBean(JMXHelper.ASYNC_DOMAIN, ((AsyncMappable )o).getClassName() + "-" + ref);
-//	} catch (Throwable e) {
-//	}
-    if (o == null) {
-      return;
-    }
-    else {
-      objectStore.remove(ref);
-      if (accessStore.containsKey(ref)) {
-        accessStore.remove(ref);
-      }
-      o = null;
-    }
+//	  System.err.println("About to remove async instance: " + ref);
+	  Object o = objectStore.get(ref);
+
+	  if (o == null) {
+		  return;
+	  }
+	  else {
+		  objectStore.remove(ref);
+		  if (accessStore.containsKey(ref)) {
+			  accessStore.remove(ref);
+		  }
+		  o = null;
+	  }
   }
   
   public String getVERSION() {
