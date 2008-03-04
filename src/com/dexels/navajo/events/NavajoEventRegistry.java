45d1f682542ab76088c544df005007c436265d11
Arjen Schoneveld <aschoneveld@dexels.com>
2008-03-04 15:58:44 +0000

    Added support for JMX notification listener

--- src/com/dexels/navajo/events/NavajoEventRegistry.java
+++ src/com/dexels/navajo/events/NavajoEventRegistry.java
@@ -5,18 +5,32 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
+import javax.management.AttributeChangeNotification;
+import javax.management.Notification;
+import javax.management.NotificationBroadcasterSupport;
+import javax.management.NotificationListener;
+import javax.management.monitor.MonitorNotification;
+
+import com.dexels.navajo.server.GenericThread;
+import com.dexels.navajo.server.jmx.JMXHelper;
+
 /**
  * A very simple event registry class.
  * 
  * @author arjen
  *
  */
-public class NavajoEventRegistry {
+public class NavajoEventRegistry extends NotificationBroadcasterSupport implements NavajoEventRegistryMXBean, NotificationListener {
 
 	private volatile static NavajoEventRegistry instance = null;
 	
-	private HashMap<Class<? extends NavajoEvent>, HashSet<NavajoListener>> registry = 
+	private final HashMap<Class<? extends NavajoEvent>, HashSet<NavajoListener>> registry = 
 															new HashMap<Class<? extends NavajoEvent>, HashSet<NavajoListener>>();
+	
+	/**
+	 * Map contains JMX monitored eventtypes.
+	 */
+	private final HashSet<String> monitoredEvents = new HashSet<String>();
 	
 	private static Object semaphore = new Object();
 	
@@ -38,6 +52,12 @@ public class NavajoEventRegistry {
 				
 				System.err.println("Creating NavajoEventRegistry()....");
 				instance = new NavajoEventRegistry();
+				try {
+					JMXHelper.registerMXBean(instance, JMXHelper.NAVAJO_DOMAIN, "Navajo Event Registry");
+				} catch (Throwable t) {
+					t.printStackTrace(System.err);
+				} 
+				
 				
 				System.err.println("Returning instance = " + instance);
 			}
@@ -77,6 +97,20 @@ public class NavajoEventRegistry {
 			registered.remove(l);
 		}
 	}
+
+	private void publishMonitoredEvent(final NavajoEvent ne) {
+
+		System.err.println("In publishMonitoredEvent(" + ne.getClass().getName() + ")");
+		if ( monitoredEvents.contains( ne.getClass().getName() )) {
+			
+			System.err.println("About to send JMX notification....");
+			Notification n = 
+				new Notification(ne.getClass().getName(), "Navajo Event Registry", GenericThread.notificationSequence++,
+								 System.currentTimeMillis(), ne.toString());
+
+			sendNotification(n); 
+		}
+	}
 	
 	/**
 	 * Publish an event and handle the listeners asynchronously
@@ -86,6 +120,8 @@ public class NavajoEventRegistry {
 	public void publishAsynchronousEvent(final NavajoEvent ne) {
 
 		System.err.println("Asynchronous Event Triggered: " + ne.getClass());
+		publishMonitoredEvent(ne);
+		
 		Set<NavajoListener> copy = getInterestedParties(ne);
 		if ( copy != null ) {
 			Iterator<NavajoListener> i = copy.iterator();
@@ -112,6 +148,8 @@ public class NavajoEventRegistry {
 	public void publishEvent(NavajoEvent ne) {
 
 		System.err.println("Synchronous Event Triggered: " + ne.getClass());
+		publishMonitoredEvent(ne);
+		
 		Set<NavajoListener> copy = getInterestedParties(ne);
 		if ( copy != null )  {
 			Iterator<NavajoListener> i = copy.iterator();
@@ -137,6 +175,42 @@ public class NavajoEventRegistry {
 				return null;
 			}
 		}
+	}
+
+	public void addMonitoredEvent(String type) {
+		monitoredEvents.add(type);
+	}
+
+	public void removeMonitoredEvent(String type) {
+		monitoredEvents.remove(type);
+	}
+
+	public String getMonitoredEvents() {
+		return monitoredEvents.toString();
+	}
+
+	/** 
+	 * Handles a JMX notification.
+	 */
+	public void handleNotification(Notification notification, Object handback) {
+
+		if (notification instanceof MonitorNotification) { 
+			
+			System.err.println(">>>>>>>>>>>> RECEIVED NOTIFICATION: " + notification.getType() );
+			System.err.println(">>>>>>>>>>>> RECEIVED NOTIFICATION: " + notification.getMessage() );
+			System.err.println(">>>>>>>>>>>> RECEIVED NOTIFICATION: " + notification.getSequenceNumber() );
+			System.err.println(">>>>>>>>>>>> RECEIVED NOTIFICATION: " + notification.getTimeStamp() );
+			System.err.println(">>>>>>>>>>>> RECEIVED NOTIFICATION: " + notification.getSource() );		
+			System.err.println(">>>>>>>>>>>> RECEIVED NOTIFICATION: " + ((MonitorNotification) notification).getObservedObject() );
+			System.err.println(">>>>>>>>>>>> RECEIVED NOTIFICATION: " + ((MonitorNotification) notification).getObservedAttribute() );
+			System.err.println(">>>>>>>>>>>> RECEIVED NOTIFICATION: " + ((MonitorNotification) notification).getTrigger() );
+			
+		}
+
+	}
+
+	public int getNumberOfRegisteredListeners() {
+		return registry.size();
 	}
 	
 }
