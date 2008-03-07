37846cd9b6b6ab95b634f7ce1a0b9b026d9a045b
Arnoud Philip <arnoud@dexels.com>
2008-03-07 15:43:45 +0000

    Added setSkin method

---
+++ src/com/dexels/navajo/tipi/swing/substance/TipiSetSkin.java
@@ -0,0 +1,57 @@
+package com.dexels.navajo.tipi.swing.substance;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+import org.jvnet.substance.*;
+
+import com.dexels.navajo.document.*;
+import com.dexels.navajo.tipi.*;
+import com.dexels.navajo.tipi.components.swingimpl.*;
+import com.dexels.navajo.tipi.internal.*;
+
+public class TipiSetSkin extends TipiAction {
+
+	protected void execute(TipiEvent event) throws TipiBreakException, TipiException {
+		Operand valueOp = getEvaluatedParameter("value", event);
+		String value = null;
+		if(valueOp!=null) {
+			value = (String) valueOp.value;
+		}
+		if (value==null) {
+			setDefaultSkin();
+		} else {
+			setSubstanceSkin(value);
+		}
+	}
+
+	private void setDefaultSkin() {
+		try {
+			SubstanceLookAndFeel.setSkin("org.jvnet.substance.skin.BusinessSkin");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void setSubstanceSkin(String value) {
+		// TODO Auto-generated method stub
+		SubstanceLookAndFeel.setSkin(value);
+		Frame[] f = Frame.getFrames();
+		try {
+			UIManager.setLookAndFeel(new SubstanceLookAndFeel());
+		} catch (UnsupportedLookAndFeelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if(((SwingTipiContext)myContext).getAppletRoot()!=null) {
+		} else {
+			for (int i = 0; i < f.length; i++) {
+				SwingUtilities.updateComponentTreeUI(f[i]);
+			}
+		}
+		
+	}
+
+}
