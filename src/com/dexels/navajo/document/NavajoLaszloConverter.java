3268fd6da6d0f6ad742279820a7df2d32375f388
Arjen Schoneveld <aschoneveld@dexels.com>
2008-04-03 12:26:14 +0000

    Reverted changes

--- src/com/dexels/navajo/document/NavajoLaszloConverter.java
+++ src/com/dexels/navajo/document/NavajoLaszloConverter.java
@@ -46,7 +46,7 @@ public class NavajoLaszloConverter {
 	}
 
 	public static Document createLaszloFromNavajo(Navajo in) {
-		return createLaszloFromNavajo(in, "navajo");
+		return createLaszloFromNavajo(in, "navajoDataSource");
 	}
 
 	
@@ -54,7 +54,7 @@ public class NavajoLaszloConverter {
 		return createLaszloFromNavajo(in, false, serviceName);
 	}
 	public static Document createLaszloFromNavajo(Navajo in, boolean includeSelections) {
-		return createLaszloFromNavajo(in, includeSelections, "navajo");
+		return createLaszloFromNavajo(in, includeSelections, "navajoDataSource");
 	}
 	public static Document createLaszloFromNavajo(Navajo in, boolean includeSelections,  String serviceName) {
 		Document doc = XMLDocumentUtils.createDocument();
@@ -64,9 +64,11 @@ public class NavajoLaszloConverter {
 			Element root = doc.createElement(nodeName);
 			doc.appendChild(root);
 			Element tml = doc.createElement("tml");
-//			tml.setAttribute("rpc_usr", in.getHeader().getRPCUser());
-//			tml.setAttribute("rpc_pwd", in.getHeader().getRPCPassword());
-//			tml.setAttribute("rpc_name", serviceName);
+			if ( in.getHeader() != null ) {
+				tml.setAttribute("rpc_usr", in.getHeader().getRPCUser());
+				tml.setAttribute("rpc_pwd", in.getHeader().getRPCPassword());
+				tml.setAttribute("rpc_name", serviceName);
+			}
 			root.appendChild(tml);
 			ArrayList<Message> l = in.getAllMessages();
 			for (int i = 0; i < l.size(); i++) {
