48129fb6ed9f1a919c3eacf52b69da162656a31a
Frank Lyaruu <frank@dexels.com>
2008-04-29 15:25:13 +0000

    new style cookies

--- src/com/dexels/navajo/tipi/actions/TipiSetCookie.java
+++ src/com/dexels/navajo/tipi/actions/TipiSetCookie.java
@@ -1,15 +1,7 @@
 package com.dexels.navajo.tipi.actions;
 
-import javax.servlet.http.Cookie;
-
-import nextapp.echo2.app.ApplicationInstance;
-import nextapp.echo2.app.Command;
-import nextapp.echo2.webcontainer.ContainerContext;
-import nextapp.echo2.webcontainer.command.BrowserOpenWindowCommand;
-import nextapp.echo2.webcontainer.command.BrowserSetCookieCommand;
 
 import com.dexels.navajo.document.*;
-import com.dexels.navajo.parser.*;
 import com.dexels.navajo.tipi.internal.*;
 
 /**
@@ -40,32 +32,11 @@ public final class TipiSetCookie extends TipiAction {
 		}
 		String name = (String) nameO.value;
 		String value = (String) valueO.value;
-		Cookie cc = getCookie(name);
-		if (cc == null) {
-			cc = createCookie(name);
-		}
-		cc.setValue(value);
-		BrowserSetCookieCommand bs = new BrowserSetCookieCommand(cc);
-		ApplicationInstance.getActive().enqueueCommand(bs);
+		
+		myContext.setCookie(name, value);
 	}
 
-	private Cookie createCookie(String s) {
-		Cookie cc = new Cookie(s, "");
-		cc.setPath("/");
-		cc.setMaxAge(60*60*24*365);
 
-		return cc;
-	}
 
-	private Cookie getCookie(String s) {
-		ContainerContext containerContext = (ContainerContext) ApplicationInstance.getActive().getContextProperty(
-				ContainerContext.CONTEXT_PROPERTY_NAME);
-		Cookie[] cc = containerContext.getCookies();
-		for (int i = 0; i < cc.length; i++) {
-			if (cc[i].getName().equals(s)) {
-				return cc[i];
-			}
-		}
-		return null;
-	}
+
 }
