57dd8b2cbba5a9c1349afee604aa302a7a154e6b
Frank Lyaruu <frank@dexels.com>
2008-06-16 11:55:50 +0000

    Initial version for SwingX integration

---
+++ src/com/dexels/navajo/tipi/swingx/TipiJXPanel.java
@@ -0,0 +1,15 @@
+package com.dexels.navajo.tipi.swingx;
+
+import org.jdesktop.swingx.*;
+
+import com.dexels.navajo.tipi.components.swingimpl.*;
+
+public class TipiJXPanel extends TipiSwingDataComponentImpl {
+
+	@Override
+	public Object createContainer() {
+		JXPanel p = new JXPanel();
+		return p;
+	}
+
+}
---
+++ src/tipi/TipiSwingXExtension.java
@@ -0,0 +1,57 @@
+package tipi;
+
+import java.util.*;
+
+import com.dexels.navajo.tipi.*;
+
+public class TipiSwingXExtension implements TipiExtension {
+
+	public void initialize(TipiContext tc) {
+		// Do nothing
+		
+	}
+
+	public String getDescription() {
+		return "SwingX extended libraries";
+	}
+
+	public String[] getIncludes() {
+		return new String[]{"com/dexels/navajo/tipi/swingxclassdef.xml"};
+	}
+
+	public boolean isMainImplementation() {
+		return false;
+	}
+
+
+	public String getId() {
+		
+		return "swingx";
+	}
+
+
+	public String requiresMainImplementation() {
+		return "swing";
+	}
+
+	public List<String> getLibraryJars() {
+		List<String> l = new LinkedList<String>();
+		l.add("swingx.jar");
+		
+		return l;
+	}
+
+	public List<String> getMainJars() {
+		List<String> l = new LinkedList<String>();
+		l.add("TipiSwingX.jar");
+		return l;
+	} 
+	public String getConnectorId() {
+		return null;
+	}
+
+	public List<String> getRequiredExtensions() {
+		return null;
+	}
+
+}
