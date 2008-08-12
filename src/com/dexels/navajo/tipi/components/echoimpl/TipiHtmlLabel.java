bcc76a3d25422ddc36488f1af552fd3cf49ce069
Frank Lyaruu <frank@dexels.com>
2008-08-12 09:18:38 +0000

    added binary support

--- src/com/dexels/navajo/tipi/components/echoimpl/TipiHtmlLabel.java
+++ src/com/dexels/navajo/tipi/components/echoimpl/TipiHtmlLabel.java
@@ -1,7 +1,12 @@
 package com.dexels.navajo.tipi.components.echoimpl;
 
+import java.io.*;
 import java.net.*;
 
+import org.w3c.tidy.*;
+
+import com.dexels.navajo.document.types.*;
+import com.dexels.navajo.functions.*;
 import com.dexels.navajo.tipi.components.echoimpl.parsers.ColorParser;
 
 import echopointng.*;
@@ -30,30 +35,66 @@ import nextapp.echo2.app.ResourceImageReference;
  */
 
 public class TipiHtmlLabel extends TipiEchoComponentImpl {
-    private TemplatePanel templatePanel = null;
+	private TemplatePanel templatePanel = null;
 
 	public TipiHtmlLabel() {
-    }
+	}
 
-    public Object createContainer() {
-//		DirectHtml directHtml = new DirectHtml(markup);
+	public Object createContainer() {
+		// DirectHtml directHtml = new DirectHtml(markup);
 
 		templatePanel = new TemplatePanel();
 		return templatePanel;
-    }
+	}
 
-    
-    protected void setComponentValue(String name, Object object) {
+	protected void setComponentValue(String name, Object object) {
         if ("text".equals(name)) {
         	String bare = ""+object;
-        	String emb = "<div xmlns='http://www.w3.org/1999/xhtml'>"+bare+"</div>";
-        	System.err.println("Result: "+emb);
-        	StringTemplateDataSource stds = new StringTemplateDataSource(emb);
-    		templatePanel.setTemplateDataSource(stds);
-    		//stds.setContentType("text/html");
-            
+        	setText(bare);
+        }
+        if("binary".equals(name)) {
+        	Binary b = (Binary)object;
+        	String text;
+//			try {
+				text = new String(b.getData());
+				StripBody sb = new StripBody();
+				
+				
+				text = sb.bodyStrip(text);
+				text = sb.tidyString(text);
+				text = sb.bodyStrip(text);
+				
+				
+				setText(text);
+				
+//	        	System.err.println("HTMLLABEL TEXT:\n\n"+text+"\n\n");
+//			} catch (UnsupportedEncodingException e) {
+//				e.printStackTrace();
+//			}
         }
         super.setComponentValue(name, object);
     }
+
+	private String stripBody(String s) {
+		int startIndex = s.indexOf("<body");
+		if (startIndex != -1) {
+			startIndex = s.indexOf(">", startIndex + ("<body".length()));
+		}
+		s = s.substring(startIndex + 1);
+		int endIndex = s.indexOf("</body>");
+		s = s.substring(0, endIndex);
+		System.err.println("Result: " + s);
+
+		// return "<h3>aap</h3><br/> bla bla bla <br/>";
+		return s;
+	}
+
+	private void setText(String bare) {
+		String emb = "<div xmlns='http://www.w3.org/1999/xhtml'>" + bare + "</div>";
+		System.err.println("Result: " + emb);
+		StringTemplateDataSource stds = new StringTemplateDataSource(emb);
+		templatePanel.setTemplateDataSource(stds);
+		System.err.println("Total result: "+emb);
+	}
 
 }
