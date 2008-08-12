a866124b6e84de7edecec069c91a1dd016b16af2
Frank Lyaruu <frank@dexels.com>
2008-08-12 08:57:52 +0000

    extension fix

--- src/com/dexels/navajo/tipi/components/swingimpl/actions/TipiOpenBinary.java
+++ src/com/dexels/navajo/tipi/components/swingimpl/actions/TipiOpenBinary.java
@@ -9,6 +9,8 @@ package com.dexels.navajo.tipi.components.swingimpl.actions;
 import java.io.*;
 import java.util.*;
 
+import metadata.*;
+
 import com.dexels.navajo.document.*;
 import com.dexels.navajo.document.types.*;
 import com.dexels.navajo.tipi.*;
@@ -29,52 +31,65 @@ public class TipiOpenBinary extends TipiAction {
      * @see com.dexels.navajo.tipi.internal.TipiAction#execute(com.dexels.navajo.tipi.internal.TipiEvent)
      */
     protected void execute(TipiEvent event) throws TipiBreakException, TipiException {
-        Operand value  = getEvaluatedParameter("value", event);
-        Operand extension  = getEvaluatedParameter("extension", event);
-        Operand fileName  = getEvaluatedParameter("fileName", event);
-         if (value==null) {
-            throw new TipiException("TipiOpenBinary: no value supplied");
-        }
-        if (value.value==null) {
-            throw new TipiException("TipiOpenBinary: null value supplied");
-        }
-        if (!(value.value instanceof Binary)) {
-            throw new TipiException("TipiOpenBinary: Type of value is not Binary, but: "+value.value.getClass());
-        }
-        String extString = null;
-        if (extension!=null) {
-            extString = (String)extension.value;
-        }
-        String fileNameEval = null;
-        if (fileName!=null) {
-        	fileNameEval = (String)fileName.value;
-        }
-       Binary b = (Binary)value.value;
-        if (extString==null) {
-            String mime = b.guessContentType();
-            if (mime!=null) {
-                if (mime.indexOf("/") != -1) {
-                    StringTokenizer st = new StringTokenizer(mime,"/");
-                    String major = st.nextToken();
-                    String minor = st.nextToken();
-                    System.err.println("Binary type: "+major+" and minor: "+minor);
-                    extString = minor;
-                }
-            }
-        }
+		Operand value = getEvaluatedParameter("value", event);
+		Operand extension = getEvaluatedParameter("extension", event);
+		Operand fileName = getEvaluatedParameter("fileName", event);
+		if (value == null) {
+			throw new TipiException("TipiOpenBinary: no value supplied");
+		}
+		if (value.value == null) {
+			throw new TipiException("TipiOpenBinary: null value supplied");
+		}
+		if (!(value.value instanceof Binary)) {
+			throw new TipiException("TipiOpenBinary: Type of value is not Binary, but: " + value.value.getClass());
+		}
+		String extString = null;
+		if (extension != null) {
+			extString = (String) extension.value;
+		}
+		String fileNameEval = null;
+		if (fileName != null) {
+			fileNameEval = (String) fileName.value;
+		}
+		Binary b = (Binary) value.value;
+		if (extString == null) {
+			String mime = b.guessContentType();
+			String ext = null;
+			FormatDescription fd = b.getFormatDescription();
+			if(fd!=null) {
+				List<String> extensions = fd.getFileExtensions();
+				if(!extensions.isEmpty()) {
+					ext = extensions.get(0);
 
-            try {
-            	if(fileNameEval==null) {
-            		fileNameEval = "data_";
-            	}
-                File f = File.createTempFile(fileNameEval, "."+extString);
-                TipiSaveValue.saveFile(b, f);
-                DefaultBrowser.displayURL(f.getAbsolutePath());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        b.getHandle();
-    }
+				}
+			}
+			if (mime != null) {
+				if (mime.indexOf("/") != -1) {
+					StringTokenizer st = new StringTokenizer(mime, "/");
+					String major = st.nextToken();
+					String minor = st.nextToken();
+					System.err.println("Binary type: " + major + " and minor: " + minor);
+					if(ext!=null) {
+						extString = ext;
+					} else {
+						extString = minor;
+					}
+				}
+			}
+		}
+
+		try {
+			if (fileNameEval == null) {
+				fileNameEval = "data_";
+			}
+			File f = File.createTempFile(fileNameEval, "." + extString);
+			TipiSaveValue.saveFile(b, f);
+			DefaultBrowser.displayURL(f.getAbsolutePath());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		b.getHandle();
+	}
 
     public static void main(String[] args) throws Exception {
         File f = File.createTempFile("tipi_", ""+".pdf");
