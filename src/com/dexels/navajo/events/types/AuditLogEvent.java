79213fcaa96dc941af41f3368546e46be8dbce6b
Arjen Schoneveld <aschoneveld@dexels.com>
2008-08-25 15:37:58 +0000

    *** empty log message ***

---
+++ src/com/dexels/navajo/events/types/AuditLogEvent.java
@@ -0,0 +1,53 @@
+package com.dexels.navajo.events.types;
+
+import java.util.Date;
+
+import com.dexels.navajo.events.NavajoEvent;
+import com.dexels.navajo.server.Dispatcher;
+
+public class AuditLogEvent implements NavajoEvent {
+
+	private static String instanceName = null; //Dispatcher.getInstance().getNavajoConfig().getInstanceName();
+	private String subSystem;
+	private String message;
+	private String level;
+	private Date created;
+	
+	public AuditLogEvent(String subSystem, String msg, String level) {
+		message = msg;
+		this.subSystem = subSystem;
+		this.level = level;
+		this.created = new java.util.Date();
+		try {
+			if ( instanceName == null ) {
+				if ( Dispatcher.getInstance() != null ) {
+					instanceName = Dispatcher.getInstance().getNavajoConfig().getInstanceName();
+				}
+			}
+		} catch (Throwable te) {}
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public String getSubSystem() {
+		return subSystem;
+	}
+
+	public String getLevel() {
+		return level;
+	}
+	
+	public String toString() {
+		return (instanceName + ":" + subSystem + "-> " + message + " (" + level + ")");
+	}
+
+	public String getInstanceName() {
+		return instanceName;
+	}
+
+	public Date getCreated() {
+		return created;
+	}
+}
--- src/com/dexels/navajo/server/enterprise/statistics/DummyStatisticsRunner.java
+++ src/com/dexels/navajo/server/enterprise/statistics/DummyStatisticsRunner.java
@@ -1,11 +1,25 @@
 package com.dexels.navajo.server.enterprise.statistics;
 
 import com.dexels.navajo.events.NavajoEvent;
+import com.dexels.navajo.events.NavajoEventRegistry;
+import com.dexels.navajo.events.NavajoListener;
+import com.dexels.navajo.events.types.AuditLogEvent;
 import com.dexels.navajo.mapping.AsyncMappable;
 import com.dexels.navajo.server.Access;
 
-public class DummyStatisticsRunner implements StatisticsRunnerInterface {
+public class DummyStatisticsRunner implements StatisticsRunnerInterface, NavajoListener {
 
+	private static final DummyStatisticsRunner instance;
+	
+	static {
+		instance = new DummyStatisticsRunner();
+		NavajoEventRegistry.getInstance().addListener(AuditLogEvent.class, instance);
+	}
+	
+	public final static DummyStatisticsRunner getInstance() {
+		return instance;
+	}
+	
 	public boolean isEnabled() {
 		return false;
 	}
@@ -19,6 +33,12 @@ public class DummyStatisticsRunner implements StatisticsRunnerInterface {
 	}
 
 	public void onNavajoEvent(NavajoEvent ne) {
+		if ( ne instanceof AuditLogEvent ) {
+			System.err.println("DummyStatisticsRunner: " + ne);
+		}
+	}
+
+	public void addAuditLog(String subsystem, String message, String level) {
 		// TODO Auto-generated method stub
 		
 	}
