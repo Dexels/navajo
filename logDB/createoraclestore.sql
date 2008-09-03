4b1162f9ff2383d9dd1177f732089848aff8bca0
Arjen Schoneveld <aschoneveld@dexels.com>
2008-09-03 14:17:26 +0000

    Added AuditLog table.

--- logDB/createoraclestore.sql
+++ logDB/createoraclestore.sql
@@ -181,9 +181,10 @@ DROP TABLE auditlog
 CREATE TABLE auditlog (
    instance VARCHAR2(255),
    subsystem VARCHAR2(255),
-   message VARCHAR2(255),
+   message VARCHAR2(4000),
    auditlevel VARCHAR2(255),
-   lastupdate DATE DEFAULT sysdate
+   accessid VARCHAR2(255),
+   lastupdate TIMESTAMP(6) WITH TIME ZONE DEFAULT sysdate
 )
 pctfree 20
 pctused 40
@@ -194,6 +195,5 @@ storage
     pctincrease 0
 )
 tablespace USERS
-/
 
 QUIT
