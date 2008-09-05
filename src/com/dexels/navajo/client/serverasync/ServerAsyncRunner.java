cecbde411683582a27fe9591d5f2cf09ab86a237
Arjen Schoneveld <aschoneveld@dexels.com>
2008-09-05 16:28:11 +0000

    doSimpleSend instead of Specific send(!) BIG NOTE: ONLY USE THIS CHANGE IN COMBINATION WITH A VERSION OF THE DISPATCHER

--- src/com/dexels/navajo/client/serverasync/ServerAsyncRunner.java
+++ src/com/dexels/navajo/client/serverasync/ServerAsyncRunner.java
@@ -52,7 +52,8 @@ public class ServerAsyncRunner
   }
 
   private Navajo doSimpleSend(Navajo n, String method) throws ClientException {
-    return myClientInterface.doSpecificSend(n, method,serverIndex);
+    //return myClientInterface.doSpecificSend(n, method,serverIndex);
+	 return myClientInterface.doSimpleSend(n, method);
   }
 
   /**
