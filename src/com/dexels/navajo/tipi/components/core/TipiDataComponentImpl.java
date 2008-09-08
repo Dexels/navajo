7aadc1627d552a33a0084d07c93cbb4cb86b97ec
Frank Lyaruu <frank@dexels.com>
2008-09-08 12:57:49 +0000

    tiny refactoring

--- src/com/dexels/navajo/tipi/components/core/TipiDataComponentImpl.java
+++ src/com/dexels/navajo/tipi/components/core/TipiDataComponentImpl.java
@@ -289,15 +289,15 @@ public abstract class TipiDataComponentImpl extends TipiComponentImpl implements
 			}
 		}
 		// hmmmmmmmmm
-		doPerformOnLoad(method,n);
+		doPerformOnLoad(method,n,true);
 		doLayout();
 	}
 
-	protected void doPerformOnLoad(String method, Navajo n) throws TipiException {
+	protected void doPerformOnLoad(String method, Navajo n,boolean sync) throws TipiException {
 		Map<String,Object> m = new HashMap<String,Object>();
 		m.put("service", method);
 		m.put("navajo", n);
-		performTipiEvent("onLoad", m, true);
+		performTipiEvent("onLoad", m, sync);
 	}
 
 	protected void doLayout() {
