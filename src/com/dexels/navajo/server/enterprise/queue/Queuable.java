8f51faced57470e6d6462dc1375fde575856df67
Arjen Schoneveld <aschoneveld@dexels.com>
2008-09-18 10:19:12 +0000

    Some code formatting.

--- src/com/dexels/navajo/server/enterprise/queue/Queuable.java
+++ src/com/dexels/navajo/server/enterprise/queue/Queuable.java
@@ -8,66 +8,79 @@ import com.dexels.navajo.server.Access;
 
 public interface Queuable extends Serializable, QueuableMXBean {
 	
-    /**
+    
+	/**
      * Initiates the queued send operation.
      */ 
 	public void setQueuedSend(boolean b);
-    /**
+    
+	/**
      * Set maximum number of retries before giving up.
      */
 	public void setMaxRetries(int r);
-    /**
+    
+	/**
      * Set waiting time interval to use between retries.
      */
 	public void setWaitUntil(long w);
+	
 	/**
 	 * Get maximum number of retries before giving up.
 	 */
 	public int getMaxRetries();
+	
 	/**
 	 * Get waiting time interval to use between retries.
 	 * @return
 	 */
 	public long getWaitUntil();
+	
 	/**
 	 * send() implements the work that should be done upon processing of the queable object.
 	 * 
-	 * @return
+	 * @return true on success. Note that all exceptions (checked and un-checked) should be handled within the send() method.
 	 */
 	public boolean send();
+	
 	/**
 	 * getResponse() will retrieve the response of the queable object when finished.
 	 * 
 	 * @return
 	 */
 	public Binary getResponse();
+	
 	/**
 	 * getRequest() will return the request payload for to be used in queable object send() method.
 	 * @return
 	 */
 	public Binary getRequest();
+	
 	/**
 	 * Get access object associated with the request.
 	 * 
 	 * @return
 	 */
 	public Access getAccess();
+	
 	/**
 	 * Get the navajo object associated with the request.
 	 * 
 	 * @return
 	 */
 	public Navajo getNavajo();
+	
 	/**
 	 * Reset retry counter.
 	 */
 	public void resetRetries();
+	
 	/**
 	 * Get number of processed retries thus far.
 	 * 
 	 * @return
 	 */
 	public int getRetries();
+	
 	/**
 	 * Get the maximum number of specified instances that MAY run simultaneously.
 	 * -1 if there is no limit (default)
@@ -75,6 +88,7 @@ public interface Queuable extends Serializable, QueuableMXBean {
 	 * @return
 	 */
 	public int getMaxRunningInstances();
+	
 	/**
 	 * Sets the maximum number of specified instances that MAY run simultaneously.
 	 * -1 if there is no limit (default)
