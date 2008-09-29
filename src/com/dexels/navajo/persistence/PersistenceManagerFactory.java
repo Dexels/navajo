09caf0983a33f02dfbd380642a205aa1f94a5a75
Arjen Schoneveld <aschoneveld@dexels.com>
2008-09-29 15:46:33 +0000

    Several changes for Navajo Refactor (The NavaUnitTest Version)

--- src/com/dexels/navajo/persistence/PersistenceManagerFactory.java
+++ src/com/dexels/navajo/persistence/PersistenceManagerFactory.java
@@ -12,21 +12,40 @@ package com.dexels.navajo.persistence;
 
 public class PersistenceManagerFactory {
 
-    public static PersistenceManager getInstance(String className, String configPath) {
-        try {
-            PersistenceManager pm = (PersistenceManager) Class.forName(className).newInstance();
-            return pm;
-        } catch (Exception e) {
-        	System.err.println("Could NOT FIND PersistenceManager: " + className + ", trying SimplePersistenceManagerImpl..");
-        	PersistenceManager pm;
-			try {
-				pm = (PersistenceManager) Class.forName("com.dexels.navajo.persistence.SimplePersistenceManagerImpl").newInstance();
-			} catch (Exception  e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-				return null;
-			} 
-        	return pm;
-        }
-    }
+	static volatile PersistenceManager instance;
+	final static Object object = new Object();
+	
+	public static PersistenceManager getInstance() {
+		if ( instance != null ) {
+			return instance;
+		} else {
+			return null;
+		}
+	}
+	
+	public static PersistenceManager getInstance(String className, String configPath) {
+
+		if ( instance != null ) {
+			return instance;
+		}
+
+		synchronized (object) {
+
+			if ( instance == null ) {
+				try {
+					instance = (PersistenceManager) Class.forName(className).newInstance();
+
+				} catch (Exception e) {
+					System.err.println("Could NOT FIND PersistenceManager: " + className + ", trying SimplePersistenceManagerImpl..");
+					try {
+						instance = (PersistenceManager) Class.forName("com.dexels.navajo.persistence.SimplePersistenceManagerImpl").newInstance();
+					} catch (Exception  e1) {
+						throw new RuntimeException(e1);
+					} 
+
+				}
+			}
+		}
+		return instance;
+	}
 }
