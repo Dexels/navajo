416da5054a070082fd91cabdcba1e287fd1aa56a
Frank Lyaruu <frank@dexels.com>
2008-10-10 17:19:26 +0000

    not sure

---
+++ src/com/dexels/navajo/tipi/components/swingimpl/TipiHeadlessFrame.java
@@ -0,0 +1,34 @@
+package com.dexels.navajo.tipi.components.swingimpl;
+
+
+import com.dexels.navajo.tipi.components.core.*;
+
+/**
+ * <p>
+ * Title:
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * <p>
+ * Copyright: Copyright (c) 2003
+ * </p>
+ * <p>
+ * Company:
+ * </p>
+ * 
+ * @author not attributable
+ * @version 1.0
+ */
+public class TipiHeadlessFrame extends TipiDataComponentImpl {
+	// private JFrame myFrame = null;
+	
+
+	public TipiHeadlessFrame() {
+	}
+
+	public Object createContainer() {
+		return null;
+	}
+
+}
