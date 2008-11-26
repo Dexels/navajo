f3807b1f692063369b4c516725c390867039f9b9
Frank Lyaruu <frank@dexels.com>
2008-11-26 17:35:23 +0000

    sound support for apps

--- src/com/dexels/navajo/tipi/components/swingimpl/actions/TipiPlaySound.java
+++ src/com/dexels/navajo/tipi/components/swingimpl/actions/TipiPlaySound.java
@@ -6,12 +6,17 @@
  */
 package com.dexels.navajo.tipi.components.swingimpl.actions;
 
+import java.io.*;
 import java.net.*;
+
+import javax.sound.sampled.*;
+import javax.sound.sampled.spi.*;
 
 import com.dexels.navajo.document.*;
 import com.dexels.navajo.tipi.*;
 import com.dexels.navajo.tipi.components.swingimpl.*;
 import com.dexels.navajo.tipi.internal.*;
+import com.sun.media.sound.*;
 
 /**
  * @author Administrator
@@ -29,11 +34,45 @@ public class TipiPlaySound extends TipiAction {
     protected void execute(TipiEvent event) throws TipiBreakException, TipiException {
         Operand url  = getEvaluatedParameter("url", event);
         URL urlVal = (URL)url.value;
-        TipiApplet rr = ((SwingTipiContext)myContext).getAppletRoot();
-        if(rr!=null) {
-        	rr.play(urlVal);
-			
-        }
+        playUrl(urlVal);
+
+        
     }
 
+	/**
+	 * @param urlVal
+	 */
+	private void playUrl(URL urlVal) {
+		TipiApplet rr = null;
+		if(myContext!=null) {
+			rr = ((SwingTipiContext)myContext).getAppletRoot();
+		}
+        if(rr!=null) {
+        	rr.play(urlVal);
+        } else {
+        	try {
+				AudioInputStream ais = AudioSystem.getAudioInputStream(urlVal);
+				Clip c = AudioSystem.getClip();
+				c.open(ais);
+				c.start();
+			} catch (UnsupportedAudioFileException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (LineUnavailableException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        	
+        }
+	}
+
+    public static void main(String[] args) throws MalformedURLException, InterruptedException {
+//    	http://www.sound-effect.com/sounds1/animal/Treeanimals/monkeys1.wav//
+    	new TipiPlaySound().playUrl(new URL("http://www.sound-effect.com/sounds1/animal/Treeanimals/monkeys1.wav"));
+    	Thread.sleep(10000);
+    }
+    
 }
