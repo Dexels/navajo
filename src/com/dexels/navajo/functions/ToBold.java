78becba36b570fcbab4080e381f9cc4121ed19cb
Frank Lyaruu <frank@dexels.com>
2008-11-26 17:43:48 +0000

    hehe

---
+++ src/com/dexels/navajo/functions/ToBold.java
@@ -0,0 +1,48 @@
+/*
+ * Created on May 23, 2005
+ *
+ */
+package com.dexels.navajo.functions;
+
+import com.dexels.navajo.parser.*;
+
+/**
+ * @author frank
+ * 
+ */
+public class ToBold extends FunctionInterface {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.dexels.navajo.parser.FunctionInterface#remarks()
+	 */
+	public String remarks() {
+		return "Make text bold. Html-style";
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.dexels.navajo.parser.FunctionInterface#usage()
+	 */
+	public String usage() {
+		return "ToBold(string) duh.";
+	}
+
+	// GetComponent({component://init/desktop},{event:/from})
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.dexels.navajo.parser.FunctionInterface#evaluate()
+	 */
+	public Object evaluate() throws TMLExpressionException {
+		Object pp = getOperand(0);
+
+		if (!(pp instanceof String)) {
+			throw new TMLExpressionException(this, "Invalid operand: " + pp.getClass().getName());
+			}
+		return "<html><b>"+pp+"</b></html>";
+	}
+
+}
