9bfd46820cb0c4bd56d2b93b493885037352b2b7
Frank Lyaruu <frank@dexels.com>
2008-12-16 16:05:18 +0000

    *** empty log message ***

--- src/com/dexels/navajo/tipi/components/swingimpl/actions/TipiAskValue.java
+++ src/com/dexels/navajo/tipi/components/swingimpl/actions/TipiAskValue.java
@@ -8,44 +8,51 @@ import com.dexels.navajo.document.*;
 import com.dexels.navajo.tipi.*;
 import com.dexels.navajo.tipi.internal.*;
 
-
 /**
- * <p>Title: </p>
- * <p>Description: </p>
- * <p>Copyright: Copyright (c) 2004</p>
- * <p>Company: </p>
+ * <p>
+ * Title:
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * <p>
+ * Copyright: Copyright (c) 2004
+ * </p>
+ * <p>
+ * Company:
+ * </p>
+ * 
  * @author not attributable
  * @version 1.0
  */
 
 public class TipiAskValue extends TipiAction {
-  public TipiAskValue() {
-  }
-  protected void execute(TipiEvent event) throws com.dexels.navajo.tipi.TipiBreakException, com.dexels.navajo.tipi.TipiException {
-    Operand text = getEvaluatedParameter("text",event);
-    Operand globalvalue = getEvaluatedParameter("value",event);
-    Operand initialValue = getEvaluatedParameter("initialValue",event);
-    String initVal = "";
-    if (initialValue!=null) {
-      if (initialValue.value!=null) {
-        initVal = ""+initialValue.value;
-      }
-    }
-      String response = JOptionPane.showInputDialog( (Component) myContext.getTopLevel(), text.value, initVal);
-//      , JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
-      if (response != null) {
-        myContext.setGlobalValue(""+globalvalue.value,response);
-//        TipiReference tr = (TipiReference)globalvalue.value;
-//        Operand o = new Operand(response,"string",null);
-//
-//        tr.setValue(response,o,myComponent);
-//
-//        System.err.println("Response accepted");
-      } else {
-          throw new TipiBreakException(TipiBreakException.USER_BREAK);
+	public TipiAskValue() {
+	}
 
-      }
+	protected void execute(TipiEvent event) throws com.dexels.navajo.tipi.TipiBreakException, com.dexels.navajo.tipi.TipiException {
+		final Operand text = getEvaluatedParameter("text", event);
+		final Operand globalvalue = getEvaluatedParameter("value", event);
+		final Operand initialValue = getEvaluatedParameter("initialValue", event);
+		
+		myContext.runSyncInEventThread(new Runnable() {
 
-  }
+			public void run() {
+				String initVal = "";
+				if (initialValue != null) {
+					if (initialValue.value != null) {
+						initVal = "" + initialValue.value;
+					}
+				}
+				String response = JOptionPane.showInputDialog((Component) myContext.getTopLevel(), text.value, initVal);
+				if (response != null) {
+					myContext.setGlobalValue("" + globalvalue.value, response);
+				} else {
+					throw new TipiBreakException(TipiBreakException.USER_BREAK);
+
+				}
+			}
+		});
+	}
 
 }
