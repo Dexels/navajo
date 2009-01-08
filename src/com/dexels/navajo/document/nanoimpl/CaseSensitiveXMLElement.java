56f9bdaa6df71583a9dee42e3113e604def8242c
Arjen Schoneveld <aschoneveld@dexels.com>
2009-01-08 10:40:31 +0000

    Added constructor with skipLeadingWhiteSpace.

--- src/com/dexels/navajo/document/nanoimpl/CaseSensitiveXMLElement.java
+++ src/com/dexels/navajo/document/nanoimpl/CaseSensitiveXMLElement.java
@@ -16,6 +16,10 @@ public class CaseSensitiveXMLElement extends XMLElement {
 	public CaseSensitiveXMLElement() {
     super(new Hashtable<String,char[]>(),false,false);
   }
+	public CaseSensitiveXMLElement(boolean skipLeadingWhiteSpace) {
+	    super(new Hashtable<String,char[]>(), skipLeadingWhiteSpace, false);
+	  }
+	
   protected XMLElement createAnotherElement()
   {
       return new CaseSensitiveXMLElement();
