5bcf87cbd656a9f2e3a91fb25e33a25137cb76b4
Arjen Schoneveld <aschoneveld@dexels.com>
2009-01-22 08:48:35 +0000

    *** empty log message ***

---
+++ testsrc/com/dexels/navajo/jabber/JabberTriggerTest.java
@@ -0,0 +1,15 @@
+package com.dexels.navajo.jabber;
+
+import junit.framework.TestCase;
+
+public class JabberTriggerTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
