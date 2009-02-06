65856cc390474647d5bdf8ebb109849bba1dc05f
Arjen Schoneveld <aschoneveld@dexels.com>
2009-02-06 16:22:25 +0000

    Changes for new-style scripts via ServiceRegistry lookup.

--- src/com/dexels/navajo/adapter/StandardAdapterLibrary.java
+++ src/com/dexels/navajo/adapter/StandardAdapterLibrary.java
@@ -4,6 +4,12 @@ import java.io.InputStream;
 
 import navajo.ExtensionDefinition;
 
+/**
+ * Reference to the XML definition file for this set of adapters defined in this project.
+ * 
+ * @author arjen
+ *
+ */
 public class StandardAdapterLibrary implements ExtensionDefinition {
 
 	public InputStream getDefinitionAsStream() {
