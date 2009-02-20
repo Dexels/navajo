f8aca7553e0c3714627918b53b8c2a8f05b02e79
Frank Lyaruu <frank@dexels.com>
2009-02-20 15:16:13 +0000

    *** empty log message ***

---
+++ src/tipi/TipiSwingMigExtension.java
@@ -0,0 +1,19 @@
+package tipi;
+
+import com.dexels.navajo.tipi.TipiContext;
+
+	public class TipiSwingMigExtension extends TipiAbstractXMLExtension  {
+
+		public TipiSwingMigExtension() {
+			loadXML();
+		}
+
+		public void initialize(TipiContext tc) {
+			// Do nothing
+
+		}
+
+
+
+
+	}
