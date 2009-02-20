dbb3ca4fa9937cae79cb2fa6842b2ed43120b443
Frank Lyaruu <frank@dexels.com>
2009-02-20 15:13:45 +0000

    *** empty log message ***

--- src/tipi/TipiSwingXExtension.java
+++ src/tipi/TipiSwingXExtension.java
@@ -1,70 +1,19 @@
 package tipi;
 
-import java.util.*;
+import com.dexels.navajo.tipi.TipiContext;
 
-import com.dexels.navajo.tipi.*;
+	public class TipiSwingXExtension extends TipiAbstractXMLExtension  {
 
-public class TipiSwingXExtension extends AbstractTipiExtension implements TipiExtension {
+		public TipiSwingXExtension() {
+			loadXML();
+		}
 
-	public void initialize(TipiContext tc) {
-		// Do nothing
+		public void initialize(TipiContext tc) {
+			// Do nothing
+
+		}
+
+
+
 
 	}
-
-	public String getDescription() {
-		return "SwingX extended libraries";
-	}
-
-	public String[] getIncludes() {
-		return new String[] { "com/dexels/navajo/tipi/swingxclassdef.xml" };
-	}
-
-	public boolean isMainImplementation() {
-		return false;
-	}
-
-	public String getId() {
-
-		return "swingx";
-	}
-
-	public String requiresMainImplementation() {
-		return "swing";
-	}
-
-	public List<String> getLibraryJars() {
-		List<String> l = new LinkedList<String>();
-		l.add("swingx.jar");
-
-		return l;
-	}
-
-	public List<String> getMainJars() {
-		List<String> l = new LinkedList<String>();
-		l.add("TipiSwingX.jar");
-		return l;
-	}
-
-	public String getConnectorId() {
-		return null;
-	}
-
-	public List<String> getRequiredExtensions() {
-		return null;
-	}
-
-	public String getProjectName() {
-		return "TipiSwingX";
-	}
-
-	public String getDeploymentDescriptor() {
-		return "TipiSwingX.jnlp";
-	}
-
-	public List<String> getDependingProjectUrls() {
-		List<String> l = new LinkedList<String>();
-		l.add("https://swingx.dev.java.net/");
-		return l;
-	}
-
-}
