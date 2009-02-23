f6594098e2a468e78a7725eab2ca400e7c96e34e
Frank Lyaruu <frank@dexels.com>
2009-02-23 12:56:36 +0000

    *** empty log message ***

---
+++ src/tipi/TipiRichExtension.java
@@ -0,0 +1,19 @@
+package tipi;
+
+import com.dexels.navajo.tipi.TipiContext;
+
+	public class TipiRichExtension extends TipiAbstractXMLExtension  {
+
+		public TipiRichExtension() {
+			loadXML();
+		}
+
+		public void initialize(TipiContext tc) {
+			// Do nothing
+
+		}
+
+
+
+
+	}
