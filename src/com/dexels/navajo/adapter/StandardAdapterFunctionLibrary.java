cd92f5d326f0e7a813ba696caa2d9f99c3f27bef
Arjen Schoneveld <aschoneveld@dexels.com>
2009-02-23 16:12:40 +0000

    *** empty log message ***

--- src/com/dexels/navajo/adapter/StandardAdapterFunctionLibrary.java
+++ src/com/dexels/navajo/adapter/StandardAdapterFunctionLibrary.java
@@ -1,6 +1,7 @@
 package com.dexels.navajo.adapter;
 
 import java.io.InputStream;
+import java.util.List;
 
 import navajo.ExtensionDefinition;
 
@@ -14,6 +15,64 @@ public class StandardAdapterFunctionLibrary implements ExtensionDefinition {
 
 	public InputStream getDefinitionAsStream() {
 		return getClass().getClassLoader().getResourceAsStream("com/dexels/navajo/adapter/adapterfunctions.xml");
+	}
+
+	public String getConnectorId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<String> getDependingProjectUrls() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getDeploymentDescriptor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getDescription() {
+		return "The Standard Navajo Function Library";
+	}
+
+	public String getId() {
+		return "NavajoFunctions";
+	}
+
+	public String[] getIncludes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<String> getLibraryJars() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<String> getMainJars() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getProjectName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<String> getRequiredExtensions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean isMainImplementation() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public String requiresMainImplementation() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- src/com/dexels/navajo/adapter/StandardAdapterLibrary.java
+++ src/com/dexels/navajo/adapter/StandardAdapterLibrary.java
@@ -1,6 +1,7 @@
 package com.dexels.navajo.adapter;
 
 import java.io.InputStream;
+import java.util.List;
 
 import navajo.ExtensionDefinition;
 
@@ -14,6 +15,64 @@ public class StandardAdapterLibrary implements ExtensionDefinition {
 
 	public InputStream getDefinitionAsStream() {
 		return getClass().getClassLoader().getResourceAsStream("com/dexels/navajo/adapter/adapters.xml");
+	}
+
+	public String getConnectorId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<String> getDependingProjectUrls() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getDeploymentDescriptor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getDescription() {
+		return "The Standard Navajo Adapter Library";
+	}
+
+	public String getId() {
+		return "NavajoAdapters";
+	}
+
+	public String[] getIncludes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<String> getLibraryJars() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<String> getMainJars() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getProjectName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<String> getRequiredExtensions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean isMainImplementation() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public String requiresMainImplementation() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
