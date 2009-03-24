10504216f39d4a051d81d5cfc4ea8eee63be337d
Arjen Schoneveld <aschoneveld@dexels.com>
2009-03-24 10:00:16 +0000

    New script introspection stuff. Added support for determining CVS meta data.

--- src/com/dexels/navajo/util/ScriptIntrospection.java
+++ src/com/dexels/navajo/util/ScriptIntrospection.java
@@ -1,15 +1,16 @@
 package com.dexels.navajo.util;
 
 import java.io.File;
-import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
 
 import com.dexels.navajo.document.NavajoFactory;
-import com.dexels.navajo.loader.NavajoClassLoader;
 import com.dexels.navajo.loader.NavajoClassSupplier;
 import com.dexels.navajo.mapping.CompiledScript;
+import com.dexels.navajo.mapping.compiler.meta.AdapterFieldDependency;
 import com.dexels.navajo.mapping.compiler.meta.Dependency;
 import com.dexels.navajo.mapping.compiler.meta.IncludeDependency;
 import com.dexels.navajo.mapping.compiler.meta.InheritDependency;
@@ -25,24 +26,21 @@ public class ScriptIntrospection {
 
 	public static final String DEFAULT_SERVER_XML = "config/server.xml";
 	
-	public ScriptIntrospection() {
-		
-	}
+	private CompiledScript myCompiledScript;
+	private CVSVersionControl cvs;
+	private String rootPath;
 	
-	private final static void showDependencies(ArrayList<Dependency> deps, Class check)  {
-		for (int i = 0; i < deps.size(); i++) {
-			Dependency dep = deps.get(i);
-			if ( dep.getClass().getName().equals(check.getName()) ) {
-				System.err.println(check.getSimpleName() + ": " + deps.get(i).getId());
-			}
+	public ScriptIntrospection(String rootPath, String script) {
+		try {
+			this.rootPath = rootPath;
+			init(rootPath, script);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 	
-	public static void main(String [] args) throws Exception {
-		
-		String rootPath = args[0] + "/";
-		String script = args[1];
-		
+	private void initializeDispatcher(String rootPath) throws Exception {
 		URL configurationUrl = null;
 		try {
 			File f = new File(rootPath);
@@ -58,22 +56,137 @@ public class ScriptIntrospection {
 		TestDispatcher td = new TestDispatcher(nc);
 		new DispatcherFactory(td);
 		
+	}
+	
+	private void init(String rootPath, String script) throws Exception 
+	{
+		if ( DispatcherFactory.getInstance() == null ) {
+			initializeDispatcher(rootPath);
+		}
 		GenericHandler gh = new GenericHandler();
 		StringBuffer compilerErrors = new StringBuffer();
-		CompiledScript cso = gh.compileScript(new Access(), script, compilerErrors);
-		
-		ArrayList<Dependency> dependencies = cso.getDependentObjects();
+		myCompiledScript = gh.compileScript(new Access(), script, compilerErrors);
+		if ( !compilerErrors.toString().equals("") ) {
+			System.err.println(compilerErrors.toString());
+		}
+		// CVS
+		cvs = new CVSVersionControl(new File(DispatcherFactory.getInstance().getNavajoConfig().getScriptPath() + "/" + script + ".xml"));
+	}
+	
+	private final void printAllDependencies(int indent, Class specific) {
+		ArrayList<Dependency> dependencies = myCompiledScript.getDependentObjects();
+		showDependencies(indent, dependencies, specific, specific);
+	}
+	
+	private final void printAllDependencies(int indent) {
+		ArrayList<Dependency> dependencies = myCompiledScript.getDependentObjects();
 		
 		System.err.println("================ DEPENDENCIES ==================");
-		showDependencies(dependencies, InheritDependency.class);
-		showDependencies(dependencies, IncludeDependency.class);
-		showDependencies(dependencies, JavaDependency.class);
-		showDependencies(dependencies, NavajoDependency.class);
+		showDependencies(indent, dependencies, InheritDependency.class, null);
+		showDependencies(indent, dependencies, IncludeDependency.class, null);
+		showDependencies(indent, dependencies, JavaDependency.class, null);
+		showDependencies(indent, dependencies, NavajoDependency.class, null);
+		showDependencies(indent, dependencies, AdapterFieldDependency.class, null);
 		System.err.println("================================================");
+	}
+	
+	private final void printVersionInfo() {
+		if ( cvs == null ) {
+			return;
+		}
+		System.err.println("================= VERSION INFO =================");
+		System.err.println("Version: " + cvs.getVersion());
+		System.err.println("Date   : " + cvs.getDate());
+		System.err.println("Author : " + cvs.getAuthor());
+		System.err.println("Root   : " + cvs.getCvsRoot());
+		System.err.println("Repos. : " + cvs.getRepository());
+		System.err.println("================================================");
+	}
+	
+	private final String printIndent(int count) {
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < count; i++) {
+			sb.append(' ');
+		}
+		return sb.toString();
+	}
+	
+	private final void showDependencies(int indent, ArrayList<Dependency> deps, Class check, Class subCheck)  {
+		for (int i = 0; i < deps.size(); i++) {
+			Dependency dep = deps.get(i);
+			if ( dep.getClass().getName().equals(check.getName()) ) {
+				if ( check.equals(AdapterFieldDependency.class) ) {
+					System.err.println(printIndent(indent) + check.getSimpleName() + ": " + ((AdapterFieldDependency) deps.get(i)).getEvaluatedId() + " (class=" + ((AdapterFieldDependency) deps.get(i)).getJavaClass() + ", type=" + ((AdapterFieldDependency) deps.get(i)).getType() +", id=" + deps.get(i).getId() + ")");
+					// Show additional deps if type = script.
+//					if ( ((AdapterFieldDependency) deps.get(i)).getType().equals("script")) {
+//						String depScriptName = ((AdapterFieldDependency) deps.get(i)).getEvaluatedId();
+//						System.err.println(printIndent(indent) + "---> " + depScriptName);
+//						ScriptIntrospection depScript = new ScriptIntrospection(this.rootPath, depScriptName);
+//						if ( subCheck != null ) {
+//							depScript.printAllDependencies(indent+4, subCheck);
+//						} else {
+//							depScript.printAllDependencies(indent+4);
+//						}
+//					}
+				} else {
+					System.err.println(printIndent(indent) + check.getSimpleName() + ": " + deps.get(i).getId());
+				}
+			}
+		}
+	}
+	
+	public void getAllScriptDependentScripts(HashSet<String> allScripts) {
 		
-		System.err.println(compilerErrors.toString());
+		ArrayList<Dependency> deps = myCompiledScript.getDependentObjects();
+		for (int i = 0; i < deps.size(); i++) {
+			Dependency dep = deps.get(i);
+			
+			if ( dep instanceof IncludeDependency ) {
+				allScripts.add(deps.get(i).getId());
+			}
+			if ( dep instanceof AdapterFieldDependency ) {
+				String type = ((AdapterFieldDependency) deps.get(i)).getType();
+				if ( type.equals("script") )  {
+					String name = ((AdapterFieldDependency) deps.get(i)).getEvaluatedId();
+					allScripts.add(name);
+					// Check recursively.
+					ScriptIntrospection depScript = new ScriptIntrospection(this.rootPath, name);
+					depScript.getAllScriptDependentScripts(allScripts);
+				}
+			}
+		}
+		
+	}
+	
+	public static void main(String [] args) throws Exception {
+		
+		String rootPath = args[0] + "/";
+		String script = args[1];
+		
+		ScriptIntrospection si = new ScriptIntrospection(rootPath, script);
+		si.printAllDependencies(0);
+		si.printVersionInfo();		
+		HashSet<String> allScripts = new HashSet<String>();
+		si.getAllScriptDependentScripts(allScripts);
+		System.err.println("================== ALL DEPENDENT SCRIPTS ====================");
+		Iterator all = allScripts.iterator();
+		while ( all.hasNext() ) {
+			System.err.println(all.next());
+		}
+		System.err.println("=============================================================");
+		System.err.println("Original author    : " + si.getMyCompiledScript().getAuthor());
+		System.err.println("Script description : " + si.getMyCompiledScript().getDescription());
+		System.err.println("Script type        : " + si.getMyCompiledScript().getScriptType());
 		
 		System.exit(1);
 		
+	}
+
+	public CompiledScript getMyCompiledScript() {
+		return myCompiledScript;
+	}
+
+	public CVSVersionControl getCvs() {
+		return cvs;
 	}
 }
