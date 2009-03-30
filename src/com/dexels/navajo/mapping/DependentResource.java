7f70ce9c88b0573507ffa6bfacfb99662aa0f37a
Arjen Schoneveld <aschoneveld@dexels.com>
2009-03-30 08:48:01 +0000

    Comment added.

--- src/com/dexels/navajo/mapping/DependentResource.java
+++ src/com/dexels/navajo/mapping/DependentResource.java
@@ -1,5 +1,12 @@
 package com.dexels.navajo.mapping;
 
+/**
+ * Interface to define dependent resource templates.
+ * Used in HasDependentResources objects to specify what bean attributes contain dependent resources (like databases, urls, etc.)
+ * 
+ * @author arjen
+ *
+ */
 public interface DependentResource {
 
 	public String getType();
