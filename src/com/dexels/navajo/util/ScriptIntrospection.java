21e0fa8876b37b7cf5b92053312ea810408619bc
Arjen Schoneveld <aschoneveld@dexels.com>
2009-03-30 08:51:43 +0000

    Some usefull changes.

--- src/com/dexels/navajo/util/ScriptIntrospection.java
+++ src/com/dexels/navajo/util/ScriptIntrospection.java
@@ -1,9 +1,11 @@
 package com.dexels.navajo.util;
 
+import java.beans.BeanDescriptor;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.Iterator;
 
@@ -26,43 +28,49 @@ public class ScriptIntrospection {
 
 	public static final String DEFAULT_SERVER_XML = "config/server.xml";
 	
+	// Setters
+	public String script;
+	
 	private CompiledScript myCompiledScript;
 	private CVSVersionControl cvs;
 	private String rootPath;
 	
+	public ScriptIntrospection() {
+		// Empty constructor, can be used from scripts.
+	}
+	
 	public ScriptIntrospection(String rootPath, String script) {
 		try {
 			this.rootPath = rootPath;
-			init(rootPath, script);
+			initializeDispatcher(rootPath);
+			this.script = script;
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
-	private void initializeDispatcher(String rootPath) throws Exception {
-		URL configurationUrl = null;
-		try {
-			File f = new File(rootPath);
-			URL rootUrl = f.toURI().toURL();
-			configurationUrl = new URL(rootUrl, DEFAULT_SERVER_XML);
-		} catch (MalformedURLException e) {
-			throw NavajoFactory.getInstance().createNavajoException(e);
+	protected static void initializeDispatcher(String rootPath) throws Exception {
+		if ( DispatcherFactory.getInstance() == null ) {
+			URL configurationUrl = null;
+			try {
+				File f = new File(rootPath);
+				URL rootUrl = f.toURI().toURL();
+				configurationUrl = new URL(rootUrl, DEFAULT_SERVER_XML);
+			} catch (MalformedURLException e) {
+				throw NavajoFactory.getInstance().createNavajoException(e);
+			}
+
+			NavajoConfig nc = new NavajoConfig(new com.dexels.navajo.server.FileInputStreamReader(), 
+					(NavajoClassSupplier) null,
+					configurationUrl.openStream(), rootPath); 
+			TestDispatcher td = new TestDispatcher(nc);
+			new DispatcherFactory(td);
 		}
-		
-		NavajoConfig nc = new NavajoConfig(new com.dexels.navajo.server.FileInputStreamReader(), 
-				                           (NavajoClassSupplier) null,
-				                           configurationUrl.openStream(), rootPath); 
-		TestDispatcher td = new TestDispatcher(nc);
-		new DispatcherFactory(td);
-		
+
 	}
 	
-	private void init(String rootPath, String script) throws Exception 
-	{
-		if ( DispatcherFactory.getInstance() == null ) {
-			initializeDispatcher(rootPath);
-		}
+	private void initializeScript(String script) throws Exception {
 		GenericHandler gh = new GenericHandler();
 		StringBuffer compilerErrors = new StringBuffer();
 		myCompiledScript = gh.compileScript(new Access(), script, compilerErrors);
@@ -78,8 +86,8 @@ public class ScriptIntrospection {
 		showDependencies(indent, dependencies, specific, specific);
 	}
 	
-	private final void printAllDependencies(int indent) {
-		ArrayList<Dependency> dependencies = myCompiledScript.getDependentObjects();
+	private final void printAllDependencies(int indent) throws Exception {
+		ArrayList<Dependency> dependencies = getCompiledScript().getDependentObjects();
 		
 		System.err.println("================ DEPENDENCIES ==================");
 		showDependencies(indent, dependencies, InheritDependency.class, null);
@@ -116,18 +124,10 @@ public class ScriptIntrospection {
 			Dependency dep = deps.get(i);
 			if ( dep.getClass().getName().equals(check.getName()) ) {
 				if ( check.equals(AdapterFieldDependency.class) ) {
-					System.err.println(printIndent(indent) + check.getSimpleName() + ": " + ((AdapterFieldDependency) deps.get(i)).getEvaluatedId() + " (class=" + ((AdapterFieldDependency) deps.get(i)).getJavaClass() + ", type=" + ((AdapterFieldDependency) deps.get(i)).getType() +", id=" + deps.get(i).getId() + ")");
-					// Show additional deps if type = script.
-//					if ( ((AdapterFieldDependency) deps.get(i)).getType().equals("script")) {
-//						String depScriptName = ((AdapterFieldDependency) deps.get(i)).getEvaluatedId();
-//						System.err.println(printIndent(indent) + "---> " + depScriptName);
-//						ScriptIntrospection depScript = new ScriptIntrospection(this.rootPath, depScriptName);
-//						if ( subCheck != null ) {
-//							depScript.printAllDependencies(indent+4, subCheck);
-//						} else {
-//							depScript.printAllDependencies(indent+4);
-//						}
-//					}
+					System.err.println(printIndent(indent) + check.getSimpleName() + ": " + 
+							((AdapterFieldDependency) deps.get(i)).getEvaluatedId() + " (class=" + 
+							((AdapterFieldDependency) deps.get(i)).getJavaClass() + ", type=" + 
+							((AdapterFieldDependency) deps.get(i)).getType() +", id=" + deps.get(i).getId() + ")");
 				} else {
 					System.err.println(printIndent(indent) + check.getSimpleName() + ": " + deps.get(i).getId());
 				}
@@ -135,9 +135,9 @@ public class ScriptIntrospection {
 		}
 	}
 	
-	public void getAllScriptDependentScripts(HashSet<String> allScripts) {
+	public void getAllScriptDependentScripts(HashSet<String> allScripts) throws Exception {
 		
-		ArrayList<Dependency> deps = myCompiledScript.getDependentObjects();
+		ArrayList<Dependency> deps = getCompiledScript().getDependentObjects();
 		for (int i = 0; i < deps.size(); i++) {
 			Dependency dep = deps.get(i);
 			
@@ -174,19 +174,30 @@ public class ScriptIntrospection {
 			System.err.println(all.next());
 		}
 		System.err.println("=============================================================");
-		System.err.println("Original author    : " + si.getMyCompiledScript().getAuthor());
-		System.err.println("Script description : " + si.getMyCompiledScript().getDescription());
-		System.err.println("Script type        : " + si.getMyCompiledScript().getScriptType());
+		System.err.println("Original author    : " + si.getCompiledScript().getAuthor());
+		System.err.println("Script description : " + si.getCompiledScript().getDescription());
+		System.err.println("Script type        : " + si.getCompiledScript().getScriptType());
+		
 		
 		System.exit(1);
 		
 	}
 
-	public CompiledScript getMyCompiledScript() {
+	public CompiledScript getCompiledScript() throws Exception {
+		if ( myCompiledScript == null ) {
+			initializeScript(script);
+		}
 		return myCompiledScript;
 	}
 
-	public CVSVersionControl getCvs() {
+	public CVSVersionControl getCvs() throws Exception {
+		if ( myCompiledScript == null ) {
+			initializeScript(script);
+		}
 		return cvs;
+	}
+
+	public void setScript(String script) {
+		this.script = script;
 	}
 }
