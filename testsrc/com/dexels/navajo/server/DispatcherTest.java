239350c350ad91d98f38cf05034fc0e63586dc86
Arjen Schoneveld <aschoneveld@dexels.com>
2009-04-02 15:50:11 +0000

    *** empty log message ***

--- testsrc/com/dexels/navajo/server/DispatcherTest.java
+++ testsrc/com/dexels/navajo/server/DispatcherTest.java
@@ -1,6 +1,7 @@
 package com.dexels.navajo.server;
 
 import com.dexels.navajo.document.Header;
+import com.dexels.navajo.document.Message;
 import com.dexels.navajo.document.Navajo;
 import com.dexels.navajo.document.NavajoFactory;
 import com.dexels.navajo.server.test.TestNavajoConfig;
@@ -28,6 +29,22 @@ public class DispatcherTest extends TestCase {
 		Assert.assertNotNull(r);
 		Assert.assertNotNull(r.getMessage("ping"));
 		
+	}
+	
+	public void testRemoveSpecialMessages() throws Exception {
+		Navajo doc = NavajoFactory.getInstance().createNavajo();
+		Message globals = NavajoFactory.getInstance().createMessage(doc, "__globals__");
+		Message parms = NavajoFactory.getInstance().createMessage(doc, "__parms__");
+		Message something = NavajoFactory.getInstance().createMessage(doc, "something");
+		doc.addMessage(globals);
+		doc.addMessage(something);
+		doc.addMessage(parms);
+		assertNotNull(doc.getMessage("__parms__"));
+		assertNotNull(doc.getMessage("__globals__"));
+		Navajo r = DispatcherFactory.getInstance().removeInternalMessages(doc);
+		assertNull(r.getMessage("__parms__"));
+		assertNull(r.getMessage("__globals__"));
+		assertNotNull(r.getMessage("something"));
 	}
 
 }
