2627231193bd925894a5cba242d2eeff0f1cae06
Arjen Schoneveld <aschoneveld@dexels.com>
2009-04-08 13:47:19 +0000

    Added comments

--- src/com/dexels/navajo/mapping/compiler/meta/Dependency.java
+++ src/com/dexels/navajo/mapping/compiler/meta/Dependency.java
@@ -1,6 +1,22 @@
 package com.dexels.navajo.mapping.compiler.meta;
 
 
+/**
+ * Abstract class that is used for modelling Dependent artefacts in a script. 
+ * A Dependency object is user wherever either one of the following situations hold:
+ * 1. The script needs to be recompiled whenever a dependent artefacts is 'dirty'.
+ * 2. To keep track of important dependent resources like database, mail servers etc.
+ * 
+ * Currently the following Dependency classes are defined:
+ * - AdapterFieldDependency. To indicate a dependency on a specific field of an adapter. An adapter that contains dependent fields 
+ *   should implement the HasDependentResource interface.
+ * - IncludeDependency. To indicate a dependency on a script that is 'included' via the <include> tag.
+ * - InheritDependency. To indicate a dependency on a script that is 'inherited' via the <inject> tag.
+ * - JavaDependency. To indicate a dependency on a Java object/adapter.
+ *   
+ * @author arjen
+ *
+ */
 public abstract class Dependency {
 
 	private long timestamp;
