da9cdf07546d31867478a3e06337840edb9c8282
Frank Lyaruu <frank@dexels.com>
2009-04-24 15:32:24 +0000

    *** empty log message ***

--- src/tipi/TipiFlickrExtension.java
+++ src/tipi/TipiFlickrExtension.java
@@ -1,66 +1,19 @@
 package tipi;
 
-import java.util.*;
+import com.dexels.navajo.tipi.TipiContext;
 
-import com.dexels.navajo.tipi.*;
+	public class TipiFlickrExtension extends TipiAbstractXMLExtension  {
 
-public class TipiFlickrExtension extends AbstractTipiExtension implements TipiExtension {
+		public TipiFlickrExtension() {
+			loadXML();
+		}
 
-	public void initialize(TipiContext tc) {
-		// Do nothing
-		
-	}
+		public void initialize(TipiContext tc) {
+			// Do nothing
 
-	public String getDescription() {
-		return "Flickr extensions";
-	}
-
-	public String[] getIncludes() {
-		return new String[]{"com/dexels/navajo/tipi/flickr/flickrclassdef.xml"};
-	}
-
-	public boolean isMainImplementation() {
-		return false;
-	}
-	public String getId() {
-		return "flickr";
-	}
+		}
 
 
-	public String requiresMainImplementation() {
-		return null;
-	}
 
-	public List<String> getLibraryJars() {
-		List<String> l = new LinkedList<String>();
-		l.add("flickrapi-1.0b4.jar");
-		return l;
-	}
-
-	public List<String> getMainJars() {
-		List<String> l = new LinkedList<String>();
-		l.add("TipiFlickr.jar");
-		return l;
-	} 
-	public String getConnectorId() {
-		return "flickr";
-	}
-
-	public List<String> getRequiredExtensions() {
-		return null;
-	}
-	public String getProjectName() {
-		return "TipiFlickr";
-	}
-
-	public String getDeploymentDescriptor() {
-		return "TipiFlickr.jnlp";
-	}
-
-	public List<String> getDependingProjectUrls() {
-		List<String> l = new LinkedList<String>();
-		l.add("http://flickrj.sourceforge.net/");
-		return l;
 
 	}
-}
