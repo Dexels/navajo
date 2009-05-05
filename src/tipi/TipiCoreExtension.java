47b29c2550eb667e91bf845a85c362d2226e0250
Frank Lyaruu <frank@dexels.com>
2009-05-05 15:12:38 +0000

    clean up

--- src/tipi/TipiCoreExtension.java
+++ src/tipi/TipiCoreExtension.java
@@ -5,11 +5,8 @@ import com.dexels.navajo.tipi.TipiContext;
 public class TipiCoreExtension extends TipiAbstractXMLExtension  {
 
 	public TipiCoreExtension() {
-		// bit of
 		super();
-		System.err.println("Loading core xml");
 		loadXML("TipiExtension.xml");
-		System.err.println("Core xml loaded!");
 	}
 
 	public void initialize(TipiContext tc) {
