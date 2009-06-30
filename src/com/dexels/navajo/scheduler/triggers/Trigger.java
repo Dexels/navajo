5ddd724ab09cea191cb738d11d10f1abb84266a6
Arjen Schoneveld <aschoneveld@dexels.com>
2009-06-30 14:55:10 +0000

    *** empty log message ***

--- src/com/dexels/navajo/scheduler/triggers/Trigger.java
+++ src/com/dexels/navajo/scheduler/triggers/Trigger.java
@@ -30,7 +30,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.lang.reflect.Constructor;
-import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Iterator;
 
