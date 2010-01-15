b98261e9d6d664e7cc56edf6e6b416fc1937f8a0
Frank Lyaruu <frank@dexels.com>
2010-01-15 20:11:19 +0000

    Split from TipiSwing, to reduce size

---
+++ src/tipi/TipiSwingDockingExtension.java
@@ -0,0 +1,27 @@
+package tipi;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.*;
+
+import com.dexels.navajo.tipi.*;
+import com.dexels.navajo.tipi.tipixml.CaseSensitiveXMLElement;
+import com.dexels.navajo.tipi.tipixml.XMLElement;
+import com.dexels.navajo.tipi.tipixml.XMLParseException;
+
+public class TipiSwingDockingExtension extends TipiAbstractXMLExtension implements TipiExtension {
+
+	public TipiSwingDockingExtension() throws XMLParseException,
+			IOException {
+		loadXML();
+	}
+
+	public void initialize(TipiContext tc) {
+		// Do nothing
+		
+	}
+
+	
+
+}
