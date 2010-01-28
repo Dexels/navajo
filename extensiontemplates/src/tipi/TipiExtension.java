71fe7967de47426f5ee70e2561c76764af37a6c4
Frank Lyaruu <frank@dexels.com>
2010-01-28 16:15:09 +0000

    *** empty log message ***

---
+++ extensiontemplates/src/tipi/TipiExtension.java
@@ -0,0 +1,17 @@
+package tipi;
+
+import java.util.*;
+
+import com.dexels.navajo.tipi.*;
+
+public class @extensionName@Extension extends TipiAbstractXMLExtension implements TipiExtension {
+
+	public @extensionName@Extension() {
+		loadXML();
+	}
+
+	public void initialize(TipiContext tc) {
+		// Do nothing
+	}
+}
+
