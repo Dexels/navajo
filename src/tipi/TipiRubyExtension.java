af35cb44fcfae7166eff2895090d8e7f0e6b9304
Frank Lyaruu <frank@dexels.com>
2010-01-28 16:13:08 +0000

    *** empty log message ***

---
+++ src/tipi/TipiRubyExtension.java
@@ -0,0 +1,17 @@
+package tipi;
+
+import java.util.*;
+
+import com.dexels.navajo.tipi.*;
+
+public class TipiRubyExtension extends TipiAbstractXMLExtension implements TipiExtension {
+
+	public TipiRubyExtension() {
+		loadXML();
+	}
+
+	public void initialize(TipiContext tc) {
+		// Do nothing
+	}
+}
+
