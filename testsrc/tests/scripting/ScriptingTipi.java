f28e6b3fc35f4a096fb64ac924b1024c0f61c8ab
Frank Lyaruu <frank@dexels.com>
2010-02-11 11:50:30 +0000

    test case for scripting

---
+++ testsrc/tests/scripting/ScriptingTipi.java
@@ -0,0 +1,31 @@
+package tests.scripting;
+
+import java.io.*;
+
+import com.dexels.navajo.tipi.testimpl.*;
+
+
+public class ScriptingTipi extends AbstractTipiTest {
+	public ScriptingTipi() {
+		super("testTipi");
+	}
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+		setContext("init", new File("testsrc/tests/scripting"));
+		System.err.println("Setup complete");
+	}
+
+	public void testTipi() {
+//		try {
+//			Thread.sleep(100);
+//		} catch (InterruptedException e) {
+//		}
+		getContext().shutdown();
+		String xx = getContext().getInfoBuffer();
+//		assertEquals("event1\nevent2\n0.99\n",xx);
+
+	}
+
+
+}
