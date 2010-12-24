43acbd15a1ea9a4164e48a9d5d6c4ded9234e50f
Frank Lyaruu <frank@dexels.com>
2010-12-24 09:39:51 +0000

    *** empty log message ***

---
+++ src/navajoasyncclient/Version.java
@@ -0,0 +1,29 @@
+package navajoasyncclient;
+
+import org.osgi.framework.BundleContext;
+
+public class Version extends dexels.Version {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bc) throws Exception {
+		super.start(bc);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		Version.context = null;
+	}
+
+}
