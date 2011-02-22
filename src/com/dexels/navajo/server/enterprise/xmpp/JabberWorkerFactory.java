9e534d1008f2b69d07024d2e2143ee4538e3fe5d
Arjen Schoneveld <aschoneveld@dexels.com>
2011-02-22 08:47:22 +0000

    Load Jabber configuration from jabber.xml config

--- src/com/dexels/navajo/server/enterprise/xmpp/JabberWorkerFactory.java
+++ src/com/dexels/navajo/server/enterprise/xmpp/JabberWorkerFactory.java
@@ -3,6 +3,8 @@ package com.dexels.navajo.server.enterprise.xmpp;
 import java.lang.reflect.Method;
 import java.util.logging.Level;
 
+import com.dexels.navajo.document.Navajo;
+import com.dexels.navajo.server.DispatcherFactory;
 import com.dexels.navajo.util.AuditLog;
 
 public class JabberWorkerFactory {
@@ -24,6 +26,19 @@ public class JabberWorkerFactory {
 						JabberWorkerInterface dummy = (JabberWorkerInterface) c.newInstance();
 						Method m = c.getMethod("getInstance", (Class[])null);
 						instance = (JabberWorkerInterface) m.invoke(dummy, (Object[])null);
+						// Set Jabber parameters.
+						Navajo jabber = DispatcherFactory.getInstance().getNavajoConfig().readConfig("jabber.xml");
+						if ( jabber != null ) {
+							String jabberServer = jabber.getProperty("/Jabber/Server").getValue();
+							String jabberPort = jabber.getProperty("/Jabber/Port").getValue();
+							String jabberService = jabber.getProperty("/Jabber/Service").getValue();
+							String bootstrapUrl = jabber.getProperty("/Jabber/URL").getValue();
+							System.err.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>> jabberServer = " + jabberServer);
+							instance.configJabber(jabberServer, jabberPort, jabberService, bootstrapUrl);
+						} else {
+							AuditLog.log("INIT", "WARNING: Missing Jabber configuration", Level.WARNING);
+							instance = new DummyJabberWorker();
+						}
 					} catch (Throwable e) {
 						//e.printStackTrace(System.err);
 						AuditLog.log("INIT", "WARNING: Jabber Worker not available", Level.WARNING);
