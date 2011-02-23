ff75dd8583e2791e523f0c8e330e94ab39e9f7f4
Frank Lyaruu <frank@dexels.com>
2011-02-23 17:10:25 +0000

    Unified code from InstallerContext

--- src/com/dexels/navajo/server/listener/NavajoContextListener.java
+++ src/com/dexels/navajo/server/listener/NavajoContextListener.java
@@ -7,19 +7,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-import com.dexels.navajo.document.Header;
-import com.dexels.navajo.document.Navajo;
 import com.dexels.navajo.document.NavajoException;
-import com.dexels.navajo.document.NavajoFactory;
 import com.dexels.navajo.server.Dispatcher;
 import com.dexels.navajo.server.DispatcherFactory;
 import com.dexels.navajo.server.DispatcherInterface;
@@ -47,62 +42,67 @@ public class NavajoContextListener implements ServletContextListener {
 		System.err.println("==========================================================");
 		System.err.println("INITIALIZING NAVAJO INSTANCE: " +  sc.getServletContext().getContextPath());
 		System.err.println("==========================================================");
-		String configurationPath = null;
-		String rootPath = null;
-				String path;
-				try {
-					path = setupConfigurationPath(sc.getServletContext());
-				} catch (IOException e) {
-					path = null;
-					e.printStackTrace();
-				}
-				if(path!=null) {
-					System.err.println("Path found. Using: "+path);
-					File f = new File(path);
-					configurationPath = path;
-				}
-			// Check whether defined bootstrap webservice is present.
-			System.setProperty(DOC_IMPL,QDSAX);
-			System.err.println("Configuration path: "+configurationPath);
+		
+		if(!isValidInstallationForContext(sc.getServletContext())) {
+			System.err.println("No valid installation found, abandoning Context initialization.");
+			return;
+		}
 
-			boolean verified = false;
-
-			URL configUrl;
-			InputStream is = null;
-			try {
-				configUrl = new URL(configurationPath);
-				is = configUrl.openStream();
-				verified = true;
-			} catch (MalformedURLException e) {
-				//e.printStackTrace(System.err);
-			} catch (IOException e) {
-			} finally {
-				if(is!=null) {
-					try {
-						is.close();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-			if( configurationPath==null || "".equals(configurationPath)|| !verified) {
-				rootPath = sc.getServletContext().getRealPath("");
-			}
-			System.err.println("Resolved Configuration path: "+configurationPath);
-			System.err.println("Resolved Root path: "+rootPath);
-
-			// Startup Navajo instance.
-			try {
-				DispatcherInterface d = initDispatcher(sc.getServletContext(),rootPath,configurationPath);
-//				Navajo n = NavajoFactory.getInstance().createNavajo();
-
-			} catch (Exception e) {
-				e.printStackTrace(System.err);
-			}
+		initializeContext(sc.getServletContext());
 
 		
 		}
-	
+
+	// Should be called after installation, so the context will still be initialized.
+	public void initializeContext(ServletContext sc) {
+		String configurationPath = null;
+		String rootPath = null;
+
+		String path;
+		path = getInstallationPath(sc);
+		if (path != null) {
+			System.err.println("Path found. Using: " + path);
+			configurationPath = path;
+		}
+		// Check whether defined bootstrap webservice is present.
+		System.setProperty(DOC_IMPL, QDSAX);
+		System.err.println("Configuration path: " + configurationPath);
+
+		boolean verified = false;
+
+		URL configUrl;
+		InputStream is = null;
+		try {
+			configUrl = new URL(configurationPath);
+			is = configUrl.openStream();
+			verified = true;
+		} catch (MalformedURLException e) {
+			// e.printStackTrace(System.err);
+		} catch (IOException e) {
+		} finally {
+			if (is != null) {
+				try {
+					is.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		if (configurationPath == null || "".equals(configurationPath)
+				|| !verified) {
+			rootPath = sc.getRealPath("");
+		}
+		System.err.println("Resolved Configuration path: " + configurationPath);
+		System.err.println("Resolved Root path: " + rootPath);
+
+		// Startup Navajo instance.
+		try {
+			initDispatcher(sc, rootPath, configurationPath);
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+		}
+	}
+
 	protected final DispatcherInterface initDispatcher(ServletContext sc, String rootPath, String configurationPath) throws NavajoException {
 
 		String servletContextRootPath = sc.getRealPath("");
@@ -116,28 +116,124 @@ public class NavajoContextListener implements ServletContextListener {
 
 	}
 		
-	private String setupConfigurationPath(ServletContext context) throws IOException {
-		String contextName =  context.getContextPath().substring(1);
-		String navajoPath = getSystemPath(context,contextName);
-		return navajoPath;
-	}
+//	private String setupConfigurationPath(ServletContext context) throws IOException {
+//		String navajoPath = getSystemPath(context);
+//		return navajoPath;
+//	}
 		
-	private String getSystemPath(ServletContext context, String name) throws IOException {
+//	private String getSystemPath(ServletContext context) throws IOException {
+//		String name =  context.getContextPath().substring(1);
+//
+//		String force = context.getInitParameter("forcedNavajoPath");
+//		System.err.println("Force: "+force);
+//		if(force!=null) {
+////			System.err.println("Using the force! navajo.properties will be ignored!");
+//			return force;
+//		}
+//		Map<String,String> systemContexts = new HashMap<String, String>();
+//		File home = new File(System.getProperty("user.home"));
+//		File navajo = new File(home,"navajo.properties");
+//		System.err.println("Assuming navajo path: "+navajo.getAbsolutePath());
+//		if(!navajo.exists()) {
+//			return null;
+//		}
+//		
+//		BufferedReader br = new BufferedReader(new FileReader(navajo));
+//		while(true) {
+//			String line = br.readLine();
+//			if(line==null) {
+//				break;
+//			}
+//			String[] r = line.split("=");
+//			systemContexts.put(r[0], r[1]);
+//		}
+//		br.close();
+//		System.err.println("Maps: "+systemContexts);
+//		// So the path is not forced, and the navajo.properties file exists.
+//		// check the com.dexels.navajo.server.EngineInstance property
+//		
+//		String engineInstance = System.getProperty("com.dexels.navajo.server.EngineInstance");
+//		if(engineInstance!=null) {
+//			String engineQualifiedContext = systemContexts.get(engineInstance+"/"+name);
+//			if(engineQualifiedContext==null) {
+//				System.err.println("Warning: com.dexels.navajo.server.EngineInstance set (to: "+engineInstance+"), but no such context found.");
+//				System.err.println("Available contexts: "+systemContexts);
+//				// ignore engineInstance
+//			} else {
+//				return engineQualifiedContext;
+//			}
+//			
+//		}
+//		return systemContexts.get(name);
+//	}
+//
+//	
+	
+	public boolean isValidInstallationForContext(ServletContext context) {
+		String installPath = getInstallationPath(context);
 
-		String force = context.getInitParameter("forcedNavajoPath");
-		System.err.println("Force: "+force);
-		if(force!=null) {
-//			System.err.println("Using the force! navajo.properties will be ignored!");
-			return force;
+		if(installPath==null) {
+			return false;
 		}
-		Map<String,String> systemContexts = new HashMap<String, String>();
+		File f = new File(installPath);
+		if(!f.exists()) {
+			return false;
+		}
+		File scripts = new File(f,"scripts");
+		if(!scripts.exists()) {
+			return false;
+		}
+		File config = new File(f,"config");
+		if(!config.exists()) {
+			return false;
+		}
+		return config!=null;
+	}
+
+	
+	public String getInstallationPath(ServletContext context) {
+		String force = context.getInitParameter("forcedNavajoPath");
+		if(force!=null) {
+			return force;
+		} else {
+			try {
+				String contextPath = context.getContextPath().substring(1);
+				Map<String,String> systemContexts = loadSystemContexts();
+				return getInstallationPath(systemContexts, contextPath);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+	
+//	private String initialize(String contextPath) throws IOException {
+//		Map<String,String> systemContexts = loadSystemContexts();
+//		return getInstallationPath(systemContexts,contextPath);
+//	}
+
+	private String getInstallationPath(Map<String,String> systemContexts,String contextPath) {
+		String engineInstance = System.getProperty("com.dexels.navajo.server.EngineInstance");
+		String key = contextPath;
+		if(engineInstance!=null) {
+			key = engineInstance + "/"+contextPath;
+		}
+		String result = systemContexts.get(key);
+		if(result!=null) {
+			return result;
+		}
+		return systemContexts.get(contextPath);
+	}
+	
+	private Map<String,String> loadSystemContexts() throws IOException {
 		File home = new File(System.getProperty("user.home"));
 		File navajo = new File(home,"navajo.properties");
-		System.err.println("Assuming navajo path: "+navajo.getAbsolutePath());
 		if(!navajo.exists()) {
 			return null;
 		}
+		Map<String,String> systemContexts = new HashMap<String, String>();
 		BufferedReader br = new BufferedReader(new FileReader(navajo));
+		
 		while(true) {
 			String line = br.readLine();
 			if(line==null) {
@@ -146,11 +242,10 @@ public class NavajoContextListener implements ServletContextListener {
 			String[] r = line.split("=");
 			systemContexts.put(r[0], r[1]);
 		}
+		
 		br.close();
-		System.err.println("Maps: "+systemContexts);
-		return systemContexts.get(name);
+		return systemContexts;
 	}
-		
-		
+	
 
 }
