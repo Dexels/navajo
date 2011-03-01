0108f01e05b37ad3411e2936b2d463d0b7334f04
Frank Lyaruu <frank@dexels.com>
2011-03-01 13:43:33 +0000

    Implement these classes to use with a NavajoFilterServlet

---
+++ src/com/dexels/navajo/server/listener/http/wrapper/NavajoResponseWrapper.java
@@ -0,0 +1,23 @@
+package com.dexels.navajo.server.listener.http.wrapper;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.dexels.navajo.document.Navajo;
+import com.dexels.navajo.server.listener.http.wrapper.impl.NavajoServletRequestWrapper;
+import com.dexels.navajo.server.listener.http.wrapper.impl.NavajoServletResponseWrapper;
+
+public interface NavajoResponseWrapper {
+	/**
+	 * @param originalRequest
+	 * @param wrappedRequest could be null, if no inputFilter has been supplied
+	 * @param wrappedResponse could be null, if no outputFilter has been supplied
+	 * @param originalResponse
+	 * @throws IOException 
+	 */
+	public void processResponse(HttpServletRequest originalRequest, Navajo indoc, Navajo outdoc ,
+			HttpServletResponse originalResponse) throws IOException;
+
+}
