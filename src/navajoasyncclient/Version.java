753bdf57b2cfe8883fd9e719565484cd59176a78
Frank Lyaruu <frank@dexels.com>
2011-03-03 10:21:14 +0000

    upgraded to OSGi based version

--- src/navajoasyncclient/Version.java
+++ src/navajoasyncclient/Version.java
@@ -1,29 +1,16 @@
 package navajoasyncclient;
 
-import org.osgi.framework.BundleContext;
+
 
 public class Version extends dexels.Version {
 
-	private static BundleContext context;
-
-	static BundleContext getContext() {
-		return context;
+		
+	//	 Included packages.
+	
+	public Version() {
+//		javax.mail.Address a;
+//		setReleaseDate(RELEASEDATE);
 	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext bc) throws Exception {
-		super.start(bc);
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext bundleContext) throws Exception {
-		Version.context = null;
-	}
+	
 
 }
