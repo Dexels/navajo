87b9e7804b7d0b41f3eb0d468da552d428ecaa6b
Frank Lyaruu <frank@dexels.com>
2011-03-10 11:41:53 +0000

    minor refactor in preparation to OSGi enabled version

--- src/com/dexels/navajo/server/enterprise/tribe/TribeManagerFactory.java
+++ src/com/dexels/navajo/server/enterprise/tribe/TribeManagerFactory.java
@@ -15,7 +15,6 @@ public class TribeManagerFactory {
 		instance = new DummyTribeManager();
 	}
 	
-	@SuppressWarnings("unchecked")
 	public static TribeManagerInterface getInstance() {
 		
 		if ( instance != null ) {
@@ -23,32 +22,43 @@ public class TribeManagerFactory {
 		} else {
 
 			synchronized (semaphore) {
-				
 				if ( instance == null ) {
-					try {
-						Class c = Class.forName("com.dexels.navajo.tribe.TribeManager");
-						TribeManagerInterface dummy = (TribeManagerInterface) c.newInstance();
-						Method m = c.getMethod("getInstance", (Class[]) null);
-						instance = (TribeManagerInterface) m.invoke(dummy, (Object[])null);
-						tribeManagerFound = true;
-					} catch (Throwable e) {
-						e.printStackTrace(System.err);
-						AuditLog.log("INIT", "WARNING: Tribe Manager not available", Level.WARNING);
-						instance = new DummyTribeManager();
-					}	
-				}
 				
+					instance = getTribeManagerService();
+					if(instance==null || instance instanceof DummyTribeManager) {
+						tribeManagerFound = false;
+					} else {
+						tribeManagerFound = true;
+					}
+				}
 				return instance;
 			}
 		}
 		
+	}
+
+	@SuppressWarnings("unchecked")
+	public static TribeManagerInterface getTribeManagerService() {
+			try {
+				/**
+				 * These constructions will never work in an OSGi-like environment.
+				 * TODO rewrite 
+				 */
+				Class<? extends TribeManagerInterface> c = (Class<? extends TribeManagerInterface>) Class.forName("com.dexels.navajo.tribe.TribeManager");
+				TribeManagerInterface dummy = c.newInstance();
+				Method m = c.getMethod("getInstance", (Class[]) null);
+				return (TribeManagerInterface) m.invoke(dummy, (Object[])null);
+			} catch (Throwable e) {
+				AuditLog.log("INIT", "WARNING: Tribe Manager not available", Level.WARNING);
+				 return new DummyTribeManager();
+			}	
 	}
 	
 	public static void startStatusCollector() {
 		if ( instance != null && tribeManagerFound ) {
 			// Startup tribal status collector.
 			try {
-				Class<?> c = (Class<?>) Class.forName("com.dexels.navajo.tribe.TribalStatusCollector");
+				Class<?> c = Class.forName("com.dexels.navajo.tribe.TribalStatusCollector");
 				Object dummy = c.newInstance();
 				Method m = c.getMethod("getInstance",(Class[]) null);
 				m.invoke(dummy, (Object[])null);
