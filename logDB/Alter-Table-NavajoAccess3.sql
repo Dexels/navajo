e493bdb22a660796165bdf46508cd77d2740779c
Arjen Schoneveld <aschoneveld@dexels.com>
2011-03-14 09:46:40 +0000

    *** empty log message ***

--- logDB/Alter-Table-NavajoAccess3.sql
+++ logDB/Alter-Table-NavajoAccess3.sql
@@ -2,5 +2,9 @@ ALTER TABLE navajoaccess ADD queuetime NUMBER
 /
 ALTER TABLE navajoaccess ADD processingtime NUMBER 
 /
+ALTER TABLE navajoaccess ADD queueid VARCHAR2(256)
+/
+ALTER TABLE navajoaccess ADD queuelength NUMBER
+/
 
 QUIT
