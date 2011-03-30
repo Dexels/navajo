a32eabac9c596a344915105dc5cd55ad0b63dc09
Frank Lyaruu <frank@dexels.com>
2011-03-30 21:08:39 +0000

    refactor because of suspected trouble with Tycho

---
+++ src/com/dexels/navajo/version/Version.java
@@ -0,0 +1,68 @@
+package com.dexels.navajo.version;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+
+import dexels.BaseVersion;
+
+public class Version extends BaseVersion implements BundleActivator {
+
+	protected BundleContext context = null;
+	public void start(BundleContext bc) throws Exception {
+		context = bc;
+		System.err.println("Bundle started: "+bc.getBundle().toString());
+	}
+
+	public void stop(BundleContext arg0) throws Exception {
+		context = null;
+		
+	}
+
+	@Override
+	public int getMajor() {
+		return context.getBundle().getVersion().getMajor();
+	}
+
+	@Override
+	public int getMinor() {
+		return context.getBundle().getVersion().getMajor();
+	}
+
+	@Override
+	public int getPatchLevel() {
+		return context.getBundle().getVersion().getMicro();
+	}
+
+	@Override
+	public String getVendor() {
+		return "Dexels";
+	}
+
+	@Override
+	public String getProductName() {
+		return context.getBundle().getSymbolicName();
+	}
+
+	public void shutdown() {
+		if(context==null) {
+			System.err.println("No OSGi present.");
+		}
+		System.err.println("Shutting down bundle: "+getClass().getName());
+	}
+	
+
+	  @SuppressWarnings("unchecked")
+	public static void shutdownNavajoExtension(String name) {
+		  // This should be replaced by OSGi bundle management
+		  try {
+			Class <? extends Version> version = (Class<? extends Version>) Class.forName(name.toLowerCase()+".Version");
+			Version v = version.newInstance();
+			v.shutdown();
+		  } catch (Throwable e) {
+			  System.err.println("Extension shutdown failed.");
+		}
+		  
+	}
+
+}
