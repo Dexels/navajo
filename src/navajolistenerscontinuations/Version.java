989756c8b95318de0a041de9cbf4a2b1526ee825
Frank Lyaruu <frank@dexels.com>
2011-03-30 21:48:12 +0000

    refactor because of suspected trouble with Tycho

--- src/navajolistenerscontinuations/Version.java
+++ src/navajolistenerscontinuations/Version.java
@@ -26,7 +26,7 @@ package navajolistenerscontinuations;
 
 
 
-public class Version extends dexels.Version {
+public class Version extends com.dexels.navajo.version.Version {
 
 		
 	//	 Included packages.
