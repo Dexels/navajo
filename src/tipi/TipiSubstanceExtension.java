6be0867f3f5d7be87359df90fe7c7448953855b8
Frank Lyaruu <frank@dexels.com>
2011-04-05 07:02:25 +0000

    *** empty log message ***

--- src/tipi/TipiSubstanceExtension.java
+++ src/tipi/TipiSubstanceExtension.java
@@ -4,66 +4,28 @@ import java.util.*;
 
 import com.dexels.navajo.tipi.*;
 
-public class TipiSubstanceExtension extends AbstractTipiExtension implements TipiExtension {
+public class TipiSubstanceExtension extends TipiAbstractXMLExtension implements TipiExtension {
 
-	public void initialize(TipiContext tc) {
-		// Do nothing
- 
-	}
+//	public void initialize(TipiContext tc) {
+//		// Do nothing
+// 
+//	}
 
-	public String getDescription() {
-		return "Substance extensions";
-	}
 
-	public String[] getIncludes() {
-		return new String[] { "com/dexels/navajo/tipi/swing/substance/substanceclassdef.xml" };
-	}
+//
+//
+//	public List<String> getLibraryJars() {
+//		ArrayList<String> jars = new ArrayList<String>();
+//		jars.add("substance.jar");
+//		return jars;
+//	}
+//
+//	public List<String> getMainJars() {
+//		ArrayList<String> jars = new ArrayList<String>();
+//		jars.add("TipiSubstance.jar");
+//		return jars;
+//	}
 
-	public boolean isMainImplementation() {
-		return false;
-	}
 
-	public String getId() {
-		return "substance";
-	}
-
-	public String requiresMainImplementation() {
-		return "swing";
-	}
-
-	public List<String> getLibraryJars() {
-		ArrayList<String> jars = new ArrayList<String>();
-		jars.add("substance.jar");
-		return jars;
-	}
-
-	public List<String> getMainJars() {
-		ArrayList<String> jars = new ArrayList<String>();
-		jars.add("TipiSubstance.jar");
-		return jars;
-	}
-
-	public String getConnectorId() {
-		return null;
-	}
-
-	public List<String> getRequiredExtensions() {
-
-		return null;
-	}
-
-	public String getProjectName() {
-		return "TipiSubstance";
-	}
-
-	public String getDeploymentDescriptor() {
-		return "TipiSubstance.jnlp";
-	}
-
-	public List<String> getDependingProjectUrls() {
-		List<String> l = new LinkedList<String>();
-		l.add("https://substance.dev.java.net/");
-		return l;
-	}
 
 }
