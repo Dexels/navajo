c46bdd275fdcc9514a1069ca70cf797adba68998
Frank Lyaruu <frank@dexels.com>
2011-04-07 22:20:51 +0000

    added support for Adapters

--- src/com/dexels/navajo/functions/util/OsgiFunctionFactory.java
+++ src/com/dexels/navajo/functions/util/OsgiFunctionFactory.java
@@ -14,19 +14,42 @@ import org.osgi.framework.*;
 public class OsgiFunctionFactory extends JarFunctionFactory {
 
 
+	public FunctionInterface getInstance(final ClassLoader cl, final String functionName) throws TMLExpressionException  {
+		FunctionInterface osgiResolution = (FunctionInterface) getComponent(functionName, "functionName", FunctionInterface.class);
+		if (osgiResolution==null) {
+			System.err.println("OSGi failed. Going old skool");
+			return super.getInstance(cl, functionName);
+		} else {
+			return osgiResolution;
 
-	public FunctionInterface getInstance(final ClassLoader cl, final String functionName) throws TMLExpressionException {
-		System.err.println("GETTINH FUNCTION FROM OSHI!!!!: "+functionName);
+		}
+	}
+	
+	
+	public Object getComponent( final String name, String serviceKey, Class interfaceClass)  {
+		System.err.println("GETTINH FUNCTION FROM OSHI!!!!: "+name);
 		BundleContext context = NavajoBundleManager.getInstance().getBundleContext();
 		try {
-			ServiceReference[] refs = context.getServiceReferences(FunctionInterface.class.getName(), "(functionName="+functionName+")");
-			FunctionInterface function = (FunctionInterface) context.getService(refs[0]);
+			ServiceReference[] refs = context.getServiceReferences(interfaceClass.getName(), "("+serviceKey+"="+name+")");
+			if(refs==null) {
+				System.err.println("Service resolution failed: Query: "+"("+serviceKey+"="+name+")"+" class: "+interfaceClass.getName());
+			}
+			return context.getService(refs[0]);
 			
-			return function;
 		} catch (InvalidSyntaxException e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
 	
+	@Override
+	public  Class<?> getAdapterClass(String adapterClassName, ClassLoader cl) {
+			Class osgiResolution = (Class) getComponent(adapterClassName, "adapterClass", Class.class);
+			if (osgiResolution==null) {
+				System.err.println("OSGi failed. Going old skool");
+				return super.getAdapterClass(adapterClassName, cl);
+			} else {
+				return osgiResolution;
+			}
+	}	
 }
