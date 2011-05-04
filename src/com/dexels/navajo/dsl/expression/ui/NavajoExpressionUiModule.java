552f9ac4d23cca6ed4e26ae3ec976e941efc9fa6
Frank Lyaruu <frank@dexels.com>
2011-05-04 15:48:34 +0000

    some OSGi reg, don't know if it is functional

--- src/com/dexels/navajo/dsl/expression/ui/NavajoExpressionUiModule.java
+++ src/com/dexels/navajo/dsl/expression/ui/NavajoExpressionUiModule.java
@@ -5,14 +5,33 @@ package com.dexels.navajo.dsl.expression.ui;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.IHighlightingHelper;
+import org.osgi.framework.BundleContext;
 
 /**
  * Use this class to register components to be used within the IDE.
  */
 public class NavajoExpressionUiModule extends com.dexels.navajo.dsl.expression.ui.AbstractNavajoExpressionUiModule {
+	
+//	private final AbstractUIPlugin myPlugin;
+	
+	private static NavajoExpressionUiModule instance;
+	
 	public NavajoExpressionUiModule(AbstractUIPlugin plugin) {
 		super(plugin);
+//		myPlugin = plugin;
+		NavajoExpressionUiModule.instance = this;
 		System.err.println("Registering UI module!");
+		registerToOSGi(plugin);
+	}
+	
+	public static NavajoExpressionUiModule getInstance() {
+		return NavajoExpressionUiModule.instance;
+	}
+
+	private void registerToOSGi(AbstractUIPlugin plug) {
+		BundleContext bc = plug.getBundle().getBundleContext();
+		bc.registerService("com.dexels.navajo.dsl.expression.ui.AbstractNavajoExpressionUiModule", this, null);
+		System.err.println("<<<<<<<< Registered ui module!");
 	}
 
 	@Override
