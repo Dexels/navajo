2a362ebc3bec1a6a2a116a980c4f44955025eb66
Frank Lyaruu <frank@dexels.com>
2011-05-11 15:35:29 +0000

    retrofitted old skool service management

--- src/navajo/Version.java
+++ src/navajo/Version.java
@@ -1,10 +1,13 @@
 package navajo;
 
+import java.util.Properties;
+
 import org.osgi.framework.BundleContext;
 
-import com.dexels.navajo.document.Navajo;
-import com.dexels.navajo.document.NavajoFactory;
+import com.dexels.navajo.adapter.NavajoCoreAdapterLibrary;
 import com.dexels.navajo.events.NavajoEventRegistry;
+import com.dexels.navajo.functions.util.FunctionFactoryFactory;
+import com.dexels.navajo.functions.util.FunctionFactoryInterface;
 import com.dexels.navajo.server.DispatcherFactory;
 import com.dexels.navajo.server.GenericHandler;
 import com.dexels.navajo.server.GenericThread;
@@ -15,30 +18,6 @@ import com.dexels.navajo.server.enterprise.tribe.TribeManagerFactory;
 import com.dexels.navajo.server.enterprise.xmpp.JabberWorkerFactory;
 import com.dexels.navajo.server.jmx.JMXHelper;
 import com.dexels.navajo.server.resource.ResourceCheckerManager;
-/**
- * <p>Title: Navajo Product Project</p>
- * <p>Description: This is the official source for the Navajo server</p>
- * <p>Copyright: Copyright (c) 2005</p>
- * <p>Company: Dexels BV</p>
- * @author 
- * @version $Id$.
- *
- * DISCLAIMER
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED.  IN NO EVENT SHALL DEXELS BV OR ITS CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
- * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
- * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
- * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- * ====================================================================
- */
 import com.dexels.navajo.util.AuditLog;
 
 /**
@@ -245,6 +224,28 @@ public class Version extends com.dexels.navajo.version.AbstractVersion {
 	@Override
 	public void start(BundleContext bc) throws Exception {
 		super.start(bc);
+		FunctionFactoryInterface fi= FunctionFactoryFactory.getInstance();
+		fi.init();
+		
+		fi.clearFunctionNames();
+
+		NavajoCoreAdapterLibrary library = new NavajoCoreAdapterLibrary();
+		fi.injectExtension(library);
+		System.err.println("Adapterlib injected!");
+		for(String adapterName: fi.getAdapterNames(library)) {
+
+			String adapterClass = fi.getAdapterClass(adapterName,library);
+			Class c = Class.forName(adapterClass);
+
+			 Properties props = new Properties();
+			 System.err.println("Registering adapter: "+adapterName + "class: "+adapterClass);
+			 props.put("adapterName", adapterName);
+			 props.put("adapterClass", c.getName());
+			if(adapterClass!=null) {
+				context.registerService(Class.class.getName(), c, props);
+			}
+		}
+
 	}
 
 
