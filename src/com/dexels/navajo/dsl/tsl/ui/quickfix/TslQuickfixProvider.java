5f481d2927a97993a1631fc3a0835d32176750a1
Frank Lyaruu <frank@dexels.com>
2011-06-03 13:47:54 +0000

    value checking quickfixes

--- src/com/dexels/navajo/dsl/tsl/ui/quickfix/TslQuickfixProvider.java
+++ src/com/dexels/navajo/dsl/tsl/ui/quickfix/TslQuickfixProvider.java
@@ -1,6 +1,8 @@
 
 package com.dexels.navajo.dsl.tsl.ui.quickfix;
 
+import java.util.List;
+
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.xtext.ui.editor.model.IXtextDocument;
 import org.eclipse.xtext.ui.editor.model.edit.IModification;
@@ -11,8 +13,12 @@ import org.eclipse.xtext.ui.editor.quickfix.IssueResolutionAcceptor;
 import org.eclipse.xtext.validation.Issue;
 
 import com.dexels.navajo.dsl.tsl.validation.TslJavaValidator;
+import com.google.inject.Inject;
 
 public class TslQuickfixProvider extends DefaultQuickfixProvider {
+
+	@Inject
+	protected  TslJavaValidator tslJavaValidator;
 
 //	@Fix(MyJavaValidator.INVALID_NAME)
 //	public void capitalizeName(final Issue issue, IssueResolutionAcceptor acceptor) {
@@ -27,7 +33,9 @@ public class TslQuickfixProvider extends DefaultQuickfixProvider {
 
 	@Fix(TslJavaValidator.ISSUE_ILLEGAL_ATTRIBUTE)
 	public void removeIllegalAttribute(final Issue issue,IssueResolutionAcceptor acceptor) {
-		
+		if(tslJavaValidator!=null) {
+			System.err.println("I'll be damned, injection worked");
+		}
 		acceptor.accept(issue, "Remove attribute", "Remove attribute", null , new IModification() {
 		public void apply(IModificationContext context) throws BadLocationException {
 			IXtextDocument xtextDocument = context.getXtextDocument();
@@ -36,6 +44,20 @@ public class TslQuickfixProvider extends DefaultQuickfixProvider {
 	});
 	}
 
+//	// Indentical to removeIllegalAttrivute
+//	@Fix(TslJavaValidator.ISSUE_ILLEGAL_OPTIONAL_ATTRIBUTE_VALUE)
+//	public void removeIllegalAttributeValue(final Issue issue,IssueResolutionAcceptor acceptor) {
+//		
+//		acceptor.accept(issue, "Remove attribute", "Remove attribute", null , new IModification() {
+//		public void apply(IModificationContext context) throws BadLocationException {
+//			IXtextDocument xtextDocument = context.getXtextDocument();
+//			xtextDocument.replace(issue.getOffset(), issue.getLength(),"");
+//		}
+//	});
+//	}
+
+	
+	
 	@Fix(TslJavaValidator.ISSUE_SHOULD_BE_EXPRESSION)
 	public void convertToExpression(final Issue issue,IssueResolutionAcceptor acceptor) {
 		
@@ -84,14 +106,32 @@ public class TslQuickfixProvider extends DefaultQuickfixProvider {
 //		});
 //	}	
 	
+	@Fix(TslJavaValidator.ISSUE_ILLEGAL_ATTRIBUTE_VALUE)
+	public void setIllegalAttriuteValue(final Issue issue,IssueResolutionAcceptor acceptor) {
+		for (String val : issue.getData()) {
+			acceptor.accept(issue, "Set attribute to: "+val,  "Set attribute to: "+val, null , setAttributeValue(issue, val));
+		}
+	}	
+
+//	@Fix(TslJavaValidator.ISSUE_ILLEGAL_OPTIONAL_ATTRIBUTE_VALUE)
+//	public void setOrRemoveIllegalAttriuteValue(final Issue issue,IssueResolutionAcceptor acceptor) {
+//		for (String val : issue.getData()) {
+//			acceptor.accept(issue, "Set attribute to: "+val,  "Set attribute to: "+val, null , setAttributeValue(issue, val));
+//		}
+//	}	
 
 	@Fix(TslJavaValidator.ISSUE_MISSING_ATTRIBUTE)
 	public void insertMissingAttribute(final Issue issue,IssueResolutionAcceptor acceptor) {
-		String value = TslJavaValidator.getDefaultValueForAttribute(issue.getData()[0], issue.getData()[1]);
-		if(value==null) {
-			value = "unknown";
+		List<String> values = TslJavaValidator.getDefaultValueForAttribute(issue.getData()[0], issue.getData()[1]);
+		
+		if(values==null || values.size()==0) {
+			String value = "unknown";
+			acceptor.accept(issue, "Insert: "+issue.getData()[1]+" with \""+value+"\" to: "+issue.getData()[0], "Insert missing attribute: ", null , addAttributeToElement(issue, issue.getData()[1],value));
+		} else {
+			for (String value : values) {
+				acceptor.accept(issue, "Insert: "+issue.getData()[1]+" with \""+value+"\" to: "+issue.getData()[0], "Insert missing attribute: ", null , addAttributeToElement(issue, issue.getData()[1],value));
+			}
 		}
-		acceptor.accept(issue, "Insert: "+issue.getData()[1]+" with \""+value+"\" to: "+issue.getData()[0], "Insert missing attribute", null , addAttributeToElement(issue, issue.getData()[1],value));
 	}	
 
 	@Fix(TslJavaValidator.ISSUE_SHOULD_NOT_BE_EXPRESSION)
@@ -126,16 +166,27 @@ public class TslQuickfixProvider extends DefaultQuickfixProvider {
 
 				String localKey = key;
 				String atInsertionPoint = xtextDocument.get(issue.getOffset()+index-1, 1);
-				System.err.println("AT: "+atInsertionPoint);
 				// ensure leading whitespace
 				if(!Character.isWhitespace(atInsertionPoint.charAt(0))) {
 					localKey = " "+localKey;
 				}
 				xtextDocument.replace(issue.getOffset()+index, 0, localKey+"=\""+value+"\"");
-//				int start = issuetext.indexOf("=\"");
-//				int end = issuetext.lastIndexOf(";\"");
-//				xtextDocument.replace(issue.getOffset()+end-1,2,"");
-//				xtextDocument.replace(issue.getOffset()+start+2, 2,"");
+			}
+		};
+		return ii;
+	}
+	
+	protected IModification setAttributeValue(final Issue issue,final String value) {
+		IModification ii = new IModification() {
+			public void apply(IModificationContext context) throws BadLocationException {
+				IXtextDocument xtextDocument = context.getXtextDocument();
+				String issuetext = xtextDocument.get(issue.getOffset(),issue.getLength());
+				System.err.println("Fixxing issuetext: "+issuetext);
+				int start = issuetext.indexOf("\"");
+				int end = issuetext.lastIndexOf("\"");
+				System.err.println("End: "+end+" start: "+start);
+				xtextDocument.replace(issue.getOffset()+start+1, end-start-1, value);
+
 			}
 		};
 		return ii;
