77169bc089574df73cfe0a2df1cd8cf817d3231c
Frank Lyaruu <frank@dexels.com>
2011-06-15 15:49:34 +0000

    upgraded to new style OSGipi

--- src/tipi/TipiMailExtension.java
+++ src/tipi/TipiMailExtension.java
@@ -1,16 +1,37 @@
 package tipi;
 
+import org.osgi.framework.BundleContext;
+
 import com.dexels.navajo.tipi.TipiContext;
 
 public class TipiMailExtension extends TipiAbstractXMLExtension implements TipiExtension {
 
+	private static TipiMailExtension instance = null;
+	
+	public static TipiMailExtension getInstance() {
+		return instance;
+	}
+	
 	public TipiMailExtension() {
-		loadXML();
+		//will be handled by registerTipiExtension
+		//loadDescriptor();
 		
 	}
 	public void initialize(TipiContext tc) {
 		// Do nothing
 		
 	}
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		registerTipiExtension(context);
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+	}
+
+
+
 }
 
