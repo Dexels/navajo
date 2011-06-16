ce4484b96101a3996a514c20407924b9d9a12517
Frank Lyaruu <frank@dexels.com>
2011-06-16 15:41:08 +0000

    fixed exportability

--- src/tipi/TipiSwingEditorExtension.java
+++ src/tipi/TipiSwingEditorExtension.java
@@ -1,5 +1,7 @@
 package tipi;
 
+import org.osgi.framework.BundleContext;
+
 import com.dexels.navajo.tipi.TipiContext;
 
 public class TipiSwingEditorExtension extends TipiAbstractXMLExtension implements TipiExtension {
@@ -7,6 +9,15 @@ public class TipiSwingEditorExtension extends TipiAbstractXMLExtension implement
 	public void initialize(TipiContext tc) {
 		// Do nothing
 		
+	}
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		registerTipiExtension(context);
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
 	}
 
 }
