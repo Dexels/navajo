a307c17473b33d185c68238d2aa961669078980a
Frank Lyaruu <frank@dexels.com>
2011-06-16 15:42:48 +0000

    fix

--- src/tipi/TipiAbstractOSGiExtension.java
+++ src/tipi/TipiAbstractOSGiExtension.java
@@ -1,8 +1,18 @@
 package tipi;
 
+import java.io.File;
+import java.io.FilenameFilter;
+import java.net.MalformedURLException;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleException;
 import org.osgi.framework.ServiceReference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import tipipackage.ITipiExtensionRegistry;
 
@@ -10,10 +20,12 @@ public abstract class TipiAbstractOSGiExtension implements TipiExtension,
 		BundleActivator {
 
 	private BundleContext context = null;
+	private static final Logger logger = LoggerFactory.getLogger(TipiAbstractOSGiExtension.class); 
 
 	protected void registerTipiExtension(BundleContext context)
 			throws Exception {
 		this.context = context;
+		logger.info("Registering tipi extension: "+getClass().getName());
 		loadDescriptor();
 		ServiceReference refs = context
 				.getServiceReference(ITipiExtensionRegistry.class.getName());
@@ -23,6 +35,12 @@ public abstract class TipiAbstractOSGiExtension implements TipiExtension,
 
 	}
 
+	protected void deregisterTipiExtension(BundleContext context)
+			throws Exception {
+///		this.context = context;
+		// deregister
+	}	
+	
 	public ITipiExtensionRegistry getTipiExtensionRegistry() {
 		ServiceReference refs = context
 				.getServiceReference(ITipiExtensionRegistry.class.getName());
@@ -39,5 +57,47 @@ public abstract class TipiAbstractOSGiExtension implements TipiExtension,
 	@Override
 	public void stop(BundleContext context) throws Exception {
 
+	}
+	
+	public BundleContext getBundleContext() {
+		return context;
+	}
+
+	public void installAllExtensions(File extensionDir) {
+		File[] list =extensionDir.listFiles(new FilenameFilter() {
+			
+			@Override
+			public boolean accept(File dir, String name) {
+				return name.endsWith(".jar");
+			}
+		});
+		
+		// TODO Refactor to agnostic impl
+		System.err.println("FIles #"+list.length);
+		for (File file : list) {
+			installExtension(file);
+		}
+	}
+	
+	public void installExtension( File library) {
+		List<Bundle> loaded = new LinkedList<Bundle>();
+		try {
+			Bundle b = context.installBundle(library.toURI().toURL().toString());
+			logger.info("Bundle: "+b.getSymbolicName()+" id: "+b.getBundleId()+" loaded succesfully.");
+			loaded.add(b);
+		} catch (MalformedURLException e) {
+			logger.error("Bundle could not be loaded. Path invalid: "+library,e);
+		} catch (BundleException e) {
+			logger.error("Bundle with path: "+library+" could not be loaded.",e);
+		}
+		for (Bundle b : loaded) {
+			try {
+				b.start();
+				logger.info("Bundle: "+b.getSymbolicName()+" id: "+b.getBundleId()+" started succesfully.");
+			} catch (BundleException e) {
+				e.printStackTrace();
+				logger.error("Bundle: "+b.getSymbolicName()+" id: "+b.getBundleId()+" could not start.",e);
+			}
+		}
 	}
 }
