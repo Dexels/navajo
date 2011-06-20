475b65c1d26c4919151a0aed4c6c0534ff0b7f18
Frank Lyaruu <frank@dexels.com>
2011-06-20 21:44:51 +0000

    fixed deps and extension loaders

--- src/tipi/TipiFlickrExtension.java
+++ src/tipi/TipiFlickrExtension.java
@@ -1,11 +1,12 @@
 package tipi;
 
+import org.osgi.framework.BundleContext;
+
 import com.dexels.navajo.tipi.TipiContext;
 
 	public class TipiFlickrExtension extends TipiAbstractXMLExtension  {
 
 		public TipiFlickrExtension() {
-			loadDescriptor();
 		}
 
 		public void initialize(TipiContext tc) {
@@ -13,7 +14,14 @@ import com.dexels.navajo.tipi.TipiContext;
 
 		}
 
+		@Override
+		public void start(BundleContext context) throws Exception {
+			registerTipiExtension(context);
+		}
 
+		@Override
+		public void stop(BundleContext context) throws Exception {
+		}
 
 
 	}
