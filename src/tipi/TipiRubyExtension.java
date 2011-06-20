99866eb977394b1a29a7b18b2c806f5e883c30e7
Frank Lyaruu <frank@dexels.com>
2011-06-20 21:42:32 +0000

    fixed loading

--- src/tipi/TipiRubyExtension.java
+++ src/tipi/TipiRubyExtension.java
@@ -1,15 +1,25 @@
 package tipi;
 
+import org.osgi.framework.BundleContext;
+
 import com.dexels.navajo.tipi.TipiContext;
 
 public class TipiRubyExtension extends TipiAbstractXMLExtension implements TipiExtension {
 
 	public TipiRubyExtension() {
-		loadDescriptor();
 	}
 
 	public void initialize(TipiContext tc) {
 		// Do nothing
+	}
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		registerTipiExtension(context);
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
 	}
 }
 
