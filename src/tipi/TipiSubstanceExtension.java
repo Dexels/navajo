5bf7ce034848c4c6012996c5771217720fb93fd1
Frank Lyaruu <frank@dexels.com>
2011-06-20 21:43:59 +0000

    fixed and made exportable

--- src/tipi/TipiSubstanceExtension.java
+++ src/tipi/TipiSubstanceExtension.java
@@ -1,11 +1,15 @@
 package tipi;
 
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
 import com.dexels.navajo.tipi.TipiContext;
 
-public class TipiSubstanceExtension extends TipiAbstractXMLExtension implements TipiExtension {
+public class TipiSubstanceExtension extends TipiAbstractXMLExtension implements TipiExtension, BundleActivator {
 
 	public TipiSubstanceExtension() {
-		loadDescriptor();
+//		loadDescriptor();
+		System.err.println("Created: TipiSubstanceExtension");
 	}
 	
 	public void initialize(TipiContext tc) {
@@ -13,7 +17,14 @@ public class TipiSubstanceExtension extends TipiAbstractXMLExtension implements 
  
 	}
 
+	@Override
+	public void start(BundleContext context) throws Exception {
+		registerTipiExtension(context);
+	}
 
+	@Override
+	public void stop(BundleContext context) throws Exception {
+	}
 //
 //
 //	public List<String> getLibraryJars() {
