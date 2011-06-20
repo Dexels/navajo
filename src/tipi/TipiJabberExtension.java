296c1766ff9de167fac6021c7e4d9d94c574e2cb
Frank Lyaruu <frank@dexels.com>
2011-06-20 21:44:51 +0000

    fixed deps and extension loaders

--- src/tipi/TipiJabberExtension.java
+++ src/tipi/TipiJabberExtension.java
@@ -1,16 +1,24 @@
 package tipi;
 
+import org.osgi.framework.BundleContext;
+
 import com.dexels.navajo.tipi.TipiContext;
 
 public class TipiJabberExtension extends TipiAbstractXMLExtension implements TipiExtension {
 
 	public TipiJabberExtension() {
-		loadDescriptor();
-		
 	}
 	public void initialize(TipiContext tc) {
 		// Do nothing
 		
 	}
 
+	@Override
+	public void start(BundleContext context) throws Exception {
+		registerTipiExtension(context);
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+	}
 }
