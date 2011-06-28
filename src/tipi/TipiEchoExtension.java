a20ad0e56d39175887ded69e435edafcb1af5004
Frank Lyaruu <frank@dexels.com>
2011-06-28 12:47:46 +0000

    forgot

--- src/tipi/TipiEchoExtension.java
+++ src/tipi/TipiEchoExtension.java
@@ -2,18 +2,28 @@ package tipi;
 
 import java.io.IOException;
 
+import org.osgi.framework.BundleContext;
+
 import com.dexels.navajo.tipi.TipiContext;
 import com.dexels.navajo.tipi.tipixml.XMLParseException;
 
 public class TipiEchoExtension extends TipiAbstractXMLExtension implements TipiExtension {
 
 	public TipiEchoExtension() throws XMLParseException, IOException {
-		loadXML();
 	}
 
 	public void initialize(TipiContext tc) {
-		// Do nothing
 
 	}
+	
+	@Override
+	public void start(BundleContext context) throws Exception {
+		registerTipiExtension(context);
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+	}
+
 
 }
