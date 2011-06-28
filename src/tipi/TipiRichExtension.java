2ca562cdb9af843d9c4aa33b54d6e534f751da40
Frank Lyaruu <frank@dexels.com>
2011-06-28 14:28:03 +0000

    added start stop: All extension classes need those methods now

--- src/tipi/TipiRichExtension.java
+++ src/tipi/TipiRichExtension.java
@@ -1,17 +1,28 @@
 package tipi;
 
+import org.osgi.framework.BundleContext;
+
 import com.dexels.navajo.tipi.TipiContext;
 
 	public class TipiRichExtension extends TipiAbstractXMLExtension  {
 
 		public TipiRichExtension() {
-			loadDescriptor();
 		}
 
 		public void initialize(TipiContext tc) {
 			// Do nothing
 
 		}
+		
+		@Override
+		public void start(BundleContext context) throws Exception {
+			registerTipiExtension(context);
+		}
+
+		@Override
+		public void stop(BundleContext context) throws Exception {
+		}
+
 
 
 
