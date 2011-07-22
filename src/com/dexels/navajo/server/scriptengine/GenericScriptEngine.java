0a168b1cf9dd695c8afe47ffce990c43a971e637
Frank Lyaruu <frank@dexels.com>
2011-07-22 13:52:09 +0000

    added warning

--- src/com/dexels/navajo/server/scriptengine/GenericScriptEngine.java
+++ src/com/dexels/navajo/server/scriptengine/GenericScriptEngine.java
@@ -2,18 +2,14 @@ package com.dexels.navajo.server.scriptengine;
 
 import java.io.File;
 import java.io.FileReader;
-import java.util.List;
 
-import javax.script.Invocable;
 import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
-import javax.script.ScriptEngineFactory;
 import javax.script.ScriptEngineManager;
 
 import com.dexels.navajo.document.Navajo;
 import com.dexels.navajo.document.NavajoFactory;
 import com.dexels.navajo.server.Access;
-import com.dexels.navajo.server.Dispatcher;
 import com.dexels.navajo.server.DispatcherFactory;
 import com.dexels.navajo.serviceplugin.JavaPlugin;
 
@@ -38,6 +34,7 @@ public class GenericScriptEngine extends JavaPlugin {
 	}
 
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public Navajo process(Navajo in) throws Exception {
 		if(scriptFile.getName().indexOf('.')==-1) {
@@ -63,9 +60,6 @@ public class GenericScriptEngine extends JavaPlugin {
 		se.put("output", result);
 		se.put("navajoConfig", DispatcherFactory.getInstance().getNavajoConfig());
 		
-		if(se==null) {
-			System.err.println("Warning: No scriptengine found");
-		}
 		String adapterPath = DispatcherFactory.getInstance().getNavajoConfig().getAdapterPath();
 		String includePath = adapterPath + se.getFactory().getLanguageName()+"/include";
 		Class<? extends IncludeManager> manager = (Class<? extends IncludeManager>) Class.forName("com.dexels.navajo.server.scriptengine.include."+se.getFactory().getLanguageName());
