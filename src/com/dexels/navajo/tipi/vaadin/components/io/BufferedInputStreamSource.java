dc7b50805a33ba888033340075ed05988a8e7b62
Frank Lyaruu <frank@dexels.com>
2011-07-25 09:25:55 +0000

    fixed brain error

--- src/com/dexels/navajo/tipi/vaadin/components/io/BufferedInputStreamSource.java
+++ src/com/dexels/navajo/tipi/vaadin/components/io/BufferedInputStreamSource.java
@@ -1,21 +1,22 @@
 package com.dexels.navajo.tipi.vaadin.components.io;
 
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
 import com.vaadin.terminal.StreamResource.StreamSource;
 
-public class InputStreamSource implements StreamSource {
+public class BufferedInputStreamSource implements StreamSource {
 
 	private static final long serialVersionUID = -3296924859018619919L;
-	private final transient InputStream inputStream;
-
-	public InputStreamSource(InputStream inputStream) {
-		this.inputStream = inputStream;
+	private final byte[] data;
+	
+	public BufferedInputStreamSource(byte[] data) {
+		this.data = data;
 	}
 	
 	@Override
 	public InputStream getStream() {
-		return inputStream;
+		return new ByteArrayInputStream(data);
 	}
 
 }
