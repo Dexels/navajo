93472633fee041749128bf85e861315fc0e102c9
Frank Lyaruu <frank@dexels.com>
2011-08-04 13:25:24 +0000

    removed dep to NavajoListeners (by copypasting static methods)

--- src/com/dexels/navajo/jsp/server/InstallerContext.java
+++ src/com/dexels/navajo/jsp/server/InstallerContext.java
@@ -1,12 +1,14 @@
 package com.dexels.navajo.jsp.server;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletContext;
 import javax.servlet.jsp.PageContext;
-
-import com.dexels.navajo.server.listener.NavajoContextListener;
 public class InstallerContext {
 	private PageContext pageContext;
 	
@@ -14,11 +16,11 @@ public class InstallerContext {
 	 * Returns if the location dictated by this context is plausible
 	 */
 	public boolean isValidInstallation() {
-		return NavajoContextListener.isValidInstallationForContext(getServletContext());
+		return isValidInstallationForContext(getServletContext());
 	}
 	
 	public String getNavajoRoot(String serverContext) throws IOException {
-		return  NavajoContextListener.getInstallationPath(getServletContext(),null);
+		return  getInstallationPath(getServletContext(),null);
 	}
 	
 	public String getSuggestedPath() {
@@ -42,7 +44,8 @@ public class InstallerContext {
 	 * Call this after a fresh install (because in that case the context initialization has been skipped)
 	 */
 	public void initialize() {
-		NavajoContextListener.initializeContext(getPageContext().getServletContext(),null);
+		//TODO fix!
+		//initializeContext(getPageContext().getServletContext(),null);
 	}
 	
 	public String getContextPath() {
@@ -61,7 +64,86 @@ public class InstallerContext {
 		return pageContext;
 	}
 	
+	/**
+	 * Only used when the path is not forced.
+	 */
+	private static String getInstallationPath(
+			Map<String, String> systemContexts, String contextPath) {
+		String engineInstance = System
+				.getProperty("com.dexels.navajo.server.EngineInstance");
+		String key = contextPath;
+		if (engineInstance != null) {
+			key = contextPath + "@" + engineInstance;
+		}
+		String result = systemContexts.get(key);
+		if (result != null) {
+			return result;
+		}
+		return systemContexts.get(contextPath);
+	}
 
+	private static Map<String, String> loadSystemContexts() throws IOException {
+		File home = new File(System.getProperty("user.home"));
+		File navajo = new File(home, "navajo.properties");
+		Map<String, String> systemContexts = new HashMap<String, String>();
+		if (!navajo.exists()) {
+			return systemContexts;
+		}
+		BufferedReader br = new BufferedReader(new FileReader(navajo));
+
+		while (true) {
+			String line = br.readLine();
+			if (line == null) {
+				break;
+			}
+			String[] r = line.split("=");
+			systemContexts.put(r[0], r[1]);
+		}
+
+		br.close();
+		return systemContexts;
+	}
+
+
+	public static boolean isValidInstallationForContext(ServletContext context) {
+		String installPath = getInstallationPath(context, null);
+
+		if (installPath == null) {
+			return false;
+		}
+		File f = new File(installPath);
+		if (!f.exists()) {
+			return false;
+		}
+		File scripts = new File(f, "scripts");
+		if (!scripts.exists()) {
+			return false;
+		}
+		File config = new File(f, "config");
+		if (!config.exists()) {
+			return false;
+		}
+		return config != null;
+	}
+
+	public static String getInstallationPath(ServletContext context,
+			String force) {
+		if (force == null) {
+			force = context.getInitParameter("forcedNavajoPath");
+		}
+		if (force != null) {
+			return force;
+		} else {
+			try {
+				String contextPath = context.getContextPath().substring(1);
+				Map<String, String> systemContexts = loadSystemContexts();
+				return getInstallationPath(systemContexts, contextPath);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
 
 
 }
