c7b3022503fe1ca3a2c48d7d1f21e3f98b490404
Frank Lyaruu <frank@dexels.com>
2011-08-04 13:46:32 +0000

    bool fix

--- src/com/dexels/navajo/tipi/vaadin/components/impl/MessageTable.java
+++ src/com/dexels/navajo/tipi/vaadin/components/impl/MessageTable.java
@@ -4,6 +4,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import com.dexels.navajo.document.types.ClockTime;
+import com.dexels.navajo.tipi.vaadin.document.TipiFieldFactory;
 import com.vaadin.data.Container;
 import com.vaadin.data.Property;
 import com.vaadin.ui.Table;
@@ -18,10 +19,14 @@ public class MessageTable extends Table {
 
 	public MessageTable(String caption) {
 		super(caption);
+		setTableFieldFactory(new TipiFieldFactory());
+		setEditable(true);
 	}
 
 	public MessageTable(String caption, Container dataSource) {
 		super(caption, dataSource);
+		setTableFieldFactory(new TipiFieldFactory());
+		setEditable(true);
 	}
 	
 	@Override
@@ -35,6 +40,13 @@ public class MessageTable extends Table {
           if(v instanceof ClockTime) {
         	  ClockTime ct = (ClockTime)v;
         	  return ct.toShortString();
+          }
+          if(v instanceof Number) {
+        	  return ""+v.toString();
+          }
+          if(v instanceof Boolean) {
+        	  boolean b = (Boolean)v;
+        	  return b?"jazeker":"echt niet";
           }
           return super.formatPropertyValue(rowId, colId, property);
       }
