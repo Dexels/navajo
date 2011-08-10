476237b861df3ebd29dcec711c92e01b7e2cf52b
Frank Lyaruu <frank@dexels.com>
2011-08-10 17:25:56 +0000

    *** empty log message ***

---
+++ src/com/dexels/navajo/tipi/swing/application/Activator.java
@@ -0,0 +1,59 @@
+package com.dexels.navajo.tipi.swing.application;
+
+import javax.swing.SwingUtilities;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+import tipi.TipiApplicationInstance;
+
+import com.dexels.navajo.tipi.TipiException;
+import com.dexels.navajo.tipi.components.swingimpl.TipiSwingWrapper;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+	private TipiApplicationInstance instance;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+
+	public void start(BundleContext bc) throws Exception {
+		System.err.println("<<<<<<<<<<Starting swing tipi");
+		Activator.context = bc;
+
+		instance = TipiSwingWrapper.runApp(new String[] { "tipiCodeBase=/Users/frank/Documents/workspace-xtext/TipiExample/tipi/" });
+		SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					instance.getCurrentContext().switchToDefinition(instance.getDefinition());
+				} catch (TipiException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		System.err.println("Stopping bundle");
+		if(instance!=null) {
+			instance.getCurrentContext().shutdown();
+			
+		}
+		Activator.context = null;
+		
+	}
+
+}
