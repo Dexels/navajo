2d362d6b0ad5ba6a1f04b23853cccabade45f9d7
Frank Lyaruu <frank@dexels.com>
2011-08-10 14:30:46 +0000

    added package wrapper to not have to expose tipi dir, as I suspect confuses Equinox

---
+++ src/com/dexels/navajo/tipi/components/swingimpl/TipiSwingWrapper.java
@@ -0,0 +1,10 @@
+package com.dexels.navajo.tipi.components.swingimpl;
+
+import tipi.MainApplication;
+import tipi.SwingTipiApplicationInstance;
+
+public class TipiSwingWrapper {
+	public static SwingTipiApplicationInstance runApp(String[] args) {
+		return MainApplication.runApp(args);
+	}
+}
