42811576b4ca2ef9d7a9400515f9bd9a9df25d52
Frank Lyaruu <frank@dexels.com>
2011-08-11 09:24:10 +0000

    added tycho support

--- src/tipi/TipiSwingDockingExtension.java
+++ src/tipi/TipiSwingDockingExtension.java
@@ -5,6 +5,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.*;
 
+import org.osgi.framework.BundleContext;
+
 import com.dexels.navajo.tipi.*;
 import com.dexels.navajo.tipi.tipixml.CaseSensitiveXMLElement;
 import com.dexels.navajo.tipi.tipixml.XMLElement;
@@ -14,11 +16,23 @@ public class TipiSwingDockingExtension extends TipiAbstractXMLExtension implemen
 
 	public TipiSwingDockingExtension() throws XMLParseException,
 			IOException {
-		loadXML();
+//		loadXML();
 	}
 
 	public void initialize(TipiContext tc) {
 		// Do nothing
+		
+	}
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		
+		loadDescriptor();
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		// TODO Auto-generated method stub
 		
 	}
 
