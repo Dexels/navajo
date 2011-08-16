cf85e9313035c2f9905fe46975552d56793651ef
Frank Lyaruu <frank@dexels.com>
2011-08-16 11:32:06 +0000

    fix osgi

--- src/tipi/TipiCobraBrowserExtension.java
+++ src/tipi/TipiCobraBrowserExtension.java
@@ -2,17 +2,27 @@ package tipi;
 
 import java.util.*;
 
+import org.osgi.framework.BundleContext;
+
 import com.dexels.navajo.tipi.*;
 
 public class TipiCobraBrowserExtension extends TipiAbstractXMLExtension implements TipiExtension {
 
 	public TipiCobraBrowserExtension() {
-		loadXML();
 		
 	}
+	@Override
 	public void initialize(TipiContext tc) {
 		// Do nothing
-		
+	}
+	
+	@Override
+	public void start(BundleContext context) throws Exception {
+		registerTipiExtension(context);
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
 	}
 }
 
