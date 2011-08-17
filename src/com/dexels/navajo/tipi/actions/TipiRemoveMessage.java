0733f3690ff64248d511d4636fa5046f97eed162
Frank Lyaruu <frank@dexels.com>
2011-08-17 10:17:28 +0000

    Did a minor (although scary) fix to NavajoDocument. It didn't work out for arraymessages.
    Issue: http://spiritus.dexels.nl:6070/browse/TIPI-18

--- src/com/dexels/navajo/tipi/actions/TipiRemoveMessage.java
+++ src/com/dexels/navajo/tipi/actions/TipiRemoveMessage.java
@@ -53,10 +53,14 @@ public class TipiRemoveMessage extends TipiAction {
 		Operand index = getEvaluatedParameter("index", event);
 		if (index != null && index.value != null) {
 			Integer ii = (Integer) index.value;
-
+			System.err.println("Index resolved: "+ii);
 			// TODO: Perhaps refactor into NavajoDocument
 			Message mm = message.getMessage(ii.intValue());
+			System.err.println("Removing message:");
+			mm.write(System.err);
 			message.removeMessage(mm);
+			System.err.println("Arraysize: "+message.getArraySize());
+			message.write(System.err);
 			try {
 				myContext.unlink(mm.getRootDoc(), mm);
 			} catch (NavajoException e) {
@@ -65,6 +69,7 @@ public class TipiRemoveMessage extends TipiAction {
 			}
 		} else {
 			// we are in non array mode now.
+			System.err.println("Assuming non array");
 			Message parent = message.getParentMessage();
 			if (parent == null) {
 				// toplevel? Remove from navajo
