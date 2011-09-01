2d416cfc1b9d3ca92c59cebe0b1f2e29a81f3948
Arnoud Philip <arnoud@dexels.com>
2011-09-01 09:11:00 +0000

    Adapted so the user can choose how to encode spaces. Default behaviour is %20 again. Java update caused the default behaviour to encode spaces as +

--- src/com/dexels/navajo/functions/URLEncode.java
+++ src/com/dexels/navajo/functions/URLEncode.java
@@ -13,8 +13,24 @@ public class URLEncode extends FunctionInterface {
 		if (o == null) {
 			return null;
 		}
+		boolean space_as_plus = false;
+		if(getOperands().size() > 1){
+			Object b = getOperand(1);
+			if(b != null){
+				space_as_plus = (Boolean)b;
+			}
+		}
 		if (o instanceof String) {
-			return URLEncoder.encode((String) o);
+			try{
+				String source = (String) o;				
+				String encoded = URLEncoder.encode(source, "UTF-8");
+				if(!space_as_plus){
+					encoded = encoded.replace("+", "%20");
+				}
+				return encoded;
+			}catch(Exception e){
+				throw new TMLExpressionException(this, "Can not parse URL: " + o.getClass().getName());
+			}
 		} else {
 			throw new TMLExpressionException(this, "Invalid operand: " + o.getClass().getName());
 		}
@@ -27,13 +43,14 @@ public class URLEncode extends FunctionInterface {
 
 	@Override
 	public String usage() {
-		return "URLEncode(s)";
+		return "URLEncode(s, space_as_plus)";
 	}
 
 	public static void main(String [] args) throws Exception {
 		URLEncode u = new URLEncode();
 		u.reset();
-		u.insertOperand("matthijs is 's werelds");
+		u.insertOperand("matthijs is 's werelds + 1");
+		//u.insertOperand(false);
 		
 		System.err.println(u.evaluate());
 	}
