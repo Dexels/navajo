cf94ab98439c9c2d4b57a77e3c195c2e2e5109f0
Frank Lyaruu <frank@dexels.com>
2011-10-25 10:28:37 +0000

    removed code duplication: Use xml (un)escaping functions form navajo document

--- src/com/dexels/navajo/functions/XmlEscape.java
+++ src/com/dexels/navajo/functions/XmlEscape.java
@@ -4,8 +4,7 @@
  */
 package com.dexels.navajo.functions;
 
-import java.util.StringTokenizer;
-
+import com.dexels.navajo.document.jaxpimpl.xml.XMLutils;
 import com.dexels.navajo.parser.FunctionInterface;
 import com.dexels.navajo.parser.TMLExpressionException;
 
@@ -40,81 +39,11 @@ public class XmlEscape extends FunctionInterface {
 			return null;
 		}
 		if (o instanceof String) {
-			return XMLEscape((String) o);
+			return XMLutils.XMLEscape((String) o);
 		}  else {
 			throw new TMLExpressionException(this, "Invalid operand: " + o.getClass().getName());
 		}
 	}
-
-	 /**
-	   * Replace all occurrences of the characters &, ', ", < and > by the escaped
-	   * characters &amp;, &quot;, &apos;, &lt; and &gt;
-	   */
-	  private static final String XMLEscape(String s) {
-	    
-	    boolean contains = false;
-	    for ( int i = 0; i < XML_ESCAPE_DELIMITERS.length(); i++ ) {
-	        if ( s.indexOf( XML_ESCAPE_DELIMITERS.charAt(i) ) != -1 ) {
-	            contains = true;
-	        }
-	    }
-	    
-	    if ( ! contains ) {
-	        return s;
-	    }
-	    
-	      if ((s == null) || (s.length() == 0)) {
-	          return s;
-	      }
-
-	      StringTokenizer tokenizer = new StringTokenizer(s, XML_ESCAPE_DELIMITERS, true);
-	      StringBuffer    result    = new StringBuffer();
-
-	      while (tokenizer.hasMoreElements()) {
-	          String substring = tokenizer.nextToken();
-
-	          if (substring.length() == 1) {
-	              switch (substring.charAt(0)) {
-
-	              case '&' :
-	                  result.append("&amp;");
-	                  break;
-
-	              //case '\'' :
-	              //    result.append("&apos;");
-	              //    break;
-
-	              case ';' :
-	                  result.append("\\;");
-	                  break;
-
-	              case '<' :
-	                  result.append("&lt;");
-	                  break;
-
-	              case '>' :
-	                  result.append("&gt;");
-	                  break;
-
-	              case '\"' :
-	                  result.append("&quot;");
-	                  break;
-
-//	              case '\n' :
-//	                  result.append("\\n");
-//	                  break;
-
-	              default :
-	                  result.append(substring);
-	              }
-	          }
-	          else {
-	              result.append(substring);
-	          }
-	      }
-
-	      return result.toString();
-	  }
 
 
 }
--- src/com/dexels/navajo/functions/XmlUnescape.java
+++ src/com/dexels/navajo/functions/XmlUnescape.java
@@ -4,9 +4,9 @@
  */
 package com.dexels.navajo.functions;
 
+import com.dexels.navajo.document.jaxpimpl.xml.XMLutils;
 import com.dexels.navajo.parser.FunctionInterface;
 import com.dexels.navajo.parser.TMLExpressionException;
-
 /**
  * @author arjen
  *
@@ -31,110 +31,12 @@ public class XmlUnescape extends FunctionInterface {
 			return null;
 		}
 		if (o instanceof String) {
-			return XMLUnescape((String) o);
+			return XMLutils.XMLUnescape((String) o);
 		}  else {
 			throw new TMLExpressionException(this, "Invalid operand: " + o.getClass().getName());
 		}
 	}
 
 
-	  /**
-	   * Replace all occurrences of the escaped characters &amp;, &quot;, &apos;,
-	   * &lt; and &gt; by the unescaped characters &, ', ", < and >.
-	   */
-	  public static final String XMLUnescape(String s) {
-	      if ((s == null) || (s.length() == 0)) {
-	          return s;
-	      }
-
-	      int    offset;
-	      int    next;
-	      String result;
-
-	      // filter out all escaped ampersands
-	      offset = 0;
-	      result = "";
-
-	      while ((next = s.indexOf("&amp;", offset)) >= 0) {
-	          result += s.substring(offset, next) + "&";
-	          offset = next + "&amp;".length();
-	      }
-
-	      result += s.substring(offset, s.length());    // characters after last &
-	      s      = result;
-
-	      // filter out all escaped double quotes
-	      offset = 0;
-	      result = "";
-
-	      while ((next = s.indexOf("&quot;", offset)) >= 0) {
-	          result += s.substring(offset, next) + "\"";
-	          offset = next + "&quot;".length();
-	      }
-
-	      result += s.substring(offset, s.length());    // characters after last "
-	      s      = result;
-
-	      // filter out all escaped single quotes
-	      offset = 0;
-	      result = "";
-
-	      while ((next = s.indexOf("&apos;", offset)) >= 0) {
-	          result += s.substring(offset, next) + "\'";
-	          offset = next + "&apos;".length();
-	      }
-
-	      result += s.substring(offset, s.length());    // characters after last "
-	      s      = result;
-
-	      // filter out all escaped less than characters
-	      offset = 0;
-	      result = "";
-
-	      while ((next = s.indexOf("&lt;", offset)) >= 0) {
-	          result += s.substring(offset, next) + "<";
-	          offset = next + "&lt;".length();
-	      }
-
-	      result += s.substring(offset, s.length());    // characters after last <
-	      s      = result;
-
-	      // filter out all escaped greater than characters
-	      offset = 0;
-	      result = "";
-
-	      while ((next = s.indexOf("&gt;", offset)) >= 0) {
-	          result += s.substring(offset, next) + ">";
-	          offset = next + "&gt;".length();
-	      }
-
-	      result += s.substring(offset, s.length());    // characters after last >
-	      s      = result;
-
-	      // filter out all escaped newlines
-	      offset = 0;
-	      result = "";
-
-	      while ((next = s.indexOf("\\n", offset)) >= 0) {
-	          result += s.substring(offset, next) + "\n";
-	          offset = next + "\\n".length();
-	      }
-
-	      result += s.substring(offset, s.length());    // characters after last newline
-
-	       // filter out all escaped ;'s
-	      offset = 0;
-	      result = "";
-
-	      while ((next = s.indexOf("\\;", offset)) >= 0) {
-	          result += s.substring(offset, next) + ";";
-	          offset = next + "\\;".length();
-	      }
-
-	      result += s.substring(offset, s.length());    // characters after last newline
-
-	      return result;
-	  }
-	  
 	
 }
