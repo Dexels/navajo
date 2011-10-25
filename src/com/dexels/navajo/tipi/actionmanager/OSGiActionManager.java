8780d0b55538a90061ce8feaa54136053e81a6a0
Frank Lyaruu <frank@dexels.com>
2011-10-25 10:08:11 +0000

    add multi impl-action manager using interface

---
+++ src/com/dexels/navajo/tipi/actionmanager/OSGiActionManager.java
@@ -0,0 +1,52 @@
+package com.dexels.navajo.tipi.actionmanager;
+
+import java.util.Collection;
+
+import navajo.ExtensionDefinition;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceReference;
+
+import com.dexels.navajo.tipi.TipiException;
+import com.dexels.navajo.tipi.actions.TipiActionFactory;
+import com.dexels.navajo.tipi.tipixml.XMLElement;
+
+public class OSGiActionManager extends TipiActionManager implements IActionManager {
+
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1562243077965649520L;
+	private final BundleContext bundleContext;
+
+	public OSGiActionManager(BundleContext bc) {
+		this.bundleContext = bc;
+	}
+	@Override
+	public void addAction(XMLElement actionDef, ExtensionDefinition ed) {
+		// ignoring
+	}
+
+	@Override
+	public TipiActionFactory getActionFactory(String name) throws TipiException {
+		try {
+			Collection<ServiceReference<TipiActionFactory>> aa = bundleContext
+					.getServiceReferences(TipiActionFactory.class,"(&(type=tipiAction)(name="+name+"))");
+			if(aa.isEmpty()) {
+				return null;
+			}
+			ServiceReference<TipiActionFactory> xe = aa.iterator().next();
+			return bundleContext.getService(xe);
+			
+		} catch (InvalidSyntaxException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+
+
+}
