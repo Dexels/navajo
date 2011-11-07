326ea8624e500816abde2f5fe3784b67fd42b522
Frank Lyaruu <frank@dexels.com>
2011-11-07 11:25:11 +0000

    moved startup code to injection point

--- src/com/dexels/navajo/tipi/vaadin/bridged/VaadinComponent.java
+++ src/com/dexels/navajo/tipi/vaadin/bridged/VaadinComponent.java
@@ -12,37 +12,39 @@ import org.osgi.service.http.NamespaceException;
 import com.dexels.navajo.tipi.vaadin.application.servlet.TipiVaadinServlet;
 
 public class VaadinComponent {
+	private static final String VAADIN_DIR_PREFIX = "/VAADIN";
 	private static final String SERVLET_ALIAS = "/";
 	private HttpService httpService;
 
 	public void setHttpService(HttpService httpService) {
-		System.err.println("SETTING HTTPSERVICE");
+		try {
+		System.err.println("VAADIN SETTING HTTPSERVICE");
 		this.httpService = httpService;
+		System.err.println("Vaadin Staring up sevlet at " + SERVLET_ALIAS);
+		TipiVaadinServlet servlet = new TipiVaadinServlet();
+		if(httpService==null) {
+			System.err.println("WHOOMP!");
+			return;
+		}
+		  HttpContext commonContext = httpService.createDefaultHttpContext();
+		  //		httpService.registerResources("/jsp-examples", "/web", commonContext); 
+//		sh.setInitParameter("application", "com.dexels.navajo.tipi.vaadin.application.TipiVaadinApplication");
+		Hashtable<String,String> ht = new Hashtable<String,String>();
+		ht.put("application", "com.dexels.navajo.tipi.vaadin.application.TipiVaadinApplication");
+//		httpService.registerServlet(SERVLET_ALIAS, servlet, ht, null);
+		VaadinFileServlet vfs = new VaadinFileServlet();
+		//httpService.registerResources("/VAADIN", path+"/VAADIN", commonContext);
+		httpService.registerServlet(VAADIN_DIR_PREFIX, vfs, null, commonContext);
+		httpService.registerServlet(SERVLET_ALIAS, servlet, ht, commonContext);
+	} catch (ServletException e) {
+		e.printStackTrace();
+	} catch (NamespaceException e) {
+		e.printStackTrace();
+	}
 	}
 
 	protected void startup(ComponentContext ctxt) {
-		try {
-			System.out.println("Staring up sevlet at " + SERVLET_ALIAS);
-			TipiVaadinServlet servlet = new TipiVaadinServlet();
-			if(httpService==null) {
-				System.err.println("WHOOMP!");
-				return;
-			}
-			  HttpContext commonContext = httpService.createDefaultHttpContext();
-//			httpService.registerResources("/jsp-examples", "/web", commonContext); 
-//			sh.setInitParameter("application", "com.dexels.navajo.tipi.vaadin.application.TipiVaadinApplication");
-			Hashtable<String,String> ht = new Hashtable<String,String>();
-			ht.put("application", "com.dexels.navajo.tipi.vaadin.application.TipiVaadinApplication");
-//			httpService.registerServlet(SERVLET_ALIAS, servlet, ht, null);
-			VaadinFileServlet vfs = new VaadinFileServlet();
-			//httpService.registerResources("/VAADIN", path+"/VAADIN", commonContext);
-			httpService.registerServlet("/VAADIN", vfs, null, commonContext);
-			httpService.registerServlet(SERVLET_ALIAS, servlet, ht, commonContext);
-		} catch (ServletException e) {
-			e.printStackTrace();
-		} catch (NamespaceException e) {
-			e.printStackTrace();
-		}
+
 	}
 
 	protected void shutdown(ComponentContext ctxt) {
