91fd181f107eb0defbe770a2fcec13d81ee2f12e
Frank Lyaruu <frank@dexels.com>
2011-11-29 14:25:00 +0000

    added support for DateAdd(Date,Clocktime)

--- src/com/dexels/navajo/functions/DateAdd.java
+++ src/com/dexels/navajo/functions/DateAdd.java
@@ -3,6 +3,7 @@ package com.dexels.navajo.functions;
 
 import java.util.Calendar;
 
+import com.dexels.navajo.document.types.ClockTime;
 import com.dexels.navajo.parser.FunctionInterface;
 
 
@@ -28,22 +29,32 @@ public final class DateAdd extends FunctionInterface {
     }
 
     public final Object evaluate() throws com.dexels.navajo.parser.TMLExpressionException {
+		if (getOperands().size() > 2) {
+			java.util.Date datum = (java.util.Date) this.getOperands().get(0);
+			Integer arg = (Integer) this.getOperands().get(1);
+			String field = (String) this.getOperands().get(2);
+			Calendar cal = Calendar.getInstance();
+			cal.setTime(datum);
+			if (field.equals("YEAR")) {
+				cal.add(Calendar.YEAR, arg.intValue());
+			} else if (field.equals("MONTH")) {
+				cal.add(Calendar.MONTH, arg.intValue());
+			} else if (field.equals("DAY")) {
+				cal.add(Calendar.DAY_OF_MONTH, arg.intValue());
+			} else if (field.equals("WEEK")) {
+				cal.add(Calendar.WEEK_OF_YEAR, arg.intValue());
+			}
+			return cal.getTime();
+		}
+		java.util.Date datum = (java.util.Date) this.getOperands().get(0);
+		ClockTime arg = (ClockTime) this.getOperands().get(1);
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(datum);
+		cal.set(Calendar.HOUR_OF_DAY, arg.getHours());
+		cal.set(Calendar.MINUTE, arg.getMinutes());
+		cal.set(Calendar.SECOND, arg.getSeconds());
+		return cal.getTime();
 
-        java.util.Date datum = (java.util.Date) this.getOperands().get(0);
-        Integer arg = (Integer) this.getOperands().get(1);
-        String field = (String) this.getOperands().get(2);
-        Calendar cal = Calendar.getInstance();
-
-        cal.setTime(datum);
-        if (field.equals("YEAR")) {
-            cal.add(Calendar.YEAR, arg.intValue());
-        } else if (field.equals("MONTH")) {
-            cal.add(Calendar.MONTH, arg.intValue());
-        } else if (field.equals("DAY")) {
-            cal.add(Calendar.DAY_OF_MONTH, arg.intValue());
-        } else if (field.equals("WEEK")) {
-            cal.add(Calendar.WEEK_OF_YEAR, arg.intValue());
-        }
-        return cal.getTime();
+		
     }
 }
