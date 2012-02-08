610b4ec630562d27289b794a8081f6c6f32c6c88
Frank Lyaruu <frank@dexels.com>
2012-02-08 16:05:04 +0000

    added OSGi / configuration

--- src/com/dexels/navajo/server/instance/NavajoHttpServiceContextComponent.java
+++ src/com/dexels/navajo/server/instance/NavajoHttpServiceContextComponent.java
@@ -6,6 +6,8 @@ import java.util.Hashtable;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.ComponentContext;
 import org.osgi.service.http.HttpContext;
 import org.osgi.service.http.HttpService;
 import org.slf4j.Logger;
@@ -25,41 +27,63 @@ public class NavajoHttpServiceContextComponent {
 	
 	private HttpContext httpContext;
 	private ServletContext servletContext;
+	private BundleContext bundleContext;
+	private NavajoContextFactory factory;
+	private HttpService httpService;
 
 
+	public void activate(ComponentContext c) {
+		logger.info("Activating HTTP server component");
+		this.bundleContext = c.getBundleContext();
+		NavajoContextFactory ncf = new NavajoContextFactory(this.bundleContext,this.httpService);
+		this.factory = ncf;
+	}
+	
+	public void deactivate() {
+		this.factory.getFactoryRegistration().unregister();
+	}
 
 	protected void setHttpService(HttpService httpService) {
-		try {
-			httpContext = httpService.createDefaultHttpContext();
-			
-			NqlServlet ns = new NqlServlet();
-			httpService.registerServlet("/Nql", ns, null, httpContext);
-			TmlHttpServlet servlet = new TmlHttpServlet();
-			httpService.registerServlet(LEGACY_SERVLET_ALIAS, servlet, null, httpContext);
-			
-			Dictionary<String, Object> wb = new Hashtable<String, Object>();
-			wb.put("schedulerClass","com.dexels.navajo.server.listener.http.schedulers.priority.PriorityThreadPoolScheduler");
-			wb.put("priorityPoolSize", "15");
-			wb.put("normalPoolSize", "20");
-			wb.put("systemPoolSize", "2");
-			TmlContinuationServlet tcs = new TmlContinuationServlet();
-			httpService.registerServlet(SERVLET_ALIAS, tcs, wb, httpContext);
-		
-			
-			NavajoContextListener ncl = new NavajoContextListener();
-			ncl.init(ns.getServletContext());
-	
-			servletContext = servlet.getServletContext();
-			NavajoContextListener.initializeContext(servletContext, null);
-//			installationPath = NavajoContextListener.getInstallationPath(servletContext, null);
-			logger.info("Vintage OSGi HttpService started. Somebody should notice.");
-			
-		} catch (ServletException e) {
-			e.printStackTrace();
-		} catch (Throwable e) {
-			e.printStackTrace();
-		}
+//		createContext(httpService);
+		this.httpService = httpService;
 	}
+
+//	private void createContext(HttpService httpService) {
+//		try {
+//			httpContext = httpService.createDefaultHttpContext();
+//			
+//			NqlServlet ns = new NqlServlet();
+//			httpService.registerServlet("/Nql", ns, null, httpContext);
+//			TmlHttpServlet servlet = new TmlHttpServlet();
+//			httpService.registerServlet(LEGACY_SERVLET_ALIAS, servlet, null, httpContext);
+//			
+//			Dictionary<String, Object> wb = new Hashtable<String, Object>();
+//			wb.put("schedulerClass","com.dexels.navajo.server.listener.http.schedulers.priority.PriorityThreadPoolScheduler");
+//			wb.put("priorityPoolSize", "15");
+//			wb.put("normalPoolSize", "20");
+//			wb.put("systemPoolSize", "2");
+//			TmlContinuationServlet tcs = new TmlContinuationServlet();
+//			httpService.registerServlet(SERVLET_ALIAS, tcs, wb, httpContext);
+//		
+//			
+//			NavajoContextListener ncl = new NavajoContextListener();
+//			ncl.init(ns.getServletContext());
+//	
+//			servletContext = servlet.getServletContext();
+//			String contextPath = servletContext.getContextPath();
+//			String servletContextPath = servletContext.getRealPath("");
+//			String installationPath = NavajoContextListener.getInstallationPath(contextPath);
+//
+//			NavajoContextListener.initializeServletContext(contextPath,servletContextPath,installationPath);
+////			installationPath = NavajoContextListener.getInstallationPath(servletContext, null);
+//			logger.info(" OSGi HttpService started. Somebody should notice.");
+//			
+//		} catch (ServletException e) {
+//			e.printStackTrace();
+//		} catch (Throwable e) {
+//			e.printStackTrace();
+//		}
+//	}
 
 	public void removeHttpService(HttpService httpService) {
 		logger.info("Shutting down navajo instance");
