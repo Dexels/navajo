dc5911eccee48200e669f4b10915f8407b45ad86
Frank Lyaruu <frank@dexels.com>
2012-02-22 13:08:23 +0000

    added new main extension interface, to allow OSGi to detect 'main' instances

---
+++ src/tipi/TipiMainExtension.java
@@ -0,0 +1,5 @@
+package tipi;
+
+public interface TipiMainExtension extends TipiExtension {
+	// This extension is a main extension, for now: Swing, Vaadin, echo (sort of) and future: SWT and Rcp
+}
