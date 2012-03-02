7636b3896bdd07c521acc8918c955b87c7c35d75
Frank Lyaruu <frank@dexels.com>
2012-03-02 17:25:47 +0000

    *** empty log message ***

---
+++ scripts/git/enterprise.sh
@@ -0,0 +1,29 @@
+#enterprise
+./cvsgit.sh com.dexels.navajo.tools.wsdl com.dexels.navajo.wsdl
+./cvsgit.sh com.dexels.navajo.wsdl.feature com.dexels.navajo.wsdl.feature
+./cvsgit.sh com.dexels.navajo.server.enterprise.feature com.dexels.navajo.enterprise.feature
+./cvsgit.sh NavajoEnterpriseListeners com.dexels.navajo.enterprise.listeners
+./cvsgit.sh NavajoEnterpriseAdapters com.dexels.navajo.enterprise.adapters
+./cvsgit.sh NavajoEnterprise com.dexels.navajo.enterprise
+./cvsgit.sh com.dexels.navajo.enterprise.listeners.deps com.dexels.navajo.enterprise.listeners.deps
+./cvsgit.sh com.dexels.navajo.enterprise.adapters.deps com.dexels.navajo.enterprise.adapters.deps
+./cvsgit.sh com.dexels.navajo.mongo.feature com.dexels.navajo.mongo.feature
+./cvsgit.sh com.dexels.navajo.mongo com.dexels.navajo.mongo
+./cvsgit.sh com.dexels.navajo.mongo.navajostore com.dexels.navajo.mongo.navajostore
+./cvsgit.sh com.dexels.navajo.other.feature com.dexels.navajo.other.feature
+./cvsgit.sh NavajoUtilities com.dexels.navajo.other.utilities
+./cvsgit.sh com.dexels.navajo.resource.feature com.dexels.navajo.resource.feature
+./cvsgit.sh com.dexels.navajo.resource com.dexels.navajo.resource
+./cvsgit.sh com.dexels.navajo.resource.jdbc.h2 com.dexels.navajo.resource.jdbc.h2
+./cvsgit.sh com.dexels.navajo.resource.jdbc.mysql com.dexels.navajo.resource.jdbc.mysql
+./cvsgit.sh com.dexels.navajo.resource.jdbc.oracle com.dexels.navajo.resource.jdbc.oracle
+./cvsgit.sh com.dexels.navajo.resource.mongodb com.dexels.navajo.resource.mongodb
+./cvsgit.sh com.dexels.navajo.resource.test com.dexels.navajo.resource.test
+./cvsgit.sh com.dexels.navajo.resource.manager com.dexels.navajo.resource.manager
+./cvsgit.sh com.dexels.navajo.test.feature com.dexels.navajo.test.feature
+./cvsgit.sh NavajoRemoteTestLibrary com.dexels.navajo.test.remote
+./cvsgit.sh NavajoServer com.dexels.navajo.server
+./cvsgit.sh com.dexels.navajo.server.bridged com.dexels.navajo.server.bridged
+./cvsgit.sh com.dexels.navajo.server.bridged.deploy com.dexels.navajo.server.bridged.deploy
+
+
