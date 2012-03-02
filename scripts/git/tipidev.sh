c835e0f03f232ab787969453d3e470cc2ed728bc
Frank Lyaruu <frank@dexels.com>
2012-03-02 18:12:52 +0000

    gitifying

---
+++ scripts/git/tipidev.sh
@@ -0,0 +1,6 @@
+./cvsgit.sh TipiBuildLibrary com.dexels.navajo.tipi.dev.core
+./cvsgit.sh TipiAntBuild com.dexels.navajo.tipi.dev.ant
+./cvsgit.sh TipiPlugin com.dexels.navajo.tipi.dev.plugin
+./cvsgit.sh com.dexels.navajo.tipi.dev.feature com.dexels.navajo.tipi.dev.feature
+./cvsgit.sh TipiBuild com.dexels.navajo.tipi.build
+./cvsgit.sh TipiServer com.dexels.navajo.tipi.server
