7c6bfef97aa4a0ba85b27b4af730cc61660b83b2
Frank Lyaruu <frank@dexels.com>
2012-03-02 18:06:25 +0000

    *** empty log message ***

---
+++ scripts/git/tipi_optional.sh
@@ -0,0 +1,41 @@
+./cvsgit.sh com.dexels.navajo.tipi.swing.docking.feature com.dexels.navajo.tipi.swing.docking.feature
+./cvsgit.sh TipiSwingDocking com.dexels.navajo.tipi.swing.docking
+./cvsgit.sh com.dexels.navajo.tipi.swing.charting.feature com.dexels.navajo.tipi.swing.charting.feature
+./cvsgit.sh TipiCharting com.dexels.navajo.tipi.swing.charting
+./cvsgit.sh com.dexels.navajo.tipi.swing.editor.feature com.dexels.navajo.tipi.swing.editor.feature
+./cvsgit.sh TipiSwingEditor com.dexels.navajo.tipi.swing.editor
+./cvsgit.sh NavajoSwingEditor com.dexels.navajo.swing.editor
+
+
+
+./cvsgit.sh com.dexels.navajo.tipi.swing.svg.feature com.dexels.navajo.tipi.swing.svg.feature
+./cvsgit.sh TipiSvgBatik com.dexels.navajo.tipi.swing.svg
+./cvsgit.sh com.dexels.navajo.tipi.swing.mig.feature com.dexels.navajo.tipi.swing.mig.feature
+./cvsgit.sh TipiSwingMig com.dexels.navajo.tipi.swing.mig
+./cvsgit.sh com.dexels.navajo.tipi.swing.substance.feature com.dexels.navajo.tipi.swing.substance.feature
+./cvsgit.sh TipiSubstance com.dexels.navajo.tipi.swing.substance
+./cvsgit.sh com.dexels.navajo.tipi.swing.geo.feature com.dexels.navajo.tipi.swing.geo.feature
+./cvsgit.sh TipiSwingX com.dexels.navajo.tipi.swing.swingx
+./cvsgit.sh TipiGeoSwing com.dexels.navajo.tipi.swing.geo
+./cvsgit.sh TipiJxLayer com.dexels.navajo.tipi.swing.jxlayer
+./cvsgit.sh com.dexels.navajo.tipi.swing.rich.feature com.dexels.navajo.tipi.swing.rich.feature
+./cvsgit.sh NavajoRichClient com.dexels.navajo.tipi.swing.rich.client
+./cvsgit.sh NavajoRichTipi com.dexels.navajo.tipi.swing.rich
+./cvsgit.sh com.dexels.navajo.tipi.swing.cobra.feature com.dexels.navajo.tipi.swing.cobra.feature
+./cvsgit.sh TipiCobraBrowser com.dexels.navajo.tipi.swing.cobra
+./cvsgit.sh com.dexels.navajo.tipi.flickr.feature com.dexels.navajo.tipi.flickr.feature
+./cvsgit.sh TipiFlickr com.dexels.navajo.tipi.flickr
+./cvsgit.sh com.dexels.navajo.tipi.mail.feature com.dexels.navajo.tipi.mail.feature
+./cvsgit.sh TipiMail com.dexels.navajo.tipi.mail
+./cvsgit.sh com.dexels.navajo.tipi.jabber.feature com.dexels.navajo.tipi.jabber.feature
+./cvsgit.sh TipiJabber com.dexels.navajo.tipi.jabber
+./cvsgit.sh com.dexels.navajo.tipi.ruby.feature com.dexels.navajo.tipi.ruby.feature
+./cvsgit.sh TipiRuby com.dexels.navajo.tipi.ruby
+./cvsgit.sh com.dexels.navajo.tipi.rss.feature com.dexels.navajo.tipi.rss.feature
+./cvsgit.sh TipiRss com.dexels.navajo.tipi.rss
+./cvsgit.sh 
+./cvsgit.sh 
+
+./cvsgit.sh 
+
+
