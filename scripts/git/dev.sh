0777ee85184aa9856f90de5fda67a1a317bd1833
Frank Lyaruu <frank@dexels.com>
2012-03-02 17:16:30 +0000

    *** empty log message ***

--- scripts/git/dev.sh
+++ scripts/git/dev.sh
@@ -6,5 +6,15 @@
 ./cvsgit.sh com.dexels.navajo.dev com.dexels.navajo.dev.feature 
 ./cvsgit.sh NavajoScriptPlugin com.dexels.navajo.dev.script 
 ./cvsgit.sh com.dexels.navajo.server.embedded com.dexels.navajo.server.embedded
+./cvsgit.sh com.dexels.navajo.dsl.navajomanager com.dexels.navajo.dsl.navajomanager
+./cvsgit.sh com.dexels.navajo.dsl.expression com.dexels.navajo.dsl.expression
+./cvsgit.sh com.dexels.navajo.dsl.expression.model com.dexels.navajo.dsl.expression.model
+./cvsgit.sh com.dexels.navajo.dsl.expression.ui com.dexels.navajo.dsl.expression.ui
+./cvsgit.sh com.dexels.navajo.dsl.tsl com.dexels.navajo.dsl.tsl
+./cvsgit.sh com.dexels.navajo.dsl.tsl.model com.dexels.navajo.dsl.tsl.model
+./cvsgit.sh com.dexels.navajo.dsl.tsl.ui com.dexels.navajo.dsl.tsl.ui
+./cvsgit.sh com.dexels.navajo.dsl.dev.integration com.dexels.navajo.dsl.integration
+./cvsgit.sh com.dexels.navajo.language.feature com.dexels.navajo.dsl.feature
+./cvsgit.sh NavajoRemoteTestLibrary com.dexels.navajo.remotetest
 
 
