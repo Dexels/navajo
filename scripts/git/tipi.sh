7708132c579d25f9ac93a92a6e0d5e5ae864c64d
Frank Lyaruu <frank@dexels.com>
2012-03-02 17:50:22 +0000

    *** empty log message ***

---
+++ scripts/git/tipi.sh
@@ -0,0 +1,16 @@
+./cvsgit.sh NavajoTipi com.dexels.navajo.tipi
+./cvsgit.sh com.dexels.navajo.tipi.feature com.dexels.navajo.tipi.feature
+./cvsgit.sh com.dexels.navajo.tipi.swing.feature com.dexels.navajo.tipi.swing.feature
+./cvsgit.sh TipiSwingClient com.dexels.navajo.tipi.swing.client
+./cvsgit.sh NavajoSwingTipi com.dexels.navajo.tipi.swing
+./cvsgit.sh com.dexels.navajo.tipi.swing.deps com.dexels.navajo.tipi.swing.deps
+./cvsgit.sh com.dexels.navajo.tipi.swing.application com.dexels.navajo.tipi.swing.application
+./cvsgit.sh NavajoEchoTipi com.dexels.navajo.tipi.echo
+./cvsgit.sh NavajoEchoClient com.dexels.navajo.tipi.echo.client
+./cvsgit.sh com.dexels.navajo.tipi.echo.feature com.dexels.navajo.tipi.echo.feature
+./cvsgit.sh com.dexels.navajo.tipi.vaadin com.dexels.navajo.tipi.vaadin
+./cvsgit.sh com.dexels.navajo.tipi.vaadin.bridged com.dexels.navajo.tipi.vaadin.bridged
+./cvsgit.sh com.dexels.navajo.tipi.vaadin.embedded com.dexels.navajo.tipi.vaadin.embedded
+./cvsgit.sh com.dexels.navajo.tipi.vaadin.bridged.deploy com.dexels.navajo.tipi.vaadin.bridged.deploy
+./cvsgit.sh com.dexels.navajo.tipi.vaadin.feature com.dexels.navajo.tipi.vaadin.feature
+
