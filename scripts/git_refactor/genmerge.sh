f44fd05df352a15bf8cf182d2cc245dae5ba9deb
Frank Lyaruu <frank@dexels.com>
2012-03-27 15:21:34 +0000

    *** empty log message ***

--- scripts/git_refactor/genmerge.sh
+++ scripts/git_refactor/genmerge.sh
@@ -3,12 +3,13 @@ echo "PREPENDING PRECHECK: repo: $REPOSITORY"
 precheck.sh $1
 echo "genmerge: $BASEPATH/_$1"
 pwd
-mkdir $DIR/work
-cd $DIR/work
-echo "CD'd to: ${DIR}/work"
+cd $BASEPATH
+#mkdir  $DIR/work
+#cd $DIR/work
+#echo "CD'd to: ${DIR}/work"
 git clone ${BASEPATH}${REPOSITORY}_bare ${REPOSITORY}/
-cd ${BASEPATH}$REPOSITORY/$DIR/work/${REPOSITORY}/
+cd ${BASEPATH}$REPOSITORY/
 merge.sh ${REPOSITORY}/$1 ${BASEPATH}/_$1 
-cd ${DIR}/work/${REPOSITORY}/
-git push
-rm -rf ${DIR}/work/${REPOSITORY}/
+#cd ${DIR}/work/${REPOSITORY}/
+#git push
+#rm -rf ${DIR}/work/${REPOSITORY}/
