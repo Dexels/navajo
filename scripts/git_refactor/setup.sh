0de25f6a0f3512cb9a1a1ef4c6779dd5d62c9fbd
Frank Lyaruu <frank@dexels.com>
2012-03-27 15:08:15 +0000

    *** empty log message ***

--- scripts/git_refactor/setup.sh
+++ scripts/git_refactor/setup.sh
@@ -7,7 +7,7 @@ touch .gitignore
 git add .gitignore
 git config --global user.name "Dexels CVS migration"
 git config --global user.email info@dexels.com
-git commit --amend --reset-author -m "Migration"
+git commit --reset-author -m "Migration"
 cd ..
 git clone --bare -l ${BASEPATH}/${1}_initial ${BASEPATH}/${1}_bare
 echo "Converted to bare"
