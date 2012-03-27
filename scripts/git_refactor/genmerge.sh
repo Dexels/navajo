405743729d1ca30d807839905a86385bd00738ce
Frank Lyaruu <frank@dexels.com>
2012-03-27 14:55:15 +0000

    *** empty log message ***

--- scripts/git_refactor/genmerge.sh
+++ scripts/git_refactor/genmerge.sh
@@ -2,10 +2,10 @@ echo "PREPENDING PRECHECK: repo: $REPOSITORY"
 precheck.sh $1
 echo "genmerge: $BASEPATH/_$1"
 pwd
-cd ${BASEPATH}/$REPOSITORY/$DIR/work
-echo "CD'd to: ${REPOSITORY}/${DIR}/work"
-git clone ${REPOSITORY}_bare ${REPOSITORY}/
-cd ${REPOSITORY}/
+cd ${BASEPATH}$REPOSITORY/$DIR/work
+echo "CD'd to: ${BASEPATH}${REPOSITORY}/${DIR}/work"
+git clone ${BASEPATH}${REPOSITORY}_bare ${REPOSITORY}/
+cd ${BASEPATH}$REPOSITORY/$DIR/work/${REPOSITORY}/
 merge.sh ${REPOSITORY}/$1 ${BASEPATH}/_$1 
 cd ${DIR}/work/${REPOSITORY}/
 git push
--- scripts/git_refactor/teardown.sh
+++ scripts/git_refactor/teardown.sh
@@ -1,5 +1,8 @@
 echo "Remove work dir: ${BASEPATH}/${1}_initial"
+rm -rf ${BASEPATH}/${1}_initial
 echo "Remove work dir: ${BASEPATH}/${1}"
+rm -rf ${BASEPATH}/${1}
 echo "Remove work dir: ${BASEPATH}/${1}_bare"
+rm -rf ${BASEPATH}/${1}_bare
 
 #rm -rf work
