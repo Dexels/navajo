cce021bcea26985ff529d5dd46a106fbb3fbf4ee
Frank Lyaruu <frank@dexels.com>
2012-03-27 13:29:01 +0000

    trying to split repos

--- scripts/git/others.sh
+++ scripts/git/others.sh
@@ -3,29 +3,32 @@
 ./cvsgit.sh com.dexels.navajo.birt.oda.feature com.dexels.navajo.oda.feature
 ./cvsgit.sh com.dexels.navajo.oda com.dexels.navajo.oda
 ./cvsgit.sh com.dexels.navajo.oda.ui com.dexels.navajo.oda.ui
-./cvsgit.sh sportlink-serv com.sportlink.serv
+
+# V2?
+./cvsgit.sh NavajoSwingClient com.sportlink.swing.client
 ./cvsgit.sh sportlink-clnt-v2 com.sportlink.client
+
 ./cvsgit.sh sportlink-adapters com.sportlink.adapters
 ./cvsgit.sh sportlink-serv-crystal com.sportlink.crystal
 ./cvsgit.sh sportlink-serv-AAA com.sportlink.aaa
 ./cvsgit.sh sportlink-serv-comp com.sportlink.comp
-./cvsgit.sh sportlink-knvb-digitaalinschrijven com.sportlink.digitaalinschrijven
-./cvsgit.sh SportlinkDWF com.sportlink.dwf
-./cvsgit.sh SportlinkMatchForms com.sportlink.matchforms
-./cvsgit.sh GoogleEarthAccommodations com.sportlink.accommodations
-
-
-
 ./cvsgit.sh TensingOracle com.sportlink.tensing
-./cvsgit.sh SportlinkClub tipi.sportlink.club
-./cvsgit.sh SportlinkOfficialPortal tipi.sportlink.officialportal
-./cvsgit.sh NavajoSwingClient com.sportlink.swing.client
-./cvsgit.sh TipiLessonOne tipi.lesson.one
-./cvsgit.sh TipiLessonTwo tipi.lesson.two
-./cvsgit.sh TipiLessonThree tipi.lesson.three
-./cvsgit.sh TipiLessonFour tipi.lesson.four
-./cvsgit.sh TipiLessonFive tipi.lesson.five
-./cvsgit.sh TipiLessonSix tipi.lesson.six
+
+
+#./cvsgit.sh sportlink-knvb-digitaalinschrijven com.sportlink.digitaalinschrijven
+#./cvsgit.sh SportlinkDWF com.sportlink.dwf
+#./cvsgit.sh SportlinkMatchForms com.sportlink.matchforms
+#./cvsgit.sh sportlink-serv com.sportlink.serv
+#./cvsgit.sh GoogleEarthAccommodations com.sportlink.accommodations
+#./cvsgit.sh SportlinkClub com.sportlink.club
+#./cvsgit.sh SportlinkOfficialPortal com.sportlink.officialportal
+
+#./cvsgit.sh TipiLessonOne tipi.lesson.one
+#./cvsgit.sh TipiLessonTwo tipi.lesson.two
+#./cvsgit.sh TipiLessonThree tipi.lesson.three
+#./cvsgit.sh TipiLessonFour tipi.lesson.four
+#./cvsgit.sh TipiLessonFive tipi.lesson.five
+#./cvsgit.sh TipiLessonSix tipi.lesson.six
 ./cvsgit.sh TwitterAdapter com.dexels.navajo.twitter
 ./cvsgit.sh com.dexels.navajo.twitter.feature com.dexels.navajo.twitter.feature
 ./cvsgit.sh NavajoSvgRenderAdapter com.dexels.navajo.svg
