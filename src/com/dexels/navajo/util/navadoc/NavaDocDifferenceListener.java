7130a908db6ec3a2c4a4bc4553bf2a31ff6b3713
Frank Lyaruu <frank@dexels.com>
2012-04-02 10:58:57 +0000

    mvn/ tycho / warning / osgi fix

--- src/com/dexels/navajo/util/navadoc/NavaDocDifferenceListener.java
+++ src/com/dexels/navajo/util/navadoc/NavaDocDifferenceListener.java
@@ -58,6 +58,12 @@ public class NavaDocDifferenceListener implements DifferenceListener {
 
   } // public boolean haltComparison()
 
+@Override
+public int differenceFound(Difference arg0) {
+	// TODO Auto-generated method stub
+	return 0;
+}
+
 } // public class NavaDocDiferrenceListener
 
 // EOF: $RCSfile$ //
