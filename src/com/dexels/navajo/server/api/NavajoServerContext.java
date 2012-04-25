07fd71f8a26b5ed423a3939124beefed9bcb7c8b
Frank Lyaruu <frank@dexels.com>
2012-04-25 10:06:44 +0000

    upgraded api (NavajoServerContext) to Navajo

--- src/com/dexels/navajo/server/api/NavajoServerContext.java
+++
@@ -1,9 +0,0 @@
-package com.dexels.navajo.server.api;
-
-import com.dexels.navajo.server.DispatcherInterface;
-
-public interface NavajoServerContext {
-
-	public DispatcherInterface getDispatcher();
-	public String getInstallationPath();
-}
