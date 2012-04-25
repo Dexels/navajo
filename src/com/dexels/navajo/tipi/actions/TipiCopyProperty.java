5b60fdcfbe09cd9f37c9a1b664bf7ac33c39f402
Frank Lyaruu <frank@dexels.com>
2012-04-25 11:11:45 +0000

    added log action and copyProperty action

---
+++ src/com/dexels/navajo/tipi/actions/TipiCopyProperty.java
@@ -0,0 +1,88 @@
+package com.dexels.navajo.tipi.actions;
+
+import com.dexels.navajo.document.Message;
+import com.dexels.navajo.document.NavajoException;
+import com.dexels.navajo.document.Property;
+import com.dexels.navajo.tipi.TipiException;
+import com.dexels.navajo.tipi.internal.TipiAction;
+import com.dexels.navajo.tipi.internal.TipiEvent;
+
+/**
+ * <p>
+ * Title:
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * <p>
+ * Copyright: Copyright (c) 2003
+ * </p>
+ * <p>
+ * Company:
+ * </p>
+ * 
+ * @author not attributable
+ * @version 1.0
+ */
+public class TipiCopyProperty extends TipiAction {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5296764161841844878L;
+
+	public void execute(TipiEvent event)
+			throws com.dexels.navajo.tipi.TipiException,
+			com.dexels.navajo.tipi.TipiBreakException {
+		Property p = (Property)getEvaluatedParameterValue("property", event);
+		Message m =  (Message)getEvaluatedParameterValue("message", event);
+		String propertyName = (String)getEvaluatedParameterValue("propertyName", event);
+		if (p == null) {
+			throw new TipiException(
+					"Error in copyProperty action: property parameter missing!");
+		}
+		if (m == null) {
+			throw new TipiException(
+					"Error in addProperty action: navajo parameter missing!");
+		}
+	
+		try {
+			Property copy = p.copy(m.getRootDoc());
+			if(propertyName!=null) {
+				copy.setName(propertyName);
+			}
+			m.addProperty(copy);
+		} catch (NavajoException e) {
+			throw new TipiException("Error creating property: " + p);
+		}
+	}
+
+	public static void main(String[] args) {
+		String path = "1234/5678/90ab";
+		System.err.println(path);
+		String name = path.substring(path.lastIndexOf("/") + 1, path.length());
+		String pp = path.substring(0, path.lastIndexOf("/"));
+		System.err.println(name);
+		System.err.println(pp);
+
+	}
+}
+// <tipiaction name="addProperty" class="TipiAddProperty"
+// package="com.dexels.navajo.tipi.actions">
+// <param name="path" type="string" required="true"/>
+// <param name="value" type="object" required="true"/>
+// <param name="navajo" type="navajo required="true""/>
+// </tipiaction>
+//
+// <tipiaction name="addPropertyToMessage" class="TipiAddPropertyToMessage"
+// package="com.dexels.navajo.tipi.actions">
+// <param name="path" type="string" required="true"/>
+// <param name="value" type="object" required="true"/>
+// <param name="message" type="message" required="true"/>
+// </tipiaction>
+//
+// <tipiaction name="insertMessage" class="TipiInsertMessage"
+// package="com.dexels.navajo.tipi.actions">
+// <param name="path" type="string" required="true"/>
+// <param name="value" type="object" required="true"/>
+// <param name="message" type="message" required="true"/>
+// </tipiaction>
---
+++ src/com/dexels/navajo/tipi/actions/TipiLog.java
@@ -0,0 +1,75 @@
+package com.dexels.navajo.tipi.actions;
+
+import java.io.StringWriter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.dexels.navajo.document.Message;
+import com.dexels.navajo.tipi.internal.TipiAction;
+import com.dexels.navajo.tipi.internal.TipiEvent;
+
+/**
+ * <p>Title: </p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2003</p>
+ * <p>Company: </p>
+ * @author not attributable
+ * @version 1.0
+ */
+
+/** @todo Refactor, move to NavajoSwingTipi */
+public class TipiLog extends TipiAction {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4618070574411577748L;
+
+	
+	private final static Logger logger = LoggerFactory.getLogger(TipiLog.class);
+	
+	
+	public void execute(TipiEvent event)
+			throws com.dexels.navajo.tipi.TipiException,
+			com.dexels.navajo.tipi.TipiBreakException {
+		Object oo = getEvaluatedParameterValue("text", event);
+		String level = (String) getEvaluatedParameterValue("level", event);
+		String result = "";
+		if (oo == null) {
+			result = getText();
+			if (result == null) {
+				result = "Null value!";
+			}
+		} else {
+			if(oo instanceof Message) {
+				Message m = (Message)oo;
+				StringWriter sw = new StringWriter();
+				m.write(sw);
+				result = sw.toString();
+			}
+	
+			result = oo.toString();
+		}
+		if(level!=null) {
+			if(level.equals("debug")) {
+				logger.debug(result);
+			} else
+			if(level.equals("info")) {
+				logger.info(result);
+			} else
+			if(level.equals("warn")) {
+				logger.warn(result);
+			} else 
+			if(level.equals("error")) {
+				logger.error(result);
+			} else {
+				logger.warn("Unsupported log level: "+level);
+				logger.warn("Original log request: "+result);
+			}
+		} else {
+			logger.info(result);
+		}
+//		final String txt = result.replaceAll("\n", " ");
+//		myContext.showInfo(txt, "");
+	}
+}
