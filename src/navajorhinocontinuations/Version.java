c648d6fd548c0c4ea3e085b029bfdb487e51fe1a
Frank Lyaruu <frank@dexels.com>
2012-05-01 13:17:30 +0000

    fix osgi deregistration

--- src/navajorhinocontinuations/Version.java
+++ src/navajorhinocontinuations/Version.java
@@ -1,9 +1,12 @@
 package navajorhinocontinuations;
 
 import java.util.Dictionary;
+import java.util.HashSet;
 import java.util.Hashtable;
+import java.util.Set;
 
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
 
 import com.dexels.navajo.adapter.RhinoAdapterLibrary;
 import com.dexels.navajo.functions.util.FunctionFactoryFactory;
@@ -11,6 +14,7 @@ import com.dexels.navajo.functions.util.FunctionFactoryInterface;
 
 public class Version extends com.dexels.navajo.version.AbstractVersion {
 
+	private final Set<ServiceRegistration> adapterRegs = new HashSet<ServiceRegistration>();
 	@Override
 	public void start(BundleContext bc) throws Exception {
 		super.start(bc);
@@ -27,9 +31,19 @@ public class Version extends com.dexels.navajo.version.AbstractVersion {
 			 props.put("extension", library.getId());
 			 props.put("adapterClass", c.getName());
 			if(adapterClass!=null) {
-				context.registerService(Class.class.getName(), c, props);
+				ServiceRegistration sr = context.registerService(Class.class.getName(), c, props);
+				adapterRegs.add(sr);
 			}
 		}
 	
 	}
+	@Override
+	public void stop(BundleContext bc) throws Exception {
+		for (ServiceRegistration sr :adapterRegs) {
+			sr.unregister();
+		}
+		super.stop(bc);
+	}
+	
+	
 }
