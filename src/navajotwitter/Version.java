f76e1f0542569abbe1e83596e9cdc729a6ee799d
Frank Lyaruu <frank@dexels.com>
2012-05-03 16:59:08 +0000

    OSGi fix

--- src/navajotwitter/Version.java
+++ src/navajotwitter/Version.java
@@ -1,16 +1,22 @@
 package navajotwitter;
 
 import java.util.Dictionary;
+import java.util.HashSet;
 import java.util.Hashtable;
+import java.util.Set;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
 
 import com.dexels.navajo.functions.util.FunctionFactoryFactory;
 import com.dexels.navajo.functions.util.FunctionFactoryInterface;
 import com.dexels.twitter.TwitterAdapterLibrary;
 
 public class Version extends com.dexels.navajo.version.AbstractVersion implements BundleActivator {
+
+	private final Set<ServiceRegistration> adapterRegs = new HashSet<ServiceRegistration>();
+
 	@Override
 	public void start(BundleContext bc) throws Exception {
 		super.start(bc);
@@ -25,13 +31,22 @@ public class Version extends com.dexels.navajo.version.AbstractVersion implement
 			 Dictionary<String, Object> props = new Hashtable<String, Object>();
 			 props.put("adapterName", adapterName);
 			 props.put("adapterClass", c.getName());
+
 			if(adapterClass!=null) {
-				context.registerService(Class.class.getName(), c, props);
+				ServiceRegistration sr = context.registerService(Class.class.getName(), c, props);
+				adapterRegs.add(sr);
 			}
 		}
 	}
 
-
+	@Override
+	public void stop(BundleContext bc) throws Exception {
+		for (ServiceRegistration sr :adapterRegs) {
+			sr.unregister();
+		}
+		super.stop(bc);
+	}
+	
 	@Override
 	public void shutdown() {
 		super.shutdown();
