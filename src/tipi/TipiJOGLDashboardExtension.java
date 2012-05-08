7126008565181cfbd3d4a63b93ec7f9553d437a0
Frank Lyaruu <frank@dexels.com>
2012-05-08 14:29:28 +0000

    initial refactor

---
+++ src/tipi/TipiJOGLDashboardExtension.java
@@ -0,0 +1,23 @@
+package tipi;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.*;
+
+import com.dexels.navajo.tipi.*;
+import com.dexels.navajo.tipi.tipixml.CaseSensitiveXMLElement;
+import com.dexels.navajo.tipi.tipixml.XMLElement;
+import com.dexels.navajo.tipi.tipixml.XMLParseException;
+
+public class TipiJOGLDashboardExtension extends TipiAbstractXMLExtension implements TipiExtension {
+
+
+	public void initialize(TipiContext tc) {
+		// Do nothing
+		
+	}
+
+	
+
+}
