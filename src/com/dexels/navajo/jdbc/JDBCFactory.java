53b59d94cb5957fc09f63f954f25323c2cf14560
Arjen Schoneveld <aschoneveld@dexels.com>
2012-05-11 09:42:18 +0000

    Added support for non-OSGi

--- src/com/dexels/navajo/jdbc/JDBCFactory.java
+++ src/com/dexels/navajo/jdbc/JDBCFactory.java
@@ -1,12 +1,22 @@
 package com.dexels.navajo.jdbc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.dexels.navajo.adapter.JDBCMap;
 import com.dexels.navajo.adapter.SQLMap;
 
 public class JDBCFactory {
 	
+	public final static Logger logger = LoggerFactory.getLogger(JDBCFactory.class);
+	
 	private static boolean useOSGi() {
-		return navajoadapters.Version.getDefaultBundleContext()!=null;
+		try {
+			return navajoadapters.Version.getDefaultBundleContext()!=null;
+		} catch (Throwable t) {
+			logger.warn("No OSGi libraries found.");
+			return false;
+		}
 	}
 	
 	public static JDBCMappable getJDBCMap() {
