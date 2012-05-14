57180bf26a1bb7079192fe1432d0a5c1856ba9eb
Arjen Schoneveld <aschoneveld@dexels.com>
2012-05-14 08:44:55 +0000

    *** empty log message ***

--- src/com/dexels/navajo/jdbc/JDBCFactory.java
+++ src/com/dexels/navajo/jdbc/JDBCFactory.java
@@ -9,12 +9,23 @@ import com.dexels.navajo.adapter.SQLMap;
 public class JDBCFactory {
 	
 	public final static Logger logger = LoggerFactory.getLogger(JDBCFactory.class);
+	private static volatile int useOSGi = -1;
 	
-	private static boolean useOSGi() {
+	private final static boolean useOSGi() {
+		if ( useOSGi != -1) {
+			return ( useOSGi == 1);
+		}
 		try {
-			return navajoadapters.Version.getDefaultBundleContext()!=null;
+			boolean result = navajoadapters.Version.getDefaultBundleContext()!=null;
+			if ( result ) {
+				useOSGi = 1; 
+			} else {
+				useOSGi = 0;
+			}
+			return result;
 		} catch (Throwable t) {
 			logger.warn("No OSGi libraries found.");
+			useOSGi = 0;
 			return false;
 		}
 	}
