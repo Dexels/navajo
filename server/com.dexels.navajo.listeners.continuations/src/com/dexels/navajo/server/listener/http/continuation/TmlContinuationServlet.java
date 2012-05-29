b2130e90a17e3a86cc6aedeb46b37479f8cf8215
Frank Lyaruu <flyaruu@gmail.com>
2012-05-29 15:26:52 +0200

    major refactor, removing dep to NavajoListeners AND NavajoCore (... and introducing dep to ServerApi and NavajoListenersExternal)

--- server/com.dexels.navajo.listeners.continuations/src/com/dexels/navajo/server/listener/http/continuation/TmlContinuationServlet.java
+++ server/com.dexels.navajo.listeners.continuations/src/com/dexels/navajo/server/listener/http/continuation/TmlContinuationServlet.java
@@ -1,9 +1,6 @@
 package com.dexels.navajo.server.listener.http.continuation;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -14,12 +11,12 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.dexels.navajo.document.Header;
-import com.dexels.navajo.document.Navajo;
-import com.dexels.navajo.document.NavajoFactory;
+import com.dexels.navajo.script.api.AsyncRequest;
+import com.dexels.navajo.script.api.LocalClient;
 import com.dexels.navajo.script.api.SchedulableServlet;
+import com.dexels.navajo.script.api.TmlRunnable;
 import com.dexels.navajo.script.api.TmlScheduler;
-import com.jcraft.jzlib.ZInputStream;
+import com.dexels.navajo.server.listener.http.impl.BaseRequestImpl;
 
 
 
@@ -56,7 +53,7 @@ public class TmlContinuationServlet extends HttpServlet implements SchedulableSe
 
 	@SuppressWarnings("unused")
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doPost(final HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 		performInitialization();
 		TmlContinuationRunner tmlRunner = (TmlContinuationRunner) req.getAttribute("tmlRunner");
@@ -65,32 +62,43 @@ public class TmlContinuationServlet extends HttpServlet implements SchedulableSe
 			tmlRunner.endTransaction();
 			return;
 		}
+		
+		final LocalClient lc = (LocalClient) req.getServletContext().getAttribute("localClient");
+		if(lc==null) {
+			resp.sendError(500, "No local client registered in servlet context");
+			return;
+		}
 		boolean precheck = getTmlScheduler().preCheckRequest(req);
-		if(!precheck) {
+		if (!precheck) {
 			req.getInputStream().close();
 			resp.getOutputStream().close();
 			return;
 		}
-		Navajo inputDoc = parseInputNavajo(req);
-
-	  	  Object certObject = req.getAttribute( "javax.servlet.request.X509Certificate");
-			String recvEncoding = req.getHeader("Content-Encoding");
-			String sendEncoding = req.getHeader("Accept-Encoding");
-
-			boolean check = getTmlScheduler().checkNavajo(inputDoc);
-			if(!check) {
-				resp.getOutputStream().close();
-				return;
-			}
-
-			// Resolve remote address first. To prevent random IP addresses when trying to
-			// resolve it at a later time (???).
-			String remoteAddress = req.getRemoteAddr();
-			TmlContinuationRunner tr = new TmlContinuationRunner(req,inputDoc, resp,  sendEncoding, recvEncoding, certObject);
-			req.setAttribute("tmlRunner", tr);
 		
-		tr.suspendContinuation();
-		getTmlScheduler().submit(tr,false);
+		Object certObject = req
+				.getAttribute("javax.servlet.request.X509Certificate");
+		String recvEncoding = req.getHeader("Content-Encoding");
+		String sendEncoding = req.getHeader("Accept-Encoding");
+		AsyncRequest request = new BaseRequestImpl(lc,req, resp, sendEncoding, recvEncoding, certObject){
+
+			@Override
+			public TmlRunnable instantiateRunnable() {
+				String remoteAddress = req.getRemoteAddr();
+				TmlContinuationRunner tr = new TmlContinuationRunner(this,lc);
+				req.setAttribute("tmlRunner", tr);
+			
+				tr.suspendContinuation();
+				return tr;
+			}};
+
+		boolean check = getTmlScheduler().checkNavajo(request.getInputDocument());
+		if (!check) {
+			resp.getOutputStream().close();
+			return;
+		}
+
+		
+		getTmlScheduler().submit(request.instantiateRunnable(),false);
 	}
 
 	private void performInitialization() {
@@ -104,42 +112,42 @@ public class TmlContinuationServlet extends HttpServlet implements SchedulableSe
 //		}
 	}
 
-	
-	protected final Navajo parseInputNavajo(HttpServletRequest request) throws IOException, UnsupportedEncodingException {
-		InputStream is = request.getInputStream();
-		// TODO This is a bug: Accept encoding is what encoding the client will accept in the
-		// response. Here it is used as the Content Encoding of the request.
-		// Not a problem as long as it is the same, but needs to be fixed.
-		// http://spiritus.dexels.nl:6070/browse/NAVAJO-29
-		String sendEncoding = request.getHeader("Accept-Encoding");
-
-		BufferedReader r;
-		  Navajo in = null;
-		if (sendEncoding != null && sendEncoding.equals(COMPRESS_JZLIB)) {
-			r = new BufferedReader(new java.io.InputStreamReader(
-					new ZInputStream(is)));
-		} else if (sendEncoding != null && sendEncoding.equals(COMPRESS_GZIP)) {
-			r = new BufferedReader(new java.io.InputStreamReader(
-					new java.util.zip.GZIPInputStream(is), "UTF-8"));
-		} else {
-			r = new BufferedReader(new java.io.InputStreamReader(is, "UTF-8"));
-		}
-		in = NavajoFactory.getInstance().createNavajo(r);
-
-		  if (in == null) {
-			  throw new IOException("Invalid request.");
-		  }
-
-		  Header header = in.getHeader();
-		  if (header == null) {
-			  throw new IOException("Empty Navajo header.");
-		  }
-
-		  is.close();
-
-		return in;
-	}
-	
+//	
+//	protected final Navajo parseInputNavajo(HttpServletRequest request) throws IOException, UnsupportedEncodingException {
+//		InputStream is = request.getInputStream();
+//		// TODO This is a bug: Accept encoding is what encoding the client will accept in the
+//		// response. Here it is used as the Content Encoding of the request.
+//		// Not a problem as long as it is the same, but needs to be fixed.
+//		// http://spiritus.dexels.nl:6070/browse/NAVAJO-29
+//		String sendEncoding = request.getHeader("Accept-Encoding");
+//
+//		BufferedReader r;
+//		  Navajo in = null;
+//		if (sendEncoding != null && sendEncoding.equals(COMPRESS_JZLIB)) {
+//			r = new BufferedReader(new java.io.InputStreamReader(
+//					new ZInputStream(is)));
+//		} else if (sendEncoding != null && sendEncoding.equals(COMPRESS_GZIP)) {
+//			r = new BufferedReader(new java.io.InputStreamReader(
+//					new java.util.zip.GZIPInputStream(is), "UTF-8"));
+//		} else {
+//			r = new BufferedReader(new java.io.InputStreamReader(is, "UTF-8"));
+//		}
+//		in = NavajoFactory.getInstance().createNavajo(r);
+//
+//		  if (in == null) {
+//			  throw new IOException("Invalid request.");
+//		  }
+//
+//		  Header header = in.getHeader();
+//		  if (header == null) {
+//			  throw new IOException("Empty Navajo header.");
+//		  }
+//
+//		  is.close();
+//
+//		return in;
+//	}
+//	
 	@Override
 	public TmlScheduler getTmlScheduler() {
 		return (TmlScheduler) getServletContext().getAttribute("tmlScheduler");
