1613304e3609a269bc35d174dcfc9be321afa586
Frank Lyaruu <flyaruu@gmail.com>
2012-06-06 12:19:14 +0200

    simplified, removed factory pattern from navajocontext (If we want to use multiple navajo instances in one OSGi environment, we'll need some kind of isolation mechanism anyway.
     - Wrap it in J2EE
     - Virgo can do it
     - Karaf can do it

--- server/com.dexels.navajo.server.bridged/src/com/dexels/navajo/server/instance/NavajoHttpServiceContextComponent.java
+++ server/com.dexels.navajo.server.bridged/src/com/dexels/navajo/server/instance/NavajoHttpServiceContextComponent.java
@@ -1,66 +1,65 @@
 package com.dexels.navajo.server.instance;
 
-import javax.servlet.ServletContext;
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.Properties;
 
-import org.osgi.framework.BundleContext;
-import org.osgi.service.cm.ConfigurationAdmin;
 import org.osgi.service.component.ComponentContext;
-import org.osgi.service.http.HttpService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.dexels.navajo.server.DispatcherInterface;
+import com.dexels.navajo.server.api.NavajoServerContext;
+import com.dexels.navajo.server.api.impl.NavajoServerInstance;
 import com.dexels.navajo.server.listener.NavajoContextListener;
 
-public class NavajoHttpServiceContextComponent {
+public class NavajoHttpServiceContextComponent implements NavajoServerContext{
 
 	private static final Logger logger = LoggerFactory.getLogger(NavajoHttpServiceContextComponent.class);
 	
-	private ServletContext servletContext;
-	private BundleContext bundleContext;
-	private NavajoContextFactory factory;
-	private HttpService httpService;
-//	private ConfigurationAdmin configurationAdmin;
-
+	private NavajoServerInstance wrapped = null;
+	
 	public void activate(ComponentContext c) {
 		logger.info("Activating HTTP server component");
-		this.bundleContext = c.getBundleContext();
-		NavajoContextFactory ncf = new NavajoContextFactory(this.bundleContext,this.httpService);
-		this.factory = ncf;
-//		try {
-//			Configuration configuration = configurationAdmin.createFactoryConfiguration("navajo.server.context.factory");
-//			Dictionary<String,String> dict = new Hashtable<String,String>();
-//			dict.put("contextPath", "/");
-//			dict.put("installationPath", "/Users/frank/Documents/workspace-indigo/sportlink-serv/navajo-tester/auxilary");
-//			configuration.update(dict);
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-		
-//		"navajo.server.context"
-		
+		updated(c.getProperties());
+	}
+	
+	@SuppressWarnings("rawtypes")
+	public void updated(Dictionary settings) {
+		String pid = (String) settings.get("service.pid");
+		logger.info("Configuration received, pid: "+pid);
+		try {
+			logger.info("Instantiating with pid: "+pid);
+			Properties prop = new Properties(); 
+			Enumeration en = settings.keys();
+			while (en.hasMoreElements()) {
+				String key = (String) en.nextElement();
+				prop.put(key, settings.get(key));
+			}
+			// TODO add context prefix, and link?
+			String contextPath = (String)settings.get("contextPath");
+			String servletContextPath = (String)settings.get("servletContextPath");
+			String installPath = (String)settings.get("installationPath");
+			logger.info("Instantiate server: "+contextPath+" installpath: "+installPath);
+			wrapped = NavajoContextListener.initializeServletContext(contextPath,servletContextPath,installPath);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void deactivate() {
-		this.factory.getFactoryRegistration().unregister();
+		logger.info("Deactivating service component");
 	}
 
-	protected void setHttpService(HttpService httpService) {
-		this.httpService = httpService;
+	@Override
+	public DispatcherInterface getDispatcher() {
+		return wrapped.getDispatcher();
 	}
 
-
-	public void removeHttpService(HttpService httpService) {
-		logger.info("Shutting down navajo instance");
-		NavajoContextListener.destroyContext(servletContext);
-		servletContext = null;
-		httpService = null;
+	@Override
+	public String getInstallationPath() {
+		return wrapped.getInstallationPath();
 	}
 
-	protected void setConfigurationAdmin(ConfigurationAdmin configurationAdmin) {
-//		this.configurationAdmin = configurationAdmin;
-	}
-	protected void clearConfigurationAdmin(ConfigurationAdmin configurationAdmin) {
-//		this.configurationAdmin = null;
-	}
 
 }
