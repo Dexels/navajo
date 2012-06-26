ba61a92390b37678616881260c4d65468b1bb021
Frank Lyaruu <flyaruu@gmail.com>
2012-06-26 23:36:38 +0200

    added monkey example

--- example/com.dexels.navajo.example.adapter/src/com/dexels/navajo/example/adapter/Activator.java
+++ example/com.dexels.navajo.example.adapter/src/com/dexels/navajo/example/adapter/Activator.java
@@ -1,11 +1,21 @@
 package com.dexels.navajo.example.adapter;
 
+import java.util.Dictionary;
+import java.util.Hashtable;
+
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import com.dexels.navajo.functions.util.FunctionFactoryFactory;
+import com.dexels.navajo.functions.util.FunctionFactoryInterface;
+import com.dexels.navajo.server.LocalClientDispatcherWrapper;
 public class Activator implements BundleActivator {
 
 	private static BundleContext context;
+	private final static Logger logger = LoggerFactory
+			.getLogger(Activator.class);
 
 	static BundleContext getContext() {
 		return context;
@@ -17,6 +27,31 @@ public class Activator implements BundleActivator {
 	 */
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
+		FunctionFactoryInterface fi= FunctionFactoryFactory.getInstance();
+		fi.init();
+		
+		fi.clearFunctionNames();
+
+		ExampleAdapterLibrary library = new ExampleAdapterLibrary();
+		fi.injectExtension(library);
+		try {
+			for(String adapterName: fi.getAdapterNames(library)) {
+
+				String adapterClass = fi.getAdapterClass(adapterName,library);
+				Class<?> c = Class.forName(adapterClass);
+
+				 Dictionary<String, Object> props = new Hashtable<String, Object>();
+				 props.put("adapterName", adapterName);
+				 props.put("adapterClass", c.getName());
+				 System.err.println("registering: "+adapterName);
+				 if(adapterClass!=null) {
+					context.registerService(Class.class.getName(), c, props);
+				}
+			}
+		} catch (Throwable e) {
+			logger.error("Error starting navajo core bundle.",e);
+		}
+
 	}
 
 	/*
