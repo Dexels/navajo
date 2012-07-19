6e406a99e8800f29daabbde206bb8a1a3bc8f3d3
Frank Lyaruu <flyaruu@gmail.com>
2012-07-19 17:50:32 +0200

    merge java compiler components+ create DS components

--- server/com.dexels.navajo.compiler.tsl/src/com/dexels/navajo/compiler/scriptenumerator/internal/TslScriptEnumerator.java
+++ server/com.dexels.navajo.compiler.tsl/src/com/dexels/navajo/compiler/scriptenumerator/internal/TslScriptEnumerator.java
@@ -1,29 +1,24 @@
-package com.dexels.navajo.compiler.tsl.internal;
+package com.dexels.navajo.compiler.scriptenumerator.internal;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.dexels.navajo.compiler.ScriptCompiler;
+import com.dexels.navajo.compiler.ScriptEnumerator;
 import com.dexels.navajo.mapping.compiler.TslCompiler;
 import com.dexels.navajo.server.NavajoIOConfig;
 
-public class TslCompilerComponent implements ScriptCompiler {
+public class TslScriptEnumerator implements ScriptEnumerator {
 
 	private NavajoIOConfig navajoIOConfig = null;
 	
 	private final static Logger logger = LoggerFactory
-			.getLogger(TslCompilerComponent.class);
+			.getLogger(TslScriptEnumerator.class);
 	
-	/* (non-Javadoc)
-	 * @see com.dexels.navajo.compiler.tsl.ScriptCompiler#compileTsl(java.lang.String)
-	 */
-	@Override
-	public void compileTsl(String script) throws Exception {
-		String packagePath = script.substring(0,script.lastIndexOf('/'));
-		String javaFile = TslCompiler.compileToJava(script, navajoIOConfig.getScriptPath(), navajoIOConfig.getCompiledScriptPath(), packagePath, null, navajoIOConfig);
-		logger.info("Javafile: "+javaFile);
-	}
-	
+
 	public void setIOConfig(NavajoIOConfig config) {
 		this.navajoIOConfig = config;
 	}
@@ -33,7 +28,7 @@ public class TslCompilerComponent implements ScriptCompiler {
 	}
 	
 	public void activate() {
-//		logger.info("ACTIVATE");
+		logger.info("ACTIVATE");
 //		TslCompiler.compileToJava(script, input, output, packagePath, getClass().getClassLoader(),navajoIOConfig);
 	}
 	
@@ -41,5 +36,10 @@ public class TslCompilerComponent implements ScriptCompiler {
 		String script = "club/InitUpdateClub";
 		String pack = script.substring(0,script.lastIndexOf('/'));
 		System.err.println("pack: "+pack);
+	}
+
+	@Override
+	public List<String> enumerateScripts() {
+		return new ArrayList<String>();
 	}
 }
