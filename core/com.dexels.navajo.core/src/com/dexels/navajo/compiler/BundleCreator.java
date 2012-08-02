a8e578f4c91ea4b8bbb5039747af503aef243ba3
Frank Lyaruu <flyaruu@gmail.com>
2012-08-02 17:37:09 +0200

    added support for 'verify' command (load as much as possible, to debug classloader problems, I doubt if it will be of a lot of use once it is stable)

--- core/com.dexels.navajo.core/src/com/dexels/navajo/compiler/BundleCreator.java
+++ core/com.dexels.navajo.core/src/com/dexels/navajo/compiler/BundleCreator.java
@@ -4,9 +4,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Date;
 import java.util.List;
-import java.util.Set;
-
-import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleException;
 
 /**
@@ -31,7 +28,7 @@ public interface BundleCreator {
 //	public Collection<Long> installBundles(String scriptPrefix) throws BundleException;
 
 	public void createBundle(String script, String compileDate, String extension,
-			List<String> failures, List<String> success, List<String> skipped, boolean force) throws Exception;
+			List<String> failures, List<String> success, List<String> skipped, boolean force,boolean keepIntermediateFiles) throws Exception;
 
 	public Date getBundleInstallationDate(String scriptPath);
 	public Date getScriptModificationDate(String scriptPath);
@@ -41,5 +38,8 @@ public interface BundleCreator {
 	public void installBundles(String scriptPath, List<String> failures,List<String> success, List<String> skipped, boolean force) throws Exception;
 	public void installBundle(File bundleFile, String scriptPath,
 			List<String> failures, List<String> success, List<String> skipped, boolean force);
+
+	public void verifyScript(String script, List<String> failed,
+			List<String> success);
 
 }
