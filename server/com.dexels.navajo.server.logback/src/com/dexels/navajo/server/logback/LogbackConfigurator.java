c511c5c02a78127565da42a08d07250e8f3ad501
Frank Lyaruu <flyaruu@gmail.com>
2012-08-15 12:29:30 +0200

    finished logback, will now log per user, service, and stats

--- server/com.dexels.navajo.server.logback/src/com/dexels/navajo/server/logback/LogbackConfigurator.java
+++ server/com.dexels.navajo.server.logback/src/com/dexels/navajo/server/logback/LogbackConfigurator.java
@@ -5,15 +5,12 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Map;
 
 import org.slf4j.LoggerFactory;
 
-import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.joran.JoranConfigurator;
 import ch.qos.logback.core.joran.spi.JoranException;
-import ch.qos.logback.core.spi.PropertyDefiner;
 import ch.qos.logback.core.util.StatusPrinter;
 
 import com.dexels.navajo.server.api.NavajoServerContext;
@@ -22,7 +19,7 @@ import com.dexels.navajo.server.api.NavajoServerContext;
 public class LogbackConfigurator {
 
 	
-	private final static Logger logger = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+//	private final static Logger logger = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
 	
 	private NavajoServerContext navajoServerContext;
 
@@ -39,19 +36,20 @@ public class LogbackConfigurator {
 	
 	public void activate() {
 		try {
-			System.err.println("logback: ");
 			String path = navajoServerContext.getInstallationPath();
+//			Map<String,String> props = navajoServerContext.getDispatcher().getNavajoConfig().getProperties();
+//			System.err.println("props: "+props);
 			File install = new File(path);
 			File joranFile = new File(install,"config/logback.xml");
 			if(!joranFile.exists()) {
 				return; //
 			}
-			PropertyDefiner propertyDefiner = new NavajoInstallationPropertyDefiner(path);
+//			PropertyDefiner propertyDefiner = new NavajoInstallationPropertyDefiner(path);
 
 			FileInputStream joranFis = null;
 			try {
 				 joranFis = new FileInputStream(joranFile);
-				 loadLogbackConfig(joranFis);
+				 loadLogbackConfig(joranFis,path);
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
 			} finally {
@@ -68,19 +66,22 @@ public class LogbackConfigurator {
 		}
 	}
 	
-	public void loadLogbackConfig(InputStream is) {
+	private void loadLogbackConfig(InputStream is, String path) {
 		LoggerContext lc =(LoggerContext)LoggerFactory.getILoggerFactory();
+	      lc.reset(); 
 
 		StatusPrinter.print(lc);
-		
+//		MDC.put("rootPath", path);
 	    try {
-	      JoranConfigurator configurator = new JoranConfigurator();
+		  lc.putProperty("rootPath", path);
+		  JoranConfigurator configurator = new JoranConfigurator();
 	      configurator.setContext(lc);
+	      
 	      // the context was probably already configured by default configuration 
 	      // rules
-	      lc.reset(); 
 	      configurator.doConfigure(is);
-			StatusPrinter.print(lc);
+	      StatusPrinter.print(lc);
+	      
 	    } catch (JoranException je) {
 	       je.printStackTrace();
 	    }
