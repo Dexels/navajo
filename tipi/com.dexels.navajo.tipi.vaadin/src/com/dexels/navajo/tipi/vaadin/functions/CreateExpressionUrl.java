5035a47ab2f1484d9ef345950f156f0fea6e8c43
Frank Lyaruu <flyaruu@gmail.com>
2012-08-30 12:15:17 +0200

    renamed function to more sensible name

--- tipi/com.dexels.navajo.tipi.vaadin/src/com/dexels/navajo/tipi/vaadin/functions/CreateExpressionUrl.java
+++ tipi/com.dexels.navajo.tipi.vaadin/src/com/dexels/navajo/tipi/vaadin/functions/CreateExpressionUrl.java
@@ -4,20 +4,24 @@ import com.dexels.navajo.parser.FunctionInterface;
 import com.dexels.navajo.parser.TMLExpressionException;
 import com.dexels.navajo.tipi.vaadin.VaadinTipiContext;
 
-public class CreateEchoUrl extends FunctionInterface {
+public class CreateExpressionUrl extends FunctionInterface {
 
 	@Override
 	public Object evaluate() throws TMLExpressionException {
-		if (getOperands().size() != 2) {
+		if (getOperands().size() < 2) {
 			throw new TMLExpressionException("CreateEchoUrl needs two parameters");
 		}
 		Object oo = getOperand(0);
 		if(!(oo instanceof VaadinTipiContext)) {
-			throw new TMLExpressionException("CreateEchoUrl: param one should be an EchoTipiContext");
+			throw new TMLExpressionException("CreateEchoUrl: param one should be an VaadinTipiContext");
+		}
+		String mime = null;
+		if(getOperands().size()>2) {
+			mime = (String) getOperand(2);
 		}
 		VaadinTipiContext ee = (VaadinTipiContext)getOperand(0);
 		String expression = (String)getOperand(1);
-		String result = ee.createExpressionUrl(expression);
+		String result = ee.createExpressionUrl(expression,mime);
 		return result;
 	}
 
