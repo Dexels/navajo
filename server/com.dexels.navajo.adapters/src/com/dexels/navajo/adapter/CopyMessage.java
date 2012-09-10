cff7acae44c56a90aa9b435ed9f22e323dfaf229
Arjen Schoneveld <aschoneveld@dexels.com>
2012-09-10 12:40:46 +0200

    Added support for copying message from current in- our out-message stack.

--- server/com.dexels.navajo.adapters/src/com/dexels/navajo/adapter/CopyMessage.java
+++ server/com.dexels.navajo.adapters/src/com/dexels/navajo/adapter/CopyMessage.java
@@ -11,23 +11,31 @@ public class CopyMessage implements Mappable {
 
   private Navajo outputDoc;
   private Navajo inDoc;
+  private Access myAccess;
   public boolean useOutputDoc = true;
-  public String copyMessageFrom = "";
+  public String copyMessageFrom = ""; // If copyMessageFrom is empty, either the currently processed incoming (useOutputDoc = false)
+  									  // or currently processed outgoing (useOutputDoc = true) will be copied.
   public String copyMessageTo = "";
 
   public void load(Access access) throws MappableException, UserException {
     outputDoc = access.getOutputDoc();
     inDoc = access.getInDoc();
+    myAccess = access;
   }
 
   public void store() throws MappableException, UserException {
 
-    if (copyMessageFrom.equals("") && copyMessageTo.equals(""))
+    if (copyMessageTo.equals(""))
       throw new UserException( -1,
-          "copyMessageFrom and copyMessageTo have to be specified");
+          "copyMessageTo has to be specified");
 
-    Message from = (useOutputDoc) ? outputDoc.getMessage(this.copyMessageFrom) : inDoc.getMessage(this.copyMessageFrom);
-
+    Message from = null;
+    if ( copyMessageFrom.equals("") ) {
+    	from = (useOutputDoc) ? myAccess.getCompiledScript().getCurrentOutMsg() : myAccess.getCompiledScript().getCurrentInMsg();
+    } else {
+    	from = (useOutputDoc) ? outputDoc.getMessage(this.copyMessageFrom) : inDoc.getMessage(this.copyMessageFrom);
+    }
+    
     if (from == null)
       throw new UserException( -1,
                               "Could not find message " + this.copyMessageFrom +
