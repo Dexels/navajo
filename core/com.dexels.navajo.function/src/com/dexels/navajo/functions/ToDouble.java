7e3272298bc597eb62073ea3c54b67adb7b48757
Frank Lyaruu <flyaruu@gmail.com>
2012-10-03 17:56:04 +0200

    fix test main

--- core/com.dexels.navajo.function/src/com/dexels/navajo/functions/ToDouble.java
+++ core/com.dexels.navajo.function/src/com/dexels/navajo/functions/ToDouble.java
@@ -12,7 +12,12 @@ package com.dexels.navajo.functions;
 
 import java.util.Locale;
 
+import com.dexels.navajo.document.NavajoFactory;
+import com.dexels.navajo.document.Operand;
 import com.dexels.navajo.document.types.Money;
+import com.dexels.navajo.functions.util.FunctionFactoryFactory;
+import com.dexels.navajo.functions.util.FunctionFactoryInterface;
+import com.dexels.navajo.parser.DefaultExpressionEvaluator;
 import com.dexels.navajo.parser.FunctionInterface;
 
 
@@ -21,7 +26,7 @@ public final class ToDouble extends FunctionInterface {
     public ToDouble() {}
 
     public final Object evaluate() throws com.dexels.navajo.parser.TMLExpressionException {
-        Object o = this.getOperands().get(0);
+		Object o = this.getOperands().get(0);
         if (o==null) {
           return new Double(0);
         }
@@ -41,11 +46,24 @@ public final class ToDouble extends FunctionInterface {
     
     public static void main(String [] args) throws Exception {
    	 Locale.setDefault(Locale.GERMAN);
+		FunctionFactoryInterface fi= FunctionFactoryFactory.getInstance();
+		fi.init();
+		fi.getDef("ToDouble");
+		FunctionInterface td = fi.getInstance(ToDouble.class.getClassLoader(), "ToDouble");
     	Money m = new Money(5.0);
-    	ToDouble td = new ToDouble();
+    	
+    	String expr = "-1 * ToMoney( ( ToDouble(1000) / ( 100 + ( ToDouble(ToPercentage(0.2)) * 100 ) ) ) * ( 100 * ToDouble(ToPercentage(0.3)) ) )";
+    	NavajoFactory.getInstance().setExpressionEvaluator(new DefaultExpressionEvaluator());
+    	Operand o = NavajoFactory.getInstance().getExpressionEvaluator().evaluate(expr, null);
+    	System.err.println("o: "+o.value);
+    	//    	ToDouble td = new ToDouble();
     	td.reset();
     	td.insertOperand(m);
     	System.err.println(td.evaluate()+"");
+    	Class[] s =td.getReturnType();
+    	for (Class class1 : s) {
+			System.err.println("typw: "+class1);
+		}
     }
 
 }
