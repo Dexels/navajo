3e9665ea952896a94015a50e450acbcedf429c24
Frank Lyaruu <flyaruu@gmail.com>
2012-11-30 13:45:37 +0100

    incremental camel update

--- runtime/com.dexels.navajo.camel/src/com/dexels/navajo/camel/processor/NavajoMailProcessor.java
+++ runtime/com.dexels.navajo.camel/src/com/dexels/navajo/camel/processor/NavajoMailProcessor.java
@@ -1,13 +1,19 @@
 package com.dexels.navajo.camel.processor;
 
 import org.apache.camel.Exchange;
+import org.apache.camel.Message;
 import org.apache.camel.Processor;
 
 public class NavajoMailProcessor implements Processor {
 
+	public NavajoMailProcessor() {
+		System.err.println("Creating navajo mail processor");
+	}
+	
 	@Override
 	public void process(Exchange ex) throws Exception {
-		// TODO Auto-generated method stub
+		Message in = ex.getIn();
+		Message out = ex.getOut();
 
 	}
 
