815846ee2cdccca50e8842db9250013a93ba50f0
Frank Lyaruu <flyaruu@gmail.com>
2012-12-06 14:01:31 +0100

    remove auditlog dependency

--- core/com.dexels.navajo.core/src/com/dexels/navajo/server/impl/DispatcherComponent.java
+++ core/com.dexels.navajo.core/src/com/dexels/navajo/server/impl/DispatcherComponent.java
@@ -1,7 +1,6 @@
 package com.dexels.navajo.server.impl;
 
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceRegistration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,32 +10,31 @@ import com.dexels.navajo.server.Dispatcher;
 import com.dexels.navajo.server.DispatcherFactory;
 import com.dexels.navajo.server.DispatcherInterface;
 import com.dexels.navajo.server.NavajoConfigInterface;
-import com.dexels.navajo.util.AuditLog;
 
 public class DispatcherComponent extends Dispatcher implements
 		DispatcherInterface {
 
-	AuditLog auditLog = null;
+//	AuditLog auditLog = null;
 	BundleContext myContext = null;
 	NavajoEventRegistry myRegistry = null;
 	
 	private final static Logger logger = LoggerFactory
 			.getLogger(DispatcherComponent.class);
-	private ServiceRegistration<AuditLog> auditLogSr;
+//	private ServiceRegistration<AuditLog> auditLogSr;
 	
 	public void activate(BundleContext context) {
 		logger.info("Activate dispatcher!");
 		myContext = context;
 		// Register AuditLog.
-		AuditLog al = new AuditLog(getNavajoConfig().getInstanceName(), myRegistry);
-		auditLog = al;
-		auditLogSr = myContext.registerService(AuditLog.class, al, null);
+//		AuditLog al = new AuditLog(getNavajoConfig().getInstanceName(), myRegistry);
+//		auditLog = al;
+//		auditLogSr = myContext.registerService(AuditLog.class, al, null);
 		new DispatcherFactory(this);
 	}
 	public void deactivate() {
 		logger.info("Deactivate dispatcher!");
 		myContext = null;
-		auditLogSr.unregister();
+//		auditLogSr.unregister();
 	}
 
 	public void setNavajoConfig(NavajoConfigInterface nci) {
