0a468ab9a627ee7913a71dbc7a8f0b7bcfca7c0e
Frank Lyaruu <flyaruu@gmail.com>
2012-12-06 14:00:58 +0100

    OSGi support for dummy tribe manager

--- core/com.dexels.navajo.core/src/com/dexels/navajo/server/enterprise/tribe/DummyTribeManager.java
+++ core/com.dexels.navajo.core/src/com/dexels/navajo/server/enterprise/tribe/DummyTribeManager.java
@@ -3,24 +3,20 @@ package com.dexels.navajo.server.enterprise.tribe;
 import java.util.HashSet;
 import java.util.Set;
 
-import navajocore.Version;
-
 import com.dexels.navajo.document.Navajo;
 import com.dexels.navajo.server.Access;
-import com.dexels.navajo.server.DispatcherFactory;
-import com.dexels.navajo.server.NavajoConfigInterface;
 
 public class DummyTribeManager implements TribeManagerInterface {
 
-	private NavajoConfigInterface navajoConfig;
+//	private NavajoConfigInterface navajoConfig;
 
 	public void terminate() {
 	}
 
 	public DummyTribeManager() {
-		if(!Version.osgiActive()) {
-			navajoConfig = DispatcherFactory.getInstance().getNavajoConfig();
-		}
+//		if(!Version.osgiActive()) {
+//			navajoConfig = DispatcherFactory.getInstance().getNavajoConfig();
+//		}
 	}
 	
 	public Navajo forward(Navajo in) throws Exception {
@@ -35,15 +31,6 @@ public class DummyTribeManager implements TribeManagerInterface {
 
 	}
 
-	public void setNavajoConfig(NavajoConfigInterface nc) {
-		this.navajoConfig = nc;
-	}
-
-	public void clearNavajoConfig(NavajoConfigInterface nc) {
-		this.navajoConfig = null;
-	}
-	
-	
 	/**
 	 * Without a Tribe I am ALWAYS the chief!
 	 */
@@ -57,13 +44,11 @@ public class DummyTribeManager implements TribeManagerInterface {
 
 	public void tribalAfterWebServiceRequest(String service, Access a,
 			HashSet<String> ignoreTaskIds) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	public Navajo tribalBeforeWebServiceRequest(String service, Access a,
 			HashSet<String> ignoreList) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
@@ -88,31 +73,26 @@ public class DummyTribeManager implements TribeManagerInterface {
 	}
 
 	public void multicast(Object[] recipients, SmokeSignal m) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	public Answer askAnybody(Request q) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	public Navajo forward(Navajo in, Object address) throws Exception {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	public void addTribeMember(TribeMemberInterface tm) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	public String getMyName() {
-		return DispatcherFactory.getInstance().getNavajoConfig().getInstanceName();
+		return "dummytribemember";
 	}
 
 	public String getStatistics() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
