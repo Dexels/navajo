701720ad4c4c816b20a8c073b3196364bb48106f
Frank Lyaruu <flyaruu@gmail.com>
2012-12-07 17:40:47 +0100

    removed dispatcher link

--- server/com.dexels.navajo.listeners/src/com/dexels/navajo/server/api/impl/NavajoServerInstance.java
+++ server/com.dexels.navajo.listeners/src/com/dexels/navajo/server/api/impl/NavajoServerInstance.java
@@ -14,18 +14,14 @@ import com.dexels.navajo.server.api.NavajoServerContext;
 public class NavajoServerInstance implements NavajoServerContext {
 
 	private final String installationPath;
-	private final DispatcherInterface dispatcher;
+//	private final DispatcherInterface dispatcher;
 
 	public NavajoServerInstance(String installationPath,
 			DispatcherInterface dispatcher) {
 		this.installationPath = installationPath;
-		this.dispatcher = dispatcher;
+//		this.dispatcher = dispatcher;
 	}
 
-	@Override
-	public DispatcherInterface getDispatcher() {
-		return dispatcher;
-	}
 
 	@Override
 	public String getInstallationPath() {
