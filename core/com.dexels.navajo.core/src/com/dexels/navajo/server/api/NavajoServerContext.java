1608af76c4ad7d643f3dee84bdbcef133f154c80
Frank Lyaruu <flyaruu@gmail.com>
2012-12-07 17:37:44 +0100

    removed link to Dispatcher

--- core/com.dexels.navajo.core/src/com/dexels/navajo/server/api/NavajoServerContext.java
+++ core/com.dexels.navajo.core/src/com/dexels/navajo/server/api/NavajoServerContext.java
@@ -1,9 +1,8 @@
 package com.dexels.navajo.server.api;
 
-import com.dexels.navajo.server.DispatcherInterface;
 
 public interface NavajoServerContext {
 
-	public DispatcherInterface getDispatcher();
+//	public DispatcherInterface getDispatcher();
 	public String getInstallationPath();
 }
