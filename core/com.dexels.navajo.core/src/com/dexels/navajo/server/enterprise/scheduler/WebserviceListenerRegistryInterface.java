b2cee1c407a47922bf1cc8ffcf4061eb136f29dc
Frank Lyaruu <flyaruu@gmail.com>
2012-12-13 15:06:06 +0100

    added methods to Webservicelistenerinterface to be able to use the interface consistently

--- core/com.dexels.navajo.core/src/com/dexels/navajo/server/enterprise/scheduler/WebserviceListenerRegistryInterface.java
+++ core/com.dexels.navajo.core/src/com/dexels/navajo/server/enterprise/scheduler/WebserviceListenerRegistryInterface.java
@@ -1,5 +1,7 @@
 package com.dexels.navajo.server.enterprise.scheduler;
 
+import java.util.HashSet;
+
 import com.dexels.navajo.document.Navajo;
 import com.dexels.navajo.server.Access;
 
@@ -7,5 +9,14 @@ public interface WebserviceListenerRegistryInterface {
 
 	public boolean afterWebservice(String webservice, Access a);
 	public Navajo beforeWebservice(String webservice, Access a);
+	public Navajo beforeWebservice(String webservice, Access a,
+			HashSet<String> ignoreTaskList, boolean locally);
+
+	public void removeTrigger(TriggerInterface afterWebserviceTrigger);
+	public void registerTrigger(TriggerInterface afterWebserviceTrigger);
+	public boolean afterWebservice(String webservice, Access a,
+			HashSet<String> ignoreTaskList, boolean locally);
+	public boolean isRegisteredWebservice(String string);
+
 	
 }
