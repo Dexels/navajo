1ca9767fc1519abf849ecc4274d60b64456becc1
Frank Lyaruu <flyaruu@gmail.com>
2013-01-07 12:42:52 +0100

    Camel + Continuations based servlet, which will deliver all TmlRunnable requests to a Camel route

---
+++ server/com.dexels.navajo.listeners.camel/src/com/dexels/navajo/listeners/camel/FileCopierWithCamel.java
@@ -0,0 +1,30 @@
+package com.dexels.navajo.listeners.camel;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.model.RouteDefinition;
+
+
+public class FileCopierWithCamel {
+
+    public static void main(String args[]) throws Exception {
+        // create CamelContext
+        CamelContext context = new DefaultCamelContext();
+
+        // add our route to the CamelContext
+        context.addRoutes(new RouteBuilder() {
+            public void configure() {
+                RouteDefinition td = from("file:OSGI-INF?noop=true");
+                td.to("file:out");
+            }
+        });
+
+        // start the route and let it do its work
+        context.start();
+        Thread.sleep(10000);
+
+        // stop the CamelContext
+        context.stop();
+    }
+}
