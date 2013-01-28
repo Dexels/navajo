e9de3bb04af4b52d827ac0787444d445ea55d972
eversteeg <eversteeg@dexels.com>
2013-01-28 08:43:38 +0100

    In mail attachments, you can now set the content disposition. Default is attachment.

--- server/com.dexels.navajo.adapters/src/com/dexels/navajo/adapter/mailmap/AttachmentMapInterface.java
+++ server/com.dexels.navajo.adapters/src/com/dexels/navajo/adapter/mailmap/AttachmentMapInterface.java
@@ -7,10 +7,12 @@ public interface AttachmentMapInterface {
 	public void setEncoding(String s);
 	public String getEncoding();
 	public Binary getAttachFileContent();
+	public String getAttachContentDisposition();
 	public void setAttachFileContent(Binary attachFileContent);
 	public void setAttachFileName(String attachFileName);
 	public String getAttachFileName();
 	public String getAttachFile();
 	public void setAttachFile(String attachFile);
 	public void setAttachContentHeader(String s);
+	public void setAttachContentDisposition(String s);
 }
