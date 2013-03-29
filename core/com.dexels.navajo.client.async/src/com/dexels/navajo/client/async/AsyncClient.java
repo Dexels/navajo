c9e101074104005f247427547663fdb1d7892768
Frank Lyaruu <flyaruu@gmail.com>
2013-03-29 15:00:47 +0100

    update refactor

--- core/com.dexels.navajo.client.async/src/com/dexels/navajo/client/async/AsyncClient.java
+++ core/com.dexels.navajo.client.async/src/com/dexels/navajo/client/async/AsyncClient.java
@@ -2,56 +2,16 @@ package com.dexels.navajo.client.async;
 
 import java.io.IOException;
 
-import org.eclipse.jetty.client.HttpClient;
-
 import com.dexels.navajo.client.NavajoResponseHandler;
 import com.dexels.navajo.document.Navajo;
-import com.dexels.navajo.document.NavajoException;
-import com.dexels.navajo.script.api.NavajoResponseCallback;
-import com.dexels.navajo.script.api.TmlRunnable;
-import com.dexels.navajo.server.Access;
 
 public interface AsyncClient {
+	
+	public String getName();
+	public void callService(Navajo input, String service,
+			NavajoResponseHandler continuation) throws IOException;
+	public void setName(String name);
+	public Navajo callService(Navajo input, String service) throws IOException;
 
-	public abstract int getActualCalls();
-
-	public abstract void setActualCalls(int actualCalls);
-
-	public abstract void callService(String service,
-			NavajoResponseHandler continuation) throws IOException,
-			NavajoException;
-
-	public abstract void callService(Navajo input, String service,
-			NavajoResponseHandler continuation) throws IOException,
-			NavajoException;
-
-	public abstract void callService(String url, String username,
-			String password, Navajo input, String service,
-			NavajoResponseHandler continuation) throws IOException,
-			NavajoException;
-
-	public abstract void callService(Access inputAccess, Navajo input,
-			String service, TmlRunnable onSuccess, TmlRunnable onFail,
-			NavajoResponseCallback navajoResponseCallback) throws IOException,
-			NavajoException;
-
-	public abstract Navajo createBlankNavajo(String service, String user,
-			String password);
-
-	public abstract HttpClient getClient();
-
-	public abstract void setClient(HttpClient client);
-
-	public abstract String getServer();
-
-	public abstract void setServer(String server);
-
-	public abstract String getUsername();
-
-	public abstract void setUsername(String username);
-
-	public abstract String getPassword();
-
-	public abstract void setPassword(String password);
 
 }
