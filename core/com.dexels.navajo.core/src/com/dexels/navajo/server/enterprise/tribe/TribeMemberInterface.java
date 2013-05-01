7af7d70349bdc8f6709169d62f499ff5ffa73022
Arjen Schoneveld <aschoneveld@dexels.com>
2013-05-01 22:32:58 +0200

    Added getMemberId() method to TribeMemberInterface for supporting non-named tribal members

--- core/com.dexels.navajo.core/src/com/dexels/navajo/server/enterprise/tribe/TribeMemberInterface.java
+++ core/com.dexels.navajo.core/src/com/dexels/navajo/server/enterprise/tribe/TribeMemberInterface.java
@@ -5,6 +5,8 @@ import java.util.Date;
 public interface TribeMemberInterface {
 
 	public String getMemberName();
+	// Following method is added to support non-named tribal members.
+	public String getMemberId();
 	public Object getAddress();
 	public PingAnswer getStatus();
 	public void setStatus(PingAnswer pa);
