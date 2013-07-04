89a0ad53fd27d35a27d57947ad8d27a67ff1016c
Arjen Schoneveld <aschoneveld@dexels.com>
2013-07-03 17:37:22 -0700

    Added support for compensating and normal transaction context

--- server/com.dexels.navajo.entity/src/com/dexels/navajo/entity/EntityOperation.java
+++ server/com.dexels.navajo.entity/src/com/dexels/navajo/entity/EntityOperation.java
@@ -1,7 +1,6 @@
 package com.dexels.navajo.entity;
 
 import com.dexels.navajo.document.Navajo;
-import com.dexels.navajo.document.Operation;
 
 public interface EntityOperation {
 	public Navajo perform(Navajo input) throws EntityException;
--- server/com.dexels.navajo.entity/src/com/dexels/navajo/entity/transactions/NavajoTransaction.java
+++ server/com.dexels.navajo.entity/src/com/dexels/navajo/entity/transactions/NavajoTransaction.java
@@ -1,7 +1,9 @@
 package com.dexels.navajo.entity.transactions;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import java.util.Stack;
 
 import javax.transaction.HeuristicMixedException;
@@ -22,29 +24,66 @@ public class NavajoTransaction implements Transaction {
 	public final static int RUNNING = 1;
 	public final static int COMMITED = 2;
 	public final static int ROLED_BACK = 3;
-	
+
 	private int state = RUNNING;
-	
+
 	private Stack<RollbackOperation> rollbackStack = new Stack<RollbackOperation>();
+	private Set<TransactionalAdapter> transactionalResources = new HashSet<TransactionalAdapter>();
+	private Map<String,Object> transactionAttributes = new HashMap<String, Object>();
 	
-	public void logEntityService(Navajo input, Navajo original, ServiceEntityOperation seo) throws EntityException {
+	/**
+	 * Add an attribute specific to this transaction context. For example: database connection identifier.
+	 * 
+	 * @param key
+	 * @param value
+	 */
+	public void addTransactionAttribute(String key, Object value) {
+		transactionAttributes.put(key, value);
+	}
+	
+	public Object getTransactionAttribute(String key) {
+		return transactionAttributes.get(key);
+	}
+	
+	/**
+	 * Transactional resources can be added to a NavajoTransaction context.
+	 * 
+	 * @param resource
+	 */
+	public void addTransactionalResource(TransactionalAdapter resource) {
+		transactionalResources.add(resource);
+	}
+
+	/**
+	 * Add non-transactional resource, i.e. supports compensation based rollback.
+	 * 
+	 * @param input
+	 * @param original
+	 * @param seo
+	 * @throws EntityException
+	 */
+	public void addNonTransactionalEntityResource(Navajo input, Navajo original, ServiceEntityOperation seo) throws EntityException {
 
 		if (    seo.getMyOperation().getMethod().equals(Operation.POST) || 
 				seo.getMyOperation().getMethod().equals(Operation.PUT) ||
 				seo.getMyOperation().getMethod().equals(Operation.DELETE) ) {
-			
+
 			RollbackOperation ro = new RollbackOperation(seo, input, original);
 			rollbackStack.add(ro);
-	
+
 		}
 
 	}
-	
+
 	@Override
 	public void commit() throws HeuristicMixedException,
-			HeuristicRollbackException, RollbackException, SecurityException,
-			SystemException {
+	HeuristicRollbackException, RollbackException, SecurityException,
+	SystemException {
 		state = COMMITED;
+		// Commit transactional resources.
+		for ( TransactionalAdapter tr : transactionalResources ) {
+			tr.commit();
+		}
 	}
 
 	@Override
@@ -70,18 +109,27 @@ public class NavajoTransaction implements Transaction {
 	public void registerSynchronization(Synchronization arg0)
 			throws IllegalStateException, RollbackException, SystemException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void rollback() throws IllegalStateException, SystemException {
+
 		state = ROLED_BACK;
-		RollbackOperation ro = null;
-		while ( (ro = rollbackStack.pop() ) != null ) {
-			try {
-				ro.rollback();
-			} catch (EntityException e) {
-				throw new SystemException("Could not perform rollback:" + e.getMessage());
+		try {
+			RollbackOperation ro = null;
+			while ( (ro = rollbackStack.pop() ) != null ) {
+				try {
+					ro.rollback();
+				} catch (EntityException e) {
+					System.err.println("Could not perform rollback: " + e.getMessage());
+					//throw new SystemException("Could not perform rollback:" + e.getMessage());
+				}
+			}
+		} finally {
+			// Rollback transactional resources.
+			for ( TransactionalAdapter tr : transactionalResources ) {
+				tr.rollback();
 			}
 		}
 	}
@@ -89,7 +137,7 @@ public class NavajoTransaction implements Transaction {
 	@Override
 	public void setRollbackOnly() throws IllegalStateException, SystemException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
