1454b746476809948e0d4094ba9546c63b4d9c9c
Frank Lyaruu <flyaruu@gmail.com>
2013-10-07 09:27:24 +0200

    reduce logging

--- tipi_dev/com.dexels.navajo.tipi.dev.server/src/com/dexels/navajo/tipi/dev/server/CompressionFilter.java
+++ tipi_dev/com.dexels.navajo.tipi.dev.server/src/com/dexels/navajo/tipi/dev/server/CompressionFilter.java
@@ -8,19 +8,13 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
 import org.eclipse.jetty.servlets.GzipFilter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class CompressionFilter extends GzipFilter {
 
 	
-	private final static Logger logger = LoggerFactory
-			.getLogger(CompressionFilter.class);
-	
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse resp,
 			FilterChain chain) throws IOException, ServletException {
-		logger.info("Filter called!");
 		super.doFilter(req, resp, chain);
 	}
 
