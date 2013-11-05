d4519b6d1b3c9d835a5d4bba401b6a72f5b2be70
Frank Lyaruu <flyaruu@gmail.com>
2013-11-05 12:21:36 +0100

    Test release of com.dexels.navajo.version version 1.2.10

--- setup/scripts/release.sh
+++ setup/scripts/release.sh
@@ -8,16 +8,18 @@ echo "Deploying in branch name: $branch_name"
 if [ "$branch_name" == test ]; then
    commitMsg="Test release of ${PWD##*/} version $1" 
    releaseTag="Test_${PWD##*/}-$1" 
+   environmentSwitch=" -Denvironment=test"
 else
    commitMsg="Release of ${PWD##*/} version $1" 
    releaseTag="Release_${PWD##*/}-$1" 
+   environmentSwitch=" -Denvironment=release"
 fi
-mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$1
-mvn deploy -Dbranch=$branch_name
+mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$1 $environmentSwitch
+mvn deploy -Dbranch=$branch_name $environmentSwitch
 git commit -m "$commitMsg" -a
 git tag -a -f "$releaseTag" -m "$commitMsg"
-mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$2-SNAPSHOT
-mvn deploy -Dbranch=$branch_name
+mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$2-SNAPSHOT $environmentSwitch
+mvn deploy -Dbranch=$branch_name $environmentSwitch
 git commit -m "Development stream of ${PWD##*/} version $2" -a
 git push --tags
 git push
