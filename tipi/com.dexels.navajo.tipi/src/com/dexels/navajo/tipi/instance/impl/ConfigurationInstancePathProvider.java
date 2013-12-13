dcee22261098fd14ce0d1fc90bf9b4e92d84b175
Frank Lyaruu <flyaruu@gmail.com>
2013-12-13 12:25:02 +0100

    Split up the responsibilities of the InstanceConfigurationProvider. Now, it no longer uses configuration. Instead it relies on InstancePathProvider to provide the root path
    The ConfigurationPathProvider gets a path from configuration,
    essentially the responsibility the ICP used to have.
    
    Next, I'll make a InstancePathProvider using a repository

---
+++ tipi/com.dexels.navajo.tipi/src/com/dexels/navajo/tipi/instance/impl/ConfigurationInstancePathProvider.java
@@ -0,0 +1,30 @@
+package com.dexels.navajo.tipi.instance.impl;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Map;
+import java.util.Set;
+
+import org.osgi.framework.BundleContext;
+
+import com.dexels.navajo.tipi.instance.InstanceConfigurationProvider;
+import com.dexels.navajo.tipi.instance.InstancePathProvider;
+
+public class ConfigurationInstancePathProvider implements
+		InstancePathProvider {
+
+	private File instancePath;
+
+	@Override
+	public File getInstancePath() {
+		return instancePath;
+	}
+
+	public void activate(Map<String,Object> parameters, BundleContext bundleContext) throws FileNotFoundException {
+		instancePath = new File((String) parameters.get("path"));
+	}
+
+	public void deactivate() {
+		instancePath = null;
+	}
+}
