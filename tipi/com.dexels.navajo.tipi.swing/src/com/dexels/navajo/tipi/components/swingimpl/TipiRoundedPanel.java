d6cd0fbad05355cd75f6c7b91be85fc37fd38d39
eversteeg <eversteeg@dexels.com>
2013-12-21 12:15:05 +0100

    added some functionality to CreateTitledBorder and the start of a rounded tipi panel

---
+++ tipi/com.dexels.navajo.tipi.swing/src/com/dexels/navajo/tipi/components/swingimpl/TipiRoundedPanel.java
@@ -0,0 +1,38 @@
+package com.dexels.navajo.tipi.components.swingimpl;
+
+import javax.swing.JPanel;
+
+import com.dexels.navajo.tipi.TipiHelper;
+import com.dexels.navajo.tipi.components.swingimpl.swing.TipiSwingHelper;
+import com.dexels.navajo.tipi.components.swingimpl.swing.TipiSwingRoundedPanel;
+
+/**
+ * <p>
+ * Title:
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * <p>
+ * Copyright: Copyright (c) 2003
+ * </p>
+ * <p>
+ * Company:
+ * </p>
+ * 
+ * @author not attributable
+ * @version 1.0
+ */
+public class TipiRoundedPanel extends TipiSwingDataComponentImpl {
+
+	private static final long serialVersionUID = 6190251046684031410L;
+
+	public Object createContainer() {
+		JPanel myPanel = new TipiSwingRoundedPanel(TipiRoundedPanel.this);
+		TipiHelper th = new TipiSwingHelper();
+		th.initHelper(this);
+		addHelper(th);
+		return myPanel;
+	}
+
+}
