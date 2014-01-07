10cea291ccaa071356898b967d43e7ea6aa63275
Arjen Schoneveld <aschoneveld@dexels.com>
2014-01-07 17:44:25 +0100

    Added support for supplying a Navajo template for decorating raw json with types. Now using default ObjectMapper for serializing Java objects

--- core/com.dexels.navajo.document/src/com/dexels/navajo/document/json/JSONTML.java
+++ core/com.dexels.navajo.document/src/com/dexels/navajo/document/json/JSONTML.java
@@ -5,16 +5,25 @@ import java.io.OutputStream;
 import java.io.Reader;
 import java.io.Writer;
 
+import com.dexels.navajo.document.Message;
 import com.dexels.navajo.document.Navajo;
 
 public interface JSONTML {
 
+	public void setEntityTemplate(Navajo m);
+	
 	public abstract Navajo parse(InputStream is) throws Exception;
 
+	public abstract Navajo parse(InputStream is, String topLevelMessageName) throws Exception;
+	
 	public abstract Navajo parse(Reader r) throws Exception;
+	
+	public abstract Navajo parse(Reader r, String topLevelMessageName) throws Exception;
 
 	public abstract void format(Navajo n, OutputStream os) throws Exception;
 
 	public abstract void format(Navajo n, Writer w) throws Exception;
+	
+	public abstract void format(Navajo n, Writer w, boolean skipTopLevelMessage) throws Exception;
 
 }
--- core/com.dexels.navajo.document/src/com/dexels/navajo/document/json/impl/JSONTMLImpl.java
+++ core/com.dexels.navajo.document/src/com/dexels/navajo/document/json/impl/JSONTMLImpl.java
@@ -11,11 +11,13 @@ import org.codehaus.jackson.JsonFactory;
 import org.codehaus.jackson.JsonGenerator;
 import org.codehaus.jackson.JsonParser;
 import org.codehaus.jackson.JsonToken;
+import org.codehaus.jackson.map.ObjectMapper;
 
 import com.dexels.navajo.document.Message;
 import com.dexels.navajo.document.Navajo;
 import com.dexels.navajo.document.NavajoFactory;
 import com.dexels.navajo.document.Property;
+import com.dexels.navajo.document.Selection;
 import com.dexels.navajo.document.json.JSONTML;
 
 /**
@@ -27,9 +29,17 @@ import com.dexels.navajo.document.json.JSONTML;
 public class JSONTMLImpl implements JSONTML {
 
 	private JsonFactory jsonFactory = null;
+	private ObjectMapper om = null;
+	private String topLevelMessageName = null;
+	private boolean skipTopLevelMessage = false;
+	private Navajo entityTemplate = null;
+	
+	private final static String TOP_LEVEL_MSG = "__TOP__";
 	
 	public JSONTMLImpl() {
 		jsonFactory =  new JsonFactory();
+		// Use default typing.
+		om = new ObjectMapper().enableDefaultTyping();
 	}
 	 
 	/* (non-Javadoc)
@@ -42,8 +52,13 @@ public class JSONTMLImpl implements JSONTML {
 			Navajo n = parse(jp);
 			return n;
 		} catch (Exception e) {
-			throw new Exception("Could not parse JSON inputstream: " + e.getMessage());
+			throw new Exception("Could not parse JSON inputstream: " + e.getMessage(), e);
 		} 
+	}
+	
+	public Navajo parse(InputStream is, String topLevelMessageName) throws Exception {
+		this.topLevelMessageName = topLevelMessageName;
+		return parse(is);
 	}
 	
 	/* (non-Javadoc)
@@ -60,13 +75,23 @@ public class JSONTMLImpl implements JSONTML {
 		} 
 	}
 	
+	public Navajo parse(Reader r, String topLevelMessageName) throws Exception {
+		this.topLevelMessageName = topLevelMessageName;
+		return parse(r);
+	}
+	
 	/* (non-Javadoc)
 	 * @see com.dexels.navajo.document.json.impl.JSONTML#format(com.dexels.navajo.document.Navajo, java.io.OutputStream)
 	 */
 	@Override
 	public void format(Navajo n, OutputStream os) throws Exception {
 		try {
+			
+		
+			//ObjectMapper mapper = new ObjectMapper().enableDefaultTyping();
+			
 			JsonGenerator jg = jsonFactory.createJsonGenerator(os); 
+			
 			jg.useDefaultPrettyPrinter();
 			format(jg, n);
 			jg.close();
@@ -87,23 +112,30 @@ public class JSONTMLImpl implements JSONTML {
 			format(jg, n);
 			jg.close();
 		} catch (Exception e) {
+			e.printStackTrace();
 			throw new Exception("Could not parse JSON inputstream: " + e.getMessage());
 		} 
+	}
+	
+	public void format(Navajo n, Writer w, boolean skipTopLevelMessage) throws Exception {
+		this.skipTopLevelMessage = skipTopLevelMessage;
+		format(n, w);
 	}
 	
 	private void format(JsonGenerator jg, Property p) throws Exception {
 
 		jg.writeFieldName(p.getName());
-		jg.writeObject(p.getTypedValue());
+		om.writeValue(jg, p.getTypedValue());
+		//jg.writeObject(p.getTypedValue());
 
 	}
 
 	private void format(JsonGenerator jg, Message m, boolean arrayElement) throws Exception {
 
-		if (!arrayElement) {
+		if (!arrayElement && !m.getName().equals(TOP_LEVEL_MSG) ) {
 			jg.writeFieldName(  m.getName() );
 		}
-		if ( !m.isArrayMessage() ) {
+		if ( !m.isArrayMessage() && !m.getName().equals(TOP_LEVEL_MSG) ) {
 			jg.writeStartObject();
 		}
 
@@ -123,7 +155,7 @@ public class JSONTMLImpl implements JSONTML {
 
 		if ( m.isArrayMessage() ) {
 			jg.writeEndArray();
-		} else {
+		} else if ( !m.getName().equals(TOP_LEVEL_MSG) ) {
 			jg.writeEndObject();
 		}
 
@@ -135,12 +167,16 @@ public class JSONTMLImpl implements JSONTML {
 		jg.writeStartObject();
 		List<Message> messages = n.getAllMessages();
 		for ( Message m: messages) {
+			if ( skipTopLevelMessage ) {
+				m.setName(TOP_LEVEL_MSG);
+			}
 			format(jg, m, false);
 		}
 		jg.writeEndObject();
 		} catch (IOException ioe) {
 			throw ioe;
 		} catch (Exception e) {
+			e.printStackTrace(System.err);
 			throw new Exception("Could not format Navajo as JSON stream: " + e.getMessage());
 		}
 	}
@@ -148,6 +184,21 @@ public class JSONTMLImpl implements JSONTML {
 	private void parseProperty(String name, String value, Message p, JsonParser jp) throws Exception {
 		Property prop = NavajoFactory.getInstance().createProperty(p.getRootDoc(), name, Property.STRING_PROPERTY, value, 0, "", "out");
 		p.addProperty(prop);
+		if ( entityTemplate != null ) {
+			Property ep = entityTemplate.getProperty(prop.getFullPropertyName());
+			if ( ep != null ) {
+				if ( ep.getType().equals(Property.SELECTION_PROPERTY)) {
+					Selection s = NavajoFactory.getInstance().createSelection(p.getRootDoc(), value, value, true);
+					prop.setType(ep.getType());
+					prop.addSelection(s);
+				} else {
+					prop.setType(ep.getType());
+				}
+			} else {
+				System.err.println("Could not find property in template: " + prop.getFullPropertyName());
+			}
+		}
+		
 	}
 
 	private void parseArrayMessageElement(Message arrayMessage, JsonParser jp) throws Exception {
@@ -192,6 +243,11 @@ public class JSONTMLImpl implements JSONTML {
 				parseArrayMessage(name, n, parent, jp);
 			} else {
 				String value = jp.getText();
+				if ( parent == null ) {
+					System.err.println("Could not find message, creating dummy");
+					parent = NavajoFactory.getInstance().createMessage(n, (topLevelMessageName != null ? topLevelMessageName : "Request" ) );
+					n.addMessage(parent);
+				}
 				parseProperty(name, value, parent, jp);
 			}
 		}
@@ -203,6 +259,11 @@ public class JSONTMLImpl implements JSONTML {
 			parse(n, null, jp);
 		}
 		return n;
+	}
+
+	@Override
+	public void setEntityTemplate(Navajo m) {
+		this.entityTemplate = m;
 	}
 	
 
