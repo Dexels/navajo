15bbc5426c8631eeec7fe087a7eaa686f629889d
Frank Lyaruu <flyaruu@gmail.com>
2014-01-07 18:30:56 +0100

    Create a file based repository, it makes more sense for developing that JGit based

--- optional/com.dexels.navajo.repository.file/src/com/dexels/navajo/repository/file/impl/FileRepositoryInstanceImpl.java
+++ optional/com.dexels.navajo.repository.file/src/com/dexels/navajo/repository/file/impl/FileRepositoryInstanceImpl.java
@@ -1,7 +1,10 @@
-package com.dexels.githubosgi.impl;
+package com.dexels.navajo.repository.file.impl;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -12,32 +15,23 @@ import org.slf4j.LoggerFactory;
 
 import com.dexels.navajo.repository.api.AppStoreOperation;
 import com.dexels.navajo.repository.api.RepositoryInstance;
-import com.dexels.navajo.repository.api.RepositoryManager;
 
-public class RepositoryInstanceImpl implements RepositoryInstance {
+public class FileRepositoryInstanceImpl implements RepositoryInstance {
 	
 	protected String repositoryName;
 	protected File applicationFolder;
-	protected RepositoryManager repositoryManager;
 	private final Map<String,Object> settings = new HashMap<String, Object>();
 	private final Map<String,AppStoreOperation> operations = new HashMap<String, AppStoreOperation>();
 	private final Map<String,Map<String,Object>> operationSettings = new HashMap<String, Map<String,Object>>();
 	protected String type;
 	
 	private final static Logger logger = LoggerFactory
-			.getLogger(RepositoryInstanceImpl.class);
+			.getLogger(FileRepositoryInstanceImpl.class);
+	
 	
 	@Override
 	public File getRepositoryFolder() {
 		return this.applicationFolder;
-	}
-
-	public void setRepositoryManager(RepositoryManager repositoryManager) {
-		this.repositoryManager = repositoryManager;
-	}
-
-	public void clearRepositoryManager(RepositoryManager repositoryManager) {
-		this.repositoryManager = null;
 	}
 
 
@@ -46,6 +40,77 @@ public class RepositoryInstanceImpl implements RepositoryInstance {
 		return repositoryName;
 	}
 
+	
+	public void activate(Map<String,Object> configuration) throws IOException {
+
+		String path = (String) configuration.get("repository.folder");
+		repositoryName = (String) configuration.get("repository.name");
+		final String fileInstallPath= (String) configuration.get("felix.fileinstall.filename");
+
+		applicationFolder = findConfiguration(path,fileInstallPath);
+		logger.info("Repository instance activated");
+
+	}
+
+	private File findConfiguration(String path, String fileInstallPath)
+			throws IOException {
+		
+		if(path==null || "".equals(path)) {
+			path = System.getProperty("storage.path");
+		}
+		File storeFolder = null;
+		if(path==null) {
+			logger.info("No storage.path found, now trying to retrieve from felix.fileinstall.filename");
+			storeFolder = findByFileInstaller(fileInstallPath,".");
+		} else {
+			final File file = new File(path);
+			if(file.isAbsolute()) {
+				storeFolder = file;
+			}
+		}
+		if(storeFolder==null || !storeFolder.isAbsolute()) {
+			storeFolder = findByFileInstaller(fileInstallPath,path);
+		}
+		if(storeFolder==null || !storeFolder.exists()) {
+			storeFolder = findByFileInstaller(fileInstallPath,path);
+		}
+		if(storeFolder==null || !storeFolder.exists()) {
+			throw new IOException("No storage.path set in configuration!");
+		}
+		return storeFolder;
+	}
+
+	// f-in' beautiful
+	private File findByFileInstaller(final String fileNamePath,String relative) {
+		try {
+			URL url = new URL(fileNamePath);
+			File f;
+			try {
+			  f = new File(url.toURI());
+			} catch(URISyntaxException e) {
+			  f = new File(url.getPath());
+			}
+			if(f!=null) {
+				File etc = f.getParentFile();
+				if(etc!=null) {
+					File root = etc.getParentFile();
+					if(root!=null) {
+						File storage = new File(root,relative);
+						if(storage.exists()) {
+							return storage;
+						}
+					}
+				}
+			}
+		} catch (MalformedURLException e) {
+			logger.warn("Fileinstall.filename based resolution also failed.",e);
+		}
+		return null;
+	}
+	
+	public void deactivate() {
+
+	}
 
 
 	@Override
