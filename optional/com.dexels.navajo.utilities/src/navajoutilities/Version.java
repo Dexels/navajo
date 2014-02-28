b2f1a0322378cc5b8f48998a0393023bb5123304
Frank Lyaruu <flyaruu@gmail.com>
2014-02-28 14:49:55 +0100

    Add adapter registration so the ScriptIntrospection adapter works once more

---
+++ optional/com.dexels.navajo.utilities/src/navajoutilities/Version.java
@@ -0,0 +1,36 @@
+package navajoutilities;
+
+import navajoextension.AbstractCoreExtension;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+import com.dexels.navajo.util.UtilitiesLibrary;
+
+
+public class Version extends AbstractCoreExtension implements BundleActivator {
+
+	@Override
+	public void start(BundleContext bc) throws Exception {
+		super.start(bc);
+		UtilitiesLibrary library = new UtilitiesLibrary();
+		registerAll(library);
+
+	}
+
+	@Override
+	public void stop(BundleContext bc) throws Exception {
+		super.stop(bc);
+		
+	}
+	
+	@Override
+	public void shutdown() {
+		super.shutdown();
+	}
+	
+
+	public static void main(String[] args) {
+		
+	}
+}
