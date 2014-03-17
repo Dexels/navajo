11ef397717937b09d5873ca9214aad5f6165ac74
Frank Lyaruu <flyaruu@gmail.com>
2014-03-17 11:26:04 +0100

    Release of com.dexels.navajo.tipi.vaadin version 1.3.0

--- setup/scripts/createdroplet.sh
+++ setup/scripts/createdroplet.sh
@@ -7,4 +7,4 @@ size=512mb
 sshkey=67021,95246 #frank, butler
 
 curl "https://api.digitalocean.com/droplets/new?client_id=${client_id}&api_key=${api_key}&name=${name}&size_slug=${size}&region_slug=${region}&ssh_key_ids=${sshkey}&image_slug=${image}"
-
+#example response: {"status":"OK","droplet":{"id":1309889,"name":"tralala","image_id":696598,"size_id":66,"event_id":18970298}}
