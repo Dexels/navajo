80594731da22537ee99e5515b2f9ad887342a218
Frank Lyaruu <flyaruu@gmail.com>
2014-04-24 14:39:30 +0200

    Removed tipi.swing.svg (It was pretty much impossible to make it work, and it dragged in an enormous dependency tree) Besides, no-one has ever used it.

--- tipi/com.dexels.navajo.tipi.swing.svg/src/com/dexels/navajo/tipi/swing/svg/SvgAnimationListener.java
+++
@@ -1,6 +0,0 @@
-package com.dexels.navajo.tipi.swing.svg;
-
-public interface SvgAnimationListener {
-	public void onAnimationStarted(String animationId, String targetId);
-	public void onAnimationEnded(String animationId, String targetId);
-}
