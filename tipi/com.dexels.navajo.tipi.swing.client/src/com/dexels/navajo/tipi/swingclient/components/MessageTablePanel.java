49fb9cbcb101c0de19fb15e01e92d9d33521baac
Chris Brouwer <cbrouwer@dexels.com>
2014-05-16 14:00:44 +0200

    Scroll to selected row when selecting a row in a table

--- tipi/com.dexels.navajo.tipi.swing.client/src/com/dexels/navajo/tipi/swingclient/components/MessageTablePanel.java
+++ tipi/com.dexels.navajo.tipi.swing.client/src/com/dexels/navajo/tipi/swingclient/components/MessageTablePanel.java
@@ -7,6 +7,7 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -821,6 +822,12 @@ public class MessageTablePanel extends BasePanel implements CopyCompatible,
 	public void rowSelected(ListSelectionEvent e) {
 	}
 
+	private void scrollToSelectedRow() {
+		int sel = getSelectedRow();
+		messageTable.getSelectionModel().setSelectionInterval(sel, sel);
+		messageTable.scrollRectToVisible(new Rectangle(messageTable.getCellRect(sel, 0, true)));
+	}
+
 	final void messageTable_valueChanged(ListSelectionEvent e) {
 		setStateUpdated();
 		rowSelected(e);
@@ -898,6 +905,7 @@ public class MessageTablePanel extends BasePanel implements CopyCompatible,
 			return;
 		}
 		setRowSelectionInterval(row, row);
+		scrollToSelectedRow();
 	}
 
 	public void updateProperties(java.util.List<Property> l) {
