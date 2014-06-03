4776d1177e2cd3405ebb65d9bf67e05d2d4f6a24
Frank Lyaruu <flyaruu@gmail.com>
2014-06-03 12:00:26 +0200

    Remove failed OAuth2 experiment

--- server/com.dexels.navajo.oauth2/src/com/dexels/navajo/oauth2/servlet/OauthServlet.java
+++
@@ -1,9 +0,0 @@
-package com.dexels.navajo.oauth2.servlet;
-
-import javax.servlet.http.HttpServlet;
-
-public class OauthServlet extends HttpServlet {
-
-	private static final long serialVersionUID = -6265471581307593134L;
-
-}
