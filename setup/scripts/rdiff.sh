429da9b19c58c4d67a6fd51dead6f27a9a4901f1
Frank Lyaruu <flyaruu@gmail.com>
2014-06-23 12:54:27 +0200

    Added OSGI-INF to findunreleased

--- setup/scripts/rdiff.sh
+++ setup/scripts/rdiff.sh
@@ -4,4 +4,4 @@ echo "Rdiffing base: ${BASE}"
 version=$(cat $1/META-INF/MANIFEST.MF | grep Bundle-Version | xargs -I {} lastversion.sh {})
 echo $version
 echo Release_${BASE}-$version
-git diff Release_${BASE}-$version --stat $1/src
+git diff Release_${BASE}-$version --stat $1/src $1/OSGI-INF
