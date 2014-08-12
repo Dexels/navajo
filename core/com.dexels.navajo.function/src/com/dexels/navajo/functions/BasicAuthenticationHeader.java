7ad2569f382a18e6c20b88f8c68255b0d0f75dc6
Chris Brouwer <cbrouwer@dexels.com>
2014-08-12 14:13:20 +0200

    Added BasicAuthHeader function

---
+++ core/com.dexels.navajo.function/src/com/dexels/navajo/functions/BasicAuthenticationHeader.java
@@ -0,0 +1,59 @@
+/*
+ * Created on May 23, 2005
+ *
+ */
+package com.dexels.navajo.functions;
+
+import java.nio.charset.Charset;
+
+import org.dexels.utils.Base64;
+
+import com.dexels.navajo.parser.FunctionInterface;
+import com.dexels.navajo.parser.TMLExpressionException;
+
+/**
+ * @author cbrouwer
+ *
+ */
+public class BasicAuthenticationHeader extends FunctionInterface {
+
+	public BasicAuthenticationHeader() {	
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.dexels.navajo.parser.FunctionInterface#remarks()
+	 */
+	@Override
+	public String remarks() {
+		return "Returns the HTTP authorization header value for Basic authentication";
+	}
+
+	/* (non-Javadoc)
+	 * @see com.dexels.navajo.parser.FunctionInterface#evaluate()
+	 */
+	@Override
+	public Object evaluate() throws TMLExpressionException {
+		 if (this.getOperands().size() != 2) {
+	            throw new TMLExpressionException("BasicAuthenticationHeader(String, String) expected");
+		 }
+		 
+		 
+		Object o1 = this.getOperand(0);
+		Object o2 = this.getOperand(0);
+		
+		if (!(o1 instanceof String) || !(o2 instanceof String)) {
+            throw new TMLExpressionException("BasicAuthenticationHeader(String, String) expected");
+		}
+		
+		String authString = (String) o1 + ":" + (String) o2;
+		byte[] bytes = authString.getBytes(Charset.forName("UTF-8"));
+		return "Basic " + Base64.encode(bytes, 0, bytes.length, 0, "");
+
+	}
+	
+	public static void main(String [] args) throws Exception {
+			
+	}
+	
+
+}
