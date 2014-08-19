6910cb7bda0633eaea52f92b81d36fea81f0b1ef
Chris Brouwer <cbrouwer@dexels.com>
2014-08-19 15:20:53 +0200

    StatisticsRunnerComponent requires configuration

--- core/com.dexels.navajo.core/src/com/dexels/navajo/server/enterprise/statistics/StatisticsRunnerComponent.java
+++ core/com.dexels.navajo.core/src/com/dexels/navajo/server/enterprise/statistics/StatisticsRunnerComponent.java
@@ -1,13 +1,18 @@
 package com.dexels.navajo.server.enterprise.statistics;
 
+import java.util.Map;
+
+import com.dexels.navajo.events.NavajoEvent;
+import com.dexels.navajo.mapping.AsyncMappable;
+import com.dexels.navajo.script.api.Access;
 import com.dexels.navajo.server.NavajoConfigInterface;
 
-public class StatisticsRunnerComponent {
+public class StatisticsRunnerComponent implements StatisticsRunnerInterface {
 	/**
 	 * @param cc  
 	 */
 	public void activate() {
-		
+		System.out.println("hier");
 	}
 
 	public void deactivate() {
@@ -26,6 +31,45 @@ public class StatisticsRunnerComponent {
 	 */
 	public void clearNavajoConfig(NavajoConfigInterface nvi) {
 		
+	}
+
+	@Override
+	public void onNavajoEvent(NavajoEvent ne) {
+		
+		
+	}
+
+	@Override
+	public void setEnabled(boolean b) {
+		
+		
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return false;
+	}
+
+	@Override
+	public void addAccess(Access a, AsyncMappable am) {
+		
+		
+	}
+
+	@Override
+	public int getAuditLevel() {
+		return 0;
+	}
+
+	@Override
+	public void setAuditLevel(int l) {
+
+	}
+
+	@Override
+	public void initialize(String storePath, Map parameters, String storeClass)
+			throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+				
 	}
 
 }
