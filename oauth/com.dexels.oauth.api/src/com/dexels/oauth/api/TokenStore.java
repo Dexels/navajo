1bed0878552b678c77e10ad05d448445f88fea72
Frank Lyaruu <flyaruu@gmail.com>
2014-12-11 15:50:04 +0100

    Removed moved code

--- oauth/com.dexels.oauth.api/src/com/dexels/oauth/api/TokenStore.java
+++
@@ -1,9 +0,0 @@
-package com.dexels.oauth.api;
-
-import java.util.Date;
-import java.util.Map;
-
-public interface TokenStore {
-	public Token generateToken(String client_id, String[] scopes,String username, Map<String,Object> userAttributes, String redirect_uri, Date expireDate) throws TokenException;
-	public Token getTokenByString(String token) throws TokenException;
-}
