c522fa1126604a7c880df1026cd7f0aac8c4f2b5
Chris Brouwer <cbrouwer@dexels.com>
2015-02-10 13:55:16 +0100

    formatting

--- tipi/com.dexels.navajo.tipi/src/com/dexels/navajo/tipi/actions/TipiDispose.java
+++ tipi/com.dexels.navajo.tipi/src/com/dexels/navajo/tipi/actions/TipiDispose.java
@@ -25,28 +25,28 @@ import com.dexels.navajo.tipi.internal.TipiEvent;
  * @version 1.0
  */
 public class TipiDispose extends TipiAction {
-	/**
+    /**
 	 * 
 	 */
-	private static final long serialVersionUID = 2702919442460647651L;
-	
-	private final static Logger logger = LoggerFactory
-			.getLogger(TipiDispose.class);
-	
-	@Override
-	public void execute(TipiEvent event)
-			throws com.dexels.navajo.tipi.TipiException,
-			com.dexels.navajo.tipi.TipiBreakException {
-		try {
-			TipiComponent tp = (TipiComponent) getEvaluatedParameterValue(
-					"path", event);
-			if (tp != null) {
-				myContext.disposeTipiComponent(tp);
-			} else {
-				logger.warn("ATTEMPTING TO DISPOSE NULL component. ");
-			}
-		} catch (Exception ex) {
-			logger.error("Error: ",ex);
-		}
-	}
+    private static final long serialVersionUID = 2702919442460647651L;
+
+    private final static Logger logger = LoggerFactory.getLogger(TipiDispose.class);
+
+    @Override
+    public void execute(TipiEvent event) throws com.dexels.navajo.tipi.TipiException,
+            com.dexels.navajo.tipi.TipiBreakException {
+        try {
+            TipiComponent tp = (TipiComponent) getEvaluatedParameterValue("path", event);
+            if (tp != null) {
+                // myContext.disposeTipiComponent(tp);
+
+               // tp.hideComponent();
+
+            } else {
+                logger.warn("ATTEMPTING TO DISPOSE NULL component. ");
+            }
+        } catch (Exception ex) {
+            logger.error("Error: ", ex);
+        }
+    }
 }
