d2b70129133f68963441d9ccbbae93d29677b982
Chris Brouwer <cbrouwer@dexels.com>
2015-03-24 12:21:37 +0100

    Removed unused constructors

--- core/com.dexels.navajo.core/src/com/dexels/navajo/compiler/CompilationException.java
+++ core/com.dexels.navajo.core/src/com/dexels/navajo/compiler/CompilationException.java
@@ -4,24 +4,11 @@ public class CompilationException extends Exception {
 
 	private static final long serialVersionUID = 7448856689278900523L;
 
-	public CompilationException() {
-	}
 
 	public CompilationException(String message) {
 		super(message);
 	}
 
-	public CompilationException(Throwable cause) {
-		super(cause);
-	}
-
-	public CompilationException(String message, Throwable cause) {
-		super(message, cause);
-	}
-
-	public CompilationException(String message, Throwable cause,
-			boolean enableSuppression, boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-	}
+	
 
 }
