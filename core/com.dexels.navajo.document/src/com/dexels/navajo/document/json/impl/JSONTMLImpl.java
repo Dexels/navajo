360d84838e21c153df7c66cbcd66bd42e59d743a
Chris Brouwer <cbrouwer@dexels.com>
2015-07-20 10:25:20 +0200

    better support for converting array messages to JSON in combination with removeTopMessage

--- core/com.dexels.navajo.document/src/com/dexels/navajo/document/json/impl/JSONTMLImpl.java
+++ core/com.dexels.navajo.document/src/com/dexels/navajo/document/json/impl/JSONTMLImpl.java
@@ -7,6 +7,7 @@ import java.io.Reader;
 import java.io.Writer;
 import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import org.codehaus.jackson.JsonFactory;
@@ -220,31 +221,52 @@ public class JSONTMLImpl implements JSONTML {
 
 	}
 
-	private void format(JsonGenerator jg, Navajo n) throws Exception {
-		String origName = TOP_LEVEL_MSG;
-		try {
-			jg.writeStartObject();
-			List<Message> messages = n.getAllMessages();
-			for ( Message m: messages) {
-				try {
-					if (skipTopLevelMessage && !m.isArrayMessage()) {
-						origName = m.getName();
-						m.setName(TOP_LEVEL_MSG);
-					}
-					format(jg, m, false);
-				} finally {
-					if (skipTopLevelMessage) {
-						m.setName(origName);
-					}
-				}
-			}
-			jg.writeEndObject();
-		} catch (IOException ioe) {
-			throw ioe;
-		} catch (Exception e) {
-			throw new Exception("Could not format Navajo as JSON stream" ,e);
-		}
-	}
+    private void format(JsonGenerator jg, Navajo n) throws Exception {
+        try {
+            if (skipTopLevelMessage && n.getMessages().size() == 1) {
+                // If skipTopLevelMessage=true, we have one message to write and
+                // this is an array message, we have to start our message as an 
+                // array rather than object.
+
+                Collection<Message> messages = n.getMessages().values();
+                Message msg = messages.iterator().next();
+                if (msg.isArrayMessage()) {
+                    jg.writeStartArray();
+                    for (Message arrayElement : msg.getElements()) {
+                        format(jg, arrayElement, true);
+                    }
+                    jg.writeEndArray();
+                    return;
+                }
+            }
+
+            jg.writeStartObject();
+            formatMessages(jg, n.getAllMessages());
+            jg.writeEndObject();
+
+        } catch (IOException ioe) {
+            throw ioe;
+        } catch (Exception e) {
+            throw new Exception("Could not format Navajo as JSON stream", e);
+        }
+    }
+
+    private void formatMessages(JsonGenerator jg, List<Message> messages) throws Exception {
+        for (Message m : messages) {
+            String origName = TOP_LEVEL_MSG;
+            try {
+                if (skipTopLevelMessage && !m.isArrayMessage()) {
+                    origName = m.getName();
+                    m.setName(TOP_LEVEL_MSG);
+                }
+                format(jg, m, false);
+            } finally {
+                if (skipTopLevelMessage) {
+                    m.setName(origName);
+                }
+            }
+        }
+    }
 
 	private void parseProperty(String name, String value, Message p, JsonParser jp) throws Exception {
 		if (name == null) {
