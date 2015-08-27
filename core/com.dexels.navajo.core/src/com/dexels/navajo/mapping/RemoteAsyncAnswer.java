2ea1f3c5d1d966a599eec894fedfa9798a48fbae
Chris Brouwer <cbrouwer@dexels.com>
2015-08-27 11:25:14 +0200

    logging

--- core/com.dexels.navajo.core/src/com/dexels/navajo/mapping/RemoteAsyncAnswer.java
+++ core/com.dexels.navajo.core/src/com/dexels/navajo/mapping/RemoteAsyncAnswer.java
@@ -1,42 +1,46 @@
 package com.dexels.navajo.mapping;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.dexels.navajo.server.enterprise.tribe.Answer;
 import com.dexels.navajo.server.enterprise.tribe.TribeManagerFactory;
 
 public class RemoteAsyncAnswer extends Answer {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 489178532753193613L;
+    private final static Logger logger = LoggerFactory.getLogger(RemoteAsyncAnswer.class);
 
-	
-	private Object ownerOfRef = null;
-	private Object hostNameOwnerOfRef = null;
-	private boolean acknowledged = false;
-	
-	public RemoteAsyncAnswer(RemoteAsyncRequest q) {
-		super(q);
-		RemoteAsyncRequest rasr = (RemoteAsyncRequest) getMyRequest();
-		String ref = rasr.getRef();
-		if (AsyncStore.getInstance().getInstance(ref) != null ) {
-			acknowledged = true;
-			ownerOfRef = TribeManagerFactory.getInstance().getMyMembership().getAddress();
-			hostNameOwnerOfRef = TribeManagerFactory.getInstance().getMyMembership().getMemberName();
-		}
-	}
+    private static final long serialVersionUID = 489178532753193613L;
 
-	@Override
-	public boolean acknowledged() {
-		return acknowledged;
-	}
+    private Object ownerOfRef = null;
+    private Object hostNameOwnerOfRef = null;
+    private boolean acknowledged = false;
 
-	public Object getOwnerOfRef() {
-		return ownerOfRef;
-	}
+    public RemoteAsyncAnswer(RemoteAsyncRequest q) {
+        super(q);
 
-	public Object getHostNameOwnerOfRef() {
-		return hostNameOwnerOfRef;
-	}
+        RemoteAsyncRequest rasr = (RemoteAsyncRequest) getMyRequest();
+        String ref = rasr.getRef();
+        if (AsyncStore.getInstance().getInstance(ref) != null) {
+            logger.debug("Found ref {} in my AsyncStore!", ref);
+            acknowledged = true;
+            ownerOfRef = TribeManagerFactory.getInstance().getMyMembership().getAddress();
+            hostNameOwnerOfRef = TribeManagerFactory.getInstance().getMyMembership().getMemberName();
+        }
+
+    }
+
+    @Override
+    public boolean acknowledged() {
+        return acknowledged;
+    }
+
+    public Object getOwnerOfRef() {
+        return ownerOfRef;
+    }
+
+    public Object getHostNameOwnerOfRef() {
+        return hostNameOwnerOfRef;
+    }
 
 }
--- core/com.dexels.navajo.core/src/com/dexels/navajo/mapping/RemoteAsyncRequest.java
+++ core/com.dexels.navajo.core/src/com/dexels/navajo/mapping/RemoteAsyncRequest.java
@@ -1,28 +1,34 @@
 package com.dexels.navajo.mapping;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.dexels.navajo.server.enterprise.tribe.Answer;
 import com.dexels.navajo.server.enterprise.tribe.Request;
 
 public class RemoteAsyncRequest extends Request {
+    private final static Logger logger = LoggerFactory.getLogger(RemoteAsyncRequest.class);
+    private static final long serialVersionUID = 10518850435818645L;
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 10518850435818645L;
+    private String ref;
 
-	private String ref;
-	
-	public RemoteAsyncRequest(String ref) {
-		this.ref = ref;
-	}
-	
-	@Override
-	public Answer getAnswer() {
-		return new RemoteAsyncAnswer(this);
-	}
+    public RemoteAsyncRequest(String ref) {
+        this.ref = ref;
+    }
 
-	public String getRef() {
-		return ref;
-	}
+    @Override
+    public Answer getAnswer() {
+        Answer a = null;
+        try {
+            a = new RemoteAsyncAnswer(this);
+        } catch (Exception e) {
+            logger.error("Exception on constructing RemoteAsyncAnswer: ", e);
+        }
+        return a;
+    }
+
+    public String getRef() {
+        return ref;
+    }
 
 }
