32a46165deb2432c8d3e3c43d97e90d8ccc1fc2c
Chris Brouwer <cbrouwer@dexels.com>
2015-08-28 08:47:25 +0200

    Removed debugging code

--- core/com.dexels.navajo.core/src/com/dexels/navajo/mapping/RemoteAsyncRequest.java
+++ core/com.dexels.navajo.core/src/com/dexels/navajo/mapping/RemoteAsyncRequest.java
@@ -1,13 +1,9 @@
 package com.dexels.navajo.mapping;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.dexels.navajo.server.enterprise.tribe.Answer;
 import com.dexels.navajo.server.enterprise.tribe.Request;
 
 public class RemoteAsyncRequest extends Request {
-    private final static Logger logger = LoggerFactory.getLogger(RemoteAsyncRequest.class);
     private static final long serialVersionUID = 10518850435818645L;
 
     private String ref;
@@ -18,14 +14,7 @@ public class RemoteAsyncRequest extends Request {
 
     @Override
     public Answer getAnswer() {
-        Answer a = null;
-        logger.info("Constructing RemoteAsyncRequest answer for: {}", ref);
-        try {
-            a = new RemoteAsyncAnswer(this);
-        } catch (Exception e) {
-            logger.error("Exception on constructing RemoteAsyncAnswer: ", e);
-        }
-        logger.info("Returning RemoteAsyncRequest answer for: {}", ref);
+        Answer a = new RemoteAsyncAnswer(this);
         return a;
     }
 
