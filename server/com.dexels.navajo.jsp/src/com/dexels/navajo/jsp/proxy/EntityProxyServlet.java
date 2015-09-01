3429c92c2fbc96d411151673748116563f88e9b4
Frank Lyaruu <flyaruu@gmail.com>
2015-09-01 13:29:24 +0200

    Add proxy servlet to JSP backend

---
+++ server/com.dexels.navajo.jsp/src/com/dexels/navajo/jsp/proxy/EntityProxyServlet.java
@@ -0,0 +1,90 @@
+package com.dexels.navajo.jsp.proxy;
+
+import java.net.URI;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+import org.dexels.utils.Base64;
+import org.eclipse.jetty.client.api.Request;
+
+public class EntityProxyServlet extends org.eclipse.jetty.proxy.ProxyServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2635217637957904173L;
+	private String server;
+	private String username;
+	private String password;
+	private String url;
+	private String tenant;
+
+//	org.eclipse.jetty.servlets.Pro
+
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		this.server = getApplicationAttribute("NavajoServer",config);
+		this.username = getApplicationAttribute("NavajoUser",config);
+		this.password = getApplicationAttribute("NavajoPassword",config);
+		this.url = getApplicationAttribute("Entity",config);
+		this.tenant = getApplicationAttribute("Tenant",config);
+	}
+	
+	@Override
+	protected void customizeProxyRequest(Request proxyRequest,
+			HttpServletRequest request) {
+//		proxyRequest.getHeaders().remove("Host");
+		String auth = this.username+":"+this.password;
+		String enc = Base64.encode(auth.getBytes());
+		proxyRequest.getHeaders().add("Authentication", "Basic "+enc);
+		if(this.tenant!=null) {
+			proxyRequest.getHeaders().add("X-Navajo-Instance",this.tenant);
+		}
+	}
+
+	@Override
+	protected URI rewriteURI(HttpServletRequest request) {
+		String query = request.getQueryString();
+		if(this.url==null) {
+			return getEntityFromServer(query);
+		}
+		return URI.create(this.url + "?" + query );
+	}
+
+	private URI getEntityFromServer(String query) {
+		final String POSTFIX = "/navajo";
+		if(this.server.endsWith(POSTFIX)) {
+			String url = server.substring(0,server.length()-POSTFIX.length())+"/entity";
+			return URI.create(url + "?" + query );
+		}
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		EntityProxyServlet eps = new EntityProxyServlet();
+		eps.server="https://knvb-test.sportlink.com/navajo";
+		URI u = eps.getEntityFromServer("tralla");
+		System.err.println("u: "+u);
+	}
+	
+	private String getApplicationAttribute(String key,ServletConfig config) {
+		ServletContext servletContext = config.getServletContext();
+		String value = null;
+		if(servletContext!=null) {
+			value = servletContext.getInitParameter(key);
+		}
+		if(value!=null) {
+			return value;
+		}
+		value = System.getenv(key);
+		if(value!=null) {
+			return value;
+		}
+		return System.getProperty(key);
+	}
+}
