7e31bd6da85e6afefc1e517de278bda8b5a13d83
Frank Lyaruu <flyaruu@gmail.com>
2015-09-01 15:21:01 +0200

    Working now

--- server/com.dexels.navajo.jsp/src/com/dexels/navajo/jsp/proxy/EntityProxyServlet.java
+++ server/com.dexels.navajo.jsp/src/com/dexels/navajo/jsp/proxy/EntityProxyServlet.java
@@ -8,67 +8,100 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 
-import org.dexels.utils.Base64;
 import org.eclipse.jetty.client.HttpClient;
 import org.eclipse.jetty.client.api.Request;
+import org.eclipse.jetty.client.http.HttpClientTransportOverHTTP;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class EntityProxyServlet extends org.eclipse.jetty.proxy.ProxyServlet {
 
+	
+	private final static Logger logger = LoggerFactory
+			.getLogger(EntityProxyServlet.class);
+	
 	private static final long serialVersionUID = -2635217637957904173L;
 	private String server;
 	private String username;
 	private String password;
 	private String url;
-	private String tenant;
+//	private String tenant;
 
 //	org.eclipse.jetty.servlets.Pro
 
 	
 	@Override
 	public void init(ServletConfig config) throws ServletException {
-		config.getServletContext().setAttribute("org.eclipse.jetty.server.Executor", Executors.newCachedThreadPool());
+//		config.getServletContext().setAttribute("org.eclipse.jetty.server.Executor", Executors.newCachedThreadPool());
 		super.init(config);
 		this.server = getApplicationAttribute("NavajoServer",config);
 		this.username = getApplicationAttribute("NavajoUser",config);
 		this.password = getApplicationAttribute("NavajoPassword",config);
 		this.url = getApplicationAttribute("Entity",config);
-		this.tenant = getApplicationAttribute("Tenant",config);
+//		this.tenant = getApplicationAttribute("Tenant",config);
 		
 	}
 	
 	@Override
 	protected HttpClient createHttpClient() throws ServletException {
-		return super.createHttpClient();
-	}
+	    SslContextFactory sslContextFactory = new SslContextFactory();
+        HttpClientTransportOverHTTP transport = new HttpClientTransportOverHTTP();
 
+	    HttpClient httpClient = new HttpClient(transport,sslContextFactory);
+        transport.setHttpClient(httpClient);
+	    httpClient.setExecutor(Executors.newFixedThreadPool(3));
+	    try {
+			httpClient.start();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	    return httpClient;
+	}
+	
 	@Override
 	protected void customizeProxyRequest(Request proxyRequest,
 			HttpServletRequest request) {
-//		proxyRequest.getHeaders().remove("Host");
-		String auth = this.username+":"+this.password;
-		String enc = Base64.encode(auth.getBytes());
-		proxyRequest.getHeaders().add("Authentication", "Basic "+enc);
-		if(this.tenant!=null) {
-			proxyRequest.getHeaders().add("X-Navajo-Instance",this.tenant);
-		}
+		proxyRequest.getHeaders().remove("Host");
+		
 	}
 
 	@Override
 	protected URI rewriteURI(HttpServletRequest request) {
 		String query = request.getQueryString();
-		if(this.url==null) {
-			return getEntityFromServer(query);
+		String pathInfo = request.getPathInfo();
+		String construct = getEntityFromServer();
+		if(pathInfo!=null) {
+			construct = construct+pathInfo;
 		}
-		return query!=null?URI.create(this.url + "?" + query ):URI.create(this.url);
+		if(query!=null) {
+			construct = construct+"?"+query;
+		}
+		logger.info("Constructed: "+construct);
+		if(this.username!=null) {
+			construct = appendParam(construct,"username",username);
+		}
+		if(this.password!=null) {
+			construct = appendParam(construct,"password",password);
+		}
+		return URI.create(construct);
+//		return URI.create("http://www.dexels.com/");
 	}
 
-	private URI getEntityFromServer(String query) {
+	private String appendParam(String url, String key, String value) {
+		return url + (url.contains("?")?"&":"?")+key+"="+value;
+	}
+
+	private String getEntityFromServer() {
+		if(this.url!=null) {
+			return this.url;
+		}
 		final String POSTFIX = "/navajo";
 		if(this.server.endsWith(POSTFIX)) {
 			String url = server.substring(0,server.length()-POSTFIX.length())+"/entity";
-			URI uri = query!=null? URI.create(url + "?" + query ): URI.create(url ) ;
-			System.err.println("uri assembled: "+uri);
-			return uri;
+//			URI uri = query!=null? URI.create(url + "?" + query ): URI.create(url ) ;
+//			System.err.println("uri assembled: "+uri);
+			return url;
 		}
 		return null;
 	}
@@ -76,8 +109,9 @@ public class EntityProxyServlet extends org.eclipse.jetty.proxy.ProxyServlet {
 	public static void main(String[] args) {
 		EntityProxyServlet eps = new EntityProxyServlet();
 		eps.server="https://knvb-test.sportlink.com/navajo";
-		URI u = eps.getEntityFromServer("tralla");
-		System.err.println("u: "+u);
+//		String construct = 
+//		URI u = eps.getEntityFromServer("tralla");
+//		System.err.println("u: "+u);
 	}
 	
 	private String getApplicationAttribute(String key,ServletConfig config) {
