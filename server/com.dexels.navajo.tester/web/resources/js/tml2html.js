7b50748c1bfb0b060db39fd49de4e343654da6e4
Chris Brouwer <cbrouwer@dexels.com>
2015-09-10 15:03:16 +0200

    nested messages

--- server/com.dexels.navajo.tester/web/resources/js/tml2html.js
+++ server/com.dexels.navajo.tester/web/resources/js/tml2html.js
@@ -3,26 +3,45 @@ function parseTmlToHtml( element, tmlString) {
     
     var xml = $.parseXML(tmlString),
     $xml = $( xml ),
-    $messages = $xml.find('message');
+    $tml = $xml.children('tml');
     
-    $messages.each(function(index){
-       var name =  $(this).attr('name');
-       var properties = $(this).find('property')
-       var html = '';
-       html += '<ul>';
-       html += name;
-       html += '<ul>';
-       properties.each(function(index){
-           var propname = $(this).attr('name');
-           var propvalue = $(this).attr('value');
-           html +='<li>';
-           html +=propname;
-           html +=': ';
-           html +=propvalue;
-           html +='</li>';
-       });
-       html += '</ul>'
-       html += '</ul>'
-       element.append(html)
+    var html = '<ul>';
+    $tml.children('message').each(function(index){
+        html += parseTmlMessage($(this));
     });
+    html +='</ul>';
+    element.html(html);
+}
+
+function parseTmlMessage(message) {
+    var name =  message.attr('name');
+    
+    var html = '<li>';
+    html += ' <b>' + name + '</b>';
+    
+    
+    var properties = message.children('property')
+    html += '<ul>'
+    properties.each(function(index){
+        var propname = $(this).attr('name');
+        var propvalue = $(this).attr('value');
+        html +='<li>';
+        html +=propname;
+        html +=': ';
+        html +=propvalue;
+        html +='</li>';
+    });
+    
+    var submessages = message.children('message')
+    
+    submessages.each(function(index){
+        console.log("In "+ name + " child found: " + $(this).attr('name'))
+       
+        html += parseTmlMessage($(this));
+      
+    });
+    html += '</ul>'
+    html += '</li>'
+ 
+    return html;
 }
