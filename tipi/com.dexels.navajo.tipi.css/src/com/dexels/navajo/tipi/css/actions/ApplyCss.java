9b5a6434a324379ac712297de0ec6af55a5a2bcc
Chris Brouwer <cbrouwer@dexels.com>
2015-10-09 11:10:45 +0200

    revert debug code commit

--- tipi/com.dexels.navajo.tipi.css/src/com/dexels/navajo/tipi/css/actions/ApplyCss.java
+++ tipi/com.dexels.navajo.tipi.css/src/com/dexels/navajo/tipi/css/actions/ApplyCss.java
@@ -2,23 +2,17 @@ package com.dexels.navajo.tipi.css.actions;
 
 import java.net.URL;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.dexels.navajo.tipi.TipiBreakException;
 import com.dexels.navajo.tipi.TipiComponent;
 import com.dexels.navajo.tipi.TipiException;
 import com.dexels.navajo.tipi.css.CssFactory;
-import com.dexels.navajo.tipi.css.impl.CssComponentResponderImpl;
 import com.dexels.navajo.tipi.internal.TipiAction;
 import com.dexels.navajo.tipi.internal.TipiEvent;
 
 public class ApplyCss extends TipiAction {
 
-    private final static Logger logger = LoggerFactory
-            .getLogger(ApplyCss.class);
 	private static final long serialVersionUID = 5481600392557969470L;
-	private static long totalTime = 0;
+	
 	public org.akrogen.tkui.css.tipi.engine.CSSTipiEngineImpl aap;
 	
 	@Override
@@ -34,7 +28,6 @@ public class ApplyCss extends TipiAction {
 		
 	}
 	private void doExecute(final TipiEvent event) {
-	    long start = System.nanoTime();
 		String styleString = (String) getEvaluatedParameterValue("style", event);
 		TipiComponent component = (TipiComponent) getEvaluatedParameterValue("component", event);
 		URL styleResource = null; //(URL) getEvaluatedParameterValue("styleSheet", event);
@@ -73,9 +66,6 @@ public class ApplyCss extends TipiAction {
 		{
 			CssFactory.getInstance().applyCss(component, styleString, styleResource, event);
 		}
-		  long end = System.nanoTime();
-		  totalTime += (end - start);
-		  logger.info("csstime css took: {}, total {}", (end - start) / 1000000, totalTime/ 1000000);
 	}
 
 	
