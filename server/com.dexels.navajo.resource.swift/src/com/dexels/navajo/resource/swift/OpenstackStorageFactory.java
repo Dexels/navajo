993b025b8b4cdf05d04928bd5f8329328721c66f
Frank Lyaruu <frank@dexels.com>
2016-03-30 15:38:21 +0200

    swift update

---
+++ server/com.dexels.navajo.resource.swift/src/com/dexels/navajo/resource/swift/OpenstackStorageFactory.java
@@ -0,0 +1,68 @@
+package com.dexels.navajo.resource.swift;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class OpenstackStorageFactory {
+	
+	private static OpenstackStorageFactory instance = null;
+	
+	private final Map<String,OpenstackStore> defaultStores = new HashMap<>();
+	private final Map<String,Map<String,OpenstackStore>> tenantStores = new HashMap<>();
+	
+	public void activate() {
+		instance = this;
+	}
+
+	public void deactivate() {
+		instance = null;
+	}
+	
+	public static OpenstackStorageFactory getInstance() {
+		return instance;
+	}
+
+	public void addOpenstackStore(OpenstackStore store, Map<String,Object> settings) {
+		String name = (String) settings.get("name");
+		String tenant = (String) settings.get("tenant");
+		if(tenant==null) {
+			defaultStores.put(name, store);
+			return;
+		}
+		Map<String,OpenstackStore> myTenantStore = tenantStores.get(tenant);
+		if(myTenantStore==null) {
+			myTenantStore = new HashMap<>();
+			tenantStores.put(tenant, myTenantStore);
+		}
+		myTenantStore.put(name, store);
+	}
+	
+	public void removeOpenstackStore(OpenstackStore store, Map<String,Object> settings) {
+		String name = (String) settings.get("name");
+		String tenant = (String) settings.get("tenant");
+		if(tenant==null) {
+			defaultStores.remove(name);
+			return;
+		}
+		Map<String,OpenstackStore> myTenantStore = tenantStores.get(tenant);
+		if(myTenantStore==null) {
+			myTenantStore = new HashMap<>();
+			tenantStores.put(tenant, myTenantStore);
+		}
+		myTenantStore.remove(name);
+	}
+
+	public OpenstackStore getOpenstackStore(String name, String tenant) {
+		Map<String,OpenstackStore> myTenantStore = tenantStores.get(tenant);
+		if(myTenantStore!=null) {
+			OpenstackStore os = myTenantStore.get(name);
+			if(os!=null) {
+				return os;
+			}
+		}
+		return defaultStores.get(name);
+	}
+	
+	
+}
+
--- server/com.dexels.navajo.resource.swift/src/com/dexels/navajo/resource/swift/OpenstackStore.java
+++ server/com.dexels.navajo.resource.swift/src/com/dexels/navajo/resource/swift/OpenstackStore.java
@@ -5,10 +5,10 @@ import java.util.Map;
 import com.dexels.navajo.document.types.Binary;
 
 public interface OpenstackStore {
-	public void set(String name, Binary contents);
 	public Binary get(String name);
 	public void delete(String name);
 	public Map<String, Object> metadata(String name);
-
+	public void set(String name, Binary contents, Map<String, String> metadata);
+	
 }
 
