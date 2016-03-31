dd1dbdf9e6df074b97e5135976847b2fffcee14c
Frank Lyaruu <frank@dexels.com>
2016-03-31 17:02:23 +0200

    Working swift adapter (to insert objects) and SwiftValue function (to retrieve an object) No lazy evaluation optimizations yet.

---
+++ server/com.dexels.navajo.adapters.swift/src/com/dexels/navajo/adapters/swift/functions/SwiftAdapterFunctionLibrary.java
@@ -0,0 +1,91 @@
+package com.dexels.navajo.adapters.swift.functions;
+
+import java.io.InputStream;
+import java.util.List;
+
+import navajo.ExtensionDefinition;
+
+
+
+/**
+ * Reference to the XML definition file for this set of adapters defined in this project.
+ * 
+ * @author arjen
+ *
+ */
+public class SwiftAdapterFunctionLibrary implements ExtensionDefinition {
+	private static final long serialVersionUID = -2167328743333229662L;
+	private transient ClassLoader extensionClassLoader = null;
+	
+	@Override
+	public InputStream getDefinitionAsStream() {
+		return getClass().getResourceAsStream("swiftfunctions.xml");
+	}
+
+	@Override
+	public String getConnectorId() {
+		return null;
+	}
+
+	@Override
+	public List<String> getDependingProjectUrls() {
+		return null;
+	}
+
+	public String getDeploymentDescriptor() {
+		return null;
+	}
+
+	@Override
+	public String getDescription() {
+		return "The Swift Object Store Function Library";
+	}
+
+	@Override
+	public String getId() {
+		return "NavajoSwift";
+	}
+
+	@Override
+	public String[] getIncludes() {
+		return null;
+	}
+
+	public List<String> getLibraryJars() {
+		return null;
+	}
+
+	public List<String> getMainJars() {
+		return null;
+	}
+
+	@Override
+	public String getProjectName() {
+		return null;
+	}
+
+	@Override
+	public List<String> getRequiredExtensions() {
+		return null;
+	}
+
+	@Override
+	public boolean isMainImplementation() {
+		return false;
+	}
+
+	@Override
+	public String requiresMainImplementation() {
+		return null;
+	}
+	@Override
+	public ClassLoader getExtensionClassloader() {
+		return extensionClassLoader;
+	}
+
+	@Override
+	public void setExtensionClassloader(ClassLoader extClassloader) {
+		extensionClassLoader =  extClassloader;
+	}
+
+}
