a675113a11eb7ed03da86d74d2a6fc22c51bb690
Chris Brouwer <cbrouwer@dexels.com>
2016-04-08 15:13:53 +0200

    Only show FailedExpression and EvaluatedExpression in a ValidationError message if we are in DebugScripts mode - fixes #358

--- core/com.dexels.navajo.core/src/com/dexels/navajo/mapping/CompiledScript.java
+++ core/com.dexels.navajo.core/src/com/dexels/navajo/mapping/CompiledScript.java
@@ -540,15 +540,19 @@ public abstract class CompiledScript implements CompiledScriptMXBean, Mappable, 
                 }
                 Property prop1 = NavajoFactory.getInstance().createProperty(outMessage, "Description",
                         Property.STRING_PROPERTY, description, 0, "", Property.DIR_OUT);
-                Property prop2 = NavajoFactory.getInstance().createProperty(outMessage, "FailedExpression",
-                        Property.STRING_PROPERTY, condition.condition, 0, "", Property.DIR_OUT);
-                Property prop3 = NavajoFactory.getInstance().createProperty(outMessage, "EvaluatedExpression",
-                        Property.STRING_PROPERTY, eval, 0, "", Property.DIR_OUT);
-
+                
                 msg.addProperty(prop0);
                 msg.addProperty(prop1);
-                msg.addProperty(prop2);
-                msg.addProperty(prop3);
+                
+                if (System.getenv("DEBUG_SCRIPTS").equals("true")) {
+                    Property prop2 = NavajoFactory.getInstance().createProperty(outMessage, "FailedExpression",
+                            Property.STRING_PROPERTY, condition.condition, 0, "", Property.DIR_OUT);
+                    Property prop3 = NavajoFactory.getInstance().createProperty(outMessage, "EvaluatedExpression",
+                            Property.STRING_PROPERTY, eval, 0, "", Property.DIR_OUT);
+                    msg.addProperty(prop2);
+                    msg.addProperty(prop3);
+                }
+
                 messages.add(msg);
             }
         }
