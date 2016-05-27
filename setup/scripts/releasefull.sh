3f40d5d193488e3beadb6b6426b3d9b2fa05eafc
Chris Brouwer <cbrouwer@dexels.com>
2016-05-27 09:14:24 +0200

    more echoing in releasefull script

--- setup/scripts/releasefull.sh
+++ setup/scripts/releasefull.sh
@@ -24,6 +24,7 @@ then
         exit 1
 fi
 
+BUNDLENAME=`cat META-INF/MANIFEST.MF | grep Bundle-SymbolicName | awk '{ print $2 }'`
 VERSION=`cat META-INF/MANIFEST.MF | grep Bundle-Version | awk '{ print $2 }'`
 VERSION=${VERSION%.qualifier}
 BASEVERSION=`echo "$VERSION" | cut -f1-2 -d '.'`
@@ -36,7 +37,7 @@ read -p "Enter release version [$NEWMINOR1]: " TMPNEWMINOR1
 NEWMINOR1=${TMPNEWMINOR1:-$NEWMINOR1}
 BASEVERSION=`echo "$NEWMINOR1" | cut -f1-2 -d '.'`
 NEWMINOR2=$BASEVERSION.$((`echo "$NEWMINOR1"| cut -f3 -d '.'` + 1))
-echo "Going to release $NEWMINOR1 and $NEWMINOR2 - press ctrl+c to cancel within 5 seconds"
+echo "Going to release $BUNDLENAME $NEWMINOR1 and $NEWMINOR2 - press ctrl+c to cancel within 5 seconds"
 prettysleep 5
 
 
@@ -49,3 +50,12 @@ then
 fi
 
 release.sh $NEWMINOR1 $NEWMINOR2
+
+if [ $? -ne 0 ]
+then
+        echo "Something went wrong!"
+        exit 1
+fi
+
+echo "Successfully released $BUNDLENAME $NEWMINOR1"
+
