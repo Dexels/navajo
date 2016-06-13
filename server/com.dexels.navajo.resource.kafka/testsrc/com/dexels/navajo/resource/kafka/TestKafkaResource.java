71a182a78cf9eb09011817c0103a19557225b5fa
Frank Lyaruu <frank@dexels.com>
2016-06-13 16:44:31 +0200

    Create regular resource for Kafka queues

--- server/com.dexels.navajo.resource.kafka/testsrc/com/dexels/navajo/resource/kafka/TestKafkaResource.java
+++ server/com.dexels.navajo.resource.kafka/testsrc/com/dexels/navajo/resource/kafka/TestKafkaResource.java
@@ -3,10 +3,10 @@ package com.dexels.navajo.resource.kafka;
 import org.junit.After;
 import org.junit.Test;
 
-import com.dexels.kafka.api.KafkaPublisher;
 import com.dexels.kafka.factory.KafkaClientFactory;
 import com.dexels.navajo.script.api.MappableException;
 import com.dexels.navajo.script.api.UserException;
+import com.dexels.pubsub.api.MessagePublisher;
 
 public class TestKafkaResource {
 
@@ -18,7 +18,7 @@ public class TestKafkaResource {
 
 	@Test
 	public void testSimple() throws MappableException, UserException, InterruptedException {
-		KafkaPublisher kp = KafkaClientFactory.createPublisher("cerberus1.sportlink-infra.net:9092,cerberus2.sportlink-infra.net:9094","topic");
+		MessagePublisher kp = KafkaClientFactory.createPublisher("cerberus1.sportlink-infra.net:9092,cerberus2.sportlink-infra.net:9094","topic");
 		kp.publish("key", "blubblub".getBytes());
 		Thread.sleep(1000);
 	}
