29333e887cceeac482f50684f86730381fa9b0c8
Frank Lyaruu <frank@dexels.com>
2016-06-22 10:57:33 +0200

    add new tasts

--- dev/com.dexels.navajo.dev.ant/src/com/dexels/navajo/dev/ant/CopyDependencies.java
+++ dev/com.dexels.navajo.dev.ant/src/com/dexels/navajo/dev/ant/CopyDependencies.java
@@ -2,10 +2,10 @@ package com.dexels.navajo.dev.ant;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.Reader;
-import java.io.Writer;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashSet;
 import java.util.Properties;
 import java.util.Set;
@@ -13,24 +13,14 @@ import java.util.Set;
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
 
-import com.dexels.navajo.dev.ant.feature.CaseSensitiveXMLElement;
-import com.dexels.navajo.dev.ant.feature.XMLElement;
-
-public class AddDependenciesToPom extends Task {
+public class CopyDependencies extends Task {
 
 	private String input;
-	private String template;
-	private XMLElement xe;
 	private String destination;
 	private final Set<String> groups = new HashSet<>();
 	@Override
 	public void execute() throws BuildException {
-		File templateFile = new File(template);
-		xe = new CaseSensitiveXMLElement(false);
 		try {
-			try(Reader r = new FileReader(templateFile)) {
-				xe.parseFromReader(r);
-			}
 			File bundleFile = new File(input);
 			Properties p = new Properties();
 			try(FileInputStream fis = new FileInputStream(bundleFile)) {
@@ -42,10 +32,10 @@ public class AddDependenciesToPom extends Task {
 				log("Parsing: "+name);
 				String[] parts = name.split("\\.");
 				if(parts.length==1) {
-					addDependencies((String) e.getValue());
+					copyFiles((String) e.getValue());
 				} else {
 					if(groups.contains(parts[1])) {
-						addDependencies((String) e.getValue());
+						copyFiles((String) e.getValue());
 					} else {
 						log("Ignoring module: "+name+" it is not in groups: "+this.groups);
 					}
@@ -56,60 +46,62 @@ public class AddDependenciesToPom extends Task {
 			}
 //			String bootrepo = p.getProperty("bootrepo");
 //			log("pom:\n"+xe);
-			File output = new File(destination);
-			try(Writer w = new FileWriter(output)) {
-				xe.write(w);
-			}
+
 		} catch (Throwable e) {
 			log("Error creating pom",e,4);
 			throw new BuildException("Error creating pom",e);
 		}
 	}
 
-	private void addDependencies(String bootrepo) {
+	private void copyFiles(String bootrepo) throws IOException {
 		log("Added dependency: "+bootrepo);
 		if(bootrepo==null) {
 			return;
 		}
-		XMLElement deps = xe.getChildByTagName("dependencies");
 		String[] lines = bootrepo.split(",");
 		for (String line : lines) {
-//			System.err.println("line: "+line);
 			String[] proto = line.split(":");
 			if(!proto[0].equals("mvn")) {
 				throw new IllegalArgumentException("Only mvn url are supported, not: "+proto[0]);
 			}
 			String[] parts = proto[1].split("/");
 			String classifier = parts.length>3? parts[3] : null;
-			deps.addChild(appendDependency(parts[0],parts[1],parts[2],classifier));
+			copyDependency(parts[0],parts[1],parts[2],classifier);
 //			System.err.println("PRO: "+proto[1]);
 		}
 
 	}
 
-	private XMLElement appendDependency(String groupId, String artifactId, String version, String classifier) {
-		XMLElement dep = new CaseSensitiveXMLElement("dependency");
-
-		dep.addTagKeyValue("groupId", groupId);
-		dep.addTagKeyValue("artifactId", artifactId);
-		dep.addTagKeyValue("version", version);
-		if(classifier!=null) {
-			dep.addTagKeyValue("classifier", classifier);
-		}
-		return dep;
-	}
 	
+	private void copyDependency(String group, String artifact, String version, String classifier) throws IOException {
+		Path repo = Paths.get(System.getProperty("user.home"), ".m2","repository");
+		String[] groupParts = group.split("\\.");
+		Path resolved = repo;
+		for (String part : groupParts) {
+			resolved = resolved.resolve(part);
+		}
+		resolved = resolved.resolve(artifact);
+		resolved = resolved.resolve(version);
+		if (classifier==null) {
+			resolved = resolved.resolve(artifact+"-"+version+".jar");
+		} else {
+			resolved = resolved.resolve(artifact+"-"+version+"-"+classifier+".jar");
+		}
+		Path dest = Paths.get(destination).resolve(artifact+"-"+version+".jar");
+		
+		System.err.println("Copying from: "+resolved.toString()+" to "+dest);
+		Files.copy(resolved, dest);
+		
+	}
+
 	public void setDestination(String destination) {
 		this.destination = destination;
 	}
-
+	
 	public void setBundles(String input) {
 		this.input = input;
 	}
 
-	public void setTemplate(String template) {
-		this.template = template;
-	}
 
 	public void setGroups(String groups) {
 		this.groups.clear();
@@ -119,9 +111,6 @@ public class AddDependenciesToPom extends Task {
 	}
 
 	public static void main(String[] args) {
-		AddDependenciesToPom ad = new AddDependenciesToPom();
-		ad.setBundles("dexels.bundles.cfg");
-		ad.setTemplate("pom-template.xml");
-		ad.execute();
+		
 	}
 }
