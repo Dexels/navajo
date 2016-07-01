e54644e26f98f206ef489ca3a2360f2cd43c65b1
Chris Brouwer <cbrouwer@dexels.com>
2016-07-01 11:15:00 +0200

    formatting

--- core/com.dexels.navajo.core/src/com/dexels/navajo/mapping/compiler/TslCompiler.java
+++ core/com.dexels.navajo.core/src/com/dexels/navajo/mapping/compiler/TslCompiler.java
@@ -630,91 +630,71 @@ public class TslCompiler {
 		StringBuffer result = new StringBuffer();
 
 		NodeList children = n.getChildNodes();
-		for (int i = 0; i < children.getLength(); i++) {
-			if (children.item(i).getNodeName().equals("operation")) {
-				Element e = (Element) children.item(i);
-				String entity = e.getAttribute("entity");
-				String service = e.getAttribute("service");
-				String tenant = e.getAttribute("tenant");
-				String validationService = e.getAttribute("validationService");
-				String method = e.getAttribute("method");
-				String debug = e.getAttribute("debug");
-				
-				result.append(printIdent(ident) + "if (true) {\n");
+        for (int i = 0; i < children.getLength(); i++) {
+            if (children.item(i).getNodeName().equals("operation")) {
+                Element e = (Element) children.item(i);
+                String entity = e.getAttribute("entity");
+                String service = e.getAttribute("service");
+                String tenant = e.getAttribute("tenant");
+                String validationService = e.getAttribute("validationService");
+                String method = e.getAttribute("method");
+                String debug = e.getAttribute("debug");
 
-				String operationString = "com.dexels.navajo.document.Operation o = "
-						+ "NavajoFactory.getInstance().createOperation(access.getOutputDoc(), "
-						+ "\""
-						+ method
-						+ "\", \""
-						+ service;
-				if (!validationService.equals("")) {
-					operationString += "\", \"" + validationService;
-				}
-						
-				operationString	+= "\", \""+ entity + "\", null);\n";
+                result.append(printIdent(ident) + "if (true) {\n");
 
-				result.append(printIdent(ident + 2) + operationString);
-				// Find extra message definition.
-				NodeList extraMessages = e.getChildNodes();
-				DocumentBuilderFactory factory = DocumentBuilderFactory
-						.newInstance();
-				DocumentBuilder builder = factory.newDocumentBuilder();
-				DOMImplementation impl = builder.getDOMImplementation();
-				Document doc = impl.createDocument(null, null, null);
+                String operationString = "com.dexels.navajo.document.Operation o = " + "NavajoFactory.getInstance().createOperation(access.getOutputDoc(), "
+                        + "\"" + method + "\", \"" + service;
+                if (!validationService.equals("")) {
+                    operationString += "\", \"" + validationService;
+                }
 
-				String extraMessageName = null;
-				Element extraMessageElement = null;
+                operationString += "\", \"" + entity + "\", null);\n";
 
-				for (int j = 0; j < extraMessages.getLength(); j++) {
-					if (extraMessages.item(j).getNodeName().equals("message")) {
-						extraMessageElement = (Element) extraMessages.item(j);
-						extraMessageName = extraMessageElement
-								.getAttribute("name");
-						break;
-					}
-				}
+                result.append(printIdent(ident + 2) + operationString);
+                // Find extra message definition.
+                NodeList extraMessages = e.getChildNodes();
+                String extraMessageName = null;
+                Element extraMessageElement = null;
 
-				if (extraMessageName != null) {
-					DOMSource domSource = new DOMSource(extraMessageElement);
-					Transformer transformer = TransformerFactory.newInstance()
-							.newTransformer();
-					transformer.setOutputProperty(
-							OutputKeys.OMIT_XML_DECLARATION, "yes");
-					transformer.setOutputProperty(OutputKeys.METHOD, "xml");
-					transformer.setOutputProperty(OutputKeys.ENCODING,
-							"ISO-8859-1");
-					transformer.setOutputProperty(
-							"{http://xml.apache.org/xslt}indent-amount", "4");
-					transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-					StringWriter sw = new StringWriter();
-					StreamResult sr = new StreamResult(sw);
-					transformer.transform(domSource, sr);
+                for (int j = 0; j < extraMessages.getLength(); j++) {
+                    if (extraMessages.item(j).getNodeName().equals("message")) {
+                        extraMessageElement = (Element) extraMessages.item(j);
+                        extraMessageName = extraMessageElement.getAttribute("name");
+                        break;
+                    }
+                }
 
-					String extraNavajo = removeNewLines("<tml>"
-							+ sw.toString().replace('\"', '\'') + "</tml>");
+                if (extraMessageName != null) {
+                    DOMSource domSource = new DOMSource(extraMessageElement);
+                    Transformer transformer = TransformerFactory.newInstance().newTransformer();
+                    transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+                    transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+                    transformer.setOutputProperty(OutputKeys.ENCODING, "ISO-8859-1");
+                    transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+                    transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+                    StringWriter sw = new StringWriter();
+                    StreamResult sr = new StreamResult(sw);
+                    transformer.transform(domSource, sr);
 
-					String extraNavajoOperation = "Navajo extra = NavajoFactory.getInstance().createNavajo(new java.io.StringReader(\""
-							+ extraNavajo
-							+ "\"));\n"
-							+ "o.setExtraMessage(extra.getMessage(\""
-							+ extraMessageName + "\"));\n";
+                    String extraNavajo = removeNewLines("<tml>" + sw.toString().replace('\"', '\'') + "</tml>");
 
-					result.append(printIdent(ident + 2) + extraNavajoOperation);
+                    String extraNavajoOperation = "Navajo extra = NavajoFactory.getInstance().createNavajo(new java.io.StringReader(\"" + extraNavajo
+                            + "\"));\n" + "o.setExtraMessage(extra.getMessage(\"" + extraMessageName + "\"));\n";
 
-				}
-				if (debug != null && !debug.equals("")) {
-				    result.append(printIdent(ident + 2) + "o.setDebug(\"" + debug + "\");\n");
-				}
-				if (tenant != null && !tenant.equals("")) {
+                    result.append(printIdent(ident + 2) + extraNavajoOperation);
+
+                }
+                if (debug != null && !debug.equals("")) {
+                    result.append(printIdent(ident + 2) + "o.setDebug(\"" + debug + "\");\n");
+                }
+                if (tenant != null && !tenant.equals("")) {
                     result.append(printIdent(ident + 2) + "o.setTenant(\"" + tenant + "\");\n");
                 }
 
-				result.append(printIdent(ident + 2)
-						+ "access.getOutputDoc().addOperation(o);\n");
-				result.append(printIdent(ident) + "}\n");
-			}
-		}
+                result.append(printIdent(ident + 2) + "access.getOutputDoc().addOperation(o);\n");
+                result.append(printIdent(ident) + "}\n");
+            }
+        }
 
 		return result.toString();
 
