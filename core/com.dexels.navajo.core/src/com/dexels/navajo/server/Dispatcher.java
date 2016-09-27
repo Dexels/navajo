ec06da1893d303d82c8dd7c2b45b92ad14254f97
Chris Brouwer <cbrouwer@dexels.com>
2016-09-27 09:02:21 +0200

    formatting

--- core/com.dexels.navajo.core/src/com/dexels/navajo/server/Dispatcher.java
+++ core/com.dexels.navajo.core/src/com/dexels/navajo/server/Dispatcher.java
@@ -870,9 +870,9 @@ public class Dispatcher implements Mappable, DispatcherMXBean, DispatcherInterfa
             access.rpcPwd = rpcPassword;
             access.setInDoc(inMessage);
            
-            access.setClientDescription( header.getHeaderAttribute("clientdescription"));
-            access.setApplication( header.getHeaderAttribute("application"));
-            access.setOrganization( header.getHeaderAttribute("organization"));
+            access.setClientDescription(header.getHeaderAttribute("clientdescription"));
+            access.setApplication(header.getHeaderAttribute("application"));
+            access.setOrganization(header.getHeaderAttribute("organization"));
             if (clientInfo != null) {
                 access.ipAddress = clientInfo.getIP();
                 access.hostName = clientInfo.getHost();
