764223d6408e5429759425c20a9be72052b748ea
Chris Brouwer <cbrouwer@dexels.com>
2016-09-28 18:43:13 +1000

    Ensure tester.js goes to priorirty threadpool

--- server/com.dexels.navajo.tester.js/web/resources/testerjs/dexels/navajotester.js
+++ server/com.dexels.navajo.tester.js/web/resources/testerjs/dexels/navajotester.js
@@ -198,6 +198,7 @@ function runScript(script) {
             type: "POST",
             url: "/navajo/" + instance,
             data: navajoinput,
+            headers: {"X-Navajo-Priority": "true"},
             success: function(xmlObj) {
                 replaceXml(script, xmlObj);
                 var stateObj = { script: script, xml:  serializer.serializeToString(xml) };
