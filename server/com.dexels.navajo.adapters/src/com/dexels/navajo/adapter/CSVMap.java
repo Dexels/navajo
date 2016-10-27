5937b618d80e33c7307f4c32f05a1f9421913076
Chris Brouwer <cbrouwer@dexels.com>
2016-10-27 16:39:22 +0200

    formatting

--- server/com.dexels.navajo.adapters/src/com/dexels/navajo/adapter/CSVMap.java
+++ server/com.dexels.navajo.adapters/src/com/dexels/navajo/adapter/CSVMap.java
@@ -38,265 +38,258 @@ import com.dexels.navajo.script.api.UserException;
  * @author Arjen Schoneveld
  * @version 1.0
  * 
- * This mappable object can be used to read a comma-separated-file.
+ *          This mappable object can be used to read a comma-separated-file.
  * 
- * $Id$
+ *          $Id$
  * 
  */
 
 public class CSVMap implements Mappable {
 
-	public CSVEntryMap[] entries;
-	public String fileName;
-	public Binary fileContent;
-	public String separator;
-	public int entryCount;
-	public boolean includeEmpty;
-	public boolean skipFirstRow;
-	public int maximumImportCount;
+    public CSVEntryMap[] entries;
+    public String fileName;
+    public Binary fileContent;
+    public String separator;
+    public int entryCount;
+    public boolean includeEmpty;
+    public boolean skipFirstRow;
+    public int maximumImportCount;
 
-	private boolean update = false;
+    private boolean update = false;
 
+    private List<CSVEntryMap> draftEntries = null;
 
-	private List<CSVEntryMap> draftEntries = null;
-	
-	private final static Logger logger = LoggerFactory.getLogger(CSVMap.class);
-	
-	@Override
-	public void load(Access access) throws MappableException, UserException {
+    private final static Logger logger = LoggerFactory.getLogger(CSVMap.class);
 
-	}
+    @Override
+    public void load(Access access) throws MappableException, UserException {
 
-	// for scala compatibility
-	public boolean getUpdate() {
-		return update;
-	}
-	
-	public boolean isUpdate() {
-		return update;
-	}
+    }
 
-	public void setUpdate(boolean update) {
-		this.update = update;
-	}
+    // for scala compatibility
+    public boolean getUpdate() {
+        return update;
+    }
 
-	public void setEntry(CSVEntryMap newEntry) {
-		if (draftEntries == null)
-			draftEntries = new ArrayList<CSVEntryMap>();
-		draftEntries.add(newEntry);
-		update = true;
-	}
+    public boolean isUpdate() {
+        return update;
+    }
 
-	public void setEntries(CSVEntryMap[] newEntries) {
-		update = true;
-		entries = newEntries;
-	}
+    public void setUpdate(boolean update) {
+        this.update = update;
+    }
 
-	public int getEntryCount() {
-		return entries.length;
-	}
-	
-	public int getEntryFieldCount() {
-	    int result = -1;
-	    if (entries != null && entries.length > 0) {
-	        CSVEntryMap e = entries[0];
-	        result = e.getEntrySize();
-	    }
-	    return result;
-	}
+    public void setEntry(CSVEntryMap newEntry) {
+        if (draftEntries == null)
+            draftEntries = new ArrayList<CSVEntryMap>();
+        draftEntries.add(newEntry);
+        update = true;
+    }
 
-	public CSVEntryMap[] getEntries() throws UserException {
-	    BufferedReader buffer = null;
-	    
-		try {
-		    Reader f = null;
-			if (fileContent != null) {
-				f = new InputStreamReader(fileContent.getDataAsStream(),"UTF-8");
-			} else {
-				f = new FileReader(fileName);
-			}
+    public void setEntries(CSVEntryMap[] newEntries) {
+        update = true;
+        entries = newEntries;
+    }
 
-			buffer = new BufferedReader(f);
-			String line = "";
-			boolean firstLine = true;
-			int importCount = 0;
-			List<CSVEntryMap> entryList = new ArrayList<CSVEntryMap>();
-			
-			while ((line = buffer.readLine()) != null) {
-	            if (maximumImportCount != 0 && (importCount >= maximumImportCount)) {
-	                break;
-	            }
-				if (isSkipFirstRow() && firstLine) {
-				    // First line will be skipped. Probably contains headers
-				} else {
-				    if (includeEmpty) {
-						parseLineWithEmpty(line, entryList);
-					} else {
-						parseLineDefault(line, entryList);
-					}
-				    importCount++;
-				}
-			    firstLine = false;
-			}
-			entries = new CSVEntryMap[entryList.size()];
-			int i = 0;
-			for (CSVEntryMap ce : entryList) {
-				entries[i++] = ce;
-			}
-		} catch (java.io.IOException ioe) {
-			throw new UserException(-1, ioe.getMessage());
-		} finally {
-		    if (buffer != null) {
-		        try {
+    public int getEntryCount() {
+        return entries.length;
+    }
+
+    public int getEntryFieldCount() {
+        int result = -1;
+        if (entries != null && entries.length > 0) {
+            CSVEntryMap e = entries[0];
+            result = e.getEntrySize();
+        }
+        return result;
+    }
+
+    public CSVEntryMap[] getEntries() throws UserException {
+        BufferedReader buffer = null;
+
+        try {
+            Reader f = null;
+            if (fileContent != null) {
+                f = new InputStreamReader(fileContent.getDataAsStream(), "UTF-8");
+            } else {
+                f = new FileReader(fileName);
+            }
+
+            buffer = new BufferedReader(f);
+            String line = "";
+            boolean firstLine = true;
+            int importCount = 0;
+            List<CSVEntryMap> entryList = new ArrayList<CSVEntryMap>();
+
+            while ((line = buffer.readLine()) != null) {
+                if (maximumImportCount != 0 && (importCount >= maximumImportCount)) {
+                    break;
+                }
+                if (isSkipFirstRow() && firstLine) {
+                    // First line will be skipped. Probably contains headers
+                } else {
+                    if (includeEmpty) {
+                        parseLineWithEmpty(line, entryList);
+                    } else {
+                        parseLineDefault(line, entryList);
+                    }
+                    importCount++;
+                }
+                firstLine = false;
+            }
+            entries = new CSVEntryMap[entryList.size()];
+            int i = 0;
+            for (CSVEntryMap ce : entryList) {
+                entries[i++] = ce;
+            }
+        } catch (java.io.IOException ioe) {
+            throw new UserException(-1, ioe.getMessage());
+        } finally {
+            if (buffer != null) {
+                try {
                     buffer.close();
                 } catch (IOException e) {
                     // Too late to apologize!
                 }
-		    }
-		}
-		return entries;
-	}
+            }
+        }
+        return entries;
+    }
 
-	private void parseLineWithEmpty(String line, List<CSVEntryMap> entryList) {
-		String sep = separator;
-		if (sep == null) {
-			sep = " ";
-		}
-		if (sep.length() > 1) {
-			throw new IllegalArgumentException("Can not include empty when separator is > 1 char.Sorry, feel free to implement.");
-		}
-		char sepChar = sep.charAt(0);
-		int startindex = 0;
-		List<String> currentLine = new ArrayList<String>();
-		for (int i = 0; i < line.length(); i++) {
-			char c = line.charAt(i);
-			if (c == sepChar) {
-				if (startindex == -1) {
-					currentLine.add(null);
-					startindex = -1;
-				} else {
-					if (startindex==i) {
-						currentLine.add(null);
-					} else {
-						String ss = line.substring(startindex, i);
-						currentLine.add(ss);
-					}
-					startindex = i+1;
-				}
-			}
-		}
-		
-		// add the last item on the line, not ended by the separator
-		if ( startindex == line.length() ) {
-			currentLine.add( null );
-		}
-		else {
-			String ss = line.substring( startindex, line.length() );
-			currentLine.add( ss );
-		}
-		CSVEntryMap csvEntry = new CSVEntryMap();
-		csvEntry.entries = new String[currentLine.size()];
-		for (int i = 0; i < currentLine.size(); i++) {
-			csvEntry.entries[i] = currentLine.get(i);
-		}
-		entryList.add(csvEntry);
-	}
+    private void parseLineWithEmpty(String line, List<CSVEntryMap> entryList) {
+        String sep = separator;
+        if (sep == null) {
+            sep = " ";
+        }
+        if (sep.length() > 1) {
+            throw new IllegalArgumentException(
+                    "Can not include empty when separator is > 1 char.Sorry, feel free to implement.");
+        }
+        char sepChar = sep.charAt(0);
+        int startindex = 0;
+        List<String> currentLine = new ArrayList<String>();
+        for (int i = 0; i < line.length(); i++) {
+            char c = line.charAt(i);
+            if (c == sepChar) {
+                if (startindex == -1) {
+                    currentLine.add(null);
+                    startindex = -1;
+                } else {
+                    if (startindex == i) {
+                        currentLine.add(null);
+                    } else {
+                        String ss = line.substring(startindex, i);
+                        currentLine.add(ss);
+                    }
+                    startindex = i + 1;
+                }
+            }
+        }
 
-	private void parseLineDefault(String line, List<CSVEntryMap> entryList) {
-		StringTokenizer tokens = new StringTokenizer(line, separator);
-		CSVEntryMap csvEntry = new CSVEntryMap();
-		csvEntry.entries = new String[tokens.countTokens()];
-		int index = 0;
-		while (tokens.hasMoreTokens()) {
-			csvEntry.entries[index++] = tokens.nextToken();
-		}
-		entryList.add(csvEntry);
-	}
+        // add the last item on the line, not ended by the separator
+        if (startindex == line.length()) {
+            currentLine.add(null);
+        } else {
+            String ss = line.substring(startindex, line.length());
+            currentLine.add(ss);
+        }
+        CSVEntryMap csvEntry = new CSVEntryMap();
+        csvEntry.entries = new String[currentLine.size()];
+        for (int i = 0; i < currentLine.size(); i++) {
+            csvEntry.entries[i] = currentLine.get(i);
+        }
+        entryList.add(csvEntry);
+    }
 
-	public void setSeparator(String sep) {
-		this.separator = sep;
-	}
+    private void parseLineDefault(String line, List<CSVEntryMap> entryList) {
+        StringTokenizer tokens = new StringTokenizer(line, separator);
+        CSVEntryMap csvEntry = new CSVEntryMap();
+        csvEntry.entries = new String[tokens.countTokens()];
+        int index = 0;
+        while (tokens.hasMoreTokens()) {
+            csvEntry.entries[index++] = tokens.nextToken();
+        }
+        entryList.add(csvEntry);
+    }
 
-	public void setFileName(String s)  {
-		this.fileName = s;
-	}
+    public void setSeparator(String sep) {
+        this.separator = sep;
+    }
 
-	public void setFileContent(Binary b) {
-		this.fileContent = b;
-	}
+    public void setFileName(String s) {
+        this.fileName = s;
+    }
 
-	
-	@Override
-	public void store() throws MappableException, UserException {
-		if (draftEntries != null) {
-			entries = new CSVEntryMap[draftEntries.size()];
-			int i = 0;
-			for (CSVEntryMap ce : draftEntries) {
-				entries[i++] = ce;
-			}
-//			entries = (CSVEntryMap[]) draftEntries.toArray();
-		}
-		if (update) {
-			try {
-				// Write CSV.
-				if (entries != null) {
-					FileWriter writer = new FileWriter(fileName);
-					for (int i = 0; i < entries.length; i++) {
-						CSVEntryMap e = entries[i];
-						for (int j = 0; j < e.entries.length; j++) {
-							writer.write(e.getEntry(new Integer(j)));
-							if (j < (e.entries.length - 1))
-								writer.write(separator);
-						}
-						if (i < (entries.length - 1))
-							writer.write("\n");
-					}
-					writer.close();
-				}
-			} catch (IOException ioe) {
-				throw new UserException(-1, ioe.getMessage());
-			}
-		}
-	}
+    public void setFileContent(Binary b) {
+        this.fileContent = b;
+    }
 
-	@Override
-	public void kill() {
+    @Override
+    public void store() throws MappableException, UserException {
+        if (draftEntries != null) {
+            entries = new CSVEntryMap[draftEntries.size()];
+            int i = 0;
+            for (CSVEntryMap ce : draftEntries) {
+                entries[i++] = ce;
+            }
+            // entries = (CSVEntryMap[]) draftEntries.toArray();
+        }
+        if (update) {
+            try {
+                // Write CSV.
+                if (entries != null) {
+                    FileWriter writer = new FileWriter(fileName);
+                    for (int i = 0; i < entries.length; i++) {
+                        CSVEntryMap e = entries[i];
+                        for (int j = 0; j < e.entries.length; j++) {
+                            writer.write(e.getEntry(new Integer(j)));
+                            if (j < (e.entries.length - 1))
+                                writer.write(separator);
+                        }
+                        if (i < (entries.length - 1))
+                            writer.write("\n");
+                    }
+                    writer.close();
+                }
+            } catch (IOException ioe) {
+                throw new UserException(-1, ioe.getMessage());
+            }
+        }
+    }
 
-	}
+    @Override
+    public void kill() {
 
-	public static void main(String[] args) throws Exception {
-		Mappable csv = new CSVMap();
-		((CSVMap) csv).setSeparator(";");
-		((CSVMap) csv).setIncludeEmpty(true);
-		((CSVMap) csv).setSkipFirstRow(false);
+    }
 
-		Binary b = new Binary(new File("C:/Temp/LedenLIJST-vertrouwelijktest.csv"));
-		((CSVMap) csv).setFileContent(b);
-		Mappable[] all = ((CSVMap) csv).getEntries();
-		for (int i = 0; i < all.length; i++) {
-			CSVEntryMap entryMap = ((CSVEntryMap) all[i]);
-			logger.info("a = >" + entryMap.getEntry(new Integer(0)) + "< - >" + 
-			                             entryMap.getEntry(new Integer(1)) + "< - >" + 
-                                         entryMap.getEntry(new Integer(2)) + "< - >" + 
-                                         entryMap.getEntry(new Integer(3)) + "< - >" + 
-                                         entryMap.getEntry(new Integer(4)) + "< - >" + 
-                                         entryMap.getEntry(new Integer(5)) + "< - >" + 
-                                         entryMap.getEntry(new Integer(6)) + "< - >" + 
-                                         entryMap.getEntry(new Integer(7)) + "< - >" + 
-                                         entryMap.getEntry(new Integer(8)) + "< - >" + 
-                                         entryMap.getEntry(new Integer(9)) + "< - >" + 
-                                         entryMap.getEntry(new Integer(10)) + "< - >" + 
-			                             entryMap.getEntry(new Integer(11))+"<");
-		}
-	}
+    public static void main(String[] args) throws Exception {
+        Mappable csv = new CSVMap();
+        ((CSVMap) csv).setSeparator(";");
+        ((CSVMap) csv).setIncludeEmpty(true);
+        ((CSVMap) csv).setSkipFirstRow(false);
 
-	public boolean getIncludeEmpty() {
-		return includeEmpty;
-	}
+        Binary b = new Binary(new File("C:/Temp/LedenLIJST-vertrouwelijktest.csv"));
+        ((CSVMap) csv).setFileContent(b);
+        Mappable[] all = ((CSVMap) csv).getEntries();
+        for (int i = 0; i < all.length; i++) {
+            CSVEntryMap entryMap = ((CSVEntryMap) all[i]);
+            logger.info(
+                    "a = >" + entryMap.getEntry(new Integer(0)) + "< - >" + entryMap.getEntry(new Integer(1)) + "< - >"
+                            + entryMap.getEntry(new Integer(2)) + "< - >" + entryMap.getEntry(new Integer(3)) + "< - >"
+                            + entryMap.getEntry(new Integer(4)) + "< - >" + entryMap.getEntry(new Integer(5)) + "< - >"
+                            + entryMap.getEntry(new Integer(6)) + "< - >" + entryMap.getEntry(new Integer(7)) + "< - >"
+                            + entryMap.getEntry(new Integer(8)) + "< - >" + entryMap.getEntry(new Integer(9)) + "< - >"
+                            + entryMap.getEntry(new Integer(10)) + "< - >" + entryMap.getEntry(new Integer(11)) + "<");
+        }
+    }
 
-	public void setIncludeEmpty(boolean includeEmpty) {
-		this.includeEmpty = includeEmpty;
-	}
+    public boolean getIncludeEmpty() {
+        return includeEmpty;
+    }
+
+    public void setIncludeEmpty(boolean includeEmpty) {
+        this.includeEmpty = includeEmpty;
+    }
 
     public boolean isSkipFirstRow() {
         return skipFirstRow;
@@ -319,15 +312,15 @@ public class CSVMap implements Mappable {
         this.maximumImportCount = maximumImportCount;
     }
 
-	public String getFileName() {
-		return fileName;
-	}
+    public String getFileName() {
+        return fileName;
+    }
 
-	public Binary getFileContent() {
-		return fileContent;
-	}
+    public Binary getFileContent() {
+        return fileContent;
+    }
 
-	public String getSeparator() {
-		return separator;
-	}
+    public String getSeparator() {
+        return separator;
+    }
 }
