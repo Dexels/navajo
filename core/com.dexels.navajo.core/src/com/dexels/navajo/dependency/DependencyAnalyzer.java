4494ba1e020800a67bd5a24367610a2e173aea0c
Chris Brouwer <cbrouwer@dexels.com>
2016-12-16 15:21:41 +0100

    Script dependency calculation synchronized per script

--- core/com.dexels.navajo.core/src/com/dexels/navajo/dependency/DependencyAnalyzer.java
+++ core/com.dexels.navajo.core/src/com/dexels/navajo/dependency/DependencyAnalyzer.java
@@ -42,35 +42,38 @@ public class DependencyAnalyzer {
     }
 
     public void addDependencies(String script) {
+        synchronized (script) {
+            Thread t = Thread.currentThread();
+            ClassLoader cl = t.getContextClassLoader();
+            t.setContextClassLoader(getClass().getClassLoader());
+            try {
+                List<Dependency> myDependencies = new ArrayList<Dependency>();
+                String scriptTenant = tenantFromScriptPath(script);
 
-		Thread t = Thread.currentThread(); 
-		ClassLoader cl = t.getContextClassLoader(); 
-		t.setContextClassLoader(getClass().getClassLoader()); 
-		try { 
-	        List<Dependency> myDependencies = new ArrayList<Dependency>();
-	        String scriptTenant = tenantFromScriptPath(script);
+                try {
+                    precompiler.getAllDependencies(script, scriptFolder, myDependencies, scriptTenant);
+                    // codeSearch.getAllWorkflowDependencies(scriptFile,
+                    // scriptPath,
+                    // scriptFolder, myDependencies);
+                } catch (Exception e) {
+                    logger.error(" Exception on getting depencencies for: " + script, e);
+                    return;
+                }
+                dependencies.put(script, myDependencies);
 
-	        try {
-	            precompiler.getAllDependencies(script, scriptFolder, myDependencies, scriptTenant);
-	            // codeSearch.getAllWorkflowDependencies(scriptFile, scriptPath,
-	            // scriptFolder, myDependencies);
-	        } catch (Exception e) {
-	            logger.error(" Exception on getting depencencies for: "+ script, e);
-	            return;
-	        }
-	        dependencies.put(script, myDependencies);
+                updateReverseDependencies(myDependencies);
 
-	        updateReverseDependencies(myDependencies);
-	        
-	        // Also ensure any includes I depend on, have their dependencies set correct
-	        for (Dependency dep : myDependencies) {
-	            if (dep.getType() == Dependency.INCLUDE_DEPENDENCY) {
-	                addDependencies(dep.getDependee());
-	            }
-	        }		} finally { 
-		    t.setContextClassLoader(cl); 
-		} 
-	   
+                // Also ensure any includes I depend on, have their dependencies
+                // set correct
+                for (Dependency dep : myDependencies) {
+                    if (dep.getType() == Dependency.INCLUDE_DEPENDENCY) {
+                        addDependencies(dep.getDependee());
+                    }
+                }
+            } finally {
+                t.setContextClassLoader(cl);
+            }
+        }
 
     }
 
