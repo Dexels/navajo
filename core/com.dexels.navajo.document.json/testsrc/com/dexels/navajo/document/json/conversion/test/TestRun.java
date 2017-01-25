fd115174a69b3f0b38f51909a365a7a70250bcf4
Chris Brouwer <cbrouwer@dexels.com>
2017-01-25 10:05:41 +0100

    Updated testcase

--- core/com.dexels.navajo.document.json/testsrc/com/dexels/navajo/document/json/conversion/test/TestRun.java
+++ core/com.dexels.navajo.document.json/testsrc/com/dexels/navajo/document/json/conversion/test/TestRun.java
@@ -1,10 +1,17 @@
 package com.dexels.navajo.document.json.conversion.test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Calendar;
+import java.util.Date;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 import com.dexels.navajo.document.Message;
 import com.dexels.navajo.document.Navajo;
+import com.dexels.navajo.document.Property;
 import com.dexels.navajo.document.json.conversion.JsonTmlFactory;
 import com.dexels.replication.api.ReplicationMessage;
 import com.dexels.replication.factory.ReplicationFactory;
@@ -19,4 +26,20 @@ public class TestRun {
 		Assert.assertEquals(14,standings.getArraySize());
 		nn.write(System.err);
 	}
+	
+	   @Test
+    public void testTimestamp()  {
+	        ReplicationMessage msg = ReplicationFactory.getDefaultInstance().parseStream(getClass().getResourceAsStream("test3.json"));
+	        Navajo nn =  JsonTmlFactory.getInstance().toFlatNavajo("Match",msg);
+	        Message match = nn.getMessage("Match");
+	        assertNotNull( match.getProperty("matchtime"));
+	        Property matchtime = match.getProperty("matchtime");
+	        assertEquals(matchtime.getType(), Property.DATE_PROPERTY);
+	        Date matchtimeobj = (Date) matchtime.getTypedValue();
+	        Calendar cal = Calendar.getInstance();
+	        cal.setTime(matchtimeobj);
+	        assertEquals(cal.get(Calendar.YEAR), 1971);
+	        assertEquals(cal.get(Calendar.HOUR), 12);
+	        assertEquals(cal.get(Calendar.MINUTE), 0);
+	    }
 }
