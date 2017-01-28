da9ce3032db9ac3163cc3681dda20595b462aeec
Chris Brouwer <cbrouwer@dexels.com>
2017-01-28 10:15:47 +0100

    debug version where update is always written to console

--- server/com.dexels.navajo.adapters/src/com/dexels/navajo/adapter/SQLMap.java
+++ server/com.dexels.navajo.adapters/src/com/dexels/navajo/adapter/SQLMap.java
@@ -697,10 +697,9 @@ public class SQLMap implements JDBCMappable, Mappable, HasDependentResources, De
 	public void setUpdate(final String newUpdate) throws UserException {
 		update = newUpdate;
 
-		if (debug) {
 			Access.writeToConsole(myAccess, "SQLMap(): update = " + update
 					+ "\n");
-		}
+		
 
 		this.savedQuery = newUpdate;
 		this.resultSet = null;
