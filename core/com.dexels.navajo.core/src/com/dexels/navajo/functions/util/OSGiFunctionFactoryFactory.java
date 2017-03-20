584e6bd4db6a4c61d4772377404d77fd1899c6f7
Chris Brouwer <cbrouwer@dexels.com>
2017-03-20 09:24:14 +0100

    moved cache

--- core/com.dexels.navajo.core/src/com/dexels/navajo/functions/util/OSGiFunctionFactoryFactory.java
+++ core/com.dexels.navajo.core/src/com/dexels/navajo/functions/util/OSGiFunctionFactoryFactory.java
@@ -14,29 +14,32 @@ import com.dexels.navajo.parser.FunctionInterface;
 
 
 public class OSGiFunctionFactoryFactory  {
-    private static Map<String, Object> cache = new HashMap<>();
-	private final static Logger logger = LoggerFactory
-			.getLogger(OSGiFunctionFactoryFactory.class);
+    private static Map<String, FunctionDefinition> cache = new HashMap<>();
+	private final static Logger logger = LoggerFactory.getLogger(OSGiFunctionFactoryFactory.class);
 	
 	private OSGiFunctionFactoryFactory() {
 		// no instances
 	}
 	
 	public static FunctionInterface getFunctionInterface(final String functionName)  {
+	    
+	    if (cache.containsKey(functionName)) {
+            return cache.get(functionName).getFunctionInstance();
+        }
 		FunctionDefinition fd = (FunctionDefinition) getComponent(functionName,
 				"functionName", FunctionDefinition.class);
 		if(fd==null) {
 			throw NavajoFactory.getInstance().createNavajoException("No such function: "+functionName);
 		}
+        
+		cache.put(functionName, fd);
 		FunctionInterface instance = fd.getFunctionInstance();
 		return instance;
 	}
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static Object getComponent( final String name, String serviceKey, Class interfaceClass)  {
-	    if (cache.containsKey(name+serviceKey+interfaceClass)) {
-	        return cache.get(name+serviceKey+interfaceClass);
-	    }
+	    
 		BundleContext context = navajocore.Version.getDefaultBundleContext();
 		try {
 			ServiceReference[] refs = context.getServiceReferences(interfaceClass.getName(), "("+serviceKey+"="+name+")");
@@ -44,7 +47,6 @@ public class OSGiFunctionFactoryFactory  {
 				logger.error("Service resolution failed: Query: "+"("+serviceKey+"="+name+")"+" class: "+interfaceClass.getName());
 				return null;
 			}
-			cache.put(name+serviceKey+interfaceClass, context.getService(refs[0]));
 			return context.getService(refs[0]);
 			
 		} catch (InvalidSyntaxException e) {
