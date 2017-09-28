33cd042a557225708bbde405e4d98eb97ecdb580
Chris Brouwer <cbrouwer@dexels.com>
2017-09-28 16:46:11 +0200

    more replicationmsg impl

--- scala/com.dexels.navajo.scala.navajoscala/src/com/dexels/navajo/scala/message/ScalaMessage.scala
+++ scala/com.dexels.navajo.scala.navajoscala/src/com/dexels/navajo/scala/message/ScalaMessage.scala
@@ -11,7 +11,26 @@ class ScalaMessage(val parent: ReplicationMessage) {
   def this() {
     this(ReplicationFactory.fromMap(null, Collections.emptyMap(), Collections.emptyMap()))
   }
- 
+  
+  /* Getters */
+  def get(key : String) : ScalaMessage = {
+    val optionalMsg = parent.subMessage(key)
+    optionalMsg.get match {
+      case msg : ReplicationMessage => new ScalaMessage(optionalMsg.get)
+      case _ => null
+    }
+  }
+  def getString(key : String) : String = {
+    parent.columnValue(key).asInstanceOf[String]
+  }
+  def getInt(key : String) : Int = {
+    parent.columnValue(key).asInstanceOf[Int]
+  }
+  def getBoolean(key : String) : Boolean = {
+    parent.columnValue(key).asInstanceOf[Boolean]
+  }
+  
+ /* Putters */
   def put(key : String, value: Any, `type`:String = "") : ScalaMessage = {
     new ScalaMessage(parent.`with`(key, value, `type`))
   }
