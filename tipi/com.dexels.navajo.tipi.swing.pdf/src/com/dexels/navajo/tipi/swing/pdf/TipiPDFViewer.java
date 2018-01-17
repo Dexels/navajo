e04ddd756b6d627cf6e82c93d8034936094a2005
Chris Brouwer <cbrouwer@dexels.com>
2018-01-17 16:26:59 +0100

    clean

--- tipi/com.dexels.navajo.tipi.swing.pdf/src/com/dexels/navajo/tipi/swing/pdf/TipiPDFViewer.java
+++ tipi/com.dexels.navajo.tipi.swing.pdf/src/com/dexels/navajo/tipi/swing/pdf/TipiPDFViewer.java
@@ -11,57 +11,40 @@ import org.icepdf.ri.common.SwingViewBuilder;
 
 import com.dexels.navajo.tipi.components.swingimpl.TipiSwingDataComponentImpl;
 
-
-
-public class TipiPDFViewer  extends TipiSwingDataComponentImpl {
+public class TipiPDFViewer extends TipiSwingDataComponentImpl {
     private static final long serialVersionUID = -4533671175260716894L;
+
     private JPanel viewerComponentPanel;
-
-    private PDFViewerPane pdfViwer = null;
-
 
     public Object createContainer() {
         initUI();
         return viewerComponentPanel;
     }
-    
+
     private void initUI() {
-            // build a component controller
-               SwingController controller = new SwingController();
+        // build a component controller
+        SwingController controller = new SwingController();
 
-               SwingViewBuilder factory = new SwingViewBuilder(controller);
+        SwingViewBuilder factory = new SwingViewBuilder(controller);
 
-               viewerComponentPanel = factory.buildViewerPanel();
-            // add interactive mouse link annotation support via callback
-               controller.getDocumentViewController().setAnnotationCallback(
-                       new org.icepdf.ri.common.MyAnnotationCallback(
-                               controller.getDocumentViewController()));
+        viewerComponentPanel = factory.buildViewerPanel();
+        
 
-              
 
-               URL url = getClass().getResource("/resources/navajo3.pdf");
-//               try {
-                controller.openDocument("/home/chris/git/navajo/tipi/com.dexels.navajo.tipi.swing.pdf/src/resources/navajo3.pdf");
-//            } catch (URISyntaxException e) {
-//                // TODO Auto-generated catch block
-//                e.printStackTrace();
-//            }
+        controller.openDocument("/home/chris/git/navajo/tipi/com.dexels.navajo.tipi.swing.pdf/src/resources/navajo3.pdf");
+        //
     }
-    
-    
+
     @Override
     protected Object getComponentValue(String name) {
-     
 
         return super.getComponentValue(name);
-        
-    }
 
+    }
 
     @Override
     protected void setComponentValue(String name, final Object object) {
         super.setComponentValue(name, object);
-        
 
     }
 }
