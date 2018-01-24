61c75870f411597c38eb290576c1165d0cd27487
Chris Brouwer <cbrouwer@dexels.com>
2018-01-24 16:32:11 +0100

    label

--- tipi/com.dexels.navajo.tipi.swing.geo/src/com/dexels/navajo/tipi/swing/geo/impl/TipiMapSoccerField.java
+++ tipi/com.dexels.navajo.tipi.swing.geo/src/com/dexels/navajo/tipi/swing/geo/impl/TipiMapSoccerField.java
@@ -1,19 +1,14 @@
 package com.dexels.navajo.tipi.swing.geo.impl;
 
-import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Polygon;
 import java.awt.Rectangle;
 import java.awt.RenderingHints;
-import java.awt.Stroke;
-import java.awt.font.FontRenderContext;
-import java.awt.geom.AffineTransform;
 import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
-
 import org.jxmapviewer.JXMapViewer;
 import org.jxmapviewer.viewer.GeoPosition;
 import org.jxmapviewer.painter.Painter;
@@ -22,7 +17,7 @@ import com.dexels.navajo.document.Message;
 import com.dexels.navajo.document.Property;
 
 public class TipiMapSoccerField implements Painter<JXMapViewer> {
-    private static final int FIELD_TRANSPARANCY = 50;
+    private static final int FIELD_TRANSPARANCY = 235;
     
     private final double bearing;
     private boolean antiAlias = true;
@@ -89,19 +84,29 @@ public class TipiMapSoccerField implements Painter<JXMapViewer> {
         Point2D leftTop = map.getTileFactory().geoToPixel(lefTopPos, map.getZoom());
         Point2D rightTop =map.getTileFactory().geoToPixel(rightTopPos, map.getZoom());
         Point2D rightBottom =map.getTileFactory().geoToPixel(rightBottomPos, map.getZoom());
-        Point2D leftBottom =map.getTileFactory().geoToPixel(leftBottomPos, map.getZoom());
+        Point2D leftBottom = map.getTileFactory().geoToPixel(leftBottomPos, map.getZoom());
 
+        
+      
         int xpoints[] = {(int)leftTop.getX(), (int)rightTop.getX(), (int)leftBottom.getX(), (int)rightBottom.getX()};
         int ypoints[] = {(int)leftTop.getY(), (int)rightTop.getY(), (int)leftBottom.getY(), (int)rightBottom.getY()};
-        
-
-       
+        Polygon p = new Polygon(xpoints, ypoints, 4);
+ 
         Color myColour = getFieldColor();
         g2d.setColor(myColour);
-        g2d.fillPolygon(xpoints, ypoints, 4);
+        g2d.fillPolygon(p);
         g2d.setColor(Color.WHITE);
-        g2d.drawPolygon(xpoints, ypoints, 4);
+        g2d.drawPolygon(p);
 
+        Point2D centerPoint = map.getTileFactory().geoToPixel(pos, map.getZoom());
+        
+//        Color oldColor = g2d.getColor();
+//        g2d.setColor(Color.WHITE);
+//        g2d.drawString(label, (int) centerPoint.getX(),(int) centerPoint.getY());
+//        g2d.setColor(oldColor);
+          
+            
+        drawCenteredString(label, p, g2d);
         // Draw border
 //        g2d.setColor(new Color(255, 255, 255, 200));
 //        g2d.setStroke(new BasicStroke(4));
@@ -111,64 +116,6 @@ public class TipiMapSoccerField implements Painter<JXMapViewer> {
         return;
     }
 
-    private Rectangle getFieldRectangle(Point2D pt, JXMapViewer map) {
-        // https://gis.stackexchange.com/questions/2951/algorithm-for-offsetting-a-latitude-longitude-by-some-amount-of-meters
-
-//        // Earth’s radius, sphere
-//        double R = 6378137D;
-//        
-//        // We start in the middle of the field. The rectangle starts with coordinates
-//        // of the upper-left corner. Thus we first calculate where that is
-//        double dn = getFieldHeight() ;
-//        double de = -getFieldWidth();
-//
-//        // Coordinate offsets in radians
-//        double dLat = dn/R;
-//        double dLon = de/(R*Math.cos(Math.PI * pos.getLatitude()/180));
-//
-//        // OffsetPosition, decimal degrees
-//        double latO = pos.getLatitude() + dLat * 180/Math.PI - 0.00095;
-//        double lonO = pos.getLongitude() + dLon * 180/Math.PI + 0.0001;
-//        GeoPosition startPos = new GeoPosition(latO, lonO);
-//        Point2D start = map.getTileFactory().geoToPixel(startPos, map.getZoom());
-//
-//        // Next, add the field length to see where we end up
-//        dn = getFieldHeight();
-//        de = 0;
-//
-//        // Coordinate offsets in radians
-//        dLat = dn/R;
-//        dLon = de/(R*Math.cos(Math.PI*startPos.getLatitude()/180));
-//
-//        // OffsetPosition, decimal degrees
-//        double lat1 = startPos.getLatitude() + dLat * 180/Math.PI;
-//        double lon1 = startPos.getLongitude() + dLon * 180/Math.PI ;
-//
-//        Point2D endHeight = map.getTileFactory().geoToPixel(new GeoPosition(lat1, lon1), map.getZoom());
-//        
-//        // Add width to start pos
-//        dn = 0;
-//        de = getFieldWidth();
-//
-//        // Coordinate offsets in radians
-//        dLat = dn/R;
-//        dLon = de/(R*Math.cos(Math.PI*startPos.getLatitude()/180));
-//
-//        // OffsetPosition, decimal degrees
-//        double lat2 = startPos.getLatitude() + dLat * 180/Math.PI;
-//        double lon2 = startPos.getLongitude() + dLon * 180/Math.PI ;
-//
-//        Point2D endWidth = map.getTileFactory().geoToPixel(new GeoPosition(lat2, lon2), map.getZoom());
-////        
-//        Double width = Math.abs(endWidth.getX() - start.getX());
-//        Double height = Math.abs(endHeight.getY() - start.getY());
-//        
-//        return new Rectangle((int)start.getX(), (int) start.getY(),  width.intValue(), height.intValue());
-        
-        
-        
-        return new Rectangle();
-    }
 
     public static GeoPosition travel(GeoPosition start, double distance, double initialBearing) {
         double bR = Math.toRadians(initialBearing);
@@ -186,24 +133,21 @@ public class TipiMapSoccerField implements Painter<JXMapViewer> {
     private double getFieldHeight(){
         return 105D;
     }
-    private double getFieldWidth(){
-        return 68D;
-    }
 
     private Color getFieldColor() {
         if (fieldUse.equals("TRAINING")) {
-            return new Color(177, 177, 99, 125);
+            return new Color(177, 177, 99, FIELD_TRANSPARANCY);
         }
-        return new Color(36, 170, 36, 150);
+        return new Color(36, 170, 36, FIELD_TRANSPARANCY);
     }
 
-    private void drawCenteredString(String s, Rectangle r, Graphics g) {
+    private void drawCenteredString(String s, Polygon p, Graphics g) {
         FontMetrics fm = g.getFontMetrics();
-        int x = (r.width - fm.stringWidth(s)) / 2;
-        int y = (fm.getAscent() + (r.height- (fm.getAscent() + fm.getDescent())) / 2);
+        int x = (p.getBounds().width - fm.stringWidth(s)) / 2;
+        int y = (fm.getAscent() + (p.getBounds().height- (fm.getAscent() + fm.getDescent())) / 2);
         Color oldColor = g.getColor();
         g.setColor(Color.WHITE);
-        g.drawString(s, r.x + x, r.y+y);
+        g.drawString(s, p.getBounds().x + x, p.getBounds().y+y);
         g.setColor(oldColor);
       }
     
