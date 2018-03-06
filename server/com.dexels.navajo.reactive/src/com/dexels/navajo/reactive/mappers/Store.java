5af481619823c293ca87fe4b15c27f3f4ab27315
Frank Lyaruu <frank@dexels.com>
2018-03-06 16:32:07 +0100

    refactor mappers into transformers

--- server/com.dexels.navajo.reactive/src/com/dexels/navajo/reactive/mappers/Store.java
+++ server/com.dexels.navajo.reactive/src/com/dexels/navajo/reactive/mappers/Store.java
@@ -3,9 +3,11 @@ package com.dexels.navajo.reactive.mappers;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 
 import com.dexels.immutable.api.ImmutableMessage;
 import com.dexels.immutable.factory.ImmutableFactory;
@@ -13,17 +15,24 @@ import com.dexels.navajo.document.Operand;
 import com.dexels.navajo.document.Property;
 import com.dexels.navajo.document.nanoimpl.XMLElement;
 import com.dexels.navajo.document.stream.DataItem;
+import com.dexels.navajo.document.stream.DataItem.Type;
 import com.dexels.navajo.document.stream.api.StreamScriptContext;
 import com.dexels.navajo.reactive.api.ParameterValidator;
 import com.dexels.navajo.reactive.api.ReactiveMerger;
 import com.dexels.navajo.reactive.api.ReactiveParameters;
 import com.dexels.navajo.reactive.api.ReactiveResolvedParameters;
+import com.dexels.navajo.reactive.api.ReactiveTransformer;
 
+import io.reactivex.Flowable;
+import io.reactivex.FlowableTransformer;
 import io.reactivex.functions.Function;
 
-public class Store implements ReactiveMerger, ParameterValidator {
+public class Store implements ReactiveMerger, ParameterValidator, ReactiveTransformer {
 
-	public Store() {
+	private final ReactiveParameters parameters;
+	
+	public Store(ReactiveParameters parameters) {
+		this.parameters = parameters;
 	}
 
 	@Override
@@ -63,5 +72,26 @@ public class Store implements ReactiveMerger, ParameterValidator {
 		result.put("name", Property.STRING_PROPERTY);
 		result.put("value", Property.INTEGER_PROPERTY);
 		return Optional.of(Collections.unmodifiableMap(result));
+	}
+
+	@Override
+	public FlowableTransformer<DataItem, DataItem> execute(StreamScriptContext context) {
+		return flow->{
+			try {
+				return flow.map(this.execute(parameters).apply(context));
+			} catch (Exception e) {
+				return Flowable.error(e);
+			}
+		};
+	}
+
+	@Override
+	public Set<Type> inType() {
+		return new HashSet<>(Arrays.asList(new Type[] {Type.MESSAGE}));
+	}
+
+	@Override
+	public Type outType() {
+		return Type.MESSAGE;
 	}
 }
