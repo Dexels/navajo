52ce7c1971bfb1662f061bca29aa037651349265
Frank Lyaruu <frank@dexels.com>
2018-03-28 10:19:47 +0200

    Don't re-use the function object, it will create problems in stateful functions

--- core/com.dexels.navajo.core/src/com/dexels/navajo/parser/compiled/ASTFunctionNode.java
+++ core/com.dexels.navajo.core/src/com/dexels/navajo/parser/compiled/ASTFunctionNode.java
@@ -94,7 +94,7 @@ public final class ASTFunctionNode extends SimpleNode {
 
 			@Override
 			public Optional<String> returnType() {
-				return f.getReturnType();
+				return typeCheckInstance.getReturnType();
 			}
 		};
 	}
