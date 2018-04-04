8bb13429d5ee2399a44eaeb1164cb95b8fdeeaae
Chris Brouwer <cbrouwer@dexels.com>
2018-04-04 08:59:32 +0200

    Small logic change in GetRequestLocale()

--- core/com.dexels.navajo.function/src/com/dexels/navajo/functions/GetRequestLocale.java
+++ core/com.dexels.navajo.function/src/com/dexels/navajo/functions/GetRequestLocale.java
@@ -4,18 +4,24 @@ import com.dexels.navajo.parser.FunctionInterface;
 
 public class GetRequestLocale  extends FunctionInterface {
 
+    private static final Object DEFAULT_LOCALE = "nl";
+
     @Override
     public String remarks() {
-        return "Returns the locale of the Navajo request, with an optional default";
+        return "Returns the locale of the Navajo request with NL as default, optionally overriding the default";
     }
 
     @Override
     public Object evaluate() {
        String loc = getAccess().getInDoc().getHeader().getHeaderAttribute("locale");
-       if (loc != null || getOperands().size() < 1) {
+       if (loc != null) {
            return loc;
        }
-       return getOperand(0);
+       if ((getOperands().size() > 0)) {
+           return getOperand(0).toString();
+       }
+      
+       return DEFAULT_LOCALE;
        
     }
 
