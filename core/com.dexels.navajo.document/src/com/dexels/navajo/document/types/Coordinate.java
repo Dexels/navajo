4298cc8f1beef74032fa08dc117eb7298ea1d19c
Dimitrios Dontsios <ddontsios@dexels.com>
2018-04-17 14:36:21 +0200

    Added validation and fixed regex

--- core/com.dexels.navajo.document/src/com/dexels/navajo/document/types/Coordinate.java
+++ core/com.dexels.navajo.document/src/com/dexels/navajo/document/types/Coordinate.java
@@ -14,30 +14,41 @@ public class Coordinate extends NavajoType implements Serializable {
     private final static Logger logger = LoggerFactory.getLogger(Coordinate.class);
     public final static String VERSION = "$Id$";
 
-    private Double latitude;
-    private Double longitude;
+    private Double latitude = new Double(0);
+    private Double longitude = new Double(0);
 
-    public Coordinate(double longitude, double latitude) {
+    public Coordinate(double longitude, double latitude) throws Exception {
         this.latitude = latitude;
         this.longitude = longitude;
+        verifyCoordinates();
     }
 
-    public Coordinate(String arrStr) {
-
+    public Coordinate(String arrStr) throws Exception {
         // check if format is correct.
-        if (!arrStr.matches("\\[[+-]{0,1}\\d+.*\\d*,[+-]{0,1}\\d+.*\\d*\\]")) {
+        if (!arrStr.matches("\\[[+-]{0,1}\\d+\\.{0,1}\\d*,[+-]{0,1}\\d+\\.{0,1}\\d*\\]")) {
             logger.error("Not valid format given.");
+            throw new Exception("Not valid format given :: " + arrStr + ". Please use number representations");
         } else {
             String mydata = arrStr.substring(1, arrStr.length() - 1);
             String[] vals = mydata.split(",");
 
-            this.latitude = Double.parseDouble(vals[0]);
-            this.longitude = Double.parseDouble(vals[1]);
+            this.latitude = Double.parseDouble(vals[1]);
+            this.longitude = Double.parseDouble(vals[0]);
+            verifyCoordinates();
         }
 
     }
 
-    public Coordinate(Object longitude, Object latitude) {
+    private void verifyCoordinates() throws Exception {
+        if (this.latitude > 90 || this.latitude < -90) {
+            throw new Exception("Latitude must be in [-90,90]");
+        }
+        if (this.longitude > 180 || this.longitude < -180) {
+            throw new Exception("Longitute must be in [-180,180]");
+        }
+    }
+
+    public Coordinate(Object longitude, Object latitude) throws Exception {
         setLatitude(longitude);
         setLongitude(longitude);
     }
@@ -50,7 +61,7 @@ public class Coordinate extends NavajoType implements Serializable {
         return this.longitude;
     }
 
-    public void setLatitude(Object latitude) {
+    public void setLatitude(Object latitude) throws Exception {
         if (latitude instanceof String) {
             this.latitude = Double.parseDouble((String) latitude);
         } else if (latitude instanceof Double) {
@@ -60,9 +71,11 @@ public class Coordinate extends NavajoType implements Serializable {
         } else {
             this.latitude = new Double(latitude + "");
         }
+        verifyCoordinates();
+
     }
 
-    public void setLongitude(Object longitude) {
+    public void setLongitude(Object longitude) throws Exception {
         if (longitude instanceof String) {
             this.longitude = Double.parseDouble((String) longitude);
         } else if (longitude instanceof Double) {
@@ -72,6 +85,7 @@ public class Coordinate extends NavajoType implements Serializable {
         } else {
             this.longitude = new Double(longitude + "");
         }
+        verifyCoordinates();
     }
 
     @Override
@@ -97,13 +111,13 @@ public class Coordinate extends NavajoType implements Serializable {
 
     public static void main(String[] args) {
         
-        String arrStr = "[1,-1]";
-        System.out.println(arrStr.matches("\\[[+-]{0,1}\\d+.*\\d*,[+-]{0,1}\\d+.*\\d*\\]"));
+        String arrStr = "[1.23,-1]";
+        System.out.println(arrStr.matches("\\[[+-]{0,1}\\d+\\.{0,1}\\d*,[+-]{0,1}\\d+\\.{0,1}\\d*\\]"));
 
         // arrStr = "11.12";
         // System.out.println(arrStr.matches("\\d+.*\\d+"));
         
-        Coordinate test = new Coordinate("[1,1]");
+        // Coordinate test = new Coordinate("[1,1]");
 
         // Coordinate test = new Coordinate("123.33", 11);
         // System.out.println(test.toString());
