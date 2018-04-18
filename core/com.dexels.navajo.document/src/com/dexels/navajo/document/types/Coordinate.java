525be7663bcee52ed9e0e6297998a5e4c9385845
Dimitrios Dontsios <ddontsios@dexels.com>
2018-04-18 09:52:39 +0200

    entity support for coordinate property type

--- core/com.dexels.navajo.document/src/com/dexels/navajo/document/types/Coordinate.java
+++ core/com.dexels.navajo.document/src/com/dexels/navajo/document/types/Coordinate.java
@@ -14,8 +14,8 @@ public class Coordinate extends NavajoType implements Serializable {
     private final static Logger logger = LoggerFactory.getLogger(Coordinate.class);
     public final static String VERSION = "$Id$";
 
-    private Double latitude = new Double(0);
-    private Double longitude = new Double(0);
+    private Double latitude = null;
+    private Double longitude = null;
 
     public Coordinate(double longitude, double latitude) throws Exception {
         this.latitude = latitude;
@@ -25,12 +25,13 @@ public class Coordinate extends NavajoType implements Serializable {
 
     public Coordinate(String arrStr) throws Exception {
         // check if format is correct.
-        if (!arrStr.matches("\\[[+-]{0,1}\\d+\\.{0,1}\\d*,[+-]{0,1}\\d+\\.{0,1}\\d*\\]")) {
-            logger.error("Not valid format given.");
+        if (!(arrStr.matches("\\[[+-]{0,1}\\d+\\.{0,1}\\d*,[+-]{0,1}\\d+\\.{0,1}\\d*\\]") // [+1.4,4]
+                || arrStr.matches("[+-]{0,1}\\d+\\.{0,1}\\d*,[+-]{0,1}\\d+\\.{0,1}\\d*") // -2, 33
+                || arrStr.matches("[+-]{0,1}\\d+\\.{0,1}\\d* [+-]{0,1}\\d+\\.{0,1}\\d*"))) { // 31 -18
             throw new Exception("Not valid format given :: " + arrStr + ". Please use number representations");
         } else {
-            String mydata = arrStr.substring(1, arrStr.length() - 1);
-            String[] vals = mydata.split(",");
+            String mydata = arrStr.replaceAll("\\[", "").replaceAll("\\]", "");
+            String[] vals = mydata.split(",| ");
 
             this.latitude = Double.parseDouble(vals[1]);
             this.longitude = Double.parseDouble(vals[0]);
@@ -40,10 +41,10 @@ public class Coordinate extends NavajoType implements Serializable {
     }
 
     private void verifyCoordinates() throws Exception {
-        if (this.latitude > 90 || this.latitude < -90) {
+        if (this.latitude != null && (this.latitude > 90 || this.latitude < -90)) {
             throw new Exception("Latitude must be in [-90,90]");
         }
-        if (this.longitude > 180 || this.longitude < -180) {
+        if (this.longitude != null && (this.longitude > 180 || this.longitude < -180)) {
             throw new Exception("Longitute must be in [-180,180]");
         }
     }
@@ -90,8 +91,7 @@ public class Coordinate extends NavajoType implements Serializable {
 
     @Override
     public boolean isEmpty() {
-
-        return false;
+        return this.latitude == null || this.longitude == null;
     }
 
     @Override
