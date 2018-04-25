14bf92e16b3646d78563461374caee6d163a70a1
Chris Brouwer <cbrouwer@dexels.com>
2018-04-25 07:55:37 +0200

    use safer toString incase we are dealing wth an object that cannot be cast to a String

--- core/com.dexels.navajo.document/src/com/dexels/navajo/document/json/TmlPropertySerializer.java
+++ core/com.dexels.navajo.document/src/com/dexels/navajo/document/json/TmlPropertySerializer.java
@@ -34,14 +34,14 @@ public class TmlPropertySerializer extends StdSerializer<Property> {
             try {
                 jg.writeString(df.format((Date) value));
             } catch (ClassCastException e) {
-                jg.writeString((String) value);
+                jg.writeString(value.toString());
             }
         } else if (property.getType().equals(Property.TIMESTAMP_PROPERTY)) {
             DateFormat df = new SimpleDateFormat(Property.TIMESTAMP_FORMAT);
             try {
                 jg.writeString(df.format((Date) value));
             } catch (ClassCastException e) {
-                jg.writeString((String) value);
+                jg.writeString(value.toString());
             }
         } else {
             JsonSerializer<Object> serializer = provider.findValueSerializer(value.getClass());
