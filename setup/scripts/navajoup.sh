eda51792f867406be964c1ccce3ef1301157effa
Chris Brouwer <cbrouwer@dexels.com>
2018-05-24 14:11:39 +0200

    check if exists

--- setup/scripts/navajoup.sh
+++ setup/scripts/navajoup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+
 groupId=com.dexels.navajo
+urlGroupId='com/dexels/navajo'
 current=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 file=$1
 strip=`echo $file | rev | cut -c 5- | rev`
@@ -10,6 +12,16 @@ echo "ArtifactId: ${artifactArray[0]}"
 echo "Version: ${artifactArray[1]}"
 artifactId=${artifactArray[0]}
 version=${artifactArray[1]}
+
+
+#Check if this new version already exists
+STATUS=`curl  -s -w "%{http_code}" "http://repo.dexels.com/nexus/content/repositories/thirdparty/$urlGroupId/$artifactId/$version/$artifactId-$version.jar" -o /dev/null`
+if [ $STATUS -eq "200" ]
+then
+    echo "$artifactId version $version already exists - exiting!"
+    exit 1
+fi
+
 #mvn deploy:deploy-file -Durl=file://${current}/repo/ -Dfile=$1 -DgroupId=$groupId -DartifactId=$artifactId -Dpackaging=jar -Dversion=$version
 mvn deploy:deploy-file -DrepositoryId=dexels -Durl=http://repo.dexels.com/nexus/content/repositories/thirdparty -DgroupId=$groupId -DartifactId=$artifactId -Dversion=$version -Dfile=$1 -Dpackaging=jar
 
