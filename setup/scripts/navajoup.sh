2e37ae59ee422f6ce3434783a92b9697593664ab
Frank Lyaruu <frank@dexels.com>
2018-05-24 13:39:18 +0200

    Added navajoup script

---
+++ setup/scripts/navajoup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+groupId=com.dexels.navajo
+current=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+file=$1
+strip=`echo $file | rev | cut -c 5- | rev`
+echo "Before: $strip"
+artifactWithVersion="${strip##*/}"
+artifactArray=(${artifactWithVersion//-/ })
+echo "ArtifactId: ${artifactArray[0]}"
+echo "Version: ${artifactArray[1]}"
+artifactId=${artifactArray[0]}
+version=${artifactArray[1]}
+#mvn deploy:deploy-file -Durl=file://${current}/repo/ -Dfile=$1 -DgroupId=$groupId -DartifactId=$artifactId -Dpackaging=jar -Dversion=$version
+mvn deploy:deploy-file -DrepositoryId=dexels -Durl=http://repo.dexels.com/nexus/content/repositories/thirdparty -DgroupId=$groupId -DartifactId=$artifactId -Dversion=$version -Dfile=$1 -Dpackaging=jar
+
+
