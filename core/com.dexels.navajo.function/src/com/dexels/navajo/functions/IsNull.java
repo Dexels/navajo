f16a36a04652d3cbeb030bbde4f1686f84c1b85d
Chris Brouwer <cbrouwer@dexels.com>
2018-05-28 08:44:11 +0200

    formatting

--- core/com.dexels.navajo.function/src/com/dexels/navajo/functions/IsNull.java
+++ core/com.dexels.navajo.function/src/com/dexels/navajo/functions/IsNull.java
@@ -3,39 +3,53 @@ package com.dexels.navajo.functions;
 import com.dexels.navajo.document.types.NavajoType;
 import com.dexels.navajo.parser.FunctionInterface;
 
-
 /**
- * <p>Title: Navajo Product Project</p>
- * <p>Description: This is the official source for the Navajo server</p>
- * <p>Copyright: Copyright (c) 2002</p>
- * <p>Company: Dexels BV</p>
+ * <p>
+ * Title: Navajo Product Project
+ * </p>
+ * <p>
+ * Description: This is the official source for the Navajo server
+ * </p>
+ * <p>
+ * Copyright: Copyright (c) 2002
+ * </p>
+ * <p>
+ * Company: Dexels BV
+ * </p>
+ * 
  * @author not attributable
  * @version 1.0
  */
 
 public class IsNull extends FunctionInterface {
-  public IsNull() {
-  }
-  @Override
-public String remarks() {
-    /**@todo Implement this com.dexels.navajo.parser.FunctionInterface abstract method*/
-    return "Will return true if the supplied argument is null, false otherwise";
-  }
-  @Override
-public Object evaluate() throws com.dexels.navajo.parser.TMLExpressionException {
-    Object arg = this.getOperands().get(0);
-    if (arg==null) {
-        return Boolean.TRUE;
+    public IsNull() {
     }
-    if (arg instanceof NavajoType) {
-        NavajoType n = (NavajoType)arg;
-        return (n.isEmpty());
+
+    @Override
+    public String remarks() {
+        /**
+         * @todo Implement this com.dexels.navajo.parser.FunctionInterface abstract
+         *       method
+         */
+        return "Will return true if the supplied argument is null, false otherwise";
     }
-    return Boolean.FALSE;
-  }
-  @Override
-public String usage() {
-    return "IsNull( <argument>)";
-  }
+
+    @Override
+    public Object evaluate() throws com.dexels.navajo.parser.TMLExpressionException {
+        Object arg = this.getOperands().get(0);
+        if (arg == null) {
+            return Boolean.TRUE;
+        }
+        if (arg instanceof NavajoType) {
+            NavajoType n = (NavajoType) arg;
+            return (n.isEmpty());
+        }
+        return Boolean.FALSE;
+    }
+
+    @Override
+    public String usage() {
+        return "IsNull( <argument>)";
+    }
 
 }
