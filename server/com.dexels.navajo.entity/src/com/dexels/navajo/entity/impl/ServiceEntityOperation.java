a32065f51d9ef1704903ede2e598c3efe33b2a8a
Dimitrios Dontsios <ddontsios@dexels.com>
2018-07-04 13:19:33 +0200

    Added entity name in __entity__ message under property "name" . Applies to #466

--- server/com.dexels.navajo.entity/src/com/dexels/navajo/entity/impl/ServiceEntityOperation.java
+++ server/com.dexels.navajo.entity/src/com/dexels/navajo/entity/impl/ServiceEntityOperation.java
@@ -395,7 +395,10 @@ public class ServiceEntityOperation implements EntityOperation {
         Message entityInfo = NavajoFactory.getInstance().createMessage(input, "__entity__");
         Property entityVersion = NavajoFactory.getInstance().createProperty(input, "version", "string", myEntity.getMyVersion(), 0, "",
                 Property.DIR_OUT);
+        Property entityName = NavajoFactory.getInstance().createProperty(input, "name", "string", myEntity.getMessageName(), 0, "",
+                Property.DIR_OUT);
         entityInfo.addProperty(entityVersion);
+        entityInfo.addProperty(entityName);
         input.addMessage(entityInfo);
 
 		// Perform validation method if defined
