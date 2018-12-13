0bceabaa999daeb0945d5e877f6a417a3d9e5365
Dimitrios Dontsios <ddontsios@dexels.com>
2018-12-13 10:01:11 +0100

    Proof authentication header before processing a request - Save null pointer exceptions

--- server/com.dexels.navajo.authentication.api/src/com/dexels/navajo/authentication/impl/AuthenticationMethodBuilderImpl.java
+++ server/com.dexels.navajo.authentication.api/src/com/dexels/navajo/authentication/impl/AuthenticationMethodBuilderImpl.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 
 import com.dexels.navajo.authentication.api.AuthenticationMethod;
 import com.dexels.navajo.authentication.api.AuthenticationMethodBuilder;
+import com.dexels.navajo.script.api.AuthorizationException;
 
 public class AuthenticationMethodBuilderImpl implements AuthenticationMethodBuilder {
     private static final Logger logger = LoggerFactory.getLogger(AuthenticationMethodBuilderImpl.class);
@@ -25,7 +26,7 @@ public class AuthenticationMethodBuilderImpl implements AuthenticationMethodBuil
     }
 
     @Override
-    public AuthenticationMethod getInstanceForRequest(String header) {
+    public AuthenticationMethod getInstanceForRequest(String header) throws AuthorizationException {
         final AuthenticationMethod method;
         if (header != null && header.indexOf(' ') > 0) {
             method = instances.get(header.substring(0,  header.indexOf(' ')));
@@ -33,6 +34,14 @@ public class AuthenticationMethodBuilderImpl implements AuthenticationMethodBuil
                 return null;
             }
         } else {
+        	
+        	// If we are here the auth header should not contain bearer or basic
+        	// Authorization header must be like "Authorization: <type> <credentials>" 
+            if( (header.toLowerCase().contains("bearer") || header.toLowerCase().contains("basic") ) 
+            		&& header.trim().split(" ").length != 2) {
+            	throw new AuthorizationException(false, false, null, "Wrong authentication header provided");
+            } 
+            
             method = instances.get(AuthenticationMethod.DEFAULT_IDENTIFIER);
         }
         return method.getInstanceForRequest(header);
