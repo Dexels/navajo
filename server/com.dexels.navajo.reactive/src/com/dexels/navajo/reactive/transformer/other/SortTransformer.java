43bccccb35807087e8efc01b68779057ed2dba9c
Frank Lyaruu <frank@dexels.com>
2019-02-21 15:05:53 +0100

    initial sorting transformer

---
+++ server/com.dexels.navajo.reactive/src/com/dexels/navajo/reactive/transformer/other/SortTransformer.java
@@ -0,0 +1,54 @@
+package com.dexels.navajo.reactive.transformer.other;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import com.dexels.immutable.api.ImmutableMessage;
+import com.dexels.immutable.factory.ImmutableFactory;
+import com.dexels.navajo.document.Operand;
+import com.dexels.navajo.document.stream.DataItem;
+import com.dexels.navajo.document.stream.api.StreamScriptContext;
+import com.dexels.navajo.reactive.api.ReactiveParameters;
+import com.dexels.navajo.reactive.api.ReactiveResolvedParameters;
+import com.dexels.navajo.reactive.api.ReactiveTransformer;
+import com.dexels.navajo.reactive.api.TransformerMetadata;
+
+import io.reactivex.FlowableTransformer;
+
+public class SortTransformer implements ReactiveTransformer {
+
+	private final ReactiveParameters parameters;
+	private final TransformerMetadata metadata;
+
+	public SortTransformer(TransformerMetadata metadata, ReactiveParameters parameters) {
+		this.parameters = parameters;
+		this.metadata = metadata;
+	}
+
+	@Override
+	public FlowableTransformer<DataItem, DataItem> execute(StreamScriptContext context, Optional<ImmutableMessage> current, ImmutableMessage param) {
+//		int count = parms.unnamedParameters().stream().findFirst().orElse(Operand.ofInteger(0)).integerValue(); //parms.paramInteger("count");
+		return e->e.sorted((o1,o2)->{
+			ReactiveResolvedParameters parms = parameters.resolve(context, current, param, metadata);
+			List<Operand> resolved = parms.unnamedParameters().stream().collect(Collectors.toList()); //  .findFirst().orElse(Operand.ofInteger(0)).integerValue(); //parms.paramInteger("count");
+
+			return 0;
+		});
+	}
+
+	private Comparator<DataItem> createComparator(StreamScriptContext context) {
+		return (o1,o2)->{
+			List<Operand> parms1 = parameters.resolve(context, Optional.of(o1.message()), ImmutableFactory.empty(), metadata).unnamedParameters();
+			List<Operand> parms2 = parameters.resolve(context, Optional.of(o2.message()), ImmutableFactory.empty(), metadata).unnamedParameters();
+//			parms1.un
+			return 0;
+		};
+	}
+	@Override
+	public TransformerMetadata metadata() {
+		return metadata;
+	}
+
+}
---
+++ server/com.dexels.navajo.reactive/src/com/dexels/navajo/reactive/transformer/other/SortTransformerFactory.java
@@ -0,0 +1,63 @@
+package com.dexels.navajo.reactive.transformer.other;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+import com.dexels.navajo.document.stream.DataItem;
+import com.dexels.navajo.document.stream.DataItem.Type;
+import com.dexels.navajo.document.stream.ReactiveParseProblem;
+import com.dexels.navajo.reactive.api.ReactiveParameters;
+import com.dexels.navajo.reactive.api.ReactiveTransformer;
+import com.dexels.navajo.reactive.api.ReactiveTransformerFactory;
+import com.dexels.navajo.reactive.api.TransformerMetadata;
+
+public class SortTransformerFactory implements ReactiveTransformerFactory, TransformerMetadata {
+
+	public SortTransformerFactory() {
+	}
+	@Override
+	public ReactiveTransformer build(List<ReactiveParseProblem> problems,
+			ReactiveParameters parameters) {
+		return new SortTransformer(this,parameters);
+	}
+	
+	@Override
+	public Set<Type> inType() {
+		return new HashSet<>(Arrays.asList(new Type[] {DataItem.Type.MESSAGE}));
+	}
+
+	@Override
+	public Type outType() {
+		return DataItem.Type.MESSAGE;
+	}
+
+	@Override
+	public Optional<List<String>> allowedParameters() {
+		return Optional.of(Arrays.asList(new String[] {}));
+	}
+
+	@Override
+	public Optional<List<String>> requiredParameters() {
+		return Optional.of(Arrays.asList(new String[] {}));
+	}
+
+	@Override
+	public Optional<Map<String, String>> parameterTypes() {
+		Map<String, String> r = new HashMap<>();
+//		r.put("count", Property.INTEGER_PROPERTY);
+		return Optional.of(Collections.unmodifiableMap(r));
+	}
+
+
+	@Override
+	public String name() {
+		return "sort";
+	}
+
+}
