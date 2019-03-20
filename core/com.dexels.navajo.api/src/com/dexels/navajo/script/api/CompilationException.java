4776ad875da5b9fe86285829c7087fb341d91351
Frank Lyaruu <frank@dexels.com>
2019-03-20 18:15:50 +0100

    added general compile exception type

---
+++ core/com.dexels.navajo.api/src/com/dexels/navajo/script/api/CompilationException.java
@@ -0,0 +1,17 @@
+package com.dexels.navajo.script.api;
+
+public class CompilationException extends Exception {
+
+	private static final long serialVersionUID = 7448856689278900523L;
+
+
+	public CompilationException(String message) {
+		super(message);
+	}
+
+	public CompilationException(String message, Throwable cause) {
+		super(message,cause);
+	}
+	
+
+}
