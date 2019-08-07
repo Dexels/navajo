33d77c4765953e18457679d1bad9c891202b14ad
Frank Lyaruu <frank@dexels.com>
2019-08-07 14:18:21 +0200

    Revert "clean up interface overlaps + fix junit problems (missing files)"
    This reverts commit a61686bdff2f8651d301ac3857f90b300f824b2d.

--- core/com.dexels.navajo.api/src/com/dexels/navajo/script/api/SchedulerRegistryFactory.java
+++
@@ -1,9 +0,0 @@
-package com.dexels.navajo.script.api;
-
-public class SchedulerRegistryFactory {
-    private static SchedulerRegistry instance;
-
-	private static void setInstance(SchedulerRegistry sched) {
-    	instance = sched;
-    }
-}
