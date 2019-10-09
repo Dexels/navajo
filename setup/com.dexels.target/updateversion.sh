f36a71202c9ebaa7ec118d13d0c842deb15b5ff0
Frank Lyaruu <frank@dexels.com>
2019-10-09 14:11:55 +0200

    auto detect latest tp

--- setup/com.dexels.target/updateversion.sh
+++ setup/com.dexels.target/updateversion.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-VERSION=$1
+export TARGETBUILD=$(curl -s "https://circleci.com/api/v1.1/project/github/Dexels/dexels-base?circle-token=${CIRCLE_TOKEN}&offset=0&filter=successful" | jq '[.[]| select(.workflows.job_name == "targetplatform")][0].build_num')
+export MINORVERSION=3.3
+VERSION=${MINORVERSION}.${TARGETBUILD}
 sed "s/DEXELSBASEVERSION/${VERSION}/g" maven.template >maven.target
 echo $VERSION >VERSION
