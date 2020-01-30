75b516eced81948967afcc3477826c8f5fdef6e8
vgemistos <vgemistos@dexels.com>
2020-01-30 12:59:46 +0100

    implemented elasticsearch delete by id

--- optional/com.dexels.navajo.elasticsearch/src/com/dexels/navajo/elasticsearch/ElasticSearchDeleteService.java
+++ optional/com.dexels.navajo.elasticsearch/src/com/dexels/navajo/elasticsearch/ElasticSearchDeleteService.java
@@ -2,8 +2,6 @@ package com.dexels.navajo.elasticsearch;
 
 import java.io.IOException;
 
-
-
 public interface ElasticSearchDeleteService {
-	public String delete_id(String id) throws IOException;
+	public void delete(String id) throws IOException;
 }
