db2821c7c7497210f48b7d83aabad9fd9b1e94e1
vgemistos <vgemistos@dexels.com>
2020-07-24 07:12:47 +0200

    new gauges

--- optional/com.dexels.navajo.prometheus/src/com/dexels/navajo/prometheus/MetricsServlet.java
+++ optional/com.dexels.navajo.prometheus/src/com/dexels/navajo/prometheus/MetricsServlet.java
@@ -19,8 +19,10 @@ import java.io.Writer;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.PriorityBlockingQueue;
+
 
 /**
  * The MetricsServlet class exists to provide a simple way of exposing the metrics values.
@@ -34,15 +36,26 @@ public class MetricsServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	static final Gauge normalActive = Gauge.build()
-    .name("normalActive").help("Inprogress requests.").register();
+    .name("normalActive").help("Normal Active").register();
 	
 	static final Gauge normalPoolSize = Gauge.build()
-		    .name("normalPoolSize").help("Inprogress requests.").register();
+		    .name("normalPoolSize").help("Normal Pool Size").register();
 			
 	static final Gauge normalQueueSize = Gauge.build()
-		    .name("normalQueueSize").help("Inprogress requests.").register();
+		    .name("normalQueueSize").help("Normal Pool Queue Size").register();
 	
-
+	static final Gauge normalRequestsSize = Gauge.build()
+		    .name("normalRequests").help("Normal Pool Requests").register();
+	
+	static final Gauge availableCores = Gauge.build()
+		    .name("availableCores").help("Available Cores JVM").register();
+	
+	static final Gauge freeMemory = Gauge.build()
+		    .name("freeMemory").help("Free Memory JVM").register();
+	
+	static final Gauge totalMemoryUse = Gauge.build()
+		    .name("totalMemoryUse").help("Total Memory in use JVM").register();
+	
 	PriorityThreadPoolScheduler m;
 	
 	
@@ -65,15 +78,41 @@ public class MetricsServlet extends HttpServlet {
         this.registry = registry;
     }
     
+    public void checkMem() {
+    	/* Total number of processors or cores available to the JVM */
+    	  System.out.println("Gem--- Available processors (cores): " + 
+    	  Runtime.getRuntime().availableProcessors());
+    	  availableCores.set( (double) Runtime.getRuntime().availableProcessors() );
+
+    	  /* Total amount of free memory available to the JVM */
+    	  System.out.println("Gem--- Free memory (bytes): " + 
+    	  Runtime.getRuntime().freeMemory());
+    	  freeMemory.set( (double) Runtime.getRuntime().freeMemory() );
+
+    	  /* This will return Long.MAX_VALUE if there is no preset limit */
+    	  long maxMemory = Runtime.getRuntime().maxMemory();
+    	  /* Maximum amount of memory the JVM will attempt to use */
+    	  System.out.println("Gem--- Maximum memory (bytes): " + 
+    	  (maxMemory == Long.MAX_VALUE ? "no limit" : maxMemory));
+
+    	  /* Total memory currently in use by the JVM */
+    	  System.out.println("Gem--- Total memory (bytes): " + 
+    	  Runtime.getRuntime().totalMemory());
+    	  totalMemoryUse.set( (double) Runtime.getRuntime().totalMemory() );
+    	  
+    } 
 
     @Override
     protected void doGet(final HttpServletRequest req, final HttpServletResponse resp)
             throws ServletException, IOException {
         System.out.println("aaa");
         
+        checkMem();
+        
         normalActive.set( (double) m.getNormalActive() );
         normalPoolSize.set( (double) m.getNormalPoolSize() );
         normalQueueSize.set( (double) m.getNormalQueueSize() );
+        normalRequestsSize.set( (double) m.getNormalRequests().size() );
         
         resp.setStatus(HttpServletResponse.SC_OK);
         resp.setContentType(TextFormat.CONTENT_TYPE_004);
