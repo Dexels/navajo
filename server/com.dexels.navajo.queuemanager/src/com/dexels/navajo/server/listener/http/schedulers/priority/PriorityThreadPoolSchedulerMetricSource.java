273073635f835e008b7929cb4aebd4c84479967b
Gerco Ballintijn <gballintijn@dexels.com>
2020-10-05 14:29:36 +0200

    Dropped unsupported (and thus useless) metric.

--- server/com.dexels.navajo.queuemanager/src/com/dexels/navajo/server/listener/http/schedulers/priority/PriorityThreadPoolSchedulerMetricSource.java
+++ server/com.dexels.navajo.queuemanager/src/com/dexels/navajo/server/listener/http/schedulers/priority/PriorityThreadPoolSchedulerMetricSource.java
@@ -9,50 +9,54 @@ import io.prometheus.client.Gauge;
 
 public class PriorityThreadPoolSchedulerMetricSource implements MetricSource {
 
-    private static final Logger logger = LoggerFactory.getLogger(PriorityThreadPoolSchedulerMetricSource.class);
-
-    private static Gauge normalActive;
+    private static final Logger logger = LoggerFactory
+            .getLogger(PriorityThreadPoolSchedulerMetricSource.class);
 
     private Gauge normalPoolSize;
 
+    private Gauge normalActive;
+
     private Gauge normalQueueSize;
 
-    private Gauge normalRequestsSize;
-
     private PriorityThreadPoolScheduler scheduler;
-    
+
     public void activate() {
 
         logger.info("Prometheus priority threadpool scheduler data source started");
-        normalActive = MetricSource.registerGauge("normalActive", "Normal Active");
-        normalPoolSize = MetricSource.registerGauge("normalPoolSize", "Normal Pool Size");
-        normalQueueSize = MetricSource.registerGauge("normalQueueSize", "Normal Pool Queue Size");
-        normalRequestsSize = MetricSource.registerGauge("normalRequests", "Normal Pool Requests");
+
+        normalPoolSize = MetricSource.registerGauge("normalPoolSize",
+                "Maximum number of simultaneously processed requests");
+
+        normalActive = MetricSource.registerGauge("normalActive",
+                "Number of requests currently processed");
+
+        normalQueueSize = MetricSource.registerGauge("normalQueueSize",
+                "Number of queued requests");
     }
-    
+
     public void deactivate() {
 
         logger.info("Prometheus priority threadpool scheduler data source stopped");
-        MetricSource.unregisterCollector(normalActive);
+
         MetricSource.unregisterCollector(normalPoolSize);
+        MetricSource.unregisterCollector(normalActive);
         MetricSource.unregisterCollector(normalQueueSize);
-        MetricSource.unregisterCollector(normalRequestsSize);
     }
-    
+
     @Override
     public void recordValues() {
 
-        normalActive.set((double) scheduler.getNormalActive());
-        normalPoolSize.set((double) scheduler.getNormalPoolSize());
-        normalQueueSize.set((double) scheduler.getNormalQueueSize());
-        normalRequestsSize.set((double) scheduler.getNormalRequests().size());
+        normalPoolSize.set(scheduler.getNormalPoolSize());
+        normalActive.set(scheduler.getNormalActive());
+        normalQueueSize.set(scheduler.getNormalQueueSize());
     }
 
     public void setPriorityThreadPoolScheduler(PriorityThreadPoolScheduler scheduler) {
         this.scheduler = scheduler;
     }
-    
+
     public void clearPriorityThreadPoolScheduler(PriorityThreadPoolScheduler scheduler) {
         this.scheduler = null;
     }
+
 }
