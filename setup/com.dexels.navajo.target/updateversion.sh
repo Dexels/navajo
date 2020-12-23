8e682a56641cc0df4ea05093f4ef75184ec4f635
Gerco Ballintijn <gballintijn@dexels.com>
2020-12-23 17:55:11 +0100

    Cleaned-up the updateversion.sh script a bit more, mostly aesthetics.

--- setup/com.dexels.navajo.target/updateversion.sh
+++ setup/com.dexels.navajo.target/updateversion.sh
@@ -1,40 +1,47 @@
-#!/bin/sh
+#!/bin/bash
+
+# This script checks the latest build numbers from CircleCI and updates the versions in the target
+# file accordingly. The script exits with exit code 1 when no versions are found. This will cause
+# the CircleCI script to bail out as expected.
 
 set -o errexit
 
 
-# This script checks the latest build numbers from circleci and updates  the  versions in the target file accordingly.
-# The script exits with exit code 1 when no versions are found. This will cause the circleci script to bail out as expected.
-
 if [ -z "${BRANCH}" ]; then
-    export BRANCH="master"
+    BRANCH="master"
 fi
-
-export TARGETBUILD=$(
-	curl -s "https://circleci.com/api/v1.1/project/github/Dexels/dexels-base?circle-token=${CIRCLE_TOKEN}&offset=0&filter=successful" | jq "[.[]| select(.workflows.job_name == \"targetplatform\" and .branch == \"${BRANCH}\")][0].build_num"
-)
-if [ "$TARGETBUILD" = "null" ]; then
-   echo "No version found for dexels-base"
-   exit 1
-fi
-
 
 if [ "${BRANCH}" = "master" ]; then
-  SUFFXIX=""
-  echo "Using dependencies for master"
+    SUFFXIX=""
+    echo "Using dependencies for master"
 else
-  SUFFIX="-${BRANCH}"
-  echo "Using dependencies for $BRANCH branch."
+    SUFFIX="-${BRANCH}"
+    echo "Using dependencies for $BRANCH branch."
 fi
 
-export MINORVERSION=3.3
-export SEQUENCEVERSION=$(( 2800 + $TARGETBUILD ))
-VERSION=${MINORVERSION}.${TARGETBUILD}
 
-echo "Upgrading to dexels-base version: ${VERSION} sequence: ${SEQUENCEVERSION}"
+TARGETBUILD=$(curl -s "https://circleci.com/api/v1.1/project/github/Dexels/dexels-base?circle-token=${CIRCLE_TOKEN}&offset=0&filter=successful" | jq "[.[]| select(.workflows.job_name == \"targetplatform\" and .branch == \"${BRANCH}\")][0].build_num")
+if [ "$TARGETBUILD" == "null" ]; then
+    echo "No version found for dexels-base"
+    exit 1
+fi
+if [ -z "${MINORVERSION}"]; then
+    MINORVERSION="3.3"
+fi
+VERSION="${MINORVERSION}.${TARGETBUILD}"
+echo "dexels-base version: '$VERSION'"
+
+
+SEQUENCEVERSION=$(( 2800 + $TARGETBUILD ))
+echo "sequence version:    '$SEQUENCEVERSION'"
+
+
 cat maven.template | \
-  sed "s/##DEXELSBASEVERSION##/${VERSION}/g" | \
-	sed "s/##DEXELSBASESUFFIX##/${SUFFIX}/g" | \
-  sed "s/##SEQUENCE##/${SEQUENCEVERSION}/g"	\
-  > maven.target
-echo "$VERSION - $SEQUENCEVERSION" > VERSION
+    sed "s/##DEXELSBASEVERSION##/${VERSION}/g" | \
+    sed "s/##DEXELSBASESUFFIX##/${SUFFIX}/g" | \
+    sed "s/##SEQUENCE##/${SEQUENCEVERSION}/g" \
+    > maven.target
+
+echo "$VERSION"         > VERSION
+echo "$SEQUENCEVERSION" > SEQUENCE
+
