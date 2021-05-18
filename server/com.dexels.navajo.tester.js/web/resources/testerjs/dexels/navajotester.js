Dexels/navajo

commit 34f61625de12ec2c1a9b8cf7217c937eaf269951

Added functionality for generating birt sourcefile via tester

server/com.dexels.navajo.tester.js/web/resources/testerjs/dexels/navajotester.js
@@ -237,7 +237,8 @@ function runScript(script) {
             $('#HTMLview').show();
         }
 
-        var navajoinput = prepareInputNavajo(script);
+        var birtMode = (script == "BIRT")
+        var navajoinput = prepareInputNavajo(script, birtMode);
         var authHeader = "Basic " + btoa(sessionStorage.user + ":" + sessionStorage.password)
         
         $.ajax({
@@ -337,8 +338,11 @@ function hourglassOff() {
     $('html').removeClass('wait');
 }
 
-
 function prepareInputNavajo(script) {
+	return prepareInputNavajo(script, false);
+}
+
+function prepareInputNavajo(script, birtMode) {
     var $xml = $(xml);
     var $transaction = $xml.find('tml header transaction')
 
@@ -349,7 +353,11 @@ function prepareInputNavajo(script) {
     if (sessionStorage.locale) {
     		$xml.find('tml header').attr('locale', sessionStorage.locale);
     }
-   
+
+    if (birtMode)
+	{
+	    $xml.find('tml header').attr('GenerateBirt',birtMode);
+	}
     
 
     var $header = $xml.find('tml header ');
@@ -426,34 +434,42 @@ function getMyEntries(data, element) {
     }
 };
 
-/* Event handlers */
-
-
-$(document).on('click', '.script', function() {
+function callScript(script, addToRecentScripts)
+{
     var oldScript =  $('#loadedScript').text();
     var stateObj = {script: oldScript,  xml:  serializer.serializeToString(xml) };
     try {
     		history.replaceState(stateObj, oldScript, "tester.html?script=" + oldScript);
     }
     catch(err) {
-        console.log(err)
+        console.log(err) 
     }
 
-    var newScript = $(this).attr("id");
     // Remove all hoover divs and append the one to the current script
     $('.customRunOptionContainer').remove();
-
-    var isRecent = $('li.recentScript>[id=\''+newScript+'\']').length > 0
-    if (!$(this).parent().hasClass('recentScript') && !isRecent) {
-    	var clonedDiv = $(this).parent().clone(true);
-        clonedDiv.addClass('recentScript')
-        clonedDiv.find('.script').text(newScript)
-        $('#recentscriptslist').prepend(clonedDiv);
-        $('#recentscriptslist').find(".scriptli").slice(5, 10).remove();
+  
+    if (addToRecentScripts)
+    {
+	    var isRecent = $('li.recentScript>[id=\''+script+'\']').length > 0
+	    if (!$(this).parent().hasClass('recentScript') && !isRecent) {
+	    	var clonedDiv = $(this).parent().clone(true);
+	        clonedDiv.addClass('recentScript')
+	        clonedDiv.find('.script').text(script)
+	        $('#recentscriptslist').prepend(clonedDiv);
+	        $('#recentscriptslist').find(".scriptli").slice(5, 10).remove();
+	    }
+	    $('li.recentScript>[id=\''+script+'\']').parent().append(hooverdiv);
     }
-    $('li.recentScript>[id=\''+newScript+'\']').parent().append(hooverdiv);
 
-    runScript(newScript);
+    runScript(script);
+}
+
+/* Event handlers */
+
+
+$(document).on('click', '.script', function() {
+	var script = $(this).attr("id");
+	callScript(script, true);
 });
 
 $(document).on('click', '.folder', function() {
@@ -463,6 +479,11 @@ $(document).on('click', '.folder', function() {
 
 $(document).on('click', '.refreshscripts', function() {
 	getScripts();
+});
+
+$(document).on('click', '.birttitle', function() {
+	var script = $(this).attr("id");
+	callScript(script, false);
 });
 
 

core/com.dexels.navajo.core/META-INF/MANIFEST.MF
@@ -170,3 +170,4 @@ Service-Component: OSGI-INF/*.xml
 Bundle-RequiredExecutionEnvironment: JavaSE-1.8
 Bundle-ActivationPolicy: lazy
 Automatic-Module-Name: com.dexels.navajo.core
+Require-Bundle: com.dexels.navajo.birt.push

core/com.dexels.navajo.core/OSGI-INF/dispatcher.xml
@@ -10,6 +10,7 @@
    <reference cardinality="1..1" interface="com.dexels.navajo.compiler.BundleCreator" name="BundleCreator" policy="dynamic"/>
    <reference bind="addGlobalManager" cardinality="0..n" interface="com.dexels.navajo.server.global.GlobalManager" name="GlobalManager" policy="dynamic" unbind="removeGlobalManager"/>
    <reference bind="setAuthenticationMethodBuilder" cardinality="1..1" interface="com.dexels.navajo.authentication.api.AuthenticationMethodBuilder" name="AuthenticationMethodBuilder" policy="dynamic" unbind="clearAuthenticationMethodBuilder"/>
+   <reference bind="setHandlerFactory" cardinality="1..1" interface="com.dexels.navajo.server.HandlerFactory" name="HandlerFactory" policy="dynamic" unbind="clearHandlerFactory"/>
    <reference bind="setEventAdmin" cardinality="0..1" interface="org.osgi.service.event.EventAdmin" name="EventAdmin" policy="dynamic" unbind="clearEventAdmin"/>
    <reference bind="addDescriptionProvider" cardinality="0..n" interface="com.dexels.navajo.server.descriptionprovider.DescriptionProviderInterface" name="DescriptionProviderInterface" policy="dynamic" unbind="removeDescriptionProvider"/>
    <reference bind="setTenantConfig" cardinality="1..1" interface="com.dexels.navajo.tenant.TenantConfig" name="TenantConfig" policy="dynamic" unbind="clearTenantConfig"/>

core/com.dexels.navajo.core/OSGI-INF/navajo.servicehandler.default.xml
@@ -1,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<scr:component xmlns:scr="http://www.osgi.org/xmlns/scr/v1.1.0" immediate="true" name="navajo.servicehandler.default">
+   <implementation class="com.dexels.navajo.server.GenericHandler"/>
+   <service>
+      <provide interface="com.dexels.navajo.server.ServiceHandler"/>
+   </service>
+</scr:component>

core/com.dexels.navajo.core/OSGI-INF/navajo.servicehandler.factory.xml
@@ -1,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<scr:component xmlns:scr="http://www.osgi.org/xmlns/scr/v1.1.0" immediate="true" name="navajo.servicehandler.factory">
+   <implementation class="com.dexels.navajo.server.HandlerFactory"/>
+   <reference bind="addHandler" cardinality="1..n" interface="com.dexels.navajo.server.ServiceHandler" name="ServiceHandler" policy="dynamic" unbind="removeHandler"/>
+   <service>
+      <provide interface="com.dexels.navajo.server.HandlerFactory"/>
+   </service>
+</scr:component>

core/com.dexels.navajo.core/OSGI-INF/navajo.servicehandler.stress.xml
@@ -1,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<scr:component xmlns:scr="http://www.osgi.org/xmlns/scr/v1.1.0" immediate="true" name="navajo.servicehandler.stress">
+   <implementation class="com.dexels.navajo.server.StressTestHandler"/>
+   <service>
+      <provide interface="com.dexels.navajo.server.ServiceHandler"/>
+   </service>
+</scr:component>

core/com.dexels.navajo.core/src/com/dexels/navajo/server/Dispatcher.java
@@ -130,6 +130,7 @@ public class Dispatcher implements DispatcherMXBean, DispatcherInterface {
 
     protected boolean simulationMode;
     private AuthenticationMethodBuilder authMethodBuilder;
+    private HandlerFactory handlerFactory;
 
     // optional, can be null
 
@@ -312,6 +313,7 @@ public class Dispatcher implements DispatcherMXBean, DispatcherInterface {
         }
 
         Navajo in = access.getInDoc();
+        boolean birtMode = false;
 
         if (in != null) {
             Header h = in.getHeader();
@@ -335,6 +337,7 @@ public class Dispatcher implements DispatcherMXBean, DispatcherInterface {
                         access.addPiggybackData(element);
                     }
                 }
+                
             }
         } else {
             throw NavajoFactory.getInstance().createNavajoException("Null input message in dispatch");
@@ -354,8 +357,7 @@ public class Dispatcher implements DispatcherMXBean, DispatcherInterface {
         }
 
         try {
-
-            ServiceHandler sh = HandlerFactory.createHandler(navajoConfig, access, simulationMode); 
+            ServiceHandler sh = handlerFactory.createHandler(navajoConfig, access, simulationMode); 
 
             // If recompile is needed ALWAYS set expirationInterval to -1.
             // ALSO I DO NOT WANT CACHECONTROLLER DEPENDENCY @ THIS POINT.
@@ -429,7 +431,10 @@ public class Dispatcher implements DispatcherMXBean, DispatcherInterface {
                 swriter = new StringWriter();
                 writer = new PrintWriter(swriter);
                 // Remove some messages that might contain sensitive info.
-                removeInternalMessages(inMessage);
+                if (inMessage != null)
+                {
+                	inMessage.removeInternalMessages();
+                }
                 inMessage.write(writer);
 
                 message += swriter.getBuffer().toString();
@@ -585,29 +590,6 @@ public class Dispatcher implements DispatcherMXBean, DispatcherInterface {
     @Override
     public final void setUseAuthorisation(boolean a) {
         useAuthorisation = a;
-    }
-
-    /**
-     * Method can be used to remove special message before returning Navajo to
-     * some client.
-     * 
-     * 
-     * @param doc
-     * @return
-     */
-    @Override
-    public final Navajo removeInternalMessages(Navajo doc) {
-        if (doc != null) {
-            try {
-                doc.removeMessage(doc.getMessage("__globals__"));
-                doc.removeMessage(Message.MSG_AAA_BLOCK);
-                doc.removeMessage(Message.MSG_PARAMETERS_BLOCK);
-                doc.removeMessage(Message.MSG_TOKEN_BLOCK);
-            } catch (Exception e) {
-            	logger.error("Error: ", e);
-            }
-        }
-        return doc;
     }
 
     @Override
@@ -1381,6 +1363,14 @@ public class Dispatcher implements DispatcherMXBean, DispatcherInterface {
 
     public void clearAuthenticationMethodBuilder(AuthenticationMethodBuilder eventAdmin) {
         this.authMethodBuilder = null;
+    }
+    
+    public void setHandlerFactory(HandlerFactory hf) {
+        this.handlerFactory = hf;
+    }
+
+    public void clearHandlerFactory(HandlerFactory hf) {
+        this.handlerFactory = null;
     }
     
     public void addDescriptionProvider(DescriptionProviderInterface dpi) {

core/com.dexels.navajo.core/src/com/dexels/navajo/server/DispatcherInterface.java
@@ -25,15 +25,6 @@ public interface DispatcherInterface extends ResourceManager {
 	
 	public void finalizeService(Navajo inMessage, Access access, String rpcName, String rpcUser,		Throwable myException, String origThreadName, boolean scheduledWebservice, boolean afterWebServiceActivated, AfterWebServiceEmitter emit);
 
-
-	/**
-	 * Special method to remove internal Server messages.
-	 * 
-	 * @param doc
-	 * @return
-	 */
-	public Navajo removeInternalMessages(Navajo doc);
-	
 	/**
 	 * Create a temp file with the names prefix and suffix in the designated temp path.
 	 * @param prefix

commit 57188b575a06b776237372128ff6e4fb2c4d5029

Fix bug adding to recent scripts
Bug introduced in 34f61625de12ec2c1a9b8cf7217c937eaf269951. When moving
from the jQuery.click to a dedicated function the this context was
lost.

server/com.dexels.navajo.tester.js/web/resources/testerjs/dexels/navajotester.js
@@ -557,7 +557,7 @@ function callScript(script, addToRecentScripts)
 
 $(document).on('click', '.script', function() {
 	var script = $(this).attr("id");
-	callScript(script, true);
+	callScript.call(this, script, true);
 });
 
 $(document).on('click', '.folder', function() {
@@ -571,7 +571,7 @@ $(document).on('click', '.refreshscripts', function() {
 
 $(document).on('click', '.birttitle', function() {
 	var script = $(this).attr("id");
-	callScript(script, false);
+	callScript.call(this, script, false);
 });
 
 