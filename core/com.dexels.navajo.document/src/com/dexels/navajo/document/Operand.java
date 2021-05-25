a14dbf7a34d4651bfd8b64c09c9974640e9afda6
Gerco Ballintijn <gballintijn@dexels.com>
2021-05-25 11:26:29 +0200

    Fixed Long multiplying (#590).

--- core/com.dexels.navajo.document/src/com/dexels/navajo/document/Operand.java
+++ core/com.dexels.navajo.document/src/com/dexels/navajo/document/Operand.java
@@ -1,6 +1,6 @@
 /*
-This file is part of the Navajo Project. 
-It is subject to the license terms in the COPYING file found in the top-level directory of this distribution and at https://www.gnu.org/licenses/agpl-3.0.txt. 
+This file is part of the Navajo Project.
+It is subject to the license terms in the COPYING file found in the top-level directory of this distribution and at https://www.gnu.org/licenses/agpl-3.0.txt.
 No part of the Navajo Project, including this file, may be copied, modified, propagated, or distributed except according to the terms contained in the COPYING file.
 */
 package com.dexels.navajo.document;
@@ -61,13 +61,17 @@ public class Operand {
   public static Operand ofBoolean(boolean b) {
 	  return b ? TRUE : FALSE;
   }
- 
+
   public static Operand ofString(String string) {
 	  return new Operand(string,Property.STRING_PROPERTY);
   }
 
   public static Operand ofInteger(Integer value) {
 	  return new Operand(value,Property.INTEGER_PROPERTY);
+  }
+
+  public static Operand ofLong(Long value) {
+      return new Operand(value, Property.LONG_PROPERTY);
   }
 
   public static Operand ofFloat(Double value) {
