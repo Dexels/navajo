5b3d2f927f2e3bb23d5e69678254d492f65901d1
Gerco Ballintijn <gballintijn@dexels.com>
2021-05-25 16:19:02 +0200

    Cleaned-up UtilsTest in preparation for adding more tests (#590).

--- core/com.dexels.navajo.core/testsrc/com/dexels/navajo/parser/UtilsTest.java
+++ core/com.dexels.navajo.core/testsrc/com/dexels/navajo/parser/UtilsTest.java
@@ -1,107 +1,88 @@
 /*
-This file is part of the Navajo Project. 
-It is subject to the license terms in the COPYING file found in the top-level directory of this distribution and at https://www.gnu.org/licenses/agpl-3.0.txt. 
-No part of the Navajo Project, including this file, may be copied, modified, propagated, or distributed except according to the terms contained in the COPYING file.
-*/
+ * This file is part of the Navajo Project.
+ *
+ * It is subject to the license terms in the COPYING file found in the top-level directory of
+ * this distribution and at https://www.gnu.org/licenses/agpl-3.0.txt.  No part of the Navajo
+ * Project, including this file, may be copied, modified, propagated, or distributed except
+ * according to the terms contained in the COPYING file.
+ */
+
 package com.dexels.navajo.parser;
 
 import static org.junit.Assert.assertEquals;
 
 import java.util.Calendar;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 public class UtilsTest {
 
-	@Before
-	public void setUp() throws Exception {
-	}
+    @Test
+    public void testSubtractDatesWith24HoursDifference() throws Exception {
 
-	@After
-	public void tearDown() throws Exception {
-	}
+        Calendar a = Calendar.getInstance();
+        Calendar b = Calendar.getInstance();
+        b.add(Calendar.DAY_OF_YEAR, 1);
 
-	@Test
-	public void testSubtractDatesWith24HoursDifference() throws Exception {
+        Object result = Utils.subtract(a.getTime(), b.getTime());
 
-		Calendar a = Calendar.getInstance();
-		Calendar b = Calendar.getInstance();
-		b.add(Calendar.DAY_OF_YEAR, 1);
+        assertEquals("-1", result.toString());
+    }
 
-		Object o = com.dexels.navajo.parser.Utils.subtract(a.getTime(),
-				b.getTime());
+    @Test
+    public void testSubtractDatesLessThan24HoursDifference() throws Exception {
 
-		assertEquals("-1", o + "");
-	}
+        Calendar a = Calendar.getInstance();
+        Calendar b = Calendar.getInstance();
+        b.add(Calendar.DAY_OF_YEAR, 1);
+        b.add(Calendar.HOUR_OF_DAY, -3);
 
-	@Test
-	public void testSubtractDatesLessThan24HoursDifference() throws Exception {
+        Object result = Utils.subtract(a.getTime(), b.getTime());
 
-		Calendar a = Calendar.getInstance();
-		Calendar b = Calendar.getInstance();
-		b.add(Calendar.DAY_OF_YEAR, 1);
-		b.add(Calendar.HOUR_OF_DAY, -3);
+        assertEquals("0", result.toString());
+    }
 
-		Object o = com.dexels.navajo.parser.Utils.subtract(a.getTime(),
-				b.getTime());
+    @Test
+    public void testSubtractDatesMoreThan24HoursDifference() throws Exception {
 
-		assertEquals("0", o + "");
-	}
+        Calendar a = Calendar.getInstance();
+        Calendar b = Calendar.getInstance();
+        b.add(Calendar.DAY_OF_YEAR, 1);
+        b.add(Calendar.HOUR_OF_DAY, 3);
 
-	@Test
-	public void testSubtractDatesMoreThan24HoursDifference() throws Exception {
+        Object result = Utils.subtract(a.getTime(), b.getTime());
 
-		Calendar a = Calendar.getInstance();
-		Calendar b = Calendar.getInstance();
-		b.add(Calendar.DAY_OF_YEAR, 1);
-		b.add(Calendar.HOUR_OF_DAY, 3);
+        assertEquals("-1", result.toString());
+    }
 
-		Object o = com.dexels.navajo.parser.Utils.subtract(a.getTime(),
-				b.getTime());
+    @Test
+    public void testSubtractDatesInWinterTime() throws Exception {
 
-		assertEquals("-1", o + "");
-	}
+        Calendar a = Calendar.getInstance();
+        Calendar b = Calendar.getInstance();
+        a.set(2009, 0, 2, 0, 0, 0);
+        a.set(Calendar.MILLISECOND, 0);
+        b.set(2009, 0, 28, 0, 0, 0);
+        b.set(Calendar.MILLISECOND, 0);
 
-	@Test
-	public void testSubtractDatesInWinterTime() throws Exception {
+        int result = (Integer) Utils.subtract(a.getTime(), b.getTime());
 
-		Calendar a = Calendar.getInstance();
-		Calendar b = Calendar.getInstance();
-		a.set(2009, 0, 2, 0, 0, 0);
-		a.set(Calendar.MILLISECOND, 0);
-		b.set(2009, 0, 28, 0, 0, 0);
-		b.set(Calendar.MILLISECOND, 0);
+        assertEquals(-26, result);
+    }
 
-		System.err.println("a = " + a.getTime());
-		System.err.println("b = " + b.getTime());
+    @Test
+    public void testSubtractDatesInWinterAndSummerTime() throws Exception {
 
-		Integer o = (Integer) com.dexels.navajo.parser.Utils.subtract(
-				a.getTime(), b.getTime());
+        Calendar a = Calendar.getInstance();
+        Calendar b = Calendar.getInstance();
+        a.set(2009, 2, 2, 0, 0, 0);
+        a.set(Calendar.MILLISECOND, 0);
+        b.set(2009, 2, 30, 0, 0, 0);
+        b.set(Calendar.MILLISECOND, 0);
 
-		System.err.println("diff = " + o);
-		assertEquals("-26", o + "");
-	}
+        int result = (Integer) com.dexels.navajo.parser.Utils.subtract(a.getTime(), b.getTime());
 
-	@Test
-	public void testSubtractDatesInWinterAndSummerTime() throws Exception {
+        assertEquals(-28, result);
+    }
 
-		Calendar a = Calendar.getInstance();
-		Calendar b = Calendar.getInstance();
-		a.set(2009, 2, 2, 0, 0, 0);
-		a.set(Calendar.MILLISECOND, 0);
-		b.set(2009, 2, 30, 0, 0, 0);
-		b.set(Calendar.MILLISECOND, 0);
-
-		System.err.println("a = " + a.getTime());
-		System.err.println("b = " + b.getTime());
-
-		Integer o = (Integer) com.dexels.navajo.parser.Utils.subtract(
-				a.getTime(), b.getTime());
-
-		System.err.println("diff = " + o);
-
-		assertEquals("-28", o + "");
-	}
 }
