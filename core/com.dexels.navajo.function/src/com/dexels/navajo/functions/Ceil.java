8f8a56c829d685b4cfed82c2ebb8fd47924827d4
Arnoud Philip <arnoud@dexels.com>
2021-06-09 14:14:29 +0200

    Added Ceil and Floor functions to Navajo Functions

---
+++ core/com.dexels.navajo.function/src/com/dexels/navajo/functions/Ceil.java
@@ -0,0 +1,45 @@
+/*
+This file is part of the Navajo Project. 
+It is subject to the license terms in the COPYING file found in the top-level directory of this distribution and at https://www.gnu.org/licenses/agpl-3.0.txt. 
+No part of the Navajo Project, including this file, may be copied, modified, propagated, or distributed except according to the terms contained in the COPYING file.
+*/
+package com.dexels.navajo.functions;
+
+import com.dexels.navajo.expression.api.FunctionInterface;
+import com.dexels.navajo.expression.api.TMLExpressionException;
+
+public final class Ceil extends FunctionInterface {
+
+	@Override
+	public String remarks() {
+		return "Return the ceil value of the given number.";
+	}
+
+	@Override
+	public String usage() {
+		return "Ceil(number), where number is of type double";
+	}
+
+	@Override
+	public final Object evaluate() throws com.dexels.navajo.expression.api.TMLExpressionException {
+		Object a = this.getOperands().get(0);
+
+		try {
+			Double d = (Double) a;
+			double dd = d.doubleValue();
+			return Math.ceil(dd);
+		} catch (Exception e) {
+			throw new TMLExpressionException(this, "Illegal type specified in Ceil() function: " + e.getMessage());
+		}
+	}
+
+	public static void main(String[] args) throws Throwable {
+		double a = 1.1;
+		int b = 10;
+		Ceil m = new Ceil();
+		m.reset();
+		m.insertFloatOperand(Double.valueOf(a));
+		System.out.println("Ceil of " + a + " is " + m.evaluate());
+	}
+
+}
