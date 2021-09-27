911d7c252c44b66a2c0c1d612e8ea57cc44e970f
Arjen Schoneveld <aschoneveld@dexels.com>
2021-09-27 13:11:52 +0200

    removed System.err

--- server/com.dexels.navajo.adapters/src/com/dexels/navajo/adapter/MultipleEmptyMap.java
+++ server/com.dexels.navajo.adapters/src/com/dexels/navajo/adapter/MultipleEmptyMap.java
@@ -39,7 +39,6 @@ public void kill() {
   }
 
   public void setEmptyMaps(com.dexels.navajo.adapter.EmptyMap [] emptyMaps) {
-	  System.err.println("In setEmptyMaps: " + emptyMaps.length);
     this.emptyMaps = emptyMaps;
   }
 
